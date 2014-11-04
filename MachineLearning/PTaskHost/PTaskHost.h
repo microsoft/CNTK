75b923316e39055007e77ddbe3a3797c08bf217c
Jasha Droppo <jdroppo@microsoft.com>
2014-11-04 19:16:23 +0000

    Designating __declspec(dll*) macros for _WIN32 only

--- MachineLearning/PTaskHost/PTaskHost.h
+++ MachineLearning/PTaskHost/PTaskHost.h
@@ -4,10 +4,14 @@
 // that uses this DLL. This way any other project whose source files include this file see 
 // PTASKHOST_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
+#ifdef _WIN32
 #ifdef PTASKHOST_EXPORTS
 #define PTASKHOST_API __declspec(dllexport)
 #else
 #define PTASKHOST_API __declspec(dllimport)
+#endif
+#else
+#define PTASKHOST_API
 #endif
 
 #include "PTask.h"
