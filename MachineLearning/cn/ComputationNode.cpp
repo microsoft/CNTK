a9b972e735713e14000a21a808e0025591ebc004
Frank Seide <fseide@microsoft.com>
2014-11-04 16:37:39 -0800

    template syntax fix in ComputationNode.cpp, compiles now under GCC

--- MachineLearning/cn/ComputationNode.cpp
+++ MachineLearning/cn/ComputationNode.cpp
@@ -16,11 +16,11 @@ namespace Microsoft { namespace MSR { namespace CNTK {
     typedef Matrix<float> FloatMatrix;
     typedef Matrix<double> DoubleMatrix;
 
-    int64_t ComputationNode<float>::s_timeStampCounter = 0;
-    int64_t ComputationNode<double>::s_timeStampCounter = 0;
+    template<> int64_t ComputationNode<float>::s_timeStampCounter = 0;
+    template<> int64_t ComputationNode<double>::s_timeStampCounter = 0;
 
-    std::map<size_t, std::map<size_t, FloatMatrix*>> ComputationNode<float>::s_constOnes;
-    std::map<size_t, std::map<size_t, DoubleMatrix*>> ComputationNode<double>::s_constOnes;
+    template<> std::map<size_t, std::map<size_t, FloatMatrix*>> ComputationNode<float>::s_constOnes;
+    template<> std::map<size_t, std::map<size_t, DoubleMatrix*>> ComputationNode<double>::s_constOnes;
 
     template<class ElemType>
     TaskDescriptor<ElemType>* LearnableParameter<ElemType>::GetPTaskDescriptor(TaskType taskType, size_t /*inputIndex=0*/) const
