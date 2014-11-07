adf91bce120c94a448db65e371d94d383b649419
Jasha Droppo <jdroppo@microsoft.com>
2014-11-07 10:48:31 -0800

    Changes to get CNTKEval to compile on Windows

--- MachineLearning/CNTKEval/dllmain.cpp
+++ MachineLearning/CNTKEval/dllmain.cpp
@@ -5,6 +5,10 @@
 //
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
 
 BOOL APIENTRY DllMain( HMODULE /*hModule*/,
                        DWORD  ul_reason_for_call,
