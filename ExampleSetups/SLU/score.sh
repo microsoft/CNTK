c1079aac926b7eec4146c999ddca7dd299eafec7
fcb1899 <abrego@gmail.com>
2014-12-04 10:18:52 -0800

    Fixed bugs in SLU example in CNTKBook (section 7.4)
    1. Problem: RuntimeError if the full path of the output file doesn’t
    exist. Fix: make the intermediate dirs for the path of the output file.
    2. Problem: Output labels file not written (should be written according
    to the documentation). Fix: change the rnnlu.config file.
    3. Problem: mismatch between the feature and the output files when
    building the evaluation file. Fix: skip empty lines in the feature file

--- ExampleSetups/SLU/score.sh
+++ ExampleSetups/SLU/score.sh
@@ -1,9 +1,9 @@
-# need to have outputs
-# in this case is output.rec.txt
+# need to have outputs and labels
+# in this case is output.rec.txt and output.lbl.txt
 
-awk '{str1=$1; getline < "output.rec.txt"; print str1" "$1 > "output.12.txt"}' feat.txt
+awk 'NF {str1=$1; getline < "output.rec.txt"; print str1" "$1 > "output.12.txt"}' feat.txt
 
-awk '{str1=$1; str2=$2; getline < "lbl.txt"; print str1" NNP "$1 " "str2 > "output.all.txt"}' output.12.txt
+awk '{str1=$1; str2=$2; getline < "output.lbl.txt"; print str1" NNP "$1 " "str2 > "output.all.txt"}' output.12.txt
 
 cat output.all.txt | perl -ne '{chomp;s/\r//g;print $_,"\n";}' | perl conlleval.pl | head -10
 
