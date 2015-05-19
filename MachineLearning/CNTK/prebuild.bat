996059bf62531b26483414e45571d5047b690c06
Frank Seide <fseide@microsoft.com>
2015-05-19 00:10:45 -0700

    (added a missing & in a redirect in prebuild.bat)

--- MachineLearning/CNTK/prebuild.bat
+++ MachineLearning/CNTK/prebuild.bat
@@ -33,5 +33,5 @@ echo #define _CUDA_PATH_    %cuda_path%     >> buildinfo.h$$
 echo #endif >> buildinfo.h$$
 
 ::: update file only if it changed (otherwise CNTK.cpp will get rebuilt each time)
-fc buildinfo.h$$ buildinfo.h > NUL 2>1
+fc buildinfo.h$$ buildinfo.h > NUL 2>&1
 if ERRORLEVEL 1 move /Y buildinfo.h$$ buildinfo.h
