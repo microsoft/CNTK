c380b98a547c8b0848326822745deb669e554d39
Frank Seide <fseide@microsoft.com>
2015-05-18 20:32:39 -0700

    Visual Studio Solution cleanup: moved minibatchsourcehelpers.h to Common/Include, as it is shared amongst readers (exception: the HTK reader, which has a different version); fixed some incorrect #include of basetypes.h; copying of DLLs now does not copy if the file already exists and is not out of date; prebuild.bat now only updates builddate.h if it has changed, to avoid CNTK.cpp to be recompiled each time; fixed a few build warnings in the CUDA kernels (uninitialized variables); removed unused PTasks references in Linux makefiles

--- MachineLearning/CNTK/PTask.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "ComputationNetwork.h"
-#include "PTaskGraphBuilder.h"
-
-namespace Microsoft { namespace MSR { namespace CNTK {
-
-}}}
--- MachineLearning/CNTK/prebuild.bat
+++ MachineLearning/CNTK/prebuild.bat
@@ -1,9 +1,11 @@
 @echo off 
 setlocal enabledelayedexpansion
 
-echo #ifndef _BUILDINFO_H > buildinfo.h
-echo #define _BUILDINFO_H >> buildinfo.h 
+::: This is called as a pre-build step for the CNTK executable.
+::: It creates buildinfo.h, which makes version information available to the executable itself.
 
+echo #ifndef _BUILDINFO_H > buildinfo.h$$
+echo #define _BUILDINFO_H >> buildinfo.h$$ 
 
 FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i           
 :: assuming hostname always exists 
@@ -11,22 +13,25 @@ FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i
 :: not sure whether git in path ? 
 git --version 2 > nul 
 if not %ERRORLEVEL% == 9009 (
-    echo #define _GIT_EXIST >> buildinfo.h
+    echo #define _GIT_EXIST >> buildinfo.h$$
     FOR /F "usebackq" %%i IN (`git rev-parse --abbrev-ref HEAD`) DO SET BRANCH=%%i
     FOR /F "usebackq" %%i IN (`git rev-parse HEAD`) DO SET COMMIT=%%i
-    echo #define _BUILDBRANCH_  "!BRANCH!"      >> buildinfo.h
-    echo #define _BUILDSHA1_    "!COMMIT!"      >> buildinfo.h
-) 
+    echo #define _BUILDBRANCH_  "!BRANCH!"      >> buildinfo.h$$
+    echo #define _BUILDSHA1_    "!COMMIT!"      >> buildinfo.h$$
+)
 
-
-echo #define _BUILDER_ "%USERNAME%"     >> buildinfo.h 
-echo #define _BUILDMACHINE_ "!HOST!"    >> buildinfo.h
+echo #define _BUILDER_ "%USERNAME%"     >> buildinfo.h$$ 
+echo #define _BUILDMACHINE_ "!HOST!"    >> buildinfo.h$$
 
 set a=%~dp0
 set buildpath="%a:\=\\%"
-echo #define _BUILDPATH_    %buildpath%     >> buildinfo.h
+echo #define _BUILDPATH_    %buildpath%     >> buildinfo.h$$
 
 set cuda_path="%CUDA_PATH:\=\\%"
-echo #define _CUDA_PATH_    %cuda_path%     >> buildinfo.h
+echo #define _CUDA_PATH_    %cuda_path%     >> buildinfo.h$$
 
-echo #endif >> buildinfo.h
+echo #endif >> buildinfo.h$$
+
+::: update file only if it changed (otherwise CNTK.cpp will get rebuilt each time)
+fc buildinfo.h$$ buildinfo.h > NUL
+if not ERRORLEVEL 0 move /Y buildinfo.h$$ buildinfo.h
