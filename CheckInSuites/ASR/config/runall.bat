c5c863b2d038b8a6a99a052edaae33670dc67805
Frank Seide <fseide@microsoft.com>
2015-05-22 18:55:44 -0700

    reworked pathnames in CheckInSuites\ASR

--- CheckInSuites/ASR/config/runall.bat
+++ CheckInSuites/ASR/config/runall.bat
@@ -1,15 +1,25 @@
-set cnpath=d:\gitroot\cntk\CNTKSolution\x64\Release
+::: the CNTK executable is found relative to this BAT file
+set  THIS=%~dp0
+set  ROOT=%THIS%\..\..\..
+
+set  CNTK=%ROOT%\x64\Release\CNTK1.exe
+
+::: directories we pass in to CNTK config
+
+::: example setups are here
+set WorkDir=%ROOT%\ExampleSetups\ASR\TIMIT
+set  ExpDir=%THIS%\..\test_out
+set  LibDir=%WorkDir%\lib
+set  ScpDir=%LibDir%\scp
+
+::: first argument is CPU or GPU
 set proc=%1
-echo on
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_TrainSimpleNetwork.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_TrainNDLNetwork.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_TrainAutoEncoder.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_TrainMultiInput.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_TrainMultiTask.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_EvalSimpleNetwork.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_WriteScaledLogLike.config
-%cnpath%\cntk configFile=globals_%proc%.config+TIMIT_WriteBottleneck.config
 
-
-
-
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_TrainSimpleNetwork.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_TrainNDLNetwork.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_TrainAutoEncoder.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_TrainMultiInput.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_TrainMultiTask.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_EvalSimpleNetwork.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_WriteScaledLogLike.config
+%CNTK%  WorkDir=%WorkDir%  ExpDir=%ExpDir%  LibDir=%LibDir%  ScpDir=%ScpDir%  configFile=%THIS%\globals.config+%THIS%\select_%proc%.config+%THIS%\TIMIT_WriteBottleneck.config
