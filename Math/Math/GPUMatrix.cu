439e442c63e509c3107fe600cda4e9aab9f7ab40
Yinggong ZHAO <yinggong.zhao@gmail.com>
2015-06-30 04:09:12 -0700

    convert bias vector from  |V|*1 to 1*|V|

--- Math/Math/GPUMatrix.cu
+++ Math/Math/GPUMatrix.cu
@@ -1846,9 +1846,12 @@ namespace Microsoft { namespace MSR { namespace CNTK {
 
         cudaEvent_t done = nullptr;
         if (do_sync) CUDA_CALL(cudaEventCreate(&done));
-
+        //a: dim * minibatch
+        //b: dim * |vocab|
         int p = 512;
-        int width = a.GetNumCols();
+        int width = a.GetNumRows(); //dimension of hidden vector
+        //int width = a.GetNumCols(); original setup, considering column-major
+        //
         while (p / 2 > width) p = p / 2;
 
         _computeNceOutput<ElemType> << <this->GetNumElements() / 2, p >> >(
@@ -1856,7 +1859,7 @@ namespace Microsoft { namespace MSR { namespace CNTK {
             m_numRows / 2,
             sampleCount,
             my_a.GetArray(),//a
-            a.GetNumCols(),
+            a.GetNumRows(),
             my_b.GetArray(),//b
             my_bias.GetArray(),
             tmp.GetArray());//tmp
@@ -1867,7 +1870,7 @@ namespace Microsoft { namespace MSR { namespace CNTK {
         // summing up objective must be done in one block
         _assignNoiseContrastiveEstimation<ElemType> << <1, p >> >(
             this->GetArray(),
-            m_numRows,
+            m_numRows / 2,
             sampleCount, my_a.GetArray(),
             a.GetNumCols(),
             my_b.GetArray(),
@@ -1876,7 +1879,7 @@ namespace Microsoft { namespace MSR { namespace CNTK {
 
         _computeNceError<ElemType> << <1, p >> >(
             this->GetArray(),
-            m_numRows,
+            m_numRows / 2,
             tmp.GetNumCols(),
             tmp.GetArray());
 
