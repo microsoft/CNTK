a86cb8f52328e5bf6ac814ea2abbd8c645bb876f
UnderdogGeek <bhaskar.mitra@gmail.com>
2015-07-23 00:02:39 +1000

    Converting NDRM reader to SparsePC reader.

--- DataReader/SparsePCReader/SparsePCReader.h
+++ DataReader/SparsePCReader/SparsePCReader.h
@@ -1,9 +1,9 @@
 //
-// <copyright file="NDRMReader.h" company="Microsoft">
+// <copyright file="SparsePCReader.h" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //
-// NDRMReader.h - Include file for the Neural Document Ranking Model (NDRM)
+// SparsePCReader.h - Include file for the Sparse Parallel Corpus reader.
 #pragma once
 #include "DataReader.h"
 #include "DataWriter.h"
@@ -16,26 +16,26 @@
 namespace Microsoft { namespace MSR { namespace CNTK {
 
 template<class ElemType>
-class NDRMReader : public IDataReader<ElemType>
+class SparsePCReader : public IDataReader<ElemType>
 {
 private:
     size_t SPARSENESS_FACTOR_DEFAULT = 50; // We don't expect more than one in 50 input positions to have non-zero values
-    size_t FEATURE_COUNT = 2; // For NDRM dataset, it must have exactly two features
     int32_t VERIFICATION_CODE = 131313; // for data integrity check
     
     ConfigParameters m_readerConfig;
     std::wstring m_file;
-    std::vector<std::wstring> m_featureNames = std::vector<std::wstring>(FEATURE_COUNT);
-    std::vector<size_t> m_dims = std::vector<std::size_t>(FEATURE_COUNT);
+    size_t m_featureCount; // For SparsePC dataset, it must have exactly two features
+    std::vector<std::wstring> m_featureNames;
+    std::vector<size_t> m_dims;
     std::wstring m_labelName;
     size_t m_miniBatchSize;
     int64_t m_maxReadData; // For early exit during debugging
     bool m_doGradientCheck;
     bool m_returnDense;
     size_t m_sparsenessFactor;
-    std::vector<ElemType*> m_values = std::vector<ElemType*>(FEATURE_COUNT);
-    std::vector<int32_t*> m_rowIndices = std::vector<int32_t*>(FEATURE_COUNT);
-    std::vector<int32_t*> m_colIndices = std::vector<int32_t*>(FEATURE_COUNT);
+    std::vector<ElemType*> m_values;
+    std::vector<int32_t*> m_rowIndices;
+    std::vector<int32_t*> m_colIndices;
     ElemType* m_labelsBuffer;
 
     HANDLE m_hndl;
@@ -49,8 +49,8 @@ private:
     std::map<LabelType, LabelIdType> m_mapLabelToId;
 
 public:
-    NDRMReader() { };
-    virtual ~NDRMReader();
+    SparsePCReader() { };
+    virtual ~SparsePCReader();
     virtual void Destroy();
     virtual void Init(const ConfigParameters& config);
     virtual void StartMinibatchLoop(size_t mbSize, size_t epoch, size_t requestedEpochSamples=requestDataSize);
@@ -61,7 +61,7 @@ public:
     void SetSentenceSegBatch(Matrix<ElemType> &/*sentenceBegin*/, vector<MinibatchPackingFlag>& /*sentenceExistsBeginOrNoLabels*/) {};
     virtual const std::map<LabelIdType, LabelType>& GetLabelMapping(const std::wstring& sectionName);
     virtual void SetLabelMapping(const std::wstring& sectionName, const std::map<LabelIdType, typename LabelType>& labelMapping);
-    virtual bool GetData(const std::wstring& /*sectionName*/, size_t /*numRecords*/, void* /*data*/, size_t& /*dataBufferSize*/, size_t /*recordStart*/) { throw runtime_error("GetData not supported in NDRMReader"); };
+    virtual bool GetData(const std::wstring& /*sectionName*/, size_t /*numRecords*/, void* /*data*/, size_t& /*dataBufferSize*/, size_t /*recordStart*/) { throw runtime_error("GetData not supported in SparsePCReader"); };
     virtual bool DataEnd(EndDataType endDataType);
 };
 }}}
--- DataReader/SparsePCReader/stdafx.cpp
+++ DataReader/SparsePCReader/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// NDRMReader.pch will be the pre-compiled header
+// SparsePCReader.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
