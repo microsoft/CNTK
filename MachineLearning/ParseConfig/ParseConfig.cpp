067d75246b05fc52a8663d6621f8fe9da20f94cb
Frank Seide <fseide@microsoft.com>
2015-08-03 19:54:52 +0800

    added a new project ParseConfig for developing an extended configuration language; also added a spec doc for those language extensions

---
+++ MachineLearning/ParseConfig/ParseConfig.cpp
@@ -0,0 +1,21 @@
+// ParseConfig.cpp : tool for developing and testing the config parser
+//
+
+#include <stdlib.h>
+
+// ---------------------------------------------------------------------------
+// reader -- reads source code, including loading from disk
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+// lexer -- iterates over the source code and returns token by token
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+// parser -- parses configurations
+// ---------------------------------------------------------------------------
+
+int wmain(int argc, wchar_t* argv[])
+{
+    return EXIT_SUCCESS;
+}
