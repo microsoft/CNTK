6ba7757ee029067edbacbc8a04c808d6c4b4c86b
Frank Seide <fseide@microsoft.com>
2015-08-05 19:07:46 +0800

    great progress with Parser (still struggling with function definitions)

--- MachineLearning/ParseConfig/ParseConfig.cpp
+++ MachineLearning/ParseConfig/ParseConfig.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <deque>
 #include <set>
+#include <memory>
 #include <stdexcept>
 #include <algorithm>
 
@@ -22,12 +23,13 @@
 namespace Microsoft{ namespace MSR { namespace CNTK {
 
 using namespace std;
+using namespace msra::strfun;
 
 struct SourceFile               // content of one source file
 {
     /*const*/ wstring path;                     // where it came from
     /*const*/ vector<wstring> lines;            // source code lines
-    SourceFile(wstring location, wstring text) : path(location), lines(msra::strfun::split(text, L"\r\n")) { }  // from string, e.g. command line
+    SourceFile(wstring location, wstring text) : path(location), lines(split(text, L"\r\n")) { }  // from string, e.g. command line
     SourceFile(wstring path) : path(path)       // from file
     {
         File(path, fileOptionsRead).GetLines(lines);
@@ -155,7 +157,7 @@ public:
     }
 
     // advance cursor by #chars (but across line boundaries)
-    void Consume(size_t chars)
+    void ConsumeChars(size_t chars)
     {
         let ch = GotChar();
         if (!ch) LogicError("Consume: cannot run beyond end of source file");
@@ -173,7 +175,7 @@ public:
     // get the next character
     wchar_t GetChar()
     {
-        Consume(1);
+        ConsumeChars(1);
         return GotChar();
     }
 };
@@ -189,13 +191,15 @@ class Lexer : public CodeSource
 public:
     Lexer() : CodeSource(), currentToken(TextLocation())
     {
-        keywords = set<wstring> {
+        keywords = set<wstring>
+        {
             L"include",
-            L"new",
+            L"new", L"true", L"false",
             L"if", L"then", L"else",
             L"array",
         };
-        punctuations = set<wstring> {
+        punctuations = set<wstring>
+        {
             L"=", L";", L"\n",
             L"[", L"]", L"(", L")",
             L"+", L"-", L"*", L"/", L"**", L".*", L"%", L"||", L"&&", L"^",
@@ -214,10 +218,10 @@ public:
 
     struct Token
     {
-        TextLocation beginLocation; // text loc of first character of this token
-        TokenKind kind;
-        double number;              // number
         wstring symbol;             // identifier, keyword, punctuation, or string literal
+        double number;              // number
+        TokenKind kind;
+        TextLocation beginLocation; // text loc of first character of this token
         Token(TextLocation loc) : beginLocation(loc), kind(invalid), number(0.0) { }
         // diagnostic helper
         static wstring TokenKindToString(TokenKind kind)
@@ -234,12 +238,12 @@ public:
             default: return L"(unknown?)";
             }
         }
-        wstring ToString()  // string to show the content of token for debugging
+        wstring ToString() const    // string to show the content of token for debugging
         {
             let kindStr = TokenKindToString(kind);
             switch (kind)
             {
-            case numberliteral: return kindStr + msra::strfun::wstrprintf(L" %f", number);
+            case numberliteral: return kindStr + wstrprintf(L" %f", number);
             case stringliteral: return kindStr + L" '" + symbol + L"'";
             case identifier: case keyword: case punctuation: return kindStr + L" " + symbol;
             default: return kindStr;
@@ -254,8 +258,9 @@ public:
         /*implement*/ const char * kind() const { return "tokenizing"; }
     };
 
-    void Fail(string msg, Token where) { throw LexerError(msg, where.beginLocation); }
 private:
+    void Fail(string msg, Token where) { throw LexerError(msg, where.beginLocation); }
+
     Token currentToken;
     // consume input characters to form a next token
     //  - this function mutates the cursor, but does not set currentToken
@@ -263,7 +268,7 @@ private:
     //  - including files is handled here
     //  - the cursor is left on the first character that does not belong to the token
     // TODO: need to know whether we want to see '\n' or not
-    Token ConsumeToken()
+    Token NextToken()
     {
         auto ch = GotChar();
         // skip white space     --TODO: may or may not include newlines (iswblank() does not match CRLF)
@@ -276,7 +281,7 @@ private:
             if (IsInInclude())
             {
                 PopSourceFile();
-                return ConsumeToken();      // tail call--the current 't' gets dropped/ignored
+                return NextToken();      // tail call--the current 't' gets dropped/ignored
             }
             // really end of all source code: we are done. If calling this function multiple times, we will keep returning this.
             t.kind = eof;
@@ -290,7 +295,7 @@ private:
             t.kind = numberliteral;
             if (endPtr[0] == L'.' && endPtr[-1] == L'.')    // prevent 1..2 from begin tokenized 1. .2
                 endPtr--;
-            Consume(endPtr - beginPtr);
+            ConsumeChars(endPtr - beginPtr);
         }
         else if (iswalpha(ch) || ch == L'_')                            // --- identifier or keyword
         {
@@ -305,11 +310,11 @@ private:
             // special case: include "path"
             if (t.symbol == L"include")
             {
-                let nameTok = ConsumeToken();       // must be followed by a string literal
+                let nameTok = NextToken();       // must be followed by a string literal
                 if (nameTok.kind != stringliteral) Fail("'include' must be followed by a quoted string", nameTok);
                 let path = nameTok.symbol;          // TODO: some massaging of the path
                 PushSourceFile(SourceFile(path));   // current cursor is right after the pathname; that's where we will pick up later
-                return ConsumeToken();
+                return NextToken();
             }
         }
         else if (ch == L'"' || ch == 0x27)                              // --- string literal
@@ -342,8 +347,8 @@ private:
             // special case: comments
             if (t.symbol == L"#" || t.symbol == L"//")
             {
-                Consume(wcslen(GotCharPtr()));
-                return ConsumeToken();
+                ConsumeChars(wcslen(GotCharPtr()));
+                return NextToken();
             }
             else if (t.symbol == L"/*")
             {
@@ -353,29 +358,30 @@ private:
                 if (ch == 0)
                     Fail("comment without closing */", t);
                 GetChar();  // consume the final '/'
-                return ConsumeToken();  // and return the next token
+                return NextToken();  // and return the next token
             }
         }
         return t;
     }
 public:
-    Token GotToken() { return currentToken; }
-    Token GetToken()
+    const Token & GotToken() { return currentToken; }
+    void ConsumeToken() { currentToken = NextToken(); }
+    const Token & GetToken()
     {
-        currentToken = ConsumeToken();
+        ConsumeToken();
         return GotToken();
     }
 
-    // some simpel test function
+    // some simple test function
     void Test()
     {
         let lexerTest = L"new CNTK [ do = (train:eval) # main\ntrain=/*test * */if eval include 'c:/me/test.txt' then 13 else array[1..10](i=>i*i); eval=\"a\"+'b'  // line-end\n ] 'a\nb\nc' new";
         PushSourceFile(SourceFile(L"(command line)", lexerTest));
-        auto token = GetToken();   // get first token
-        while (token.kind != Lexer::TokenKind::eof)
+        while (GotToken().kind != Lexer::TokenKind::eof)
         {
+            let & token = GotToken();   // get first token
             fprintf(stderr, "%ls\n", token.ToString().c_str());
-            token = GetToken();
+            ConsumeToken();
         }
         Fail("error test", GetCursor());
     }
@@ -385,9 +391,250 @@ public:
 // parser -- parses configurations
 // ---------------------------------------------------------------------------
 
-class Parser : Lexer
+class Parser : public Lexer
 {
+    class ParseError : public LexerError
+    {
+    public:
+        ParseError(const string & msg, TextLocation where) : LexerError(msg, where) { }
+        /*implement*/ const char * kind() const { return "parsing"; }
+    };
+
+    void Fail(const string & msg, Token where) { throw LexerError(msg, where.beginLocation); }
+
+    void Expected(const wstring & what) { Fail(strprintf("%ls expected", what.c_str()), GotToken().beginLocation); }
+
+    void ConsumePunctuation(const wchar_t * s)
+    {
+        let & tok = GotToken();
+        if (tok.kind != punctuation || tok.symbol != s)
+            Expected(L"'" + wstring(s) + L"'");
+        ConsumeToken();
+    }
+
+    wstring ConsumeIdentifier()
+    {
+        let & tok = GotToken();
+        if (tok.kind != identifier)
+            Expected(L"identifier");
+        let id = tok.symbol;
+        ConsumeToken();
+        return id;
+    }
+
+    map<wstring, int> infixPrecedence;      // precedence level of infix operators
 public:
+    Parser() : Lexer()
+    {
+        infixPrecedence = map<wstring, int>
+        {
+            { L".", 11 }, { L"[", 11 }, { L"(", 11 },     // also sort-of infix operands...
+            { L"*", 10 }, { L"/", 10 }, { L".*", 10 }, { L"**", 10 }, { L"%", 10 },
+            { L"+", 9 }, { L"-", 9 },
+            { L"==", 8 }, { L"!=", 8 }, { L"<", 8 }, { L"<=", 8 }, { L">", 8 }, { L">=", 8 },
+            { L"&&", 7 },
+            { L"||", 6 },
+            { L":", 5 },
+        };
+    }
+    // --- this gets exported
+    struct Expression
+    {
+        TextLocation location;      // where in the source code (for downstream error reporting)
+        Expression(TextLocation location) : location(location) { }
+        wstring op;                 // operation, encoded as a string; 'symbol' for punctuation and keywords, otherwise used in constructors below ...TODO: use constexpr
+        double d;                   // numeric literal; op == "d"
+        wstring s;                  // string literal;  op == "s"
+        boolean b;                  // boolean literal; op == "b"
+        wstring id;                 // identifier;      op == "id" (if macro then it also has args)
+        typedef shared_ptr<struct Expression> ExpressionRef;
+        vector<ExpressionRef> args;             // position-dependent expression/function args
+        map<wstring, ExpressionRef> namedArgs;  // named expression/function args; also dictionary members
+        Expression() : d(0.0), b(false) { }
+    };
+    typedef Expression::ExpressionRef ExpressionRef;    // circumvent some circular definition problem
+    // --- end this gets exported
+    ExpressionRef ParseOperand()
+    {
+        let & tok = GotToken();
+        ExpressionRef operand = make_shared<Expression>(tok.beginLocation);
+        if (tok.kind == numberliteral)                                  // === numeral literal
+        {
+            operand->op = L"d";
+            operand->d = tok.number;
+            ConsumeToken();
+        }
+        else if (tok.kind == stringliteral)                             // === string literal
+        {
+            operand->op = L"s";
+            operand->s = tok.symbol;
+            ConsumeToken();
+        }
+        else if (tok.symbol == L"true" || tok.symbol == L"false")       // === boolean literal
+        {
+            operand->op = L"b";
+            operand->b = (tok.symbol == L"true");
+            ConsumeToken();
+        }
+        else if (tok.kind == identifier)                                // === dict member (unqualified)
+        {
+            operand->op = L"id";
+            operand->id = ConsumeIdentifier();
+        }
+        else if (tok.symbol == L"+" || tok.symbol == L"-"               // === unary operators
+            || tok.symbol == L"!")
+        {
+            operand->op = tok.symbol;
+            ConsumeToken();
+            operand->args.push_back(ParseOperand());
+        }
+        else if (tok.symbol == L"new")                                  // === new class instance
+        {
+            operand->op = tok.symbol;
+            ConsumeToken();
+            operand->id = ConsumeIdentifier();
+            operand->args.push_back(ParseOperand());
+        }
+        else if (tok.symbol == L"(")                                    // === nested parentheses
+        {
+            ConsumeToken();
+            operand = ParseExpression(0, false/*go across newlines*/);  // note: we abandon the current operand object
+            ConsumePunctuation(L")");
+        }
+        else if (tok.symbol == L"[")                                    // === dictionary constructor
+        {
+            operand->op = L"[]";
+            ConsumeToken();
+            //operand->namedArgs = ParseDictMembers();  // ...CONTINUE HERE
+            ConsumePunctuation(L"]");
+        }
+        else if (tok.symbol == L"array")                                // === array constructor
+        {
+            operand->op = tok.symbol;
+            ConsumeToken();
+            ConsumePunctuation(L"[");
+            operand->args.push_back(ParseExpression(0, false));         // [0] first index
+            ConsumePunctuation(L"..");
+            operand->args.push_back(ParseExpression(0, false));         // [1] last index
+            ConsumePunctuation(L"]");
+            ConsumePunctuation(L"(");
+            // Note: needs a new local scope for this
+            operand->id = ConsumeIdentifier();                          // identifier kept here
+            ConsumePunctuation(L"=>");
+            operand->args.push_back(ParseExpression(0, false));         // [2] function expression
+            ConsumePunctuation(L")");
+        }
+        else
+            Expected(L"operand");
+        return operand; // not using returns above to avoid "not all control paths return a value"
+    }
+    ExpressionRef ParseExpression(int requiredPrecedence, bool stopAtNewline)
+    {
+        auto left = ParseOperand();                 // get first operand
+        for (;;)
+        {
+            let & opTok = GotToken();
+            let opIter = infixPrecedence.find(opTok.symbol);
+            if (opIter == infixPrecedence.end())    // not an infix operator: we are done here, 'left' is our expression
+                break;
+            let opPrecedence = opIter->second;
+            if (opPrecedence < requiredPrecedence)  // operator below required precedence level: does not belong to this sub-expression
+                break;
+            let op = opTok.symbol;
+            ExpressionRef operation = make_shared<Expression>(opTok.beginLocation);
+            operation->op = op;
+            operation->args.push_back(left);        // [0] is left operand; [1] is right except for macro application
+            ConsumeToken();
+            // deal with special cases first
+            // We treat member lookup (.), macro application (a()), and indexing (a[i]) together with the true infix operators.
+            if (op == L".")                                 // === reference of a dictionary item
+            {
+                ConsumeToken();
+                operation->id = ConsumeIdentifier();
+            }
+            else if (op == L"(")                            // === macro application
+            {
+                operation->args.push_back(ParseMacroArgs(false));    // [1]: all arguments
+            }
+            else if (op == L"[")                            // === array index
+            {
+                ConsumeToken();
+                operation->args.push_back(ParseExpression(0, false));    // [1]: index
+                ConsumePunctuation(L"]");
+            }
+            else                                            // === regular infix operator
+            {
+                let right = ParseExpression(opPrecedence + 1, stopAtNewline);   // get right operand, or entire multi-operand expression with higher precedence
+                operation->args.push_back(right);           // [1]: right operand
+            }
+            left = operation;
+        }
+        return left;
+    }
+    // a macro-args expression lists position-dependent and optional parameters
+    // This is used both for defining macros (LHS) and using macros (RHS).
+    ExpressionRef ParseMacroArgs(bool defining)
+    {
+        ConsumePunctuation(L"(");
+        ExpressionRef macroArgs = make_shared<Expression>(GotToken().beginLocation);
+        macroArgs->op = L"()";
+        for (;;)
+        {
+            let expr = ParseExpression(0, false);   // this could be an optional arg (var = val)
+            if (defining && expr->op != L"id")      // when defining we only allow a single identifier
+                Fail("argument identifier expected", expr->location);
+            if (expr->op == L"id" && GotToken().symbol == L"=")
+            {
+                ConsumeToken();
+                let valueExpr = ParseExpression(0, false);
+                let res = macroArgs->namedArgs.insert(make_pair(expr->id, valueExpr));
+                if (res.second)
+                    Fail(strprintf("duplicate optional argument '%ls'", expr->id.c_str()), expr->location);
+            }
+            else
+                macroArgs->args.push_back(expr);    // [0..]: position args
+            if (GotToken().symbol != L",")
+                break;
+            ConsumeToken();
+        }
+        ConsumePunctuation(L")");
+        return macroArgs;
+    }
+    map<ExpressionRef, ExpressionRef> ParseDictMembers()
+    {
+        map<ExpressionRef, ExpressionRef> members;
+        auto idTok = GotToken();
+        while (idTok.kind == identifier)
+        {
+            ExpressionRef var = make_shared<Expression>(idTok.beginLocation);
+            // parse
+            var->op = L"id";
+            var->id = ConsumeIdentifier();                          // left-hand side
+            if (GotToken().symbol == L"(")                          // optionally, macro arguments
+                var->args.push_back(ParseMacroArgs(true/*defining*/));
+            ConsumePunctuation(L"=");
+            let valueExpr = ParseExpression(0, false);              // and the right-hand side
+            // insert
+            let res = members.insert(make_pair(var, valueExpr));
+            if (res.second)
+                Fail(strprintf("duplicate member definition '%ls'", var->id.c_str()), var->location);
+            // advance
+            idTok = GotToken();
+            if (idTok.symbol == L";")
+                idTok = GotToken();
+        }
+        return members;
+    }
+    void Test()
+    {
+        let parserTest = L"[ do = (train:eval) ; x = 13 ]";
+        PushSourceFile(SourceFile(L"(command line)", parserTest));
+        ConsumeToken();
+        ConsumePunctuation(L"[");
+        let topDict = ParseDictMembers();
+        ConsumePunctuation(L"]");
+        topDict;    // find item 'do' and evaluate it
+    }
 };
 
 }}}   // namespaces
@@ -398,8 +645,8 @@ int wmain(int /*argc*/, wchar_t* /*argv*/[])
 {
     try
     {
-        Lexer lexer;
-        lexer.Test();
+        Parser parser;
+        parser.Test();
     }
     catch (const ConfigError & err)
     {
