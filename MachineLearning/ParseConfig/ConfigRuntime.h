abb46fdeba7c7939b204825c95790e1e4c2bd25d
Frank Seide <fseide@microsoft.com>
2015-08-09 00:51:52 +0800

    renamed As<T> to AsConfigValue<T>, likewise for Is and DynamicCast, as prep for using other types; ConfigValueBase is gone, just using Polymorphic instead

--- MachineLearning/ParseConfig/ConfigRuntime.h
+++ MachineLearning/ParseConfig/ConfigRuntime.h
@@ -18,54 +18,55 @@ namespace Microsoft{ namespace MSR { namespace CNTK {
     };
 
     // config values
-    // All values in a ConfigRecord derive from ConfigValueBase.
+    // All values in a ConfigRecord derive from Polymorphic.
     // To get a value of an expected type T, dynamic-cast that base pointer to ConfigValue<T>.
     // Pointers to type U have the type shared_ptr<U>.
 
     struct Polymorphic { virtual ~Polymorphic() { } };
 
-    // TODO: this goes elsewhere
-    struct ConfigValueBase { virtual ~ConfigValueBase(){} };    // one value in a config dictionary
-
     // TODO: a ConfigValuePtr should be a shared_ptr to the value directly (such as ComputationNode), while having the base class
     // ConfigValues are value structs. E.g. we can copy them to construct a ConfigValuePtrfrom them.
-    template<typename T> class ConfigValue : public ConfigValueBase
+    template<typename T> class ConfigValue : public Polymorphic
     {
     public:
         /*const*/ T value;      // primitive type (e.g. double) or shared_ptr<runtime type>
         ConfigValue(T value) : value(value) { } // TODO: take a shared_ptr<T> and construct base shared_ptr from it
     };
 
-    struct ConfigValuePtr : public shared_ptr<ConfigValueBase>
+    struct ConfigValuePtr : public shared_ptr<Polymorphic>
     {
         bool currentlyResolving;    // set during resolution phase, to detect circular references
         TextLocation location;      // in source code
-        template<typename T> ConfigValue<T> * DynamicCast() const { return dynamic_cast<ConfigValue<T>*>(get()); }    // this casts the raw pointer that's inside the shared_ptr
+        template<typename T> ConfigValue<T> * DynamicCastConfigValue() const {
+            const auto p = get(); p;
+            const auto r = dynamic_cast<ConfigValue<T>*>(get());
+            return r;
+        }    // this casts the raw pointer that's inside the shared_ptr
     public:
         // construction     ---TODO: no template here
         template<typename T>
-        ConfigValuePtr(const shared_ptr<T> & p, TextLocation location) : shared_ptr<ConfigValueBase>(p), currentlyResolving(false), location(location) {}
+        ConfigValuePtr(const shared_ptr<T> & p, TextLocation location) : shared_ptr<Polymorphic>(p), currentlyResolving(false), location(location) {}
         ConfigValuePtr() : currentlyResolving(false) {} // (formally needed somehow)
         // methods for retrieving values
         // One accesses when values are constant, so we can just return values as const &.
-        operator double() const { return As<double>(); }
-        operator wstring() const { return As<wstring>(); }
-        operator bool() const { return As<bool>(); }
-        template<typename T> operator shared_ptr<T>() const { return As<shared_ptr<T>>(); }
+        operator double()  const { return AsConfigValue<double>(); }
+        operator wstring() const { return AsConfigValue<wstring>(); }
+        operator bool()    const { return AsConfigValue<bool>(); }
+        template<typename T> operator shared_ptr<T>() const { return AsConfigValue<shared_ptr<T>>(); }
         operator size_t() const
         {
-            const auto val = As<double>();
+            const auto val = AsConfigValue<double>();
             const auto ival = (size_t)val;
             if (ival != val)
                 throw EvaluationError(L"numeric value is not an integer", location);
             // TODO: ^^this cannot be done, since we don't have TextLocation here.
-            return (size_t)As<double>();
+            return ival;
         }
         // type helpers
-        template<typename T> bool Is() const { return DynamicCast<T>() != nullptr; }
-        template<typename T> T & As() const     // returns reference to what the 'value' member
+        template<typename T> bool IsConfigValue() const { return DynamicCastConfigValue<T>() != nullptr; }
+        template<typename T> T & AsConfigValue() const     // returns reference to what the 'value' member
         {
-            auto * p = DynamicCast<T>();        // -> ConfigValue<T>
+            auto * p = DynamicCastConfigValue<T>();        // -> ConfigValue<T>
             if (p == nullptr)   // TODO: can we make this look the same as TypeExpected in ConfigRuntime.cpp? We'd need the type name
                 throw EvaluationError(L"config member has wrong type", location);
             return p->value;                    // this unwraps the value out from its ConfigValue wrapper
@@ -78,7 +79,7 @@ namespace Microsoft{ namespace MSR { namespace CNTK {
             // call this when a a member might be as-of-yet unresolved, to evaluate it on-demand
             // value.get() is a pointer to ConfigValue<type of value>
             // Type of value is ExpressionPtr if the value is not yet resolved.
-            auto * p = DynamicCast<ExpressionPtr>();    // -> ConfigValue<ExpressionPtr>
+            auto * p = DynamicCastConfigValue<ExpressionPtr>();    // -> ConfigValue<ExpressionPtr>
             if (!p)                             // value is not an ExpressionPtr: we already got a proper value; done.
                 return;
             if (currentlyResolving)             // detect circular references (infinite recursion)
