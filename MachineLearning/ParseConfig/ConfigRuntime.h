bebbfa0ef6b004622e0514875a95655674b5ea45
Frank Seide <fseide@microsoft.com>
2015-08-08 22:56:40 +0800

    moved accessors directly into ConfigValuePtr

--- MachineLearning/ParseConfig/ConfigRuntime.h
+++ MachineLearning/ParseConfig/ConfigRuntime.h
@@ -26,20 +26,71 @@ namespace Microsoft{ namespace MSR { namespace CNTK {
 
     // TODO: this goes elsewhere
     struct ConfigValueBase { virtual ~ConfigValueBase(){} };    // one value in a config dictionary
-    struct ConfigValuePtr : public shared_ptr<ConfigValueBase>
-    {
-        template<typename T>
-        ConfigValuePtr(const shared_ptr<T> & val) : shared_ptr<ConfigValueBase>(val){}
-        ConfigValuePtr(){}
-    };
 
     // TODO: a ConfigValuePtr should be a shared_ptr to the value directly (such as ComputationNode), while having the base class
+    // ConfigValues are value structs. E.g. we can copy them to construct a ConfigMember from them.
     template<typename T> class ConfigValue : public ConfigValueBase
     {
     public:
-        // TODO: derive this from shared_ptr<T>, where 
         /*const*/ T value;      // primitive type (e.g. double) or shared_ptr<runtime type>
         ConfigValue(T value) : value(value) { } // TODO: take a shared_ptr<T> and construct base shared_ptr from it
+    };
+
+    struct ConfigValuePtr : public shared_ptr<ConfigValueBase>
+    {
+        bool currentlyResolving;    // set during resolution phase, to detect circular references
+    public:
+        // construction     ---TODO: no template here
+        template<typename T>
+        ConfigValuePtr(const shared_ptr<T> & p) : shared_ptr<ConfigValueBase>(p), currentlyResolving(false) {}
+        ConfigValuePtr() : currentlyResolving(false){}
+        //ConfigValuePtr & operator=(const shared_ptr<ConfigValueBase> & newPtr) { *this = newPtr; }
+        // accessing values
+        // One accesses when values are constant, so we can just return values as const &.
+        template<typename T> ConfigValue<T> * DynamicCast() const { return dynamic_cast<ConfigValue<T>*>(get()); }    // this casts the raw pointer that's inside the shared_ptr
+        template<typename T> bool Is() const { return DynamicCast<T>() != nullptr; }
+        //template<typename T> bool Is() const { return dynamic_cast<ConfigValue<T>*>(get()) != nullptr; }  // test for type
+        template<typename T> T & As() const     // returns reference to what the 'value' member
+        {
+            auto * p = DynamicCast<T>();        // -> ConfigValue<T>
+            if (p == nullptr)   // TODO: can we make this look the same as TypeExpected in ConfigRuntime.cpp? We'd need the type name
+                throw EvaluationError(L"config member has wrong type", TextLocation()); // TODO: we need location here
+            return p->value;                    // this unwraps the value out from its ConfigValue wrapper
+        }
+        operator double() const { return As<double>(); }
+        operator wstring() const { return As<wstring>(); }
+        operator bool() const { return As<bool>(); }
+        operator size_t() const
+        {
+            const auto val = As<double>();
+            const auto ival = (size_t)val;
+            if (ival != val)
+                throw EvaluationError(L"numeric value is not an integer", TextLocation());
+            // TODO: ^^this cannot be done, since we don't have TextLocation here.
+            return (size_t)As<double>();
+        }
+        // methods for retrieving values
+        template<typename T> operator shared_ptr<T>() const { return As<shared_ptr<T>>(); }
+        //operator ConfigValuePtr() const { return value; }   // or the untyped config value
+        // resolving
+        // methods for resolving the value
+        template<typename F>
+        void ResolveValue(const F & Evaluate, TextLocation location)
+        {
+            // call this when a a member might be as-of-yet unresolved, to evaluate it on-demand
+            // value.get() is a pointer to ConfigValue<type of value>
+            // Type of value is ExpressionPtr if the value is not yet resolved.
+            auto * p = DynamicCast<ExpressionPtr>();    // -> ConfigValue<ExpressionPtr>
+            if (!p)                             // value is not an ExpressionPtr: we already got a proper value; done.
+                return;
+            if (currentlyResolving)             // detect circular references (infinite recursion)
+                throw EvaluationError(L"circular reference (expression to compute identifier's value uses the identifier's value)", location);
+            currentlyResolving = true;
+            ExpressionPtr valueExpr = p->value;
+            *this = Evaluate(valueExpr);        // completely replace ourselves with the actual result
+            if (currentlyResolving)
+                LogicError("ResolveValue: spurious 'currentlyResolving' flag");
+        }
     };
 
     template<typename T> ConfigValuePtr MakeConfigValue(const T & val) { return make_shared<ConfigValue<T>>(val); }
@@ -47,51 +98,31 @@ namespace Microsoft{ namespace MSR { namespace CNTK {
     class ConfigRecord      // all configuration arguments to class construction, resolved into ConfigValuePtrs
     {
     public:
-        class ConfigMember  // TODO: can a ConfigMember not just be a ConfigValuePtr with conversion functions? and get rid of 'value'
+        class ConfigMember : public ConfigValuePtr
         {
             // TODO: got a double shared_ptr here. Instead,
             // wrap constants into objects as well
-            ConfigValuePtr value;       // ... TODO: ConfigValues can be passed around by value
-            bool currentlyResolving;    // set during resolution phase, to detect circular references
             TextLocation location;      // in source code  --TODO: initialize this to some meaningful value
+#if 0
             template<typename T> T * As() const
             {
-                auto * p = dynamic_cast<T*>(value.get());
+                auto * p = dynamic_cast<T*>(get());
                 if (p == nullptr)   // TODO: can we make this look the same as TypeExpected in ConfigRuntime.cpp? We'd need the type name
                     throw EvaluationError(L"config member has wrong type", location);
                 return p;
             }
+#endif
         public:
-            // methods for retrieving values
-            operator double() const { return As<ConfigValue<double>>()->value; }
-            operator wstring() const { return As<ConfigValue<wstring>>()->value; }
-            operator bool() const { return As<ConfigValue<bool>>()->value; }
-            operator size_t() const { return (size_t) As<ConfigValue<double>>()->value; }   // TODO: fail if fractional
-            template<typename T> operator shared_ptr<T>() const { return As<ConfigValue<shared_ptr<T>>>()->value; }
-            operator ConfigValuePtr() const { return value; }   // or the untyped config value
-            template<typename T> bool Is() const { return dynamic_cast<ConfigValue<T>*>(value.get()) != nullptr; }  // test for type
-            // BUGBUG: ^^ does not work for testing if type is derived from T
-            const char * TypeName() const { return typeid(*value.get()).name(); }
-            // methods for resolving the value
+            const char * TypeName() const { return typeid(*get()).name(); }
+            // constructors
+            ConfigMember(const ConfigValuePtr & value, TextLocation location) : ConfigValuePtr(value), location(location) {}
+            ConfigMember() {}    // needed for map below
+            // resolution
             template<typename F>
             void ResolveValue(const F & Evaluate)
             {
-                // call this when a a member might be as-of-yet unresolved, to evaluate it on-demand
-                // value.get() is a pointer to ConfigValue<type of value>
-                // Type of value is ExpressionPtr if the value is not yet resolved.
-                auto * p = dynamic_cast<ConfigValue<ExpressionPtr>*>(value.get());
-                if (!p)                             // value is not an ExpressionPtr: we already got a proper value; done.
-                    return;
-                const auto valueExpr = p->value;
-                if (currentlyResolving)             // detect circular references (infinite recursion)
-                    throw EvaluationError(L"circular reference (expression to compute identifier's value uses the identifier's value)", location);
-                currentlyResolving = true;
-                value = Evaluate(valueExpr);        // evaluate and replace 'value' with real value
-                currentlyResolving = false;
+                ConfigValuePtr::ResolveValue(Evaluate, location);
             }
-            // constructors
-            ConfigMember(ConfigValuePtr value, TextLocation location) : value(value), currentlyResolving(false), location(location) {}
-            ConfigMember() : currentlyResolving(false) {}    // needed for map below
         };
     private:
         map<wstring, ConfigMember> members;
