1ab64d6f4b035771f1f9110360acc7d5b41bab90
U-REDMOND\t-tiah <t-tiah@ZW6380851.redmond.corp.microsoft.com>
2015-08-14 11:53:16 -0700

    Copied LMSequenceReader to LMHTXSequenceReader

---
+++ DataReader/LMHTXSequenceReader/Exports.cpp
@@ -0,0 +1,31 @@
+//
+// <copyright file="Exports.cpp" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//
+// Exports.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#define DATAREADER_EXPORTS
+#include "DataReader.h"
+#include "SequenceReader.h"
+
+namespace Microsoft { namespace MSR { namespace CNTK {
+
+template<class ElemType>
+void DATAREADER_API GetReader(IDataReader<ElemType>** preader)
+{
+    *preader = new BatchSequenceReader<ElemType>();
+}
+
+extern "C" DATAREADER_API void GetReaderF(IDataReader<float>** preader)
+{
+    GetReader(preader);
+}
+extern "C" DATAREADER_API void GetReaderD(IDataReader<double>** preader)
+{
+    GetReader(preader);
+}
+
+}}}
