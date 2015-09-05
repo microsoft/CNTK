75dd81795f922f27cf8282639fac38893c702f36
Frank Seide <fseide@microsoft.com>
2015-09-05 11:00:51 +0200

    moved stuff out from CNTK to CNTKSGDLib; created an SGD.cpp that instantiates the exported classes of CNTKSGDLib; does not build since git mixed up files during move, and it won't let me git-add and git-mv in one go

---
+++ MachineLearning/CNTKSGDLib/SGD.cpp
@@ -0,0 +1,17 @@
+// SGD.cpp -- implements SGD with all bells and whistles, parallelization, randomizatiom, etc.
+
+#define _CRT_SECURE_NO_WARNINGS // "secure" CRT not available on all platforms  --add this at the top of all CPP files that give "function or variable may be unsafe" warnings
+
+#include "Basics.h"
+#include "SGD.h"
+#include "MultiNetworksSGD.h"
+
+namespace Microsoft { namespace MSR { namespace CNTK {
+
+template class SGD<float>;
+template class SGD<double>;
+
+template class MultiNetworksSGD<float>;
+template class MultiNetworksSGD<double>;
+
+}}}
