57eea8c6505ebbfeea045dec503a935314a19d52
Alexey Kamenev <alexeyk@microsoft.com>
2015-10-20 17:57:39 -0700

    cudnn: add forward implementation, unit tests.

---
+++ Math/CNTKMathTest/ConvolutionEngineTests.cpp
@@ -0,0 +1,78 @@
+//
+// <copyright file="ConvolutionEngineTests.cpp" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <algorithm>
+#include "..\Math\Matrix.h"
+#include "..\Math\CPUMatrix.h"
+#include "..\Math\GPUMatrix.h"
+#include "..\Math\ConvolutionEngine.h"
+#include "..\Math\CuDnnConvolutionEngine.h"
+
+namespace CNTKMathTest
+{
+    using namespace Microsoft::MSR::CNTK;
+    using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+    TEST_CLASS(MatrixUnitTest)
+    {
+        using ConvEng = ConvolutionEngine<float>;
+
+    public:
+        BEGIN_TEST_METHOD_ATTRIBUTE(SimpleConvolution)
+            TEST_METHOD_ATTRIBUTE(L"Category", L"Convolution")
+        END_TEST_METHOD_ATTRIBUTE()
+        TEST_METHOD(SimpleConvolution)
+        {
+            int n = 1;
+            int cmapIn = 3;
+            int inW = 5;
+            int inH = 5;
+            int kW = 3;
+            int kH = 3;
+            int sW = 2;
+            int sH = 2;
+            int cmapOut = 1;
+            int outW = GetNumOut(inW, kW, sW, false);
+            int outH = GetNumOut(inH, kH, sH, false);
+
+            int deviceId = 0;
+            auto eng = ConvEng::Create(deviceId, 1000);
+            auto inT = eng->CreateTensor(inW, inH, cmapIn, n);
+            auto filtT = eng->CreateFilter(kW, kH, cmapIn, cmapOut);
+            auto outT = eng->CreateTensor(outW, outH, cmapOut, n);
+            auto convT = eng->CreateConvDescriptor(*inT, *filtT, sW, sH, false);
+
+            std::vector<float> buf(inW * inH * cmapIn * n);
+            float seed = 0;
+            std::generate(buf.begin(), buf.end(), [&seed]{ return seed++; });
+            SingleMatrix in(inW * inH * cmapIn, n, buf.data(), matrixFlagNormal, deviceId);
+            seed = 0;
+            buf.resize(kW * kH * cmapIn * cmapOut);
+            std::generate(buf.begin(), buf.end(), [&seed]{ return seed++; });
+            SingleMatrix filt(cmapOut, kW * kH * cmapIn, buf.data(), matrixFlagNormal, deviceId);
+            SingleMatrix out(outW * outH * cmapOut, n, deviceId);
+
+            eng->Forward(*inT, in, *filtT, filt, *convT, *outT, out);
+
+            float expBuf[] = {7695.0f, 9801.0f, 18225.0f, 20331.0f};
+            SingleMatrix exp(outW * outH * cmapOut, n, expBuf, matrixFlagNormal, deviceId);
+            Assert::IsTrue(out.IsEqualTo(exp));
+        }
+
+        BEGIN_TEST_METHOD_ATTRIBUTE(SimpleConvolution2)
+            TEST_METHOD_ATTRIBUTE(L"Category", L"Convolution")
+        END_TEST_METHOD_ATTRIBUTE()
+        TEST_METHOD(SimpleConvolution2)
+        {
+        }
+    private:
+        int GetNumOut(int i, int k, int s, bool pad)
+        {
+            return (i - (pad ? 1 : k)) / s + 1;
+        }
+    };
+}
--- Math/Math/ConvolutionEngine.cpp
+++ Math/Math/ConvolutionEngine.cpp
@@ -10,75 +10,89 @@
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
-template<class ElemType>
-class DefaultConvolutionEngine : public ConvolutionEngine<ElemType>
-{
-public:
-    DefaultConvolutionEngine(DEVICEID_TYPE deviceId, size_t maxTempMemSizeInSamples)
-        : m_tempMatrix(deviceId), m_maxTempMemSizeInSamples(maxTempMemSizeInSamples)
+    template<class ElemType>
+    class DefaultConvolutionEngine : public ConvolutionEngine<ElemType>
     {
-    }
-
-public:
-    void Forward(const Tensor4D& inT, const Mat& in, const Tensor4D& filterT, const Mat& filter, const ConvolutionOptions& convOpt,
-        const Tensor4D& outT, Mat& out) override
-    {
-        size_t packedInputRows = filterT.w() * filterT.h() * inT.c();
-        size_t packedInputColsPerSample = outT.w() * outT.h();
-        size_t outputSizePerChannel = packedInputColsPerSample;
-
-        assert(inT.n() == in.GetNumCols());
-        assert(outT.n() == out.GetNumCols());
-
-        size_t batchSize = inT.n();
-        size_t maxTempMemSizeInSamples = (m_maxTempMemSizeInSamples == 0 ? batchSize : m_maxTempMemSizeInSamples);
-
-        assert(filter.GetNumCols() == packedInputRows && filter.GetNumRows() == outT.c());
-        out.Resize(outT.c(), outputSizePerChannel * batchSize);
-
-        size_t subBatchSize = min(batchSize, maxTempMemSizeInSamples);
-        size_t numSubBatches = (batchSize + subBatchSize - 1) / subBatchSize;
-
-        for (size_t i = 0; i < numSubBatches; i++)
+    public:
+        DefaultConvolutionEngine(DEVICEID_TYPE deviceId, size_t maxTempMemSizeInSamples)
+            : m_tempMatrix(deviceId), m_maxTempMemSizeInSamples(maxTempMemSizeInSamples)
         {
-            size_t startSampleId = i * subBatchSize;
-            size_t endSampleId = min(batchSize, startSampleId + subBatchSize);
-            size_t smallBatchSize = endSampleId - startSampleId;
-
-            m_tempMatrix.Resize(packedInputRows, packedInputColsPerSample * smallBatchSize);
-            Mat inputSubBatch = in.ColumnSlice(startSampleId, smallBatchSize);
-            m_tempMatrix.AssignPackedConvolutionInput(inputSubBatch,
-                inT.w(), inT.h(), inT.c(),
-                outT.w(), outT.h(), outT.c(),
-                filterT.w(), filterT.h(), convOpt.wStride(), convOpt.hStride(),
-                convOpt.padding());
-
-            Mat outputSubBatch = out.ColumnSlice(outputSizePerChannel * startSampleId, outputSizePerChannel * smallBatchSize);
-            Mat::Multiply(filter, false, m_tempMatrix, false, outputSubBatch);
         }
 
-        out.Reshape(outT.c() * outputSizePerChannel, batchSize);  //each sample becomes a column
-    }
+    public:
+        void Forward(const Tensor4D& inT, const Mat& in, const Filter& filterT, const Mat& filter, const ConvDesc& convDesc,
+            const Tensor4D& outT, Mat& out) override
+        {
+            assert(inT.c() == filterT.c());
+            assert(outT.c() == filterT.k());
+            assert(inT.n() == in.GetNumCols());
 
-    Tensor4DPtr CreateTensor(size_t w, size_t h, size_t c, size_t n) override
+            size_t packedInputRows = filterT.w() * filterT.h() * filterT.c();
+            size_t packedInputColsPerSample = outT.w() * outT.h();
+            size_t outputSizePerChannel = packedInputColsPerSample;
+
+            size_t batchSize = inT.n();
+            size_t maxTempMemSizeInSamples = (m_maxTempMemSizeInSamples == 0 ? batchSize : m_maxTempMemSizeInSamples);
+
+            assert(filter.GetNumCols() == packedInputRows && filter.GetNumRows() == outT.c());
+            out.Resize(outT.c(), outputSizePerChannel * batchSize);
+
+            size_t subBatchSize = min(batchSize, maxTempMemSizeInSamples);
+            size_t numSubBatches = (batchSize + subBatchSize - 1) / subBatchSize;
+
+            for (size_t i = 0; i < numSubBatches; i++)
+            {
+                size_t startSampleId = i * subBatchSize;
+                size_t endSampleId = min(batchSize, startSampleId + subBatchSize);
+                size_t smallBatchSize = endSampleId - startSampleId;
+
+                m_tempMatrix.Resize(packedInputRows, packedInputColsPerSample * smallBatchSize);
+                Mat inputSubBatch = in.ColumnSlice(startSampleId, smallBatchSize);
+                m_tempMatrix.AssignPackedConvolutionInput(inputSubBatch,
+                    inT.w(), inT.h(), inT.c(),
+                    outT.w(), outT.h(), outT.c(),
+                    filterT.w(), filterT.h(), convDesc.wStride(), convDesc.hStride(),
+                    convDesc.padding());
+
+                Mat outputSubBatch = out.ColumnSlice(outputSizePerChannel * startSampleId, outputSizePerChannel * smallBatchSize);
+                Mat::Multiply(filter, false, m_tempMatrix, false, outputSubBatch);
+            }
+
+            out.Reshape(outT.c() * outputSizePerChannel, batchSize);  //each sample becomes a column
+
+            assert(outT.n() == out.GetNumCols());
+        }
+
+        Tensor4DPtr CreateTensor(size_t w, size_t h, size_t c, size_t n) override
+        {
+            return std::make_unique<ConvolutionTensor4D>(w, h, c, n);
+        }
+
+        FilterPtr CreateFilter(size_t w, size_t h, size_t c, size_t k) override
+        {
+            return std::make_unique<Filter>(w, h, c, k);
+        }
+
+        ConvDescPtr CreateConvDescriptor(const Tensor4D& inT, const Filter& filterT, 
+            size_t wStride, size_t hStride, bool padding) override
+        {
+            return std::make_unique<ConvDesc>(inT, filterT, wStride, hStride, padding);
+        }
+
+    private:
+        size_t m_maxTempMemSizeInSamples;
+        Mat m_tempMatrix;
+    };
+
+    template<class ElemType>
+    std::unique_ptr<ConvolutionEngine<ElemType>> ConvolutionEngine<ElemType>::Create(DEVICEID_TYPE deviceId, size_t maxTempMemSizeInSamples)
     {
-        return std::make_unique<ConvolutionTensor4D>(w, h, c, n);
+        // REVIEW alexeyk: make cuDNN default when running on GPU and compiled with cuDNN, add config parameter to enable runtime switch between implementations.
+        if (deviceId >= 0 && CuDnnConvolutionEngine<ElemType>::IsSupported())
+            return std::make_unique<CuDnnConvolutionEngine<ElemType>>(deviceId, maxTempMemSizeInSamples);
+        return std::make_unique<DefaultConvolutionEngine<ElemType>>(deviceId, maxTempMemSizeInSamples);
     }
 
-private:
-    size_t m_maxTempMemSizeInSamples;
-    Mat m_tempMatrix;
-};
-
-template<class ElemType>
-std::unique_ptr<ConvolutionEngine<ElemType>> ConvolutionEngine<ElemType>::Create(DEVICEID_TYPE deviceId, size_t maxTempMemSizeInSamples)
-{
-    // REVIEW alexeyk: on GPU make cuDNN default, add config parameter to control that.
-    if (deviceId >= 0 && CuDnnConvolutionEngine<ElemType>::IsSupported())
-        return std::make_unique<CuDnnConvolutionEngine<ElemType>>(deviceId, maxTempMemSizeInSamples);
-    return std::make_unique<DefaultConvolutionEngine<ElemType>>(deviceId, maxTempMemSizeInSamples);
-}
-
-template class ConvolutionEngine<float>;
-template class ConvolutionEngine<double>;
+    template class ConvolutionEngine<float>;
+    template class ConvolutionEngine<double>;
 }}}
