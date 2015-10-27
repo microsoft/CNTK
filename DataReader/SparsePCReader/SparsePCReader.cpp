5d4347d76a3f8365f945c3590edfb91c63de748c
bmitra <bmitra@microsoft.com>
2015-10-27 11:48:29 -0700

    Adding layout handling to SparsePCReader.

--- DataReader/SparsePCReader/SparsePCReader.cpp
+++ DataReader/SparsePCReader/SparsePCReader.cpp
@@ -77,6 +77,7 @@ void SparsePCReader<ElemType>::Init(const ConfigParameters& readerConfig)
     m_microBatchSize = readerConfig("microbatchSize", "1");
 
     m_miniBatchSize = 0;
+    m_microBatchSize = readerConfig("microbatchSize", "1");
     m_traceLevel = readerConfig("traceLevel", "0");
     m_maxReadData = readerConfig("maxReadData", "0");
     m_doGradientCheck = readerConfig("gradientCheck", "false");
@@ -191,6 +192,8 @@ bool SparsePCReader<ElemType>::GetMinibatch(std::map<std::wstring, Matrix<ElemTy
     {
         return false;
     }
+
+    m_pMBLayout->Init(m_miniBatchSize / m_microBatchSize, m_microBatchSize, false);
 
     Matrix<ElemType>* labels = nullptr;
     auto labelEntry = matrices.find(m_labelName);
