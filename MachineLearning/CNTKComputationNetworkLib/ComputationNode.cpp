35c05dbf11fb42ffc8d84197b2ad36c12739aa8e
thhoens <thhoens@microsoft.com>
2015-11-04 17:19:21 -0800

    Fixed the Validation step for the LogisticNode. Also added it to the ExperimentalNetworkBuilder such that it accepts a variable number of arguments (and thus added the vararg functionality to ComputationNetworkBuilder). Note this can be changed in the future to accept either two or three arguments if desired.

--- MachineLearning/CNTKComputationNetworkLib/ComputationNode.cpp
+++ MachineLearning/CNTKComputationNetworkLib/ComputationNode.cpp
@@ -117,8 +117,11 @@ namespace Microsoft { namespace MSR { namespace CNTK {
     {
         // limited inference of children dimensions
         // if dimension not specified we assume two operands' dimensions should be the same
-        assert(m_children.size() == 2);
-        for (size_t index = 0; index < m_children.size(); index++)
+        // NOTE: The assert is set to check if >= 2 since this is called from nodes which have more than two children.
+        //      The number of children is formally verified elsewhere, so this will not break consistency. Should this
+        //      assert be removed?
+        assert(m_children.size() >= 2);
+        for (size_t index = 0; index < 2; index++)
         {
             auto in = Inputs(index);
             auto other = Inputs(1 - index);
