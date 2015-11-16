966215172c2adf443afac8ff5e7f92b14df0cd9e
Frank Seide <fseide@microsoft.com>
2015-11-15 22:30:23 -0800

    moved ImageLayout out from ComputationNode.h to separate new header file DataTensor.h, as this will need to be accessed by the readers as well

--- MachineLearning/CNTKComputationNetworkLib/ComputationNode.h
+++ MachineLearning/CNTKComputationNetworkLib/ComputationNode.h
@@ -9,6 +9,7 @@
 #include "Matrix.h"
 #include "ScriptableObjects.h"
 #include "Sequences.h"
+#include "DataTensor.h"
 #include "MatrixPool.h"
 
 #include <unordered_set>
@@ -23,10 +24,6 @@
 #include <atomic>
 #include <sstream>
 #include <iostream>
-// these 3 are for ImageLayout, which will move out of here. Don't forget to move these along
-#include <array>
-#include <numeric>
-#include <functional>
 
 //#define RNN_DEBUG 1
 #define DEFAULT_HIDDEN_ACTIVATION 0.1
@@ -58,60 +55,6 @@ namespace Microsoft { namespace MSR { namespace CNTK {
         copyNodeAll=3,                  // copy everything
         copyNodeChildrenCrossNetwork=4, // allow a cross network child copy
     };
-
-    // -----------------------------------------------------------------------
-    // ImageLayout -- tensor descriptor to describe the inner layout of a data vector that holds a tensor
-    //
-    // Specifically if the image is an image, then this is a 3-dimensional tensor with dimensions ( channels, width, height ),
-    // which represents the column-major interpretation of a transposed row-by-row-scanned image where each pixel stores (R,G,B) as a float3.
-    //
-    // BUGBUG: Tensors with other than 3 dimensions can currently not be used because they cannot be serialized with the current file format.
-    // -----------------------------------------------------------------------
-
-    // TODO: really support lengths other than 3, e.g. fix serialization code to handle variable-length descriptors
-    // TODO: rename to DataLayout
-    // TODO: must match ComputationNode::m_numRows; or, rather, the ImageLayout is how m_numRows is stored??
-    // TODO: move this elsewhere, maybe a separate header Tensors.h?
-    struct ImageLayout
-    {
-    public:
-        // BUGBUG: This initialization is not correct. This must match GetNumRows(). We probably cannot have all three members here.
-        // Idea: We could construct this thing with a ref to the enclosing ComputationNode, and replace 'width' by an expression.
-        ImageLayout() : m_tensorDims(3, 1) { }
-        template<class VEC>
-        ImageLayout(const VEC & dims) { m_tensorDims.reserve(dims.size()); m_tensorDims.assign(dims.begin(), dims.end()); }
-
-        void Invalidate() { m_tensorDims.assign(3, SIZE_MAX); } // TODO: clean up the valid/invalid situation (this is currently done inconsistently)
-
-        void SaveToFile(File& fstream) const
-        {
-            // TODO: need to use a generic format
-            assert(m_tensorDims.size() == 3);   // current format does not understand anything else
-            fstream << m_tensorDims[1] << m_tensorDims[2] << m_tensorDims[0]; // currently stored in order W, H, C. TODO: general tensor format will be different
-        }
-        void LoadFromFile(File& fstream)
-        {
-            // TODO: need to use a generic format
-            m_tensorDims.resize(3);     // current format is hard-coded for 3, for back compat
-            fstream >> m_tensorDims[1] >> m_tensorDims[2] >> m_tensorDims[0]; // currently stored in order W, H, C. TODO: general tensor format will be different
-        }
-
-        // interpretation as an image tensor
-        size_t GetNumChannels() const { return m_tensorDims[0]; }
-        size_t GetWidth()       const { return m_tensorDims[1]; }
-        size_t GetHeight()      const { return m_tensorDims[2]; }
-
-        size_t GetNumElements() const { return std::accumulate(m_tensorDims.begin(), m_tensorDims.end(), (size_t)1, std::multiplies<size_t>()); }
-
-        bool operator==(const ImageLayout & other) const { return m_tensorDims == other.m_tensorDims; }
-    private:
-        vector<size_t> m_tensorDims;
-    };
-    // use this function to construct an W,H,C tensor ImageLayout for images
-    static inline ImageLayout ImageLayoutWHC(size_t width, size_t height, size_t channels)
-    {
-        return ImageLayout(array<size_t, 3> { channels, width, height });
-    }
 
 #pragma region base computation class
 
