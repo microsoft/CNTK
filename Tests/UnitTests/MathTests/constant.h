10e7f42991083806d0e138fa1c5e3e6100592ed7
Wolfgang Manousek <wolfma@microsoft.com>
2015-11-18 09:49:55 +0100

    Add Math/CNTKMathTest/MatrixUnitTests to Tests/UnitTests/MathTests/MathTests
    cleanup files in Math/CNTKMathTest/CNTKMathTest and Tests/UnitTests/MathTests/MathTests

--- Tests/UnitTests/MathTests/constant.h
+++
@@ -1,9 +0,0 @@
-// <copyright file="constant.h" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-
-#pragma once
-
-extern const int c_deviceIdZero;
-
-extern const float c_epsilonFloatE5;
