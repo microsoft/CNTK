5c7c99a0eca021a2b7d41513f6934c5b82c3d7b2
Frank Seide <fseide@microsoft.com>
2015-11-21 20:00:28 -0800

    added a specialized version of operator(name, defaultValue) for ConfigParameters

--- Common/Include/commandArgUtil.h
+++ Common/Include/commandArgUtil.h
@@ -950,6 +950,15 @@ public:
         // we get here if no dict in the chain contains the entry, or if the entry's string value says "default"
         return defaultValue;
     }
+    // special version for ConfigParameters
+    template<>
+    ConfigParameters operator()<ConfigParameters>(const wchar_t * name,
+        const ConfigParameters & defaultValue) const
+    {
+        if (!defaultValue.empty())
+            LogicError("ConfigParameters::operator() for ConfigParameters can only accept an empty default value.");
+        return Find(string(name, name + wcslen(name)), "");
+    }
 
     ConfigValue Find(const std::string& name,
                      const char* defaultvalue = NULL) const
