1a1bd17c21bd3f4bfb9881a4ef0cde1a0f3abf5f
Frank Seide <fseide@microsoft.com>
2015-12-18 10:01:17 -0800

    bug fix: ComputationNode::DetermineNumCols() was an outdated pre-refactoring hold-over with a now incorrect validity check. Can just be removed, Should fix reported by user xiaoqing; removed unnecessary ad inconsistent use of 'this->' throughout Matrix.cpp, also fixed some bad indentations

--- Source/Math/Matrix.h
+++ Source/Math/Matrix.h
@@ -74,6 +74,7 @@ namespace Microsoft { namespace MSR { namespace CNTK {
         void _transferToDevice(int id_to, bool ismoved=true, bool emptyTransfer=false) const; 
         static void DecideAndMoveToRightDevice(const Matrix<ElemType>& a, const Matrix<ElemType>& b);
         static void DecideAndMoveToRightDevice(const Matrix<ElemType>& a, const Matrix<ElemType>& b, const Matrix<ElemType>& c);
+        static void DecideAndMoveToRightDevice(const Matrix<ElemType>& a, const Matrix<ElemType>& b, const Matrix<ElemType>& c, const Matrix<ElemType>& d);
         static void CopyElementsFromDenseToSparse(CPUMatrix<ElemType>& from, CPUSparseMatrix<ElemType>& dest);
 
     public:
