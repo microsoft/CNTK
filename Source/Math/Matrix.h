af4afaf6f7823fa4133d024d4e25b4b0ff102b42
Frank Seide <fseide@microsoft.com>
2015-12-18 19:59:26 -0800

    tensor library works in PlusNode prototype implementation() which reduces ForwardProp() to two lines of code; bug fix: GetTensorsForwardBinary() ignored #samples in FrameRange; bug fix: TensorView cannot hold a reference to a Matrix object since these are often temporaries. Instead, we copy a Matrix object that itself is a reference; deleted orphaned ClassDiagram.cd from Math Project

--- Source/Math/Matrix.h
+++ Source/Math/Matrix.h
@@ -175,9 +175,9 @@ namespace Microsoft { namespace MSR { namespace CNTK {
             m_baseMatrix->VerifySize(rows, cols);
         }
 
-        Matrix<ElemType> AsReference() { return ColumnSlice(0, GetNumCols()); } // get a reference (e.g. this is not resizable but can be reshaped)
-        void Reshape(const size_t numRows, const size_t numCols);               // note: reshapes in place. To get a reshaped reference, use Reshaped()
-        Matrix<ElemType> Reshaped(const size_t numRows, const size_t numCols)   // get a reshaped reference
+        Matrix<ElemType> AsReference() const { return ColumnSlice(0, GetNumCols()); }   // get a reference (e.g. this is not resizable but can be reshaped)
+        void Reshape(const size_t numRows, const size_t numCols);                       // note: reshapes in place. To get a reshaped reference, use Reshaped()
+        Matrix<ElemType> Reshaped(const size_t numRows, const size_t numCols)           // get a reshaped reference
         {
             Matrix<ElemType> result = AsReference();
             result.Reshape(numRows, numCols);
