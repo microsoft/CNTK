279a6535c11e97a3d2b054c68fad2f746a6e8a2c
Frank Seide <fseide@microsoft.com>
2015-12-23 00:17:01 -0800

    added a missing _CRT_SECURE_NO_WARNINGS

--- Source/Common/DataReader.cpp
+++ Source/Common/DataReader.cpp
@@ -6,6 +6,10 @@
 // DataReader.cpp : Defines the exported functions for the DLL application.
 //
 
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // "secure" CRT not available on all platforms  --add this at the top of all CPP files that give "function or variable may be unsafe" warnings
+#endif
+
 #include "stdafx.h"
 #define DATAREADER_LOCAL
 #include "Basics.h"
