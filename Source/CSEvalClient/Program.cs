7eb5cdc104c739c9f2dd569acf8799389b501dda
Gaizka Navarro <gaizna@microsoft.com>
2016-01-20 14:45:27 +0100

    CR Fix - Switched copyright headers to MIT license CR Fix - Removed EvalDll.lib linking

--- Source/CSEvalClient/Program.cs
+++ Source/CSEvalClient/Program.cs
@@ -1,8 +1,8 @@
 ﻿//
-// <copyright file="Program.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
+
 using System;
 using System.Collections.Generic;
 using System.IO;
