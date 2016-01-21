4691595f0f9f12a4bca49731cd03ecc44d6a4f8b
Frank Seide <fseide@microsoft.com>
2016-01-21 10:31:55 -0800

    bug fix, ClassBasedCrossEntropyWithSoftmax now working

--- Source/ComputationNetworkLib/ComputationNode.h
+++ Source/ComputationNetworkLib/ComputationNode.h
@@ -1417,6 +1417,10 @@ public:
             if (Value().HasNan("EndForwardProp"))
                 LogicError("%ls %ls operation unexpectedly produced NaN values.", NodeName().c_str(), OperationName().c_str());
 #endif
+#if 0
+            MaskMissingValueColumnsToZero(FrameRange(m_pMBLayout)); // HasNaN() operates on a whole matrix, so first flatten all gaps to 0
+            Value().Print(msra::strfun::utf8(NodeName()), 0, min(Value().GetNumRows()-1, 4), 0, min(Value().GetNumCols()-1, 4));
+#endif
         InvalidateMissingValueColumns(FrameRange(m_pMBLayout)); // blast NaNs into columns that are gaps in a packed layout
         }
 #endif
