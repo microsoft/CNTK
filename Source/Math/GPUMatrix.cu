c7c5403994fe72bd193996ad0a7909e633dd7eb9
Frank Seide <fseide@microsoft.com>
2016-01-28 09:40:57 -0800

    tensor support for RowSlice operation; completed CudaErrString<cublasStatus_t>()

--- Source/Math/GPUMatrix.cu
+++ Source/Math/GPUMatrix.cu
@@ -94,10 +94,23 @@ const char* CudaErrString<cudaError_t>(cudaError_t x)
     return cudaGetErrorString(x);
 }
 template <>
-const char* CudaErrString<cublasStatus_t>(cublasStatus_t)
+const char* CudaErrString<cublasStatus_t>(cublasStatus_t e)
 {
     cudaDeviceSynchronize();
-    return "(see cublas_api.h & look for cublasStatus_t or CUBLAS_STATUS_xxx)";
+    switch (e)
+    {
+    case CUBLAS_STATUS_SUCCESS:          return "CUBLAS_STATUS_SUCCESS";
+    case CUBLAS_STATUS_NOT_INITIALIZED:  return "CUBLAS_STATUS_NOT_INITIALIZED";
+    case CUBLAS_STATUS_ALLOC_FAILED:     return "CUBLAS_STATUS_ALLOC_FAILED";
+    case CUBLAS_STATUS_INVALID_VALUE:    return "CUBLAS_STATUS_INVALID_VALUE";
+    case CUBLAS_STATUS_ARCH_MISMATCH:    return "CUBLAS_STATUS_ARCH_MISMATCH";
+    case CUBLAS_STATUS_MAPPING_ERROR:    return "CUBLAS_STATUS_MAPPING_ERROR";
+    case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
+    case CUBLAS_STATUS_INTERNAL_ERROR:   return "CUBLAS_STATUS_INTERNAL_ERROR";
+    case CUBLAS_STATUS_NOT_SUPPORTED:    return "CUBLAS_STATUS_NOT_SUPPORTED";
+    case CUBLAS_STATUS_LICENSE_ERROR:    return "CUBLAS_STATUS_LICENSE_ERROR";
+    default:                             return "(look for CUBLAS_STATUS_xxx in cublas_api.h)";
+    }
 }
 template <>
 const char* CudaErrString<curandStatus>(curandStatus)
