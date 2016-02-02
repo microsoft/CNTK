fe75bb1650191e5dbe49bbddbe9bb78b313ba2bd
Mark Hillebrand <mahilleb@microsoft.com>
2016-02-02 08:31:52 +0100

    Extend build information included / reported by CNTK
       * Build target: GPU or CPU-only
       * Build type (Debug or Release), was missing for Windows
       * Whether or not 1bit-SGD was included
       * If there were local modifications in the Git working copy
       * Generate BUILD{ER,MACHINE,PATH} on Linux

--- Source/CNTK/prebuild.bat
+++ Source/CNTK/prebuild.bat
@@ -1,47 +1,75 @@
-@echo off 
+@echo off
 setlocal enableDelayedexpansion
 
 ::: This is called as a pre-build step for the CNTK executable.
+::: It receives the build's configuration, $(Configuration), as first paramter.
 ::: It creates buildinfo.h, which makes version information available to the executable itself.
 
 echo #ifndef _BUILDINFO_H > buildinfo.h$$
-echo #define _BUILDINFO_H >> buildinfo.h$$ 
+echo #define _BUILDINFO_H >> buildinfo.h$$
 
-FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i           
-:: assuming hostname always exists 
+FOR /F %%i IN ('hostname') DO SET HOST=%%i
+:: assuming hostname always exists
 
-:: not sure whether git in path ? 
-call git --version 2 > nul 
+:: not sure whether git in path ?
+call git --version 2 > nul
 if not %ERRORLEVEL% == 9009 (
     echo #define _GIT_EXIST >> buildinfo.h$$
-    FOR /F "usebackq" %%i IN (`git rev-parse --abbrev-ref HEAD`) DO SET BRANCH=%%i
-    FOR /F "usebackq" %%i IN (`git rev-parse HEAD`) DO SET COMMIT=%%i
+    FOR /F %%i IN ('git rev-parse --abbrev-ref HEAD') DO SET BRANCH=%%i
+    FOR /F %%i IN ('git rev-parse HEAD') DO SET COMMIT=%%i
+    set STATUS=
+    git diff --quiet --cached
+    if not errorlevel 1 git diff --quiet
+    if errorlevel 1 set STATUS= ^(modified^)
     echo #define _BUILDBRANCH_  "!BRANCH!"      >> buildinfo.h$$
-    echo #define _BUILDSHA1_    "!COMMIT!"      >> buildinfo.h$$
+    echo #define _BUILDSHA1_    "!COMMIT!!STATUS!">> buildinfo.h$$
 )
 
-echo #define _BUILDER_ "%USERNAME%"     >> buildinfo.h$$ 
-echo #define _BUILDMACHINE_ "!HOST!"    >> buildinfo.h$$
+:: For now, math lib is basically hardwired
+if exist ACML_PATH (
+    echo #define _MATHLIB_ "acml">> buildinfo.h$$
+)
 
-set a=%~dp0
-set buildpath="%a:\=\\%"
+echo #define _BUILDER_ "%USERNAME%"     >> buildinfo.h$$
+
+echo #define _BUILDER_ "%USERNAME%"     >> buildinfo.h$$
+echo #define _BUILDMACHINE_ "%HOST%"    >> buildinfo.h$$
+
+set scriptpath=%~dp0
+set buildpath="%scriptpath:\=\\%"
 echo #define _BUILDPATH_    %buildpath%     >> buildinfo.h$$
 
-if "%cuda_path%" == "" ( 
-    echo #define _CUDA_PATH_    "NOT_DEFINED"     >> buildinfo.h$$  
+set build_type=Unknown
+set build_target=Unknown
+if /i "%~1" == "Debug" set build_type=Debug&set build_target=GPU
+if /i "%~1" == "Debug_CpuOnly" set build_type=Debug&set build_target=CPU-only
+if /i "%~1" == "Release" set build_type=Release&set build_target=GPU
+if /i "%~1" == "Release_CpuOnly" set build_type=Release&set build_target=CPU-only
+
+echo #define _BUILDTYPE_ "%build_type%">> buildinfo.h$$
+echo #define _BUILDTARGET_ "%build_target%">> buildinfo.h$$
+
+if "%CNTK_ENABLE_1BitSGD%" == "true" (
+    echo #define _WITH_1BITSGD_ "yes">>buildinfo.h$$
+) else (
+    echo #define _WITH_1BITSGD_ "no">>buildinfo.h$$
+)
+
+if not %build_target% == CPU-only (
+    if "%cuda_path%" == "" (
+        echo #define _CUDA_PATH_    "NOT_DEFINED"     >> buildinfo.h$$
     ) else (
         echo #define _CUDA_PATH_    "%cuda_path:\=\\%" >> buildinfo.h$$
     )
 
-if not "%cudnn_path%" == "" (
-    echo #define _CUDNN_PATH_  "%cudnn_path:\=\\%" >> buildinfo.h$$
-    ) 
+    if not "%cudnn_path%" == "" (
+        echo #define _CUDNN_PATH_  "%cudnn_path:\=\\%" >> buildinfo.h$$
+    )
 
-if not "%cub_path%" == "" (
-    echo #define _CUB_PATH_  "%cub_path:\=\\%" >> buildinfo.h$$
-    ) 
-
-
+    if not "%cub_path%" == "" (
+        echo #define _CUB_PATH_  "%cub_path:\=\\%" >> buildinfo.h$$
+    )
+)
 
 echo #endif >> buildinfo.h$$
 
