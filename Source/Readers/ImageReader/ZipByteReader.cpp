90c2fbff304b89a66db4b9de702f1bdbd3d0dad6
Alexey Kamenev <alexeyk@microsoft.com>
2016-02-19 15:56:11 -0800

    Made ImageReader .zip reader thread-safe.

--- Source/Readers/ImageReader/ZipByteReader.cpp
+++ Source/Readers/ImageReader/ZipByteReader.cpp
@@ -21,29 +21,39 @@ std::string GetZipError(int err)
 }
 
 ZipByteReader::ZipByteReader(const std::string& zipPath)
-    : m_zip(nullptr)
+    : m_zipPath(zipPath)
 {
-    assert(!zipPath.empty());
-    int err = 0;
-    m_zip = zip_open(zipPath.c_str(), 0, &err);
-    if (ZIP_ER_OK != err)
-        RuntimeError("Failed to open %s, zip library error: %s", zipPath.c_str(), GetZipError(err).c_str());
+    assert(!m_zipPath.empty());
 }
 
-ZipByteReader::~ZipByteReader()
+ZipByteReader::ZipPtr ZipByteReader::OpenZip()
 {
-    if (m_zip != nullptr)
-        zip_close(m_zip);
+    int err = ZIP_ER_OK;
+    auto zip = zip_open(m_zipPath.c_str(), 0, &err);
+    if (ZIP_ER_OK != err)
+        RuntimeError("Failed to open %s, zip library error: %s", m_zipPath.c_str(), GetZipError(err).c_str());
+
+    return ZipPtr(zip, [](zip_t* z)
+    {
+        assert(z != nullptr);
+        int err = zip_close(z);
+        assert(ZIP_ER_OK == err);
+#ifdef NDEBUG
+        UNUSED(err);
+#endif
+    });
 }
 
 void ZipByteReader::Register(size_t seqId, const std::string& path)
 {
-    struct zip_stat stat;
+    auto zipFile = m_zips.pop_or_create([this]() { return OpenZip(); });
+    zip_stat_t stat;
     zip_stat_init(&stat);
-    int err = zip_stat(m_zip, path.c_str(), 0, &stat);
+    int err = zip_stat(zipFile.get(), path.c_str(), 0, &stat);
     if (ZIP_ER_OK != err)
         RuntimeError("Failed to get file info of %s, zip library error: %s", path.c_str(), GetZipError(err).c_str());
     m_seqIdToIndex[seqId] = std::make_pair(stat.index, stat.size);
+    m_zips.push(std::move(zipFile));
 }
 
 cv::Mat ZipByteReader::Read(size_t seqId, const std::string& path)
@@ -59,24 +69,27 @@ cv::Mat ZipByteReader::Read(size_t seqId, const std::string& path)
     auto contents = m_workspace.pop_or_create([size]() { return vector<unsigned char>(size); });
     if (contents.size() < size)
         contents.resize(size);
-    zip_file *file = zip_fopen_index(m_zip, index, 0);
+    auto zipFile = m_zips.pop_or_create([this]() { return OpenZip(); });
+    zip_file *file = zip_fopen_index(zipFile.get(), index, 0);
     assert(nullptr != file);
     if (nullptr == file)
     {
         RuntimeError("Could not open file %s in the zip file, sequence id = %lu, zip library error: %s",
-                     path.c_str(), (long)seqId, GetZipError(zip_error_code_zip(zip_get_error(m_zip))).c_str());
+                     path.c_str(), (long)seqId, GetZipError(zip_error_code_zip(zip_get_error(zipFile.get()))).c_str());
     }
     assert(contents.size() >= size);
     zip_uint64_t bytesRead = zip_fread(file, contents.data(), size);
     assert(bytesRead == size);
     if (bytesRead != size)
     {
-        zip_fclose(file);
         RuntimeError("Bytes read %lu != expected %lu while reading file %s",
                      (long)bytesRead, (long)size, path.c_str());
     }
     zip_fclose(file);
+    m_zips.push(std::move(zipFile));
+
     cv::Mat img = cv::imdecode(cv::Mat(1, (int)size, CV_8UC1, contents.data()), cv::IMREAD_COLOR);
+    assert(nullptr != img.data);
     m_workspace.push(std::move(contents));
     return img;
 }
