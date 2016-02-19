5f5fa2f8a8f7c23b4e65a3a63c2c809d2bb39a3e
Alexey Kamenev <alexeyk@microsoft.com>
2016-02-19 15:56:11 -0800

    Made ImageReader .zip reader thread-safe.

--- Source/Readers/ImageReader/ByteReader.h
+++ Source/Readers/ImageReader/ByteReader.h
@@ -9,6 +9,7 @@
 #ifdef USE_ZIP
 #include <zip.h>
 #include <unordered_map>
+#include <memory>
 #include "ConcStack.h"
 #endif
 
@@ -41,13 +42,16 @@ class ZipByteReader : public ByteReader
 {
 public:
     ZipByteReader(const std::string& zipPath);
-    ~ZipByteReader();
 
     void Register(size_t seqId, const std::string& path) override;
     cv::Mat Read(size_t seqId, const std::string& path) override;
 
 private:
-    zip* m_zip;
+    using ZipPtr = std::unique_ptr<zip_t, void(*)(zip_t*)>;
+    ZipPtr OpenZip();
+
+    std::string m_zipPath;
+    conc_stack<ZipPtr> m_zips;
     std::unordered_map<size_t, std::pair<zip_uint64_t, zip_uint64_t>> m_seqIdToIndex;
     conc_stack<std::vector<unsigned char>> m_workspace;
 };
