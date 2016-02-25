6e18a72f673bc75d51281a938f648a9cd817195e
Alexey Orlov <alexeyo@microsoft.com>
2016-02-25 11:20:25 +0100

    Binary drop script, Windows. Final touches

--- Tools/make_binary_drop_windows.ps1
+++ Tools/make_binary_drop_windows.ps1
@@ -2,6 +2,9 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 # WARNING. This will run in Microsoft Internal Environment ONLY
+# Generating CNTK Binary drops in Jenkins environment
+
+Write-Host "Making binary drops..."
 
 # Get Jenkins environment Variables
 $buildConfig =(Get-Item env:"BUILD_CONFIGURATION").Value
@@ -33,6 +36,7 @@ $sharePath = "\\muc-vfs-01a\CNTKshare\CNTK-Binary-Drop" + "\" + $targetConfig
 New-Item -Path $baseDropPath -ItemType "directory"
 
 # Copy build binaries
+Write-Host "Copying build binaries ..."
 Copy-Item $buildPath -Recurse -Destination $baseDropPath\cntk
 
 # Clean unwanted items
@@ -44,17 +48,18 @@ Remove-Item $baseDropPath\cntk\*.exp
 Remove-Item $baseDropPath\cntk\*.metagen
 
 # Copy Examples
+Write-Host "Copying Examples ..."
 Copy-Item Examples -Recurse -Destination $baseDropPath\Examples
 
 # Copy all items from the share
-Write-Host "Current user is "(Get-Item env:USERDOMAIN).Value"\"(Get-Item env:USERNAME).Value
+# For whatever reason Copy-Item in the line below does not work
 # Copy-Item $sharePath"\*"  -Recurse -Destination $baseDropPath
-# Try with Robocopy
+# Copying with Robocopy
+Write-Host "Copying dependencies and other files from Remote Share ..."
 $robocopyCmd = "robocopy " + $sharePath + " " + $baseDropPath + " /s /e"
-Write-Host "Invoking" $robocopyCmd
 Invoke-Expression $robocopyCmd
 
-
+Write-Host "Making ZIP and cleaning up..."
 
 # Make ZIP file
 $source = $PWD.Path + "\" + $basePath
@@ -63,4 +68,4 @@ Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory($source, $destination)
 
 # Remove ZIP sources
-# If (Test-Path $basePath) {Remove-Item $basePath -Recurse}
+If (Test-Path $basePath) {Remove-Item $basePath -Recurse}
