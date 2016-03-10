6374ba95ef87823c4da34ff56248da14c8a6c81d
Willi Richert <wilrich@microsoft.com>
2016-03-10 21:30:18 +0100

    Fix templating

--- LanguageBindings/Python/cntk/context.py
+++ LanguageBindings/Python/cntk/context.py
@@ -78,8 +78,13 @@ class AbstractContext(object, metaclass=ABCMeta):
         """                
         tmpl = open(CNTK_EVAL_TEMPLATE_PATH, "r").read()
         reader_config = reader.generate_config()
-        output_filename = os.path.join(self.context.directory, CNTK_OUTPUT_FILENAME)
-        return tmp%{'reader':reader_config, 'outputFile':output_filename}
+        output_filename = os.path.join(self.directory, CNTK_OUTPUT_FILENAME)
+        tmpl_dict = {
+                'reader':reader_config,
+                'outputFile':output_filename,
+                'ModelDescription':node.to_description()
+                } 
+        return tmpl%tmpl_dict
         
         
     @abstractmethod
@@ -123,7 +128,7 @@ class Context(AbstractContext):
         :param config_content: a string containing the configuration
         """
         filename = os.path.join(self.directory, config_file_name)        
-        with open(os.path.join(self.context.directory, filename), "w") as out:
+        with open(os.path.join(self.directory, filename), "w") as out:
             out.write(config_content)            
         subprocess.check_call([CNTK_EXECUTABLE_PATH, "configFile=%s"%filename])        
     
@@ -155,6 +160,16 @@ class Context(AbstractContext):
         """            
         config_content = self._generate_eval_config(node, reader) 
         self._call_cntk(CNTK_EVAL_CONFIG_FILENAME, config_content) 
+
+        import glob
+        out_file_wildcard = os.path.join(self.directory, CNTK_OUTPUT_FILENAME+'.*')
+        out_filenames = glob.glob(out_file_wildcard)
+        if len(out_filenames)!=1:
+            raise ValueError('expected exactly one file starting with "%s", but got %s'%(CNTK_OUTPUT_FILENAME, out_filenames))
+
+        data = np.loadtxt(out_filenames[0])
+
+        return data
 
 class ClusterContext(AbstractContext):
     """This is a sub-class of AbstractContext, use it to submit your workloads to the cluster.
--- LanguageBindings/Python/cntk/reader.py
+++ LanguageBindings/Python/cntk/reader.py
@@ -13,7 +13,7 @@ class AbstractReader(dict, metaclass=ABCMeta):
 class UCIFastReader(AbstractReader):        
     """This is the reader class
     
-    :param file: data file path
+    :param filename: data file path
     :param features_dim: number of feature columns
     :param labels_dim: number of label columns
     :param features_start: the index of the first feature column
@@ -24,13 +24,17 @@ class UCIFastReader(AbstractReader):
     :param custom_delimiter: the default is space and tab, you can specify other delimiters to be used
     """
     
-    def __init__(self, file, features_dim, labels_dim, features_start, labels_start, \
-                num_of_classes, label_mapping_file, custom_delimiter = None):
+    def __init__(self, filename, \
+            features_dim=None, labels_dim=None, \
+            features_start=None, labels_start=None, \
+            num_of_classes=None, \
+            label_mapping_file=None, \
+            custom_delimiter = None):
         """ Reader constructor    
         """    
             
         self["readerType"] = self.__class__.__name__
-        self["file"] = file 
+        self["filename"] = filename 
         self["featuresDim"] = features_dim
         self["labelsDim"] = labels_dim
         self["featuresStart"] = features_start
@@ -47,24 +51,51 @@ class UCIFastReader(AbstractReader):
         """
         template = '''
         		readerType = "%(readerType)s"
-        		file = "%(file)s"
+        		file = "%(filename)s"
         		randomize = "none"
         		verbosity = 1          
-               %(customDelimiter)s
+               '''
+        if self['customDelimiter']:
+            template += '''
+               customDelimiter=%(customDelimiter)s
+               '''
+
+        if self['featuresStart']:
+            template += '''
                
         		features=[
         			start = "%(featuresStart)s"
         			dim = "%(featuresDim)s"
         		]
-        	
+                '''
+        
+        if self['labelsStart']:
+            template += '''
+
         		labels=[
         			start = "%(labelsStart)s"
         			dim = "%(labelsDim)s"		          
                     labelDim="%(numOfClasses)s"        			
                     labelMappingFile="%(labelMappingFile)s" 
         		]
-        '''
+                '''
                             
-        config = template%self
-        return config 
+        return template%self
     
+def NumPyReader(data, filename): 
+    """
+    This is the reader class for bare Python arrays.
+    """
+    
+    import numpy as np
+    data = np.asarray(data)
+    format_str = ' '.join(['%f']*data.shape[1])
+    np.savetxt(filename, data, delimiter=' ', newline='\r\n', fmt=format_str)
+
+    return UCIFastReader(\
+            filename, 
+            # Features are stored per column, but CNTK transforms them and uses
+            # them per row. That's why we take the number of columns here.
+            features_dim=data.shape[1], labels_dim=None, \
+            features_start=0, labels_start=None, \
+            num_of_classes=None, label_mapping_file=None)
