65999436b3f73e1fdb7772be6539f3a56726a265
Willi Richert <wilrich@microsoft.com>
2016-03-11 18:35:34 +0100

    Fix templating

--- LanguageBindings/Python/cntk/reader.py
+++ LanguageBindings/Python/cntk/reader.py
@@ -57,12 +57,12 @@ class UCIFastReader(AbstractReader):
         		verbosity = 1          
                '''
                
-        if self['CustomDelimiter']:
+        if self['CustomDelimiter'] is not None:
             template += '''
                customDelimiter=%(CustomDelimiter)s
                '''
         # BUG: in case of features_start = 0
-        if self['FeaturesStart']:
+        if self['FeaturesStart'] is not None:
             template += '''               
         		features=[
         			start = "%(FeaturesStart)s"
@@ -70,7 +70,7 @@ class UCIFastReader(AbstractReader):
         		]
                 '''
         
-        if self['LabelsStart']:
+        if self['LabelsStart'] is not None:
             template += '''
 
         		labels=[
