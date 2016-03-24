df3cc839463986805586a40f5830457ddb933616
jeanfad <jeanfad@microsoft.com>
2016-03-24 18:49:09 +0100

    remove reader requirement from test and predict actions and update mnist example

--- contrib/Python/cntk/examples/MNIST/mnist_one_layer.py
+++ contrib/Python/cntk/examples/MNIST/mnist_one_layer.py
@@ -29,27 +29,23 @@ if (__name__ == "__main__"):
     hidden_dim=200
     
     training_filename=os.path.join("Data", "Train-28x28.txt")    
+    test_filename=os.path.join("Data", "Test-28x28.txt")    
     
     features = Input(feat_dim, var_name='features')
+    features.attach_uci_fast_reader(training_filename, 1)
     
     feat_scale = Constant(0.00390625)
     feats_scaled = Scale(feat_scale, features)
     
     labels = Input(label_dim, tag='label', var_name='labels')
+    labels.attach_uci_fast_reader(training_filename, 0, True, 1, os.path.join("Data", "labelsmap.txt"))    
     
     h1 = dnn_sigmoid_layer(feat_dim, hidden_dim, feats_scaled, 1)    
     out = dnn_layer(hidden_dim, label_dim, h1, 1)
     out.tag = 'output'
     
     ec = CrossEntropyWithSoftmax(labels, out)
-    ec.tag = 'criterion'
-    
-    # Build the reader            
-    r = UCIFastReader(filename=training_filename)
-    
-    # Add the input node to the reader
-    r.add_input(features, 1, feat_dim)
-    r.add_input(labels, 0, 1, label_dim, os.path.join("Data", "labelsmap.txt"))
+    ec.tag = 'criterion'    
     
     # Build the optimizer (settings are scaled down)
     my_sgd = SGD(epoch_size = 600, minibatch_size = 32, learning_ratesPerMB = 0.1, max_epochs = 5, momentum_per_mb = 0)
@@ -57,7 +53,10 @@ if (__name__ == "__main__"):
     # Create a context or re-use if already there
     with Context('mnist_one_layer', root_node= ec, clean_up=False) as ctx:            
         # CNTK actions
-        ctx.train(my_sgd, r)
-        r["FileName"] = os.path.join("Data", "Test-28x28.txt")
-        ctx.test(r)        
-        ctx.predict(r)       
+        #ctx.train(my_sgd)
+        features.attach_uci_fast_reader(test_filename, 1)
+        labels.attach_uci_fast_reader(test_filename, 0, True, 1, os.path.join("Data", "labelsmap.txt"))    
+        ctx.predict()       
+        ctx.test()        
+        ctx.predict()       
+
