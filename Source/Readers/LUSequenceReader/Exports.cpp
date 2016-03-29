5e327cc5dc5872b3e1d09699268066f6a464c998
Alexey Reznichenko <alrezni@microsoft.com>
2016-03-29 16:25:18 +0200

    Fix utf-8 issues in LUSequenceReader

--- Source/Readers/LUSequenceReader/Exports.cpp
+++ Source/Readers/LUSequenceReader/Exports.cpp
@@ -11,14 +11,29 @@
 #include "LUSequenceReader.h"
 #include "LUSequenceWriter.h"
 
+#ifdef _MSC_VER
+#include <codecvt>
+#endif
+
 namespace Microsoft { namespace MSR { namespace CNTK {
+
+void EnableUTF8Support() 
+{
+#ifdef _MSC_VER
+    locale::global(locale(locale::empty(), new codecvt_utf8<wchar_t>));
+#else
+    locale::global(locale("")); // assuming the user-preferred locale supports UTF-8
+#endif
+}
 
 extern "C" DATAREADER_API void GetReaderF(IDataReader** preader)
 {
+    EnableUTF8Support();
     *preader = new MultiIOBatchLUSequenceReader<float>();
 }
 extern "C" DATAREADER_API void GetReaderD(IDataReader** preader)
 {
+    EnableUTF8Support();
     *preader = new MultiIOBatchLUSequenceReader<double>();
 }
 
