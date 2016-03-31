1a2a4256a9958ea4441d5928a6c76dd689dbc06d
Clemens Marschner <clemensm@microsoft.com>
2016-03-31 17:23:17 +0200

    Set output dimensions for GatherPacked

--- Source/ComputationNetworkLib/ReshapingNodes.cpp
+++ Source/ComputationNetworkLib/ReshapingNodes.cpp
@@ -219,11 +219,24 @@ template <class ElemType>
     if (isFinalValidationPass && (!Input(INDEXDATA)->HasMBLayout()))
         LogicError("%ls requires first argument (index data) to have a time dimension.", this->NodeDescription().c_str());
 
-    if (isFinalValidationPass && Input(INDEXDATA)->GetSampleLayout().GetNumElements() != 1)
-        InvalidArgument("%ls requires the first argument (indexData) to be a scalar time sequence.", this->NodeDescription().c_str());
+    bool sourceHasTimeDimension = Input(SOURCEDATA)->HasMBLayout();
 
-    // inherit tensor dimension from sourceData
-    SetDims(Input(SOURCEDATA)->GetSampleLayout(), HasMBLayout());
+    if (isFinalValidationPass && Input(INDEXDATA)->GetSampleLayout().GetNumElements() != 1)
+        InvalidArgument("%ls requires the first argument (index data) to be a scalar time sequence.", this->NodeDescription().c_str());
+
+    // inherit tensor dimension from sourceData, minus the last (column or time) dimension. TODO this needs to become simpler...
+    if (sourceHasTimeDimension)
+        SetDims(Input(SOURCEDATA)->GetSampleLayout(), HasMBLayout());
+    else
+    {
+        SmallVector<size_t> layout = { 1 }; // Scalar
+        if (Input(SOURCEDATA)->GetSampleLayout().GetRank() > 1)
+        {
+            auto srcLayout = Input(SOURCEDATA)->GetSampleLayout().GetDims();
+            layout.assign(srcLayout.begin(), srcLayout.end() - 1);
+        }
+        SetDims(TensorShape(layout), HasMBLayout());
+    }
 }
 
 template class GatherPackedNode<float>;
