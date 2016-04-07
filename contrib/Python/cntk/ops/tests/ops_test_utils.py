352c98b4f20c8cbece216677af91f137d7498fa9
jeanfad <jeanfad@microsoft.com>
2016-04-07 18:07:09 +0200

    add parametrized fixture for cpu-gpu

--- contrib/Python/cntk/ops/tests/ops_test_utils.py
+++ contrib/Python/cntk/ops/tests/ops_test_utils.py
@@ -9,6 +9,7 @@ Utils for operations unit tests
 """
 
 import numpy as np
+import pytest
 from ...context import get_new_context
 from ...graph import *
 from ...reader import *
@@ -18,9 +19,14 @@ C = constant
 I = input
 AA = np.asarray
 
-def test_helper(root_node, expected, clean_up=True, backward_pass = False, input_node = None):
+@pytest.fixture(params=[-1,0])
+def cpu_gpu(request):
+    return request.param
+
+def test_helper(root_node, expected, device_id = -1, clean_up=True, backward_pass = False, input_node = None):
     with get_new_context() as ctx:
         ctx.clean_up = clean_up
+        ctx.device_id = device_id
         assert not ctx.input_nodes
         result = ctx.eval(root_node, None, backward_pass, input_node)
 
