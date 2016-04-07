c0d7a37ba589e03498bc5a8afa3319555ff0d59c
jeanfad <jeanfad@microsoft.com>
2016-04-07 11:51:30 +0200

    headers for new files

---
+++ contrib/Python/cntk/ops/linear.py
@@ -0,0 +1,13 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root 
+# for full license information.
+# ==============================================================================
+
+"""
+Linear algebra operations
+"""
+
+from cntk.graph import ComputationNode, InputComputationNodeBase, ImageInputComputationNodeBase
+from cntk.ops.cntk1 import Times
+
