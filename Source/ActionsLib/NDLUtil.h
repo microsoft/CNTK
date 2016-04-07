ba1a05738dadd0e93593780dc446febbab1458b7
thhoens <thhoens@microsoft.com>
2016-04-07 13:54:13 -0700

    Added a more descriptive error message to CheckOutputNodes

--- Source/ActionsLib/NDLUtil.h
+++ Source/ActionsLib/NDLUtil.h
@@ -136,7 +136,7 @@ public:
 
                 // if no evaluation value exists throw an error
                 if (!cnNode)
-                    RuntimeError("Invalid node '%s' as an output node, nonexistant or wrong type", nodes[i]->GetName().c_str());
+                    RuntimeError("Invalid node '%s' as an '%s', nonexistant or wrong type", nodes[i]->GetName().c_str(), symbolName.c_str());
 
                 // add to the desired node group
                 m_net->AddToNodeGroup(groupTag, cnNode);
