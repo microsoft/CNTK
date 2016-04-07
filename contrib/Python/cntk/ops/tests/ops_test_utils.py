a70dc19d80e31cb44cb0a4397815f48ebf306ac0
jeanfad <jeanfad@microsoft.com>
2016-04-07 17:25:09 +0200

    new structure, add test

---
+++ contrib/Python/cntk/ops/tests/ops_test_utils.py
@@ -0,0 +1,30 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root 
+# for full license information.
+# ==============================================================================
+
+"""
+Utils for operations unit tests
+"""
+
+import numpy as np
+from ...context import get_new_context
+from ...graph import *
+from ...reader import *
+
+#Keeping things short
+C = constant
+I = input
+AA = np.asarray
+
+def test_helper(root_node, expected, clean_up=True, backward_pass = False, input_node = None):
+    with get_new_context() as ctx:
+        ctx.clean_up = clean_up
+        assert not ctx.input_nodes
+        result = ctx.eval(root_node, None, backward_pass, input_node)
+
+        assert len(result) == len(expected)
+        for res, exp in zip(result, expected):  
+            assert np.allclose(res, exp)
+            assert res.shape == AA(exp).shape
