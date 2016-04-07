6bfff490ca28814a5551eb25d9ff8f594576b18a
William Darling <wdarling@microsoft.com>
2016-04-07 16:15:09 +0200

    implementation of clipbyvalue

--- Source/ComputationNetworkLib/NonlinearityNodes.h
+++ Source/ComputationNetworkLib/NonlinearityNodes.h
@@ -435,6 +435,7 @@ class ClipByValueNode : public ComputationNode<ElemType>, public NumInputs<3>
 {
     typedef ComputationNode<ElemType> Base;
     UsingComputationNodeMembersBoilerplate;
+
     static const std::wstring TypeName()
     {
         return L"ClipByValue";
