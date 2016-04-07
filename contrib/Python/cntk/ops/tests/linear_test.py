1d769b203bb93afdce0e1bd82ac463e14e0f5f37
jeanfad <jeanfad@microsoft.com>
2016-04-07 17:25:09 +0200

    new structure, add test

--- contrib/Python/cntk/ops/tests/linear_test.py
+++ contrib/Python/cntk/ops/tests/linear_test.py
@@ -5,6 +5,48 @@
 # ==============================================================================
 
 """
-Unit tests for linear algebra operations, each operation has one forward pass 
-and one backward pass test
+Unit tests for linear algebra operations, each operation is tested for 
+the forward and the backward pass
 """
+
+import numpy as np
+import pytest
+from .ops_test_utils import test_helper, C, AA, I
+from ...graph import *
+from ...reader import *
+import numpy as np
+
+# Testing inputs
+@pytest.mark.parametrize("left_operand, right_operand", [
+    ([30], [10]),
+    ([[30]], [[10]]),
+    ([[1.5,2.1]], [[10,20]]),
+    #TODO: enable once all branches are merged to master
+    #([5], [[30,40], [1,2]]),
+    #Adding two 3x2 inputs of sequence length 1
+    #([[30,40], [1,2], [0.1, 0.2]], [[10,20], [3,4], [-0.5, -0.4]]),     
+    ])
+def test_op_add(left_operand, right_operand):    
+
+    #Forward pass test
+    
+    #we compute the expected output for the forward pass
+    # we need two surrounding brackets
+    # the first for sequence of 1 element, since we have has_sequence_dimension=False
+    # the second for batch of one sample
+    expected = [[AA(left_operand) + AA(right_operand)]]
+
+    a = I([left_operand], has_sequence_dimension=False)
+    b = I([right_operand], has_sequence_dimension=False)    
+    
+    left_as_input = a + right_operand    
+    test_helper(left_as_input, expected, False)
+    
+    right_as_input = left_operand + b
+    test_helper(right_as_input, expected, False)
+    
+    #Backward pass test
+    
+    #the expected results for the backward pass is all ones
+    expected = [[[np.ones_like(x) for x in left_operand]]]
+    test_helper(left_as_input, expected, clean_up=True, backward_pass = True, input_node = a)
