11d1aabd3706cd6f5c1472b9d800e8331eb8f4d3
jeanfad <jeanfad@microsoft.com>
2016-04-08 10:52:11 +0200

    command line deviceid fixture

--- contrib/Python/cntk/conftest.py
+++ contrib/Python/cntk/conftest.py
@@ -1,3 +1,14 @@
 import sys
 
 collect_ignore = ["setup.py"]
+
+# content of conftest.py
+
+def pytest_addoption(parser):
+    parser.addoption("--deviceid", action="append", default=[],
+        help="list of device ids to pass to test functions")
+
+def pytest_generate_tests(metafunc):    
+    if 'device_id' in metafunc.fixturenames:        
+        metafunc.parametrize("device_id",
+                             metafunc.config.option.deviceid)
