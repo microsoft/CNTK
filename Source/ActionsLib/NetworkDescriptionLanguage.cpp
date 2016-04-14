46b96b551addd7f75082143607795627fde89334
William Darling <wdarling@microsoft.com>
2016-04-14 11:51:34 +0200

    implementing ClipByValue(x, minValue, maxValue) in CN lib, NDL, and BS

--- Source/ActionsLib/NetworkDescriptionLanguage.cpp
+++ Source/ActionsLib/NetworkDescriptionLanguage.cpp
@@ -158,6 +158,7 @@ bool CheckFunction(std::string& p_nodeType, bool* allowUndeterminedVariable)
     else if (EqualInsensitive(nodeType, OperationNameOf(CRFNode), L"CRF")) ret = true;
 #endif
     else if (EqualInsensitive(nodeType, OperationNameOf(ClassBasedCrossEntropyWithSoftmaxNode), L"CBCEWithSM")) ret = true;
+    else if (EqualInsensitive(nodeType, OperationNameOf(ClipByValueNode))) ret = true;
     else if (EqualInsensitive(nodeType, OperationNameOf(ConvolutionNode), L"Convolve")) ret = true;
     else if (EqualInsensitive(nodeType, OperationNameOf(PoolingNode))) ret = true;
     else if (EqualInsensitive(nodeType, OperationNameOf(CosDistanceNode), L"CosDist")) ret = true;
