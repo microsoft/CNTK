c012367fe63e97c61d1f8c766ab7eb1db9774cd0
jeanfad <jeanfad@microsoft.com>
2016-04-15 13:56:33 +0200

    add todos

--- contrib/Python/cntk/graph.py
+++ contrib/Python/cntk/graph.py
@@ -1,3 +1,11 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root 
+# for full license information.
+# ==============================================================================
+
+#TODO: Formalize the naming convention and the transformation rules from C++ to Python
+
 from abc import ABCMeta, abstractmethod
 import numpy as np
 
