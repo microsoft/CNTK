added additional tests, fixed corner case for gradient

contrib/Python/cntk/ops/tests/non_linear_test.py
@@ -20,10 +20,19 @@ import numpy as np
 CLIP_TUPLES = [
     ([1.5], [1.0], [2.0]), # value shouldn't be clipped; gradient is [1.0]
     ([0.5], [1.0], [2.0]), # value should be clipped to 1.0; gradient is [0.0]
-    ([2.5], [1.0], [2.0]), # value should be clipped to 2.0
-    ([[1.5, 2.1, 0.9]], [1.0], [2.0]), # should clip to [1.5, 2.0, 1.0]
-    # should clip to [[1.0, 2.0], [1.0, 2.0], [1.5, 2.0]]    
+    ([2.5], [1.0], [2.0]), # value should be clipped to 2.0; gradient is [0.0]
+    
+    # should clip to [1.5, 2.0, 1.0]; gradient is [[1.0, 0.0, 0.0]]
+    ([[1.5, 2.1, 0.9]], [1.0], [2.0]),
+
+    # should clip to [[1.0, 2.0], [1.0, 2.0], [1.5, 2.0]];
+    # gradient is [[0.0, 0.0], [1.0, 1.0], [1.0, 0.0]]
     ([[0.0, 3.0], [1.0, 2.0], [1.5, 2.5]], [1.0], [2.0]),
+     
+    # test what happens if a user puts a higher "min" value than their "max" value
+    # should clip to [[5.0, 5.0, 5.0, 5.0, 5.0]] because min is evaluated first
+    # gradient should be all zeros: [[0.0, 0.0, 0.0, 0.0, 0.0]]
+    ([[1.5, 2.1, 0.9, 1.0, 2.0]], [5.0], [0.5]),
     ]
 
 # -- clip_by_value operation tests --
@@ -50,6 +59,6 @@ def test_op_clip_by_value(x, min_value, max_value, device_id, precision):
     # has not been clipped, and 0 if it has been clipped
     # We only test for the case where the input_node is a -- backpropping into 
     # the others doesn't make sense (they are constants)
-    expected = [[np.array(np.logical_not(np.logical_xor(np.greater(x, max_value), np.less(x, min_value))), dtype=float)]]
+    expected = [[np.array(np.logical_not(np.logical_or(np.greater(x, max_value), np.less(x, min_value))), dtype=float)]]
     unittest_helper(result, None, expected, device_id=device_id, 
                     precision=precision, clean_up=False, backward_pass=True, input_node=a)