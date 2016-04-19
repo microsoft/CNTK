48ce445f97c02484e54cdf65c7f941f9db5bfb34
William Darling <wdarling@microsoft.com>
2016-04-19 14:25:10 +0200

    renaming gradient op to be more generic

--- Source/ComputationNetworkLib/NonlinearityNodes.h
+++ Source/ComputationNetworkLib/NonlinearityNodes.h
@@ -464,7 +464,7 @@ public:
 
         // there is only a gradient for the input tensor that is to be clipped
         if (inputIndex == 0)
-            inputGradient.AddElementwiseProductWithClipDerivativeOf(gradient, input, output);
+            inputGradient.AddCopyGradientOnEqualInputsOf(gradient, input, output);
     }
 
     virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
