fe47cde83911604bc2022401c35bd94344dcafb3
thhoens <thhoens@microsoft.com>
2016-04-20 16:06:16 -0700

    Added code to verify MaskColumnsValue is unnecessary for CPU and GPU sparse matrix.

--- Source/Math/CPUSparseMatrix.cpp
+++ Source/Math/CPUSparseMatrix.cpp
@@ -267,6 +267,39 @@ void CPUSparseMatrix<ElemType>::SetValue(const CPUSparseMatrix<ElemType>& v)
 }
 
 template <class ElemType>
+void CPUSparseMatrix<ElemType>::MaskColumnsValue(const CPUMatrix<char>& columnsMask, ElemType val)
+{
+    VerifyWritable(__func__);
+
+    size_t n = GetNumCols();
+    if (n != columnsMask.GetNumCols())
+        RuntimeError("Matrix and column mask must have equal number of columns");
+
+    if (val != 0)
+        RuntimeError("MaskColumnsValue is not implmented for a non-zero mask for sparse matrices.");
+
+
+    // Get the binary columns mask
+    char* maskedCols = columnsMask.Data();
+
+    if (GetFormat() == MatrixFormat::matrixFormatSparseCSC)
+    {
+        // If we're CSC, we only need to verify that the columns to be zeroed are empty.
+        GPUSPARSE_INDEX_TYPE* colVector = SecondaryIndexLocation();
+
+#pragma omp parallel for
+        for (long j = 0; j < n; j++)
+            if (maskedCols[j] == 0 && colVector[j + 1] != colVector[j])
+                RuntimeError("GPUSparseMatrix attempted to mask column %d, but it has %d elements in it.", j, (colVector[j + 1] - colVector[j]));
+    }
+    else
+    {
+        NOT_IMPLEMENTED;
+    }
+}
+
+
+template <class ElemType>
 void CPUSparseMatrix<ElemType>::Print(const char* matrixName) const
 {
     Print(matrixName, 0, 0, 0, 0);
