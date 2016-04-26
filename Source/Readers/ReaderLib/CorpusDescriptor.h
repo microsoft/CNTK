13ebe6419e1d46e6fa13e3c04e9fce232b6f7dfd
Eldar Akchurin <eldak@microsoft.com>
2016-04-26 16:04:00 +0200

    First implementation of the corpus descriptor

--- Source/Readers/ReaderLib/CorpusDescriptor.h
+++ Source/Readers/ReaderLib/CorpusDescriptor.h
@@ -6,29 +6,57 @@
 #pragma once
 
 #include "StringToIdMap.h"
+#include <set>
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
 // Represents a full corpus.
 // Defines which sequences should participate in the reading.
-// TODO: Currently it is only a skeleton class.
-// TODO: For HtkMlf it can be based on the set of sequences from the SCP file.
 // TODO: Extract an interface.
 class CorpusDescriptor
 {
+    bool m_includeAll;
+    set<size_t> m_sequenceIds;
+
 public:
-    CorpusDescriptor()
+    CorpusDescriptor(const std::wstring& file) : m_includeAll(false)
+    {
+        // Add all sequence ids.
+        for (msra::files::textreader r(file); r;)
+        {
+            m_sequenceIds.insert(m_stringRegistry[r.wgetline()]);
+        }
+    }
+
+    // By default include all sequences.
+    CorpusDescriptor() : m_includeAll(true)
     {}
 
     // Checks if the specified sequence should be used for reading.
     bool IsIncluded(const std::wstring& sequenceKey)
     {
-        UNUSED(sequenceKey);
-        return true;
+        if (m_includeAll)
+        {
+            return true;
+        }
+
+        size_t id;
+        if(!m_stringRegistry.TryGet(sequenceKey, id))
+        {
+            return false;
+        }
+
+        return m_sequenceIds.find(id) != m_sequenceIds.end();
     }
 
-    // Gets string registry
+    // Gets the string registry
     WStringToIdMap& GetStringRegistry()
+    {
+        return m_stringRegistry;
+    }
+
+    // Gets the string registry
+    const WStringToIdMap& GetStringRegistry() const
     {
         return m_stringRegistry;
     }
