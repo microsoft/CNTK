abf2fe1fe17d5be3d0f55f02d88c8e41789b75fa
Aayush Garg <aayushg@microsoft.com>
2016-04-27 21:50:12 -0700

    Review changes

--- contrib/Python/cntk/ops/tests/evaluation_test.py
+++ contrib/Python/cntk/ops/tests/evaluation_test.py
@@ -17,10 +17,12 @@ from ...reader import *
 from ..evaluation import crossentropy_with_softmax
 
 TARGET_OUT_PAIRS = [
-    ([[0.0, 0.0, 0.5, 0.5]], [[1., 2., 3., 4.]]),
-    ([[0.0, 0.4, 0.3, 0.3]], [[2., 1., 1., 4.]])
+    ([[0., 0., 0., 1]], [[1., 2., 3., 4.]]),
+    ([[0., 0., 0.5, 0.5]], [[1., 2., 3., 4.]]),
+    ([[0., 0.4, 0.3, 0.3]], [[2., 1., 1., 4.]])
     ]
 
+# -- CrossEntropy with softmax operation tests --
 @pytest.mark.parametrize("target_values, feature_values", TARGET_OUT_PAIRS)
 def test_op_crossentropywithsoftmax(target_values, feature_values, device_id, precision):
     
@@ -34,27 +36,29 @@ def test_op_crossentropywithsoftmax(target_values, feature_values, device_id, pr
     def numpy_op(label, softmax):
         return -np.sum(label * np.log(softmax, dtype=PRECISION_TO_TYPE[precision]), dtype=PRECISION_TO_TYPE[precision])
     
-    
     input_target = I([target_values], has_dynamic_axis=True)
     input_features = I([feature_values], has_dynamic_axis=True)
     
     op_node = crossentropy_with_softmax(input_target, input_features)
 
+    #Forward pass test
+    #==================
+    # we compute the expected output for the forward pass
     expected = [[[numpy_op(AA(target_values, dtype=PRECISION_TO_TYPE[precision]), numpy_softmax(feature_values))]]]
-    
     unittest_helper(op_node, None, expected,
                 device_id=device_id,
                 precision=precision,
-                clean_up=False, backward_pass=False)
+                clean_up=True, backward_pass=False)
                 
                 
     def numpy_grad(softmax, target):
         s = np.sum(target, dtype=np.double) #This should be 1.0
         return np.subtract(softmax * s , target)
-        
+
+    # Backward pass test
+    # ==================
+    # The expected results for the backward pass is fi*sum(ti)-ti
     expected = [numpy_grad(numpy_softmax(feature_values), AA(target_values, dtype=PRECISION_TO_TYPE[precision]))]
-
-
     unittest_helper(op_node, None, expected,
             device_id=device_id,
             precision=precision, clean_up=True, backward_pass=True,
