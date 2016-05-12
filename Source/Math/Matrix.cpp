ddd341befe4088fed0a18df0521f8651cb0768d2
yuxiao.guo <yuxiao.guo@outlook.com>
2016-05-12 17:22:10 +0800

    Revert "fix cpu build problem"
    This reverts commit 3a3be9f52d6238f6e00002fe1645a6dac8120f3d.

--- Source/Math/Matrix.cpp
+++ Source/Math/Matrix.cpp
@@ -145,15 +145,6 @@
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
-BufferManager* BufferManager::m_instance = new BufferManager();
-
-template <>
-unordered_map<DEVICEID_TYPE, unordered_map<size_t, vector<float*>>>& BufferManager::BufferContainor<float>() { return m_bufferFloatContainor; }
-template <>
-unordered_map<DEVICEID_TYPE, unordered_map<size_t, vector<double*>>>& BufferManager::BufferContainor<double>() { return m_bufferDoubleContainor; }
-template <>
-unordered_map<DEVICEID_TYPE, unordered_map<size_t, vector<char*>>>& BufferManager::BufferContainor<char>() { return m_bufferCharContainor; }
-
 MatrixBase::~MatrixBase() { }
 
 #pragma region Constructors, destructors and other static matrix builders
