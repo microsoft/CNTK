880a261dd160f6610f456b3cf6b945c3b127bb4a
thhoens <thhoens@microsoft.com>
2016-05-27 17:12:37 -0700

    Initial commit of CNTKBinaryReader

---
+++ Source/Readers/CNTKBinaryReader/stdafx.cpp
@@ -0,0 +1,13 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// stdafx.cpp : source file that includes just the standard includes
+// ParseNumber.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+//
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
