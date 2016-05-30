f1af69bc481cba4c4a0797e70505598ccb7736e1
Qiwei Ye <qiwye@microsoft.com>
2016-05-30 10:43:48 +0800

    removing dumpoutput

--- Source/Common/Include/MultiversoWrapper.h
+++ Source/Common/Include/MultiversoWrapper.h
@@ -398,22 +398,12 @@ namespace Microsoft { namespace MSR { namespace CNTK {
             
             if (m_traceLevel > 3)
             {
-              bool debug_flag =false;
               for (int widx = 0; widx < m_tableCount; widx++)
               {
                 if (m_isSparseArray[widx])
                 {
                   ElemType * px = m_deltaArray + m_tableOffsets[widx];
                   int countnum = std::count(px, px + m_tableLength[i], 0.0f);
-                  if (!debug_flag) {
-                    debug_flag = true;
-                    for (auto i = 0; i < 49232; i++) {
-                      for (auto j = 0; j < 128; j++){
-                        fprintf(stderr, "%f\t", *(px + i*128 +j));
-                      }
-                      fprintf(stderr, "\n");
-                    }
-                  }
                   fprintf(stderr, "\t\t(model averaging) zero number = %d\n", (int)countnum);
                   fflush(stderr);
                 }
