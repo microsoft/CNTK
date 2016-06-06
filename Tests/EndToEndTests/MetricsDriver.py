82f6cfacf385a7471a93ed53149d05b3019348a7
Ivan Rodriguez <t-ivrodr@microsoft.com>
2016-06-06 15:07:11 +0200

    Initial commit for script, which extracts metrics from baselines and writes them to a csv file

--- Tests/EndToEndTests/MetricsDriver.py
+++ Tests/EndToEndTests/MetricsDriver.py
@@ -4,7 +4,7 @@
 # ---------------------------------------------------------
 # This is a test driver for running end-to-end CNTK tests
 
-import sys, os, traceback, subprocess, random, re, time, stat
+import sys, os, csv, traceback, re
 
 try:
   import six
@@ -26,9 +26,42 @@ def cygpath(path, relative=False):
 
     return path
 
-# This class encapsulates an instance of the example
+class Baseline:
+  def __init__(self, fullPath, opSystem, device, flavor, testResult = "", trainResult = ""):
+    self.fullPath = fullPath
+    self.opSystem = opSystem
+    self.device = device
+    self.flavor = flavor
+    self.testResult = testResult
+    self.trainResult = trainResult
+
+  def getOsDeviceFlavor(self):
+    os_device_flavor = self.opSystem
+    if self.device:
+      os_device_flavor = os_device_flavor + " - " + self.device
+    if self.flavor:
+      os_device_flavor = os_device_flavor + " - " + self.flavor
+    return os_device_flavor
+
+  def getResultsInfo(self, baselineContent):
+    trainResults = re.findall('.*(Finished Epoch\[[ ]*\d+ of \d+\]\: \[Training\]) (.*)', baselineContent, re.MULTILINE)        
+    if trainResults:                                       
+      self.trainResult = Baseline.getLastTrainResult(trainResults[-1])          
+    testResults = re.findall('.*(Final Results: Minibatch\[1-\d+\]:(\s+\* \d+|))\s+(.*)', baselineContent, re.MULTILINE)
+    if testResults:
+      self.testResult = Baseline.getLastTestResult(testResults[-1])
+
+  @staticmethod
+  def getLastTestResult(line):
+    return line[0] + line[1] + "\n" + line[2].replace('; ', '\n').replace('    ','\n')
+
+  @staticmethod
+  def getLastTrainResult(line):  
+    epochsInfo, parameters = line[0], line[1]  
+    return epochsInfo + '\n' + parameters.replace('; ', '\n')
+
 class Example:
-  # "Suite/TestName" => instance of Test
+
   allExamplesIndexedByFullName = {} 
 
   def __init__(self, suite, name, testDir):
@@ -36,10 +69,12 @@ class Example:
     self.name = name
     self.fullName = suite + "/" + name
     self.testDir = testDir
-    self.testResult = ""
-    self.trainResult = ""
+    self.baselineList = []
+    
+    self.gitHash = ""
+    self.hardwareInfo = ""  
+    self.gpuInfo = ""
 
-  # Populates Tests.allTestsIndexedByFullName by scanning directory tree
   @staticmethod
   def discoverAllExamples():
     testsDir = thisDir
@@ -50,8 +85,7 @@ class Example:
         suiteDir = os.path.dirname(dirName)
         # suite name will be derived from the path components
         suiteName = os.path.relpath(suiteDir, testsDir).replace('\\', '/')        
-
-        #if suiteName.startswith("Examples"):
+        
         example = Example(suiteName,  exampleName, testDir)
         Example.allExamplesIndexedByFullName[example.fullName.lower()] = example
 
@@ -76,74 +110,79 @@ class Example:
         for flavor in flavors:          
           candidateName = "baseline" + o + flavor + device + ".txt"
           fullPath = cygpath(os.path.join(self.testDir, candidateName), relative=True)          
-          if os.path.isfile(fullPath):            
-            baselineFilesList.append(fullPath)
+          if os.path.isfile(fullPath):
+            baseline = Baseline(fullPath, o[1:], device[1:], flavor[1:]);            
+            baselineFilesList.append(baseline)
 
     return baselineFilesList
 
-def getLastTestResult(line):
-  return line[0] + line[1] + "\n" + line[2].replace('; ', '\n').replace('    ','\n')
+  def getHardwareInfo(self, baselineContent):
+    hardwareInfo = re.search(".*Hardware info:\s+"
+					"(CPU Model Mame:\s*.*)\s+"
+					"(CPU cores:\s*.*)\s+"
+					"(Hardware threads: \d+)\s+"
+					"Total (Memory:\s*.*)\s+"
+					"(GPU Model Name: .*)?\s+"
+					"(GPU Memory: .*)?", baselineContent)
+    if hardwareInfo is None:
+      return
+    self.hardwareInfo = "\n".join(hardwareInfo.groups()[0:5])
+    hwInfo = hardwareInfo.groups()[4:len(hardwareInfo.groups())]
 
-def getLastTrainResult(line):
-  separator = "\n[Training]\n"
-  epochsInfo, parameters = line[0], line[1]  
-  return epochsInfo + separator + parameters.replace('; ', '\n')
+    gpuInfoIndex = baselineContent.find("GPU info: ")
+    gpuInfo = re.findall("\t\t(Device ID: \d+)\s+"
+    "(Compute Capability: \d\.\d)\s+"
+    "(CUDA cores: \d+)", baselineContent[gpuInfoIndex:gpuInfoIndex+1500]) #Taking just a portion of the file, in order to avoid wasting time searching it all
+    if not gpuInfo:
+      return
+    for index in range(0, len(gpuInfo)):
+      hwInfo = hwInfo + gpuInfo[index]
+    self.gpuInfo = "\n".join(hwInfo)  
 
-def runCommand():
+
+def getExamplesMetrics():
   Example.allExamplesIndexedByFullName = list(sorted(Example.allExamplesIndexedByFullName.values(), key=lambda test: test.fullName))
   allExamples = Example.allExamplesIndexedByFullName
 
-  print ("CNTK - Metrics collector")
-  six.print_("Getting examples:  " + " ".join([y.fullName for y in allExamples]))
+  print ("CNTK - Metrics collector")  
 
-  for example in allExamples:    
-    baselineListForExample = example.findBaselineFilesList()  
+  baselineList = []
+
+  for example in allExamples:
+    baselineListForExample = example.findBaselineFilesList()
+    six.print_("example: " + example.fullName)
     for baseline in baselineListForExample:      
-      with open(baseline, "r") as f:
-        baselineContent = f.read()        
-        trainResults = re.findall('.*(Finished Epoch\[[ ]*\d+ of \d+\]\:) \[Training\] (.*)', baselineContent, re.MULTILINE)
-        testResults = re.findall('.*(Final Results: Minibatch\[1-\d+\]:(\s+\* \d+|))\s+(.*)', baselineContent, re.MULTILINE)        
-        if trainResults:
-          six.print_("==============================================================================")
-          six.print_("Suite Name " + example.suite)
-          six.print_("Example " + example.name)
-          six.print_("Baseline: " + baseline + "\n")                    
-          six.print_(getLastTrainResult(trainResults[-1]))
-          six.print_("")
-          if testResults:
-            six.print_(getLastTestResult(testResults[-1]))
-          gitHash = re.search('.*Build SHA1:\s([a-z0-9]{40})\s', baselineContent)
-          if gitHash is not None:
-            six.print_("\nBuild Hash: ")
-            six.print_(gitHash.group(1))
-          hardwareInfo = re.search(".*Hardware info:\s+"
-					"CPU Model Mame:\s*(.*)\s+"
-					"CPU cores:\s*(.*)\s+"
-					"Hardware threads: (\d+)\s+"
-					"Total Memory:\s*(.*)\s+"
-					"GPU Model Name: (.*)?\s+"
-					"GPU Memory: (.*)?", baselineContent)
-          if hardwareInfo is not None:
-            six.print_("Hardware information information: ")
-            six.print_("CPU model " + hardwareInfo.groups(1))
-            six.print_("CPU cores " + hardwareInfo.groups(2))
-            six.print_("Hardware threads: " + hardwareInfo.groups(3))          
-            six.print_("Total memory: " + hardwareInfo.groups(4))
-            six.print_("GPU name: " + hardwareInfo.groups(5))
-            six.print_("GPU name: " + hardwareInfo.groups(6))
-          gpuInfo = re.search(".*GPU info:\s+Device ID: (\d+)\s+"
-          "Compute Capability: (\d\.\d)\s+"
-          "CUDA cores: (\d+)", baselineContent)
-          if gpuInfo is not None:
-            six.print_("Additional GPU information: ")
-            six.print_("CPU model " + hardwareInfo.groups(1))
-            six.print_("CPU cores " + hardwareInfo.groups(2))
-            six.print_("Hardware threads: " + hardwareInfo.groups(3)) 
-
-six.print_("==============================================================================")
+      with open(baseline.fullPath, "r") as f:
+        baselineContent = f.read()
+        gitHash = re.search('.*Build SHA1:\s([a-z0-9]{40})\s', baselineContent)
+        if gitHash is None:
+          continue
+        example.gitHash = gitHash.group(1) 
+        example.getHardwareInfo(baselineContent)                 
+        baseline.getResultsInfo(baselineContent)                 
+      example.baselineList.append(baseline)
         
+def writeMetricsToFile():
+  metricsFile = open("metrics.csv",'wb')
+  csvWriter = csv.writer(metricsFile, dialect='excel', quoting=csv.QUOTE_ALL)
+  tableHeader = ['Example', 'Git Hash', 'Hardware', 'GPU', 'Log file', 'OS - Device - Flavor', 'Train Result', 'Test Result']
+  csvWriter.writerow(tableHeader)
+
+  for example in Example.allExamplesIndexedByFullName: 
+    if not example.baselineList:
+      continue
+    firstBaseline = example.baselineList[0]    
+      
+    csvWriter.writerow([example.fullName, example.gitHash, example.hardwareInfo, example.gpuInfo, firstBaseline.fullPath.split(thisDir)[1][1:], firstBaseline.getOsDeviceFlavor(), firstBaseline.trainResult, firstBaseline.testResult])  
+    for baseline in example.baselineList[1:]:
+      csvWriter.writerow(['', '', '', '', baseline.fullPath.split(thisDir)[1][1:], baseline.getOsDeviceFlavor(), baseline.trainResult, baseline.testResult])
+
 # ======================= Entry point =======================
-# discover all the tests
+six.print_("==============================================================================")
+
 Example.discoverAllExamples()
 
-runCommand()
+getExamplesMetrics()
+
+writeMetricsToFile()
+
