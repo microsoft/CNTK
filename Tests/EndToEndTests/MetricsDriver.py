9a34405685be47b7a68fbcd5158754e972de362d
Ivan Rodriguez <t-ivrodr@microsoft.com>
2016-06-09 16:58:56 +0200

    Fix script according to comments. Change format of wiki entry to asciidoc.

--- Tests/EndToEndTests/MetricsDriver.py
+++ Tests/EndToEndTests/MetricsDriver.py
@@ -2,7 +2,9 @@
 # ----------------------------------------------------------
 # Copyright (c) Microsoft Corporation.  All rights reserved.
 # ---------------------------------------------------------
-# This is a test driver for running end-to-end CNTK tests
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+# This script extracts information (hardware used, final results) contained in the baselines files
+# and generates a markdown file (wiki page)
 
 import sys, os, csv, traceback, re
 
@@ -32,11 +34,10 @@ class Baseline:
     self.opSystem = opSystem
     self.device = device
     self.flavor = flavor
+    self.cpuInfo = ""
+    self.gpuInfo = ""
     self.testResult = testResult
     self.trainResult = trainResult
-
-  def getOsDeviceFlavor(self):
-    return "-".join([self.opSystem, self.device, self.flavor])
 
   def getResultsInfo(self, baselineContent):
     trainResults = re.findall('.*(Finished Epoch\[[ ]*\d+ of \d+\]\: \[Training\]) (.*)', baselineContent, re.MULTILINE)        
@@ -45,6 +46,29 @@ class Baseline:
     testResults = re.findall('.*(Final Results: Minibatch\[1-\d+\]:(\s+\* \d+|))\s+(.*)', baselineContent, re.MULTILINE)
     if testResults:
       self.testResult = Baseline.getLastTestResult(testResults[-1])[0:-2]
+
+  def getHardwareInfo(self, baselineContent):
+    cpuInfo = re.search(".*Hardware info:\s+"
+                        "CPU Model (Name:\s*.*)\s+"
+                        "CPU (Cores:\s*.*)\s+"
+                        "(Hardware threads: \d+)\s+"
+                        "Total (Memory:\s*.*)\s+"
+                        "GPU Model (Name: .*)?\s+"
+                        "GPU (Memory: .*)?", baselineContent)
+    if cpuInfo is None:
+      return
+    self.cpuInfo = "\n".join(cpuInfo.groups()[0:4])
+    hwInfo = cpuInfo.groups()[4:len(cpuInfo.groups())]
+
+    gpuInfoIndex = baselineContent.find("GPU info: ")
+    gpuInfo = re.findall("\t\t(Device ID: \d+)\s+"
+                         "(Compute Capability: \d\.\d)\s+"
+                         "(CUDA cores: \d+)", baselineContent[gpuInfoIndex:gpuInfoIndex+1500]) #Taking just a portion of the file, in order to avoid wasting time searching it all
+    if not gpuInfo:
+      return
+    for index in range(0, len(gpuInfo)):
+      hwInfo = hwInfo + gpuInfo[index]
+    self.gpuInfo = "\n".join(hwInfo)
 
   @staticmethod
   def getLastTestResult(line):
@@ -67,8 +91,6 @@ class Example:
     self.baselineList = []
     
     self.gitHash = ""
-    self.cpuInfo = ""  
-    self.gpuInfo = ""
 
   @staticmethod
   def discoverAllExamples():
@@ -111,30 +133,6 @@ class Example:
 
     return baselineFilesList
 
-  def getCpuInfo(self, baselineContent):
-    cpuInfo = re.search(".*Hardware info:\s+"
-					"CPU Model (Name:\s*.*)\s+"
-					"CPU (Cores:\s*.*)\s+"
-					"(Hardware threads: \d+)\s+"
-					"Total (Memory:\s*.*)\s+"
-					"GPU Model (Name: .*)?\s+"
-					"GPU (Memory: .*)?", baselineContent)
-    if cpuInfo is None:
-      return
-    self.cpuInfo = "\n".join(cpuInfo.groups()[0:4])
-    hwInfo = cpuInfo.groups()[4:len(cpuInfo.groups())]
-
-    gpuInfoIndex = baselineContent.find("GPU info: ")
-    gpuInfo = re.findall("\t\t(Device ID: \d+)\s+"
-    "(Compute Capability: \d\.\d)\s+"
-    "(CUDA cores: \d+)", baselineContent[gpuInfoIndex:gpuInfoIndex+1500]) #Taking just a portion of the file, in order to avoid wasting time searching it all
-    if not gpuInfo:
-      return
-    for index in range(0, len(gpuInfo)):
-      hwInfo = hwInfo + gpuInfo[index]
-    self.gpuInfo = "\n".join(hwInfo)  
-
-
 def getExamplesMetrics():  
   Example.allExamplesIndexedByFullName = list(sorted(Example.allExamplesIndexedByFullName.values(), key=lambda test: test.fullName))  
 
@@ -142,7 +140,7 @@ def getExamplesMetrics():
 
   print ("CNTK - Metrics collector")  
 
-  for example in allExamples:    
+  for example in allExamples:
     baselineListForExample = example.findBaselineFilesList() 
     six.print_("Example: " + example.fullName)   
     for baseline in baselineListForExample:            
@@ -152,54 +150,37 @@ def getExamplesMetrics():
         if gitHash is None:
           continue
         example.gitHash = gitHash.group(1) 
-        example.getCpuInfo(baselineContent)                 
+        baseline.getHardwareInfo(baselineContent)
         baseline.getResultsInfo(baselineContent)                 
       example.baselineList.append(baseline)    
         
-def writeMetricsToCsvFile():
-  metricsFile = open("metrics.csv",'wb')
-  csvWriter = csv.writer(metricsFile, dialect='excel', quoting=csv.QUOTE_ALL)
-  tableHeader = ['Example', 'Git Hash', 'Hardware', 'GPU', 'Log file', 'OS - Device - Flavor', 'Train Result', 'Test Result']
-  csvWriter.writerow(tableHeader)
-
-  for example in Example.allExamplesIndexedByFullName: 
-    firstBaseline = example.baselineList[0]    
-      
-    csvWriter.writerow([example.fullName, example.gitHash, example.cpuInfo, example.gpuInfo, firstBaseline.fullPath.split(thisDir)[1][1:], firstBaseline.getOsDeviceFlavor(), firstBaseline.trainResult, firstBaseline.testResult])  
-    for baseline in example.baselineList[1:]:
-      csvWriter.writerow(['', '', '', '', baseline.fullPath.split(thisDir)[1][1:], baseline.getOsDeviceFlavor(), baseline.trainResult, baseline.testResult])
-
-def createMarkdownExampleList(file):
+def createAsciidocExampleList(file):
   for example in Example.allExamplesIndexedByFullName:
     if not example.baselineList:
       continue
-    file.write("".join(["* [", example.fullName, "](#", example.fullName, ")\n"]))
+    file.write("".join(["<<", example.fullName.replace("/","").lower(),",", example.fullName, ">> +\n"]))
   file.write("\n")
 
-def writeMetricsToMarkdown():
-  metricsFile = open("metrics.md",'wb')
+def writeMetricsToAsciidoc():
+  metricsFile = open("metrics.adoc",'wb')
 
-  createMarkdownExampleList(metricsFile)
-  
+  createAsciidocExampleList(metricsFile)
+
   for example in Example.allExamplesIndexedByFullName:
     if not example.baselineList:
       continue
-
-    lineBreak = "  \n"
-    metricsFile.write("".join(["**Example:** ", "<a name=\"", example.fullName, "\"></a>", example.fullName, lineBreak]))
-    metricsFile.write("".join(["**Git Hash:** ", example.gitHash, lineBreak]))    
-    metricsFile.write("".join(["**CPU:** ", example.cpuInfo.replace('\n', '. ')  , lineBreak]))
-    metricsFile.write("".join(["**GPU:** ", example.gpuInfo.replace('\n', '. ')  , lineBreak]))
-
-    metricsFile.write('\n|Log file |OS - Device - Flavor | Train Result | Test Result|\n')
-    metricsFile.write('|---|---|---|---|\n')
+    metricsFile.write("".join(["===== ", example.fullName, "\n"]))
+    metricsFile.write("".join(["**Git Hash: **", example.gitHash, "\n\n"]))
+    metricsFile.write("[cols=3, options=\"header\"]\n")
+    metricsFile.write("|====\n")
+    metricsFile.write("|Log file / Configuration | Train Result | Test Result\n")
     for baseline in example.baselineList:
-      pipeChar = "|"
-      
-      metricsFile.write("".join(['|[', baseline.fullPath.split("/")[-1], "](../blob/master/Tests/EndToEndTests/", 
-                        baseline.fullPath.split(thisDir)[1][1:], ")|", baseline.getOsDeviceFlavor(), pipeChar, baseline.trainResult.replace("\n", " "), pipeChar,  baseline.testResult.replace("\n", " "), pipeChar, "\n"]))
+      metricsFile.write("".join(["|link:../blob/master/Tests/EndToEndTests/", baseline.fullPath.split(thisDir)[1][1:], "[",
+                                 baseline.fullPath.split("/")[-1], "] .2+|", baseline.trainResult.replace("\n", " "), " .2+|",
+                                 baseline.testResult.replace("\n", " "), "|\n"]))
+      metricsFile.write("".join(["CPU: ", re.sub("[\r]?\n", ' ', baseline.cpuInfo), "GPU: ", re.sub("[\r]?\n", ' ', baseline.gpuInfo)]))
 
-  metricsFile.write("\n")
+    metricsFile.write("\n|====\n\n")
 
 # ======================= Entry point =======================
 six.print_("==============================================================================")
@@ -208,7 +189,4 @@ Example.discoverAllExamples()
 
 getExamplesMetrics()
 
-#writeMetricsToCsvFile()
-
-writeMetricsToMarkdown()
-
+writeMetricsToAsciidoc()
