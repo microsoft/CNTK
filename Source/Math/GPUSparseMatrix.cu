179cc80ae17b111e251cd7491b3444a831aeea8a
jaurora <zhjie@umich.edu>
2016-06-13 14:11:42 -0700

    use setValue to update sparse matrix c

--- Source/Math/GPUSparseMatrix.cu
+++ Source/Math/GPUSparseMatrix.cu
@@ -2055,7 +2055,7 @@ template <class ElemType>
 void GPUSparseMatrix<ElemType>::ElementProductOf(const GPUSparseMatrix<ElemType>& a, const GPUSparseMatrix<ElemType>& b, GPUSparseMatrix<ElemType>& c)
 {
     GPUSparseMatrix<ElemType> as = ElementProductOf(a, b);
-    c = as;
+    c.SetValue(as);
 }
 
 template <class ElemType>
