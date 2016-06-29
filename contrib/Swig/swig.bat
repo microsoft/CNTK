2604048516e95da458b7444933b7b019ef473cc4
Willi Richert <wilrich@microsoft.com>
2016-06-29 13:27:09 +0200

    Prepare the API to work with Swig bindings

---
+++ contrib/Swig/swig.bat
@@ -0,0 +1,5 @@
+set PYTHON_INCLUDE=c:\Anaconda3\include
+set PYTHON_LIB=c:\anaconda3\libs\python34.lib
+
+C:\blis\PyCNTK\swigwin-3.0.10\swig -c++ -python -D_MSC_VER -IC:\blis\CNTK\Source\CNTKv2LibraryDll\API\ swig_cntk.i 
+
