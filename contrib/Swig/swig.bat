c8aa633958c6b18b22ea3f6e8ea34be0d1931524
Willi Richert <wilrich@microsoft.com>
2016-07-01 21:11:32 +0200

    Fixed swig.bat

--- contrib/Swig/swig.bat
+++ contrib/Swig/swig.bat
@@ -1,9 +1,9 @@
-set PYTHON_INCLUDE=c:\Anaconda3\include
+REM set PYTHON_INCLUDE=c:\Anaconda3\include
 set PYTHON_LIB=c:\anaconda3\libs\python34.lib
 
-; Please change this
-;set SWIG=c:\PyCNTK\swigwin-3.0.10\swig
-set SWIG=f:\swigwin-3.0.10\swig
+REM Please change this
+set SWIG=c:\blis\PyCNTK\swigwin-3.0.10\swig
+REM set SWIG=f:\swigwin-3.0.10\swig
 
 %SWIG% -c++ -python -D_MSC_VER -I..\..\Source\CNTKv2LibraryDll\API\ swig_cntk.i 
 
