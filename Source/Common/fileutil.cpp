a6b3adfa836cf616296cc61e39adad26c10c3157
Vadim Mazalov <vadimma@microsoft.com>
2016-07-12 13:46:24 -0700

    (1) Move basetypes.h to Common/Include; (2) use attemt function to read file

--- Source/Common/fileutil.cpp
+++ Source/Common/fileutil.cpp
@@ -16,6 +16,7 @@
 #endif
 #define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
 #include "Basics.h"
+#include "basetypes.h" //for attemp()
 #include "fileutil.h"
 #include "ProgressTracing.h"
 
@@ -287,44 +288,15 @@ void fsetmode(FILE* f, char type)
 
 void freadOrDie(void* ptr, size_t size, size_t count, FILE* f)
 {
-    int numberOfTries = 3;
-    bool readSuccess = false;
-    void* originalPtr = ptr;
-    size_t originalCount = count;
-    long startPosition = ftell(f);
-    if (startPosition == -1L) // failure to get position in the stream
-        startPosition = 0;
-
-    while (numberOfTries > 0 && !readSuccess)
+    // \\XXX\C$ reads are limited, with some randomness (e.g. 48 MB), on Windows 7 32 bit, so we break this into chunks of some MB. Meh.
+    while (count > 0)
     {
-        readSuccess = true;
-        // \\XXX\C$ reads are limited, with some randomness (e.g. 48 MB), on Windows 7 32 bit, so we break this into chunks of some MB. Meh.
-        while (count > 0)
-        {
-            size_t chunkn = min(count, (size_t)15 * 1024 * 1024); // BUGBUG: I surely meant this limit to be bytes, not units of 'size'...
-            size_t n = fread(ptr, size, chunkn, f);
-            if (n != chunkn)
-            {
-                // This attempt failed.
-                // Reset local variables and try again
-                numberOfTries--;
-                ptr = originalPtr;
-                count = originalCount;
-                readSuccess = false;
-                if (fseek(f, startPosition, SEEK_SET) != 0) // failure to set position in the stream
-                    numberOfTries = 0;
-
-                fprintf(stderr, "error reading from file: %s; %d attempt(s) left", strerror(errno), numberOfTries);
-                break;
-            }
-            count -= n;
-            ptr = n * size + (char*)ptr;
-        }
-    }
-
-    if (!readSuccess)
-    {
-        RuntimeError("error reading from file: %s", strerror(errno));
+        size_t chunkn = min(count, (size_t) 15 * 1024 * 1024); // BUGBUG: I surely meant this limit to be bytes, not units of 'size'...
+        size_t n = fread(ptr, size, chunkn, f);
+        if (n != chunkn)
+            RuntimeError("error reading from file: %s", strerror(errno));
+        count -= n;
+        ptr = n * size + (char*) ptr;
     }
 }
 
@@ -1661,6 +1633,11 @@ static size_t fgetfilechars(const std::wstring& path, vector<char>& buffer)
     return len;
 }
 
+static void fgetfilechars(const std::wstring& path, vector<char>& buffer, size_t& len)
+{
+    len = fgetfilechars(path, buffer);
+}
+
 template <class LINES>
 static void strtoklines(char* s, LINES& lines)
 {
@@ -1682,9 +1659,13 @@ void msra::files::fgetfilelines(const std::wstring& path, vector<char>& buffer, 
 // same as above but returning const char* (avoiding the memory allocation)
 vector<char*> msra::files::fgetfilelines(const wstring& path, vector<char>& buffer)
 {
-    // load it into RAM in one huge chunk
-    const size_t len = fgetfilechars(path, buffer);
-
+    size_t len = 0;
+    msra::util::attempt(5, [&]() // (can be reading from network)
+    {
+        // load it into RAM in one huge chunk
+        fgetfilechars(path, buffer, len);
+    });
+    
     // parse into lines
     vector<char*> lines;
     lines.reserve(len / 20);
