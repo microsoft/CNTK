74fffe337baab40d601a105941019569b4bce59c
Ivan Rodriguez <t-ivrodr@microsoft.com>
2016-08-01 14:41:12 +0200

    Use input and output files for each test

--- Tests/UnitTests/BrainScriptTests/ParserTests.cpp
+++ Tests/UnitTests/BrainScriptTests/ParserTests.cpp
@@ -8,7 +8,7 @@
 #include "stdafx.h"
 #include "Basics.h"
 #include "BrainScriptParser.h"
-#include "ParserTestsData.h"
+#include "boost/filesystem.hpp"
 
 #include <utility>
 
@@ -20,29 +20,70 @@ using namespace Microsoft::MSR::CNTK;
 #define let const auto
 #endif
 
+struct ParserTestsFixture
+{
+public:
+    ParserTestsFixture(){
+        boost::filesystem::path path(boost::unit_test::framework::master_test_suite().argv[0]);
+        wstring parentPath = boost::filesystem::canonical(path.parent_path()).generic_wstring();
+
+        m_testDataPath = parentPath + L"/../../../Tests/UnitTests/BrainScriptTests";
+        boost::filesystem::path absTestPath(m_testDataPath);
+        absTestPath = boost::filesystem::canonical(absTestPath);
+        m_testDataPath = absTestPath.generic_wstring();
+    }
+
+    const wstring getDataPath(){
+        return m_testDataPath;
+    }
+private:
+    wstring m_testDataPath;
+};
+
+
 namespace Microsoft { namespace MSR { namespace CNTK { namespace Test {
 
 
-BOOST_AUTO_TEST_SUITE(ParserTests)
+BOOST_FIXTURE_TEST_SUITE(ParserTests, ParserTestsFixture)
 
-void parseLine(const std::pair<wstring, wstring> & testLine)
+void parseLine(const wstring & input, const wstring & expectedOutput)
 {        
-    let expr = BS::ParseConfigDictFromString(testLine.first, L"Test", vector<wstring>());
+    let expr = BS::ParseConfigDictFromString(input, L"Test", vector<wstring>());
 
     wstringstream actualStream;
     expr->DumpToStream(actualStream);
 
-    BOOST_TEST(actualStream.str() == testLine.second, boost::test_tools::per_element());
-
+    BOOST_TEST(actualStream.str() == expectedOutput, boost::test_tools::per_element());
 }
 
 BOOST_AUTO_TEST_CASE(ParseExpressionsAndCompareTree)
 {
-    int testCount = 0;
-    for (auto & testPair : parserTestVector)
+    wstring inputPrefix(L"Input");
+    wstring outputPrefix(L"ExpectedOutput");
+
+    wstring dataPath = getDataPath() + L"/Data/Parser/";
+
+    bool filesAvailable = true;
+    int testCount = 1;
+    while (filesAvailable)
     {
-        fprintf(stderr, "Test %d:\n", testCount);
-        parseLine(testPair);
+        wifstream inputFile(dataPath + inputPrefix + to_wstring(testCount) + L".txt");
+        wifstream outputFile(dataPath +outputPrefix + to_wstring(testCount) + L".txt");
+
+        if (!inputFile.is_open()){
+            filesAvailable = false;
+            continue;
+        }
+
+        fprintf(stderr, "Test %d...\n", testCount);
+
+        auto inputStream = std::wostringstream{};
+        inputStream << inputFile.rdbuf();
+
+        auto expectedStream = std::wostringstream{};
+        expectedStream << outputFile.rdbuf();
+
+        parseLine(inputStream.str().c_str(), expectedStream.str().c_str());
         testCount++;
     }
 }
