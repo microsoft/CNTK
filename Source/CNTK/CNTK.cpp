8db871e97a94c170aa78acf6d5a5c27138724eed
Wolfgang Manousek <wolfma61@github.com>
2016-08-02 17:08:12 +0200

    addressed final code review comments

--- Source/CNTK/CNTK.cpp
+++ Source/CNTK/CNTK.cpp
@@ -834,9 +834,13 @@ int HandleDebugAssert(int,               // reportType  - ignoring reportType, p
                       char *message,     // message     - fully assembled debug user message
                       int * returnValue) // returnValue - retVal value of zero continues execution
 {
-    *returnValue = 0;
     fprintf(stderr, "C-Runtime: %s\n", message);
-    throw std::runtime_error(message);
+
+    if (returnValue) {
+        *returnValue = 0;   // return value of 0 will continue operation and NOT start the debugger
+    }
+
+    return TRUE;            // returning TRUE will make sure no message box is displayed
 }
 #endif
 
