3d80725a16cc9bf51ef9912be161c54de68186d1
Zhou Wang <zhouwang@microsoft.com>
2016-08-19 11:16:33 +0200

    Define THREAD_LOCAL and force currentDevice to be THREAD_LOCAL

--- Source/Common/Include/Platform.h
+++ Source/Common/Include/Platform.h
@@ -11,6 +11,12 @@
 #define __UNIX__
 #endif
 
+#ifdef _MSC_VER
+#define THREAD_LOCAL __declspec(thread)
+#else
+#define THREAD_LOCAL thread_local
+#endif
+
 // ===========================================================================
 // compiler differences
 // ===========================================================================
