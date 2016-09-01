29efa41a773106ea623d00706b0ae8b0759f4eba
Mark Hillebrand <mahilleb@microsoft.com>
2016-09-01 17:17:04 +0200

    bindings/python/setup.py: package CNTK DLLs as well

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,5 +1,6 @@
 import setuptools
 from distutils.core import setup, Extension
+import distutils.sysconfig
 import os
 import numpy
 
@@ -12,31 +13,33 @@ print("Using CNTK libs at '%s'"%os.path.abspath(CNTK_LIB_PATH))
 
 print( os.path.join(CNTK_SOURCE_PATH, "CNTKv2LibraryDll", "API"))
 
+libs=[
+   os.path.join(CNTK_LIB_PATH, "CNTKLibrary-2.0"),
+   os.path.join(CNTK_LIB_PATH, "BinaryReader"),
+   os.path.join(CNTK_LIB_PATH, "CNTKTextFormatReader"),
+   os.path.join(CNTK_LIB_PATH, "CompositeDataReader"),
+   os.path.join(CNTK_LIB_PATH, "DSSMReader"),
+   os.path.join(CNTK_LIB_PATH, "EvalDll"),
+   os.path.join(CNTK_LIB_PATH, "EvalWrapper"),
+   os.path.join(CNTK_LIB_PATH, "HTKDeserializers"),
+   os.path.join(CNTK_LIB_PATH, "HTKMLFReader"),
+   os.path.join(CNTK_LIB_PATH, "ImageReader"),
+   os.path.join(CNTK_LIB_PATH, "LibSVMBinaryReader"),
+   os.path.join(CNTK_LIB_PATH, "LMSequenceReader"),
+   os.path.join(CNTK_LIB_PATH, "LUSequenceReader"),
+   os.path.join(CNTK_LIB_PATH, "nvml"),
+   os.path.join(CNTK_LIB_PATH, "SparsePCReader"),
+   os.path.join(CNTK_LIB_PATH, "UCIFastReader"),
+   os.path.join(CNTK_LIB_PATH, "Math")
+   ]
+
 ext_modules = [
     Extension(
            name="_cntk_py",
 
            sources=[os.path.join("cntk", "swig", "cntk_py_wrap.cxx")],
 
-           libraries=[
-               os.path.join(CNTK_LIB_PATH, "CNTKLibrary-2.0"),
-               os.path.join(CNTK_LIB_PATH, "BinaryReader"),               
-               os.path.join(CNTK_LIB_PATH, "CNTKTextFormatReader"),               
-               os.path.join(CNTK_LIB_PATH, "CompositeDataReader"),               
-               os.path.join(CNTK_LIB_PATH, "DSSMReader"),               
-               os.path.join(CNTK_LIB_PATH, "EvalDll"),               
-               os.path.join(CNTK_LIB_PATH, "EvalWrapper"),               
-               os.path.join(CNTK_LIB_PATH, "HTKDeserializers"),               
-               os.path.join(CNTK_LIB_PATH, "HTKMLFReader"),               
-               os.path.join(CNTK_LIB_PATH, "ImageReader"),               
-               os.path.join(CNTK_LIB_PATH, "LibSVMBinaryReader"),               
-               os.path.join(CNTK_LIB_PATH, "LMSequenceReader"),               
-               os.path.join(CNTK_LIB_PATH, "LUSequenceReader"),               
-               os.path.join(CNTK_LIB_PATH, "nvml"),               
-               os.path.join(CNTK_LIB_PATH, "SparsePCReader"),               
-               os.path.join(CNTK_LIB_PATH, "UCIFastReader"),                              
-               os.path.join(CNTK_LIB_PATH, "Math")
-               ],
+           libraries=libs,
            library_dirs=[CNTK_LIB_PATH],
 
            include_dirs=[
@@ -49,14 +52,14 @@ ext_modules = [
            language="c++",
 
            extra_compile_args=[
-               "-DUNICODE", 
-               "/EHsc", 
+               "-DUNICODE",
+               "/EHsc",
                "/DEBUG", # TODO remove for release
                "/Zi",     # TODO remove for release
                "/Od"     # TODO remove for release
                ],
-           extra_link_args=[ "/DEBUG"],           
+           extra_link_args=[ "/DEBUG"],
       )
     ]
 
-setup(name="cntk", ext_modules = ext_modules, packages=setuptools.find_packages())
+setup(name="cntk", ext_modules = ext_modules,  data_files = [(distutils.sysconfig.get_python_lib(), [ lib + ".dll" for lib in libs ])], packages=setuptools.find_packages())
