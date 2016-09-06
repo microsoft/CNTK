c37240a8a798c1f21f2bc331816dac007ae0a920
yuxiao.guo <yuxiao.guo@outlook.com>
2016-09-06 14:31:43 +0800

    delete two more lines

--- Source/ComputationNetworkLib/ComputationNetworkEvaluation.cpp
+++ Source/ComputationNetworkLib/ComputationNetworkEvaluation.cpp
@@ -1,5 +1,3 @@
-#include "ComputationNetwork.h"
-#include "ComputationNetwork.h"
 //
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
