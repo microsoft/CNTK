a32222351faa6ede8999aa5603a9822f58ede28b
Willi Richert <wilrich@microsoft.com>
2016-09-09 15:42:47 +0200

    Merged

--- bindings/python/cntk/swig/swig.bat
+++ bindings/python/cntk/swig/swig.bat
@@ -1,12 +1,12 @@
 REM set PYTHON_INCLUDE=c:\Anaconda3\include
 
 REM Please change this
-REM set PYTHON_LIB=c:\anaconda3\libs\python34.lib
-set PYTHON_LIB=E:\WinPython-64bit-3.4.3.7\python-3.4.3.amd64\libs
+set PYTHON_LIB=c:\anaconda3\libs\python34.lib
+rem set PYTHON_LIB=E:\WinPython-64bit-3.4.3.7\python-3.4.3.amd64\libs
 
 REM Please change this
-REM set SWIG=c:\blis\PyCNTK\swigwin-3.0.10\swig
-set SWIG=E:\swigwin-3.0.10\swig
+set SWIG=c:\blis\swigwin-3.0.10\swig
+rem set SWIG=E:\swigwin-3.0.10\swig
 
 %SWIG% -c++ -python -D_MSC_VER -I..\..\..\..\Source\CNTKv2LibraryDll\API\ cntk_py.i
 
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -7,7 +7,7 @@ import numpy
 
 CNTK_PATH = os.path.join(os.path.dirname(__file__), "..", "..")
 CNTK_SOURCE_PATH = os.path.join(CNTK_PATH, "Source")
-CNTK_LIB_PATH = os.path.join(CNTK_PATH, "x64", "Release")
+CNTK_LIB_PATH = os.path.join(CNTK_PATH, "x64", "Release_CpuOnly")
 print("Using CNTK libs at '%s'"%os.path.abspath(CNTK_LIB_PATH))
 #CNTK_LIB_PATH = os.path.join(CNTK_PATH, "x64", "Debug_CpuOnly")
 
