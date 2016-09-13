756a35a006efc0fbd4fe8d275f53b498536b10b9
Willi Richert <wilrich@microsoft.com>
2016-09-13 12:15:22 +0200

    Support for clip

--- bindings/python/cntk/ops/tests/non_diff_test.py
+++ bindings/python/cntk/ops/tests/non_diff_test.py
@@ -13,49 +13,9 @@ import numpy as np
 import pytest
 from .ops_test_utils import unittest_helper, _test_unary_op, _test_binary_op, AA, I, precision, PRECISION_TO_TYPE 
 
-<<<<<<< HEAD
-TENSORS = [
-        ([12.3, -12.3]), 
-        ([10.2, -10.2]), 
-        ([0.5, -0.5]), 
-        ([0.01, -0.01]), 
-        ([0.499, -0.499]), 
-        ([5.0, -5.0]), 
-        ([0.0]),
-        ([[2.1, 9.9], [4.7, 5.3]])
-]
-@pytest.mark.parametrize("operand", TENSORS)
-def test_op_floor(operand, device_id, precision):
-    operand = AA(operand)
-    expected = np.floor(operand)
-
-    expected_forward = [[expected]]
-    expected_backward = {
-            'arg': [[np.zeros_like(expected)]],
-            }
-
-    from .. import floor
-    _test_unary_op(precision, device_id, floor, operand,
-        expected_forward, expected_backward)
-
-@pytest.mark.parametrize("operand", TENSORS)
-def test_op_ceil(operand, device_id, precision):
-    operand = AA(operand)
-    expected = np.ceil(operand)
-
-    expected_forward = [[expected]]
-    expected_backward = {
-            'arg': [[np.zeros_like(expected)]],
-            }
-
-    from .. import ceil
-    _test_unary_op(precision, device_id, ceil, operand,
-        expected_forward, expected_backward)
-=======
 EPS_IN_LOG = 1e-37        # 1e-37 is the highest guaranteed precision
 BACKWARD_RESULST_FOR_LOG_EPS = 9.08782e+36 # the backward result returned by CNTK log() for epsilon
 LOG_OF_EPS_IN_LOG =  -85.1 # log(EPS_IN_LOG)
->>>>>>> 712c192... Add forgotten test file
 
 TENSORS = [
         ([12.3, -12.3]), 
