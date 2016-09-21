e8f4c3450c51798407974e7c25f30c7941074096
Eldar Akchurin <eldak@microsoft.com>
2016-09-21 17:26:05 +0200

    Removing parallization from packer

--- Source/Readers/ReaderLib/SequencePacker.cpp
+++ Source/Readers/ReaderLib/SequencePacker.cpp
@@ -124,7 +124,6 @@ MBLayoutPtr SequencePacker::PackDenseStream(const StreamBatch& batch, size_t str
 
     // Iterate over sequences in the layout, copy samples from the
     // source sequences into the buffer (at appropriate offsets).
-#pragma omp parallel for schedule(static)
     for (int i = 0; i < sequenceInfos.size(); ++i)
     {
         const auto& sequenceInfo = sequenceInfos[i];
