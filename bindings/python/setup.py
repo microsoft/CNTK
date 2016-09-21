4a0fcf527651dfe59e41f6c9d7d96a4d08c90dbe
Willi Richert <wilrich@microsoft.com>
2016-09-21 12:21:39 +0100

    Storing .so files relatively

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,7 +1,6 @@
-import setuptools
-from distutils.core import setup, Extension
-import distutils.sysconfig
+from setuptools import setup, Extension, find_packages
 import os
+import shutil
 import platform
 import numpy
 
@@ -9,6 +8,7 @@ IS_WINDOWS = platform.system() == 'Windows'
 
 CNTK_PATH = os.path.join(os.path.dirname(__file__), "..", "..")
 CNTK_SOURCE_PATH = os.path.join(CNTK_PATH, "Source")
+PROJ_LIB_PATH = os.path.join(os.path.dirname(__file__), "cntk", "libs")
 
 if 'CNTK_LIB_PATH' in os.environ:
     CNTK_LIB_PATH = os.environ['CNTK_LIB_PATH']
@@ -20,47 +20,61 @@ print("Using CNTK libs at '%s'"%os.path.abspath(CNTK_LIB_PATH))
 
 #Todo: trim down the list of libs
 
+def lib_path(fn):
+    return os.path.normpath(os.path.join(CNTK_LIB_PATH, fn))
+    #return os.path.join(CNTK_LIB_PATH, fn)
+
+def proj_lib_path(fn):
+    return os.path.normpath(os.path.join(PROJ_LIB_PATH, fn))
+
+dep_libs_names = ["CNTKTextFormatReader", "CompositeDataReader", "HTKDeserializers", "HTKMLFReader", "ImageReader", "LibSVMBinaryReader", "LMSequenceReader", "LUSequenceReader", "SparsePCReader",
+"UCIFastReader"]
+
 if IS_WINDOWS:
     libs=[
-       os.path.join(CNTK_LIB_PATH, "CNTKLibrary-2.0"),
-       os.path.join(CNTK_LIB_PATH, "Math"),
-       os.path.join(CNTK_LIB_PATH, "BinaryReader"),
-       os.path.join(CNTK_LIB_PATH, "DSSMReader"),
-       os.path.join(CNTK_LIB_PATH, "EvalDll"),
-       os.path.join(CNTK_LIB_PATH, "EvalWrapper"),
-       os.path.join(CNTK_LIB_PATH, "nvml"),
-       os.path.join(CNTK_LIB_PATH, "mkl_cntk_p"),
-       os.path.join(CNTK_LIB_PATH, "libiomp5md"),
-       os.path.join(CNTK_LIB_PATH, "cusparse64_75"),
-       os.path.join(CNTK_LIB_PATH, "curand64_75"),
-       os.path.join(CNTK_LIB_PATH, "cudnn64_5"),
-       os.path.join(CNTK_LIB_PATH, "cudart64_75"),
-       os.path.join(CNTK_LIB_PATH, "cublas64_75"),
-       os.path.join(CNTK_LIB_PATH, "opencv_world310"),
+       "CNTKLibrary-2.0",
+       "Math",
+       "BinaryReader",
+       "DSSMReader",
+       "EvalDll",
+       "EvalWrapper",
+       "nvml",
+       "mkl_cntk_p",
+       "libiomp5md",
+       "cusparse64_75",
+       "curand64_75",
+       "cudnn64_5",
+       "cudart64_75",
+       "cublas64_75",
+       "opencv_world310",
     ]
+    fn_ext = '.dll'
 else:
     libs=[
-       os.path.join(CNTK_LIB_PATH, "libcntklibrary-2.0"),
-       os.path.join(CNTK_LIB_PATH, "libcntkmath"),
-       os.path.join(CNTK_LIB_PATH, "libeval"),
+       "cntklibrary-2.0",
+       "cntkmath"
     ]
+    fn_ext = '.so'
 
-libs += [
-   os.path.join(CNTK_LIB_PATH, "CNTKTextFormatReader"),
-   os.path.join(CNTK_LIB_PATH, "CompositeDataReader"),
-   os.path.join(CNTK_LIB_PATH, "HTKDeserializers"),
-   os.path.join(CNTK_LIB_PATH, "HTKMLFReader"),
-   os.path.join(CNTK_LIB_PATH, "ImageReader"),
-   os.path.join(CNTK_LIB_PATH, "LibSVMBinaryReader"),
-   os.path.join(CNTK_LIB_PATH, "LMSequenceReader"),
-   os.path.join(CNTK_LIB_PATH, "LUSequenceReader"),
-   os.path.join(CNTK_LIB_PATH, "SparsePCReader"),
-   os.path.join(CNTK_LIB_PATH, "UCIFastReader"),
-   ]
+dep_libs_names = [l+fn_ext for l in dep_libs_names + ['lib'+l for l in libs]]
 
-extra_compile_args=[
+
+# copy over the libraries to the cntk base directory so that the rpath is correctly set
+if os.path.exists(PROJ_LIB_PATH):
+    shutil.rmtree(PROJ_LIB_PATH)
+
+os.mkdir(PROJ_LIB_PATH)
+
+for fn in dep_libs_names:
+    shutil.copy(lib_path(fn), proj_lib_path(fn))
+
+# for package_data we need to have names relative to the cntk module
+dep_libs = [os.path.join('libs', fn) for fn in dep_libs_names]
+
+extra_compile_args = [
 	"-DSWIG",
-	"-DUNICODE"]
+	"-DUNICODE"
+    ]
 	       
 if IS_WINDOWS:
     extra_compile_args += [
@@ -69,19 +83,25 @@ if IS_WINDOWS:
 	"/Zi",
 	"/EHsc",
     ]
+    runtime_library_dirs = []
+    #TODO: do not copy the dlls to the root, try to find a better location that is accessible
 else:
     extra_compile_args += [
 	'--std=c++11',
     ]
+    # expecting the dependent libs (libcntklibrary-2.0.so, etc.) inside site-package/cntk
+    runtime_library_dirs = ['$ORIGIN/cntk/libs']
+    os.environ["CXX"] = "mpic++"
 
-ext_modules = [
-    Extension(
+cntk_module = Extension(
            name="_cntk_py",
 
            sources=[os.path.join("cntk", "swig", "cntk_py_wrap.cxx")],
 
            libraries=libs,
            library_dirs=[CNTK_LIB_PATH],
+
+           runtime_library_dirs=runtime_library_dirs,
 
            include_dirs=[
                os.path.join(CNTK_SOURCE_PATH, "CNTKv2LibraryDll", "API"),
@@ -94,26 +114,18 @@ ext_modules = [
 
            language="c++",
       )
-    ]
 
-#TODO: do not copy the dlls to the root, try to find a better location that is accessible
-if IS_WINDOWS:
-    data_files = [('.\\', [ lib + ".dll" for lib in libs ])] 
-else:
-    data_files = [('./', [ lib + ".so" for lib in libs ])] 
-    os.environ["CC"] = "/usr/local/openmpi-1.10.1/bin/mpic++" 
-    os.environ["CXX"] = "/usr/local/openmpi-1.10.1/bin/mpic++"
-
-packages = [x for x in setuptools.find_packages() if x.startswith('cntk')]
+# do not include tests and examples
+packages = [x for x in find_packages() if x.startswith('cntk')]
 
 setup(name="cntk", 
-      version="2.0.alpha2",
+      version="2.0.a2",
       url="http://cntk.ai",
-      ext_modules = ext_modules,  
-      data_files = data_files,
+      ext_modules = [cntk_module],  
       packages=packages,
+      package_data = { 'cntk': dep_libs },
       install_requires=[
         'numpy>=0.17',
         'scipy>=0.11'
-      ]
+      ],
      )
