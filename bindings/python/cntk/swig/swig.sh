6d31a869d2bedc494f5622bff8f662201a28f094
Willi Richert <wilrich@microsoft.com>
2016-09-23 08:01:15 +0100

    Move cntk_py.py under Linux; ignore cntk/swig in tests

--- bindings/python/cntk/swig/swig.sh
+++ bindings/python/cntk/swig/swig.sh
@@ -4,6 +4,8 @@ SWIG=${SWIG-swig-3.0.10/root/bin/swig}
 
 if [[ -x "$SWIG" ]]; then
   ${SWIG} -c++ -python -I../../../../Source/CNTKv2LibraryDll/API/ cntk_py.i
+  mv cntk_py.py ..
+
 else
   printf "Error: Cannot find executable at '%s'\n" $SWIG
   printf "       Please install swig (>= 3.0.10), and let the \$SWIG environment\n"
