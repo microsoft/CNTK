6dc8f370b064dc432e8ad8b3e1260b83013e3641
Zhou Wang <zhouwang@microsoft.com>
2016-09-26 16:30:31 +0200

    fix thread-safety issue for multi-threaded eval on GPU due to local static variable in VS2013

--- Tests/UnitTests/V2LibraryTests/Main.cpp
+++ Tests/UnitTests/V2LibraryTests/Main.cpp
@@ -35,8 +35,12 @@ int main()
     TrainSequenceToSequenceTranslator();
 
     // Test multi-threads evaluation
-    // Todo: Also test on GPUDevice()
+    fprintf(stderr, "Test multi-threaded evaluation on CPU.\n");
     EvalMultiThreadsWithNewNetwork(DeviceDescriptor::CPUDevice(), 2);
+#ifndef CPUONLY
+    fprintf(stderr, "Test multi-threaded evaluation on GPU\n");
+    EvalMultiThreadsWithNewNetwork(DeviceDescriptor::GPUDevice(0), 2);
+#endif
 
     fprintf(stderr, "\nCNTKv2Library tests: Passed\n");
     fflush(stderr);
