282d4052b8a21007c70735326841f44b3c3b6839
Friedel van Megen <fmegen@microsoft.com>
2016-10-07 16:20:40 +0200

    fix tabs to spaces

--- Source/Common/MPIWrapper.cpp
+++ Source/Common/MPIWrapper.cpp
@@ -29,33 +29,33 @@ template void MPIWrapper::AllReduce(std::vector<float>&) const;
 
 int operator||(int rc, const MpiFail &what)
 {
-	if (rc == MPI_SUCCESS)
-	{
-		return rc;
-	}
+    if (rc == MPI_SUCCESS)
+    {
+        return rc;
+    }
 
-	fprintf(stderr, "%s, MPI error %d\n", what.c_str(), rc);
-	fflush(stderr);
+    fprintf(stderr, "%s, MPI error %d\n", what.c_str(), rc);
+    fflush(stderr);
 
 #if HAS_OPENMPI
-	// (special case: we use that code to indicate a missing msmpi.dll...)
-	if (rc != MPI_ERR_INTERN)
-	{
-		char errbuf[MPI_MAX_ERROR_STRING + 1] = { 0 };
-		int len;
-		MPI_Error_string(rc, &errbuf[0], &len);
-		fprintf(stderr, "%s, MPI error %d: %s\n", what.c_str(), rc, errbuf);
-		fflush(stderr);
+    // (special case: we use that code to indicate a missing msmpi.dll...)
+    if (rc != MPI_ERR_INTERN)
+    {
+        char errbuf[MPI_MAX_ERROR_STRING + 1] = { 0 };
+        int len;
+        MPI_Error_string(rc, &errbuf[0], &len);
+        fprintf(stderr, "%s, MPI error %d: %s\n", what.c_str(), rc, errbuf);
+        fflush(stderr);
 
-		// we abort through this, so that the MPI system gets the memo
-		MPI_Abort(MPI_COMM_WORLD, rc);
+        // we abort through this, so that the MPI system gets the memo
+        MPI_Abort(MPI_COMM_WORLD, rc);
 
-		// TODO: or does that only signal an issue, and we should still terminate ourselves?
-		// BUGBUG: We'd also need to Abort through the other sub-set communicator
-	}
+        // TODO: or does that only signal an issue, and we should still terminate ourselves?
+        // BUGBUG: We'd also need to Abort through the other sub-set communicator
+    }
 #endif
 
-	RuntimeError("%s", what.c_str());
+    RuntimeError("%s", what.c_str());
 }
 
 int MPIWrapper::s_myRank = -1;
@@ -65,34 +65,34 @@ std::shared_ptr<MPIWrapper> MPIWrapper::s_mpi = nullptr;
 int MPIWrapper::MPI_Init_DL()
 {
 #if !HAS_OPENMPI
-	return MPI_SUCCESS;
+    return MPI_SUCCESS;
 #else
 #ifdef WIN32
-	__try
+    __try
 #endif
-	{
-		// don't initialize if that has been done already
-		int flag = 0;
-		MPI_Initialized(&flag);
-		if (flag)
-			return MPI_SUCCESS;
+    {
+        // don't initialize if that has been done already
+        int flag = 0;
+        MPI_Initialized(&flag);
+        if (flag)
+            return MPI_SUCCESS;
 
-		int argc = 0;
-		char **argv = NULL;
-		int requiredThreadLevelSupport = MPI_THREAD_SERIALIZED;
-		int provided;
-		int ret = MPI_Init_thread(&argc, &argv, requiredThreadLevelSupport, &provided);
-		if (provided != requiredThreadLevelSupport)
-			LogicError("Failed to initialize MPI with the desired level of thread support");
+        int argc = 0;
+        char **argv = NULL;
+        int requiredThreadLevelSupport = MPI_THREAD_SERIALIZED;
+        int provided;
+        int ret = MPI_Init_thread(&argc, &argv, requiredThreadLevelSupport, &provided);
+        if (provided != requiredThreadLevelSupport)
+            LogicError("Failed to initialize MPI with the desired level of thread support");
 
-		return ret;
-	}
+        return ret;
+    }
 #ifdef WIN32
-	__except (EXCEPTION_EXECUTE_HANDLER)
-	{
-		fprintf(stderr, "mpihelper: msmpi.dll missing\n");
-		return MPI_ERR_INTERN;
-	}
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+        fprintf(stderr, "mpihelper: msmpi.dll missing\n");
+        return MPI_ERR_INTERN;
+    }
 #endif
 #endif
 }
@@ -106,61 +106,61 @@ int MPIWrapper::MPI_Init_DL()
 // allowing MPI to sequentially handle terminations
 void MPIWrapper::MPIWorkaroundAtExit()
 {
-	Sleep(s_myRank * 50);
+    Sleep(s_myRank * 50);
 }
 
 MPIWrapper::MPIWrapper()
-	: m_currentComm(MPI_COMM_WORLD)
+    : m_currentComm(MPI_COMM_WORLD)
 {
-	static bool initialized = false;
-	if (initialized)
-	{
-		LogicError("MPIWrapper: this is a singleton class that can only be instantiated once per process");
-	}
+    static bool initialized = false;
+    if (initialized)
+    {
+        LogicError("MPIWrapper: this is a singleton class that can only be instantiated once per process");
+    }
 
-	initialized = true;
-	fprintf(stderr, "MPIWrapper: initializing MPI\n");
-	fflush(stderr);
+    initialized = true;
+    fprintf(stderr, "MPIWrapper: initializing MPI\n");
+    fflush(stderr);
 
 #if HAS_OPENMPI
-	MPI_Init_DL() || MpiFail("mpiaggregator: MPI_Init");
-	MPI_Comm_rank(MPI_COMM_WORLD, &m_myRank);
-	MPI_Comm_size(MPI_COMM_WORLD, &m_numMPINodes);
+    MPI_Init_DL() || MpiFail("mpiaggregator: MPI_Init");
+    MPI_Comm_rank(MPI_COMM_WORLD, &m_myRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &m_numMPINodes);
 #else
-	m_myRank = 0;
-	m_numMPINodes = 0;
+    m_myRank = 0;
+    m_numMPINodes = 0;
 #endif
 
-	m_numNodesInUse = m_numMPINodes;
+    m_numNodesInUse = m_numMPINodes;
 
-	// Verify that the environment variable used by GetTotalNumberOfMPINodes()  
-	// matches what the MPI API says. There're actually two possible cases:
-	// 1) when we're running with mpiexec both values have to match;
-	// 2) when we're running without mpiexec, the former will return 0, and
-	// the later will be set to 1.
-	assert((GetTotalNumberOfMPINodes() == 0 && m_numNodesInUse == 1) ||
-		(GetTotalNumberOfMPINodes() == m_numNodesInUse));
+    // Verify that the environment variable used by GetTotalNumberOfMPINodes()  
+    // matches what the MPI API says. There're actually two possible cases:
+    // 1) when we're running with mpiexec both values have to match;
+    // 2) when we're running without mpiexec, the former will return 0, and
+    // the later will be set to 1.
+    assert((GetTotalNumberOfMPINodes() == 0 && m_numNodesInUse == 1) ||
+        (GetTotalNumberOfMPINodes() == m_numNodesInUse));
 
-	// Applying MPI workaround
-	s_myRank = m_myRank;
-	atexit(&MPIWrapper::MPIWorkaroundAtExit);
+    // Applying MPI workaround
+    s_myRank = m_myRank;
+    atexit(&MPIWrapper::MPIWorkaroundAtExit);
 
-	// by default we use all of them
-	RequestNodes("MPIWrapper");
+    // by default we use all of them
+    RequestNodes("MPIWrapper");
 
-	if (m_numMPINodes > 1)
-		fprintf(stderr, "mpihelper: we are cog %d in a gearbox of %d\n", (int)m_myRank, (int)m_numMPINodes);
-	else
-		fprintf(stderr, "mpihelper: only one MPI process: MPI operation will be boring\n");
+    if (m_numMPINodes > 1)
+        fprintf(stderr, "mpihelper: we are cog %d in a gearbox of %d\n", (int)m_myRank, (int)m_numMPINodes);
+    else
+        fprintf(stderr, "mpihelper: only one MPI process: MPI operation will be boring\n");
 
-	fflush(stderr);
+    fflush(stderr);
 
-	// do an initial handshake
-	Ping("mpihelper");
+    // do an initial handshake
+    Ping("mpihelper");
 
-	// stagger the jobs just a little to get a sort-of deterministic order e.g. in GPU allocation when running on one machine
-	// continue 0.5 seconds apart
-	::Sleep((DWORD)(500 * CurrentNodeRank()));
+    // stagger the jobs just a little to get a sort-of deterministic order e.g. in GPU allocation when running on one machine
+    // continue 0.5 seconds apart
+    ::Sleep((DWORD)(500 * CurrentNodeRank()));
 }
 
 // Note that specifically, this function is such that it does not require
@@ -170,21 +170,21 @@ MPIWrapper::MPIWrapper()
 int MPIWrapper::GetTotalNumberOfMPINodes()
 {
 #if !HAS_OPENMPI
-	return 0;
+    return 0;
 #else
 #ifdef WIN32
-	const char* p = std::getenv("PMI_SIZE");
+    const char* p = std::getenv("PMI_SIZE");
 #else
-	const char* p = std::getenv("OMPI_COMM_WORLD_SIZE");
+    const char* p = std::getenv("OMPI_COMM_WORLD_SIZE");
 #endif
-	if (!p)
-	{
-		return 0;
-	}
-	else
-	{
-		return std::stoi(string(p));
-	}
+    if (!p)
+    {
+        return 0;
+    }
+    else
+    {
+        return std::stoi(string(p));
+    }
 #endif
 }
 
@@ -192,24 +192,24 @@ int MPIWrapper::GetTotalNumberOfMPINodes()
 // It's OK since this class is a singleton anyway that gets instantiated exactly once at program startup.
 MPIWrapper::~MPIWrapper()
 {
-	fprintf(stderr, "~MPIWrapper\n");
+    fprintf(stderr, "~MPIWrapper\n");
 
-	// Do not finalize in event of an exception since calling MPI_Finalize without
-	// all pending communications being finished results in a hang
-	int rc = fflush(stderr);
-	if (!std::uncaught_exception())
-	{
-		if (rc != FFLUSH_SUCCESS)
-		{
+    // Do not finalize in event of an exception since calling MPI_Finalize without
+    // all pending communications being finished results in a hang
+    int rc = fflush(stderr);
+    if (!std::uncaught_exception())
+    {
+        if (rc != FFLUSH_SUCCESS)
+        {
 #ifdef _WIN32
-			RuntimeError("MPIWrapper: Failed to flush stderr, %d", ::GetLastError());
+            RuntimeError("MPIWrapper: Failed to flush stderr, %d", ::GetLastError());
 #else
-			RuntimeError("MPIWrapper: Failed to flush stderr, %d", errno);
+            RuntimeError("MPIWrapper: Failed to flush stderr, %d", errno);
 #endif
-		}
+        }
 
-		MPI_Finalize();
-	}
+        MPI_Finalize();
+    }
 }
 
 void MPIWrapper::Ping(const char *msg) const
@@ -217,121 +217,121 @@ void MPIWrapper::Ping(const char *msg) const
 #if HAS_OPENMPI
 #undef USE2NDCOMM
 #ifndef USE2NDCOMM
-	if (NumNodesInUse() != m_numMPINodes)
-	{
-		fprintf(stderr, "ping [%s]: cannot be applied to subset (%d) of nodes, skipping\n", msg, (int)NumNodesInUse());
-		fflush(stderr);
-		return;
-	}
+    if (NumNodesInUse() != m_numMPINodes)
+    {
+        fprintf(stderr, "ping [%s]: cannot be applied to subset (%d) of nodes, skipping\n", msg, (int)NumNodesInUse());
+        fflush(stderr);
+        return;
+    }
 #endif
-	std::array<int, 1> handshake;
-	handshake[0] = 1;
+    std::array<int, 1> handshake;
+    handshake[0] = 1;
 
-	fprintf(stderr, "ping [%s]: %d nodes pinging each other\n", msg, (int)NumNodesInUse());
-	fflush(stderr);
+    fprintf(stderr, "ping [%s]: %d nodes pinging each other\n", msg, (int)NumNodesInUse());
+    fflush(stderr);
 
-	AllReduce(handshake);
-	fprintf(stderr, "ping [%s]: all %d nodes responded\n", msg, handshake[0]);
-	fflush(stderr);
+    AllReduce(handshake);
+    fprintf(stderr, "ping [%s]: all %d nodes responded\n", msg, handshake[0]);
+    fflush(stderr);
 #endif
 }
 
 void MPIWrapper::RequestNodes(const char *msg, size_t requestednodes /*default: all*/)
 {
 #if HAS_OPENMPI
-	Ping("requestnodes (before change)");
+    Ping("requestnodes (before change)");
 
-	// undo current split
+    // undo current split
 #ifdef USE2NDCOMM
-	if (m_currentComm != MPI_COMM_WORLD /*no subset*/ && m_currentComm != MPI_COMM_NULL /*idle nodes*/)
-	{
-		fprintf(stderr, "requestnodes: MPI_Comm_free %x\n", (int)m_currentComm);
-		fflush(stderr);
-		MPI_Comm_free(&m_currentComm) || MpiFail("requestnodes: MPI_Comm_free"); // will leave MPI_COMM_NULL here
-	}
+    if (m_currentComm != MPI_COMM_WORLD /*no subset*/ && m_currentComm != MPI_COMM_NULL /*idle nodes*/)
+    {
+        fprintf(stderr, "requestnodes: MPI_Comm_free %x\n", (int)m_currentComm);
+        fflush(stderr);
+        MPI_Comm_free(&m_currentComm) || MpiFail("requestnodes: MPI_Comm_free"); // will leave MPI_COMM_NULL here
+    }
 #endif
-	// reset to MPI_COMM_WORLD
-	m_currentComm = MPI_COMM_WORLD;
-	// create a new split (unless all nodes were requested)
-	if (requestednodes < (size_t)m_numMPINodes)
-	{
+    // reset to MPI_COMM_WORLD
+    m_currentComm = MPI_COMM_WORLD;
+    // create a new split (unless all nodes were requested)
+    if (requestednodes < (size_t)m_numMPINodes)
+    {
 #ifdef USE2NDCOMM
-		fprintf(stderr, "requestnodes: MPI_Comm_split %d\n", (node() < requestednodes) ? 1 : MPI_UNDEFINED);
-		fflush(stderr);
-		MPI_Comm_split(communicator(), (node() < requestednodes) ? 1 : MPI_UNDEFINED, 0, &m_currentComm) || MpiFail("requestnodes: MPI_Comm_split");
-		fprintf(stderr, "requestnodes: MPI_Comm_split -> %x\n", (int)m_currentComm);
-		fflush(stderr);
+        fprintf(stderr, "requestnodes: MPI_Comm_split %d\n", (node() < requestednodes) ? 1 : MPI_UNDEFINED);
+        fflush(stderr);
+        MPI_Comm_split(communicator(), (node() < requestednodes) ? 1 : MPI_UNDEFINED, 0, &m_currentComm) || MpiFail("requestnodes: MPI_Comm_split");
+        fprintf(stderr, "requestnodes: MPI_Comm_split -> %x\n", (int)m_currentComm);
+        fflush(stderr);
 #endif
-	}
-	else
-	{
-		// leave m_currentComm as MPI_COMM_WORLD
-		// and clip to #nodes
-		requestednodes = m_numMPINodes;
-	}
+    }
+    else
+    {
+        // leave m_currentComm as MPI_COMM_WORLD
+        // and clip to #nodes
+        requestednodes = m_numMPINodes;
+    }
 
-	m_numNodesInUse = requestednodes;
-	fprintf(stderr, "requestnodes [%s]: using %d out of %d MPI nodes (%d requested); we (%d) are %s\n",
-		msg, (int)m_numNodesInUse, (int)m_numMPINodes, (int)requestednodes,
-		(int)CurrentNodeRank(), IsIdle() ? "out (idle)" : "in (participating)");
-	fflush(stderr);
-	Ping("requestnodes (after change)");
+    m_numNodesInUse = requestednodes;
+    fprintf(stderr, "requestnodes [%s]: using %d out of %d MPI nodes (%d requested); we (%d) are %s\n",
+        msg, (int)m_numNodesInUse, (int)m_numMPINodes, (int)requestednodes,
+        (int)CurrentNodeRank(), IsIdle() ? "out (idle)" : "in (participating)");
+    fflush(stderr);
+    Ping("requestnodes (after change)");
 #endif
 }
 
 MPIWrapperPtr MPIWrapper::GetInstance(bool create)
 {
-	static bool initialized = false;
-	if (create)
-	{
-		if (initialized)
-			LogicError("Creating MPIWrapper instance after a GetInstance call has been already made!");
-		else
-			s_mpi = std::make_shared<MPIWrapper>();
-	}
+    static bool initialized = false;
+    if (create)
+    {
+        if (initialized)
+            LogicError("Creating MPIWrapper instance after a GetInstance call has been already made!");
+        else
+            s_mpi = std::make_shared<MPIWrapper>();
+    }
 
-	initialized = true;
-	return s_mpi;
+    initialized = true;
+    return s_mpi;
 }
 
 void MPIWrapper::DeleteInstance()
 {
-	s_mpi = nullptr;
+    s_mpi = nullptr;
 }
 
 MPI_Comm MPIWrapper::Communicator() const
 {
-	return m_currentComm;
+    return m_currentComm;
 }
 
 size_t MPIWrapper::NumNodesInUse() const
 {
-	return m_numNodesInUse;
+    return m_numNodesInUse;
 }
 
 size_t MPIWrapper::CurrentNodeRank() const
 {
-	return m_myRank;
+    return m_myRank;
 }
 
 bool MPIWrapper::IsMainNode() const
 {
-	return m_myRank == 0;
+    return m_myRank == 0;
 } // we are the chosen one--do extra stuff like saving the model to disk
 
 bool MPIWrapper::IsIdle() const
 {
-	return CurrentNodeRank() >= NumNodesInUse();
+    return CurrentNodeRank() >= NumNodesInUse();
 } // user had requested to not use this many nodes
 
 bool MPIWrapper::UsingAllNodes() const
 {
-	return NumNodesInUse() == m_numMPINodes;
+    return NumNodesInUse() == m_numMPINodes;
 } // all nodes participate (used to check whether we can use MPI_Allreduce directly)
 
 size_t MPIWrapper::MainNodeRank() const
 {
-	return 0;
+    return 0;
 }
 
 // -----------------------------------------------------------------------
@@ -341,27 +341,27 @@ size_t MPIWrapper::MainNodeRank() const
 // helpers to determine the MPI_Datatype of a pointer
 MPI_Datatype MPIWrapper::GetDataType(char *)
 {
-	return MPI_CHAR;
+    return MPI_CHAR;
 }
 
 MPI_Datatype MPIWrapper::GetDataType(int *)
 {
-	return MPI_INT;
+    return MPI_INT;
 }
 
 MPI_Datatype MPIWrapper::GetDataType(float *)
 {
-	return MPI_FLOAT;
+    return MPI_FLOAT;
 }
 
 MPI_Datatype MPIWrapper::GetDataType(double *)
 {
-	return MPI_DOUBLE;
+    return MPI_DOUBLE;
 }
 
 MPI_Datatype MPIWrapper::GetDataType(size_t *)
 {
-	return sizeof(size_t) == 4 ? MPI_UNSIGNED : MPI_LONG_LONG_INT;
+    return sizeof(size_t) == 4 ? MPI_UNSIGNED : MPI_LONG_LONG_INT;
 }
 
 // allreduce of a vector
@@ -369,14 +369,14 @@ template <typename VECTORLIKEOBJECT>
 void MPIWrapper::AllReduce(VECTORLIKEOBJECT &accumulator) const
 {
 #if HAS_OPENMPI
-	auto *dataptr = accumulator.data();
-	size_t totalnumelements = accumulator.size();
+    auto *dataptr = accumulator.data();
+    size_t totalnumelements = accumulator.size();
 
-	// use MPI to compute the sum over all elements in (dataptr, totalnumelements) and redistribute to all nodes
-	if ((NumNodesInUse() > 1) && (Communicator() != MPI_COMM_NULL))
-	{
-		MPI_Allreduce(MPI_IN_PLACE, dataptr, (int)totalnumelements, GetDataType(dataptr), MPI_SUM, Communicator()) || MpiFail("allreduce: MPI_Allreduce");
-	}
+    // use MPI to compute the sum over all elements in (dataptr, totalnumelements) and redistribute to all nodes
+    if ((NumNodesInUse() > 1) && (Communicator() != MPI_COMM_NULL))
+    {
+        MPI_Allreduce(MPI_IN_PLACE, dataptr, (int)totalnumelements, GetDataType(dataptr), MPI_SUM, Communicator()) || MpiFail("allreduce: MPI_Allreduce");
+    }
 #endif
 }
 
@@ -385,10 +385,10 @@ template <class ElemType>
 void MPIWrapper::AllReduce(ElemType *pData, size_t nData)
 {
 #if HAS_OPENMPI
-	if ((NumNodesInUse() > 1 && (Communicator() != MPI_COMM_NULL)))
-	{
-		MPI_Allreduce(MPI_IN_PLACE, pData, (int)nData, GetDataType(pData), MPI_SUM, Communicator()) || MpiFail("Allreduce: MPI_Allreduce");
-	}
+    if ((NumNodesInUse() > 1 && (Communicator() != MPI_COMM_NULL)))
+    {
+        MPI_Allreduce(MPI_IN_PLACE, pData, (int)nData, GetDataType(pData), MPI_SUM, Communicator()) || MpiFail("Allreduce: MPI_Allreduce");
+    }
 #endif
 }
 
@@ -396,10 +396,10 @@ template <class ElemType>
 void MPIWrapper::Bcast(ElemType *pData, size_t nData, size_t srcRank)
 {
 #if HAS_OPENMPI
-	if ((NumNodesInUse() > 1) && (Communicator() != MPI_COMM_NULL))
-	{
-		MPI_Bcast(pData, (int)nData, GetDataType(pData), (int)srcRank, Communicator()) || MpiFail("Bcast: MPI_Bcast");
-	}
+    if ((NumNodesInUse() > 1) && (Communicator() != MPI_COMM_NULL))
+    {
+        MPI_Bcast(pData, (int)nData, GetDataType(pData), (int)srcRank, Communicator()) || MpiFail("Bcast: MPI_Bcast");
+    }
 #endif
 }
 
@@ -407,7 +407,7 @@ void MPIWrapper::Bcast(ElemType *pData, size_t nData, size_t srcRank)
 void MPIWrapper::WaitAll()
 {
 #if HAS_OPENMPI
-	MPI_Barrier(m_currentComm) || MpiFail("waitall: MPI_Barrier");
+    MPI_Barrier(m_currentComm) || MpiFail("waitall: MPI_Barrier");
 #endif
 }
 
