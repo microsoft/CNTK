e87e5cd17be97d09b66851f95fda1b6c560a02d3
feiga <gf0109@gmail.com>
2016-10-08 16:50:25 +0800

    Update interface of NoMultiversoWrapper

--- Source/Common/Include/NoMultiversoWrapper.h
+++ Source/Common/Include/NoMultiversoWrapper.h
@@ -2,60 +2,54 @@
 
 #include "ASGDCommon.h"
 
-namespace Microsoft {
-	namespace MSR {
-		namespace CNTK {
+namespace Microsoft { namespace MSR { namespace CNTK {
 
-			template<class ElemType = float>
-			class MultiversoHelper
-	 		{
-            public:
-				MultiversoHelper(const std::list<ComputationNodeBasePtr> & learnableNodes,
-					int localWorkerNumber,
-					bool isPipeline = true,
-					AdjustLearningRateatBeginning adjusttype = AdjustLearningRateatBeginning::None,
-					double adjustcoef = 0.2,
-					size_t adjustnbmb = 0)
+template<class ElemType = float>
+class MultiversoHelper
+{
+public:
+    MultiversoHelper(const std::list<ComputationNodeBasePtr> & learnableNodes,
+        int nodeNumRanks,
+        bool useAsyncBuffered = true,
+        bool isSimModelAveragingSGD = false,
+        AdjustLearningRateatBeginning adjusttype = AdjustLearningRateatBeginning::None,
+        double adjustcoef = 0.2,
+        size_t adjustnbmb = 600,
+        int traceLevel = 0,
+        int syncPerfStats = 0,
+        const MPIWrapperPtr& pMPI = nullptr)
 				{
-					
 				}
 
-				~MultiversoHelper()
-				{
-
+        ~MultiversoHelper()
+        {
 				}
 
-				void InitModel(const std::list<ComputationNodeBasePtr> & learnableNode)
-				{
+        void InitModel(const std::list<ComputationNodeBasePtr> & learnableNode)
+        {
+        }
 
-				}
-
-				void PushAndPullModel(const std::list<ComputationNodeBasePtr> & learnableNode)
-				{
-
-				}
+        void PushAndPullModel(const std::list<ComputationNodeBasePtr> & learnableNodes, size_t sampleSinceLastSynced = 0)
+        {
+        }
 			
-				void PushModel(const std::list<ComputationNodeBasePtr> & learnableNode)
-				{
+        void PushModel(const std::list<ComputationNodeBasePtr> & learnableNode)
+        {
+        }
 
-				}
-
-				void PullModel(const std::list<ComputationNodeBasePtr> & learnableNode)
-				{
-
-				}
+        void PullModel(const std::list<ComputationNodeBasePtr> & learnableNode)
+        { 
+        }
 
 				void WaitAll()
-				{
-
-				}
+        { 
+        }
 
 				void WaitAsyncBuffer()
-				{
+        { 
+        }
 
-				}
-
-			};
-		}
-	}
+    };
+}
+}
 }
