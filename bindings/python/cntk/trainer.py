ac3fd83c780c25db16efcdca09b7ae8e87704033
Willi Richert <wilrich@microsoft.com>
2016-10-10 15:43:42 +0200

    Support for retaining outputs; tests

--- bindings/python/cntk/trainer.py
+++ bindings/python/cntk/trainer.py
@@ -5,7 +5,7 @@
 
 from . import cntk_py
 from .cntk_py import DeviceDescriptor
-from .utils import sanitize_var_map, sanitize_function, typemap
+from .utils import sanitize_var_map, sanitize_function, typemap, value_to_seq
 
 class Trainer(cntk_py.Trainer):
     '''
@@ -30,7 +30,7 @@ class Trainer(cntk_py.Trainer):
         super(Trainer, self).__init__(model, loss_function, eval_function,
                 parameter_learners)
 
-    def train_minibatch(self, arguments, seqStarts=None, device=None):
+    def train_minibatch(self, arguments, output_map=None, seqStarts=None, device=None):
         '''
         Optimize model parameters using the specified 'arguments' minibatch of training samples.
         Returns false if all parameter learners indicate end of learning (through their Update method's return value).
@@ -41,6 +41,9 @@ class Trainer(cntk_py.Trainer):
               * list of inputs in the order that the function expects or 
               * a single input, if the function only has one argument. 
               Data should be either NumPy arrays or a `:class:cntk.io.MinibatchSource`
+            output_map (`dict` or `None`): mapping of output variables to
+             `None`, which will be filled during the training run with the
+             corresponding NumPy arrays.
             seqStarts (`list` of `bool`s or `None`): if `None`, every sequence is
              treated as a new sequence. Otherwise, it is interpreted as a list of
              Booleans that tell whether a sequence is a new sequence (`True`) or a
@@ -57,7 +60,16 @@ class Trainer(cntk_py.Trainer):
         arguments = sanitize_var_map(self.model().arguments(), arguments,
                 seqStarts)
 
-        return super(Trainer, self).train_minibatch(arguments, device)
+        if output_map:
+            result = super(Trainer, self).train_minibatch(arguments,
+                    output_map, device)
+            for k,v in output_map.items():
+                output_map[k] = value_to_seq(v)
+        else:
+            result = super(Trainer, self).train_minibatch(arguments, device)
+
+        return result
+
 
     def test_minibatch(self, arguments, seqStarts=None, device=None):
         '''
