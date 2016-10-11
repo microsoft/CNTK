5eac52b2045bbd41ccc90ace818659f27dcccca4
Willi Richert <wilrich@microsoft.com>
2016-10-11 09:20:33 +0100

    Add replace placeholder test

--- bindings/python/cntk/ops/tests/function_tests.py
+++ bindings/python/cntk/ops/tests/function_tests.py
@@ -11,9 +11,29 @@ Unit tests for the function class.
 import numpy as np
 import pytest
 from ..functions import *
-from .. import constant
+from ...trainer import *
+from .. import constant, parameter, input_variable, placeholder_variable
+
 
 def test_variable_forwarding():
     op = constant(value=2, shape=(3,4)) + 1
     assert op.shape().dimensions() == (3,4)
+
+
+def test_replace_placeholders():
+    p = placeholder_variable(shape=(1,))
+    i = input_variable(shape=(1,),
+                       needs_gradient=True,
+                       name='a')
+    res = p + 3
+    res.replace_placeholders({p: i})
+
+    assert res.eval({i: [3]}) == [6]
+
+    res2 = p + 2
+    from .. import plus
+    func = plus(res2, 10)
+    res2.replace_placeholders({p: func})
+
+    assert res2.eval({i: [3]}) == [15]
 
