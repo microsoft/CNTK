1c936ef5957cfb01eadcddb4c78bfefb6fe3ef08
Willi Richert <wilrich@microsoft.com>
2016-10-13 11:07:26 +0100

    Skip test on CPU

--- bindings/python/examples/test/cifar_resnet_test.py
+++ bindings/python/examples/test/cifar_resnet_test.py
@@ -7,14 +7,18 @@
 import numpy as np
 import os
 from cntk import DeviceDescriptor
+from cntk.utils import cntk_device
+from cntk.cntk_py import DeviceKind_GPU
 from cntk.io import ReaderConfig, ImageDeserializer
+import pytest
 
 from examples.CifarResNet.CifarResNet import cifar_resnet
 
 TOLERANCE_ABSOLUTE = 2E-1
 
 def test_cifar_resnet_error(device_id):
-    from cntk.utils import cntk_device
+    if cntk_device(device_id).type() != DeviceKind_GPU:
+        pytest.skip('test only runs on GPU')
     DeviceDescriptor.set_default_device(cntk_device(device_id))
 
     try:
