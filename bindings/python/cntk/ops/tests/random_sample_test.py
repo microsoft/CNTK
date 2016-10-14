415460e76890fa5dc584c5ea078004a047d452e5
Thilo Will <thilow@microsoft.com>
2016-10-14 17:19:39 +0200

    beautified excetion test

--- bindings/python/cntk/ops/tests/random_sample_test.py
+++ bindings/python/cntk/ops/tests/random_sample_test.py
@@ -17,37 +17,32 @@ from  cntk import random_sample_inclusion_frequency, times
 
 TEST_CASES = [
     # drawing 1 sample
-    (np.full((4), 42.),                                                               1,     True,   np.full((4), 1/4),                                    0.0001),
+    (np.full((4), 42.),                                                               1,     True,   np.full((4), 1/4),                                    0.0001, False),
 
     # drawing 13 samples
-    (np.full((4), 42.),                                                              13,     True,   np.full((4), 13/4),                                   0.0001),
+    (np.full((4), 42.),                                                              13,     True,   np.full((4), 13/4),                                   0.0001, False),
 
     # drawing more samples than there are classes
-    ([1.,2.,3.],                                                                     42,     True,   [42/(1+2+3), 2*42/(1+2+3), 3*42/(1+2+3)],             0.0001),
+    ([1.,2.,3.],                                                                     42,     True,   [42/(1+2+3), 2*42/(1+2+3), 3*42/(1+2+3)],             0.0001, False),
 
     # Use 300 weights where the first 200 hundred weights are high compared to the rest. Sample 200 without replacement. 
-    (np.concatenate((np.full((100),100),np.full((100),10),np.full((100),0.1))),    200,     False,  np.concatenate((np.full((200),1),np.full((100),0))),   0.05),
+    (np.concatenate((np.full((100),100),np.full((100),10),np.full((100),0.1))),    200,     False,  np.concatenate((np.full((200),1),np.full((100),0))),   0.05,   False),
     
     # Having more classes than samples is not allowed when sampling without replacment. Check if exception is thrown.
-    (np.full((4), 42.),                                                              50,     False,  np.full((4), 13/4),                                   0.0001), 
+    (np.full((4), 42.),                                                              50,     False,  np.full((4), 13/4),                                   0.0001, True), 
 
     # Non positive sampling weigts are not allowed.
-    ([1,-1.],                                                                         1,     True,   [0],                                   0.0001), 
-    ([1,-1.],                                                                         1,     False,  [0],                                   0.0001), 
+    ([1,-1.],                                                                         1,     True,   [0],                                                  0.0001, True), 
+    ([1,-1.],                                                                         1,     False,  [0],                                                  0.0001, True), 
 ]
 
-@pytest.mark.parametrize("weights, num_samples, allow_duplicates, expected, tolerance", TEST_CASES)
-def test_random_sample_inclusion_frequency(weights, num_samples, allow_duplicates, expected, tolerance, device_id, precision):
+@pytest.mark.parametrize("weights, num_samples, allow_duplicates, expected, tolerance, raises_exception", TEST_CASES)
+def test_random_sample_inclusion_frequency(weights, num_samples, allow_duplicates, expected, tolerance, raises_exception, device_id, precision):
 
     weights = AA(weights);
     result = random_sample_inclusion_frequency(weights, num_samples, allow_duplicates)
 
-    if num_samples >= len(weights) and not allow_duplicates:
-        # num_samples => len(weights) should give an error
-        with pytest.raises(RuntimeError):
-            result.eval()
-    elif np.any(np.less_equal(weights, 0)):
-        #weights less equal zero should give an error
+    if raises_exception:
         with pytest.raises(RuntimeError):
             result.eval()
     else:
