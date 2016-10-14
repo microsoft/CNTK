a183815dd1ece8560da777109a9eb4f76ab1e299
Friedel van Megen <fmegen@microsoft.com>
2016-10-14 10:09:40 +0200

    enabling cross-compile of the cntk sources for arm64 devices. This commit replaces implementations of functions based on sse with a non-sse version and updates the makefile such that SSE_FLAGS can be overriden when compiling for non-Intel architectures.

--- Source/Math/BlockHandlerSSE.cpp
+++ Source/Math/BlockHandlerSSE.cpp
@@ -3,6 +3,14 @@
 // Licensed under the MIT license. See LICENSE.md file in the project root for full licence information.
 //
 #include "stdafx.h"
+
+// This class implements a block handler based on the SSE intrinsics available on intel platforms.
+// Since we don't have SSE on ARM64 (NEON has similar functionality but is not identical) we cannot
+// use the BlockHandlerSSE implementation no ARM64.
+// Therefore, exclude the implementation on ARM64 builds for now.
+// TODO: In the future, we should provide a NEON based implementation instead.
+#if !defined(__aarch64__)
+
 #include <xmmintrin.h>
 #include <emmintrin.h>
 #include <tmmintrin.h>
@@ -30,3 +38,5 @@ int BlockHandlerSSE::RowToColOffsetRewrittenB(int col, int kOffset, int blockSiz
 
 
 }}}
+
+#endif
