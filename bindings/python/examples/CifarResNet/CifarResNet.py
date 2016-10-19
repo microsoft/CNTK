2068d29c3574212b87b05b4e21df402279f3de6f
KeDengMS <kedeng@microsoft.com>
2016-10-19 14:27:28 -0700

    Update python image examples

--- bindings/python/examples/CifarResNet/CifarResNet.py
+++ bindings/python/examples/CifarResNet/CifarResNet.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft. All rights reserved.
+﻿# Copyright (c) Microsoft. All rights reserved.
 
 # Licensed under the MIT license. See LICENSE.md file in the project root
 # for full license information.
@@ -9,14 +9,14 @@ import sys
 import os
 from cntk import Trainer
 from cntk.device import cpu, set_default_device
-from cntk.learner import sgd
-from cntk.ops import input_variable, constant, parameter, cross_entropy_with_softmax, combine, classification_error, times, pooling, AVG_POOLING
+from cntk.learner import momentum_sgd, learning_rate_schedule
+from cntk.ops import input_variable, constant, parameter, cross_entropy_with_softmax, combine, classification_error, times, element_times, pooling, AVG_POOLING, relu
 from cntk.io import ReaderConfig, ImageDeserializer
-from cntk.initializer import glorot_uniform
+from cntk.initializer import he_normal, glorot_uniform
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(abs_path, "..", ".."))
-from examples.common.nn import conv_bn_relu_layer, conv_bn_layer, resnet_node2, resnet_node2_inc, print_training_progress
+from examples.common.nn import conv_bn_relu_layer, conv_bn_layer, linear_layer, print_training_progress
 
 TRAIN_MAP_FILENAME = 'train_map.txt'
 MEAN_FILENAME = 'CIFAR-10_mean.xml'
@@ -64,9 +64,7 @@ def create_test_mb_source(features_stream_name, labels_stream_name, image_height
 
     image = ImageDeserializer(map_file)
     image.map_features(features_stream_name,
-            [ImageDeserializer.crop(crop_type='Random', ratio=0.8,
-                jitter_type='uniRatio'),
-             ImageDeserializer.scale(width=image_width, height=image_height,
+            [ImageDeserializer.scale(width=image_width, height=image_height,
                  channels=num_channels, interpolations='linear'),
              ImageDeserializer.mean(mean_file)])
     image.map_labels(labels_stream_name, num_classes)
@@ -74,60 +72,50 @@ def create_test_mb_source(features_stream_name, labels_stream_name, image_height
     rc = ReaderConfig(image, epoch_size=sys.maxsize)
     return rc.minibatch_source()
 
-def get_projection_map(out_dim, in_dim):
-    if in_dim > out_dim:
-        raise ValueError(
-            "Can only project from lower to higher dimensionality")
+def resnet_basic(input, out_feature_map_count, bn_time_const):
+    c1 = conv_bn_relu_layer(input, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
+    c2 = conv_bn_layer(c1, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
+    p = c2 + input
+    return relu(p)
 
-    projection_map_values = np.zeros(in_dim * out_dim, dtype=np.float32)
-    for i in range(0, in_dim):
-        projection_map_values[(i * in_dim) + i] = 1.0
-        shape = (out_dim, in_dim, 1, 1)
-        return constant(value=projection_map_values.reshape(shape))
+def resnet_basic_inc(input, out_feature_map_count, strides, bn_time_const):
+    c1 = conv_bn_relu_layer(input, out_feature_map_count, [3, 3], strides, bn_time_const)
+    c2 = conv_bn_layer(c1, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
+    s  = conv_bn_layer(input, out_feature_map_count, [1, 1], strides, bn_time_const)
+    p = c2 + s
+    return relu(p)
 
+def resnet_basic_stack2(input, out_feature_map_count, bn_time_const):
+    r1 = resnet_basic(input, out_feature_map_count, bn_time_const)
+    r2 = resnet_basic(r1, out_feature_map_count, bn_time_const)
+    return r2
+
+def resnet_basic_stack3(input, out_feature_map_count, bn_time_const):
+    r12 = resnet_basic_stack2(input, out_feature_map_count, bn_time_const)
+    r3 = resnet_basic(r12, out_feature_map_count, bn_time_const)
+    return r3
+    
 # Defines the residual network model for classifying images
 def resnet_classifer(input, num_classes):
-    conv_w_scale = 7.07
-    conv_b_value = 0
-
-    fc1_w_scale = 0.4
-    fc1_b_value = 0
-
-    sc_value = 1
     bn_time_const = 4096
 
-    kernel_width = 3
-    kernel_height = 3
-
-    conv1_w_scale = 0.26
     c_map1 = 16
+    
+    feat_scale = 0.00390625
 
-    conv1 = conv_bn_relu_layer(input, c_map1, kernel_width, kernel_height,
-                               1, 1, conv1_w_scale, conv_b_value, sc_value, bn_time_const)
-    rn1_1 = resnet_node2(conv1, c_map1, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
-    rn1_2 = resnet_node2(rn1_1, c_map1, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
-    rn1_3 = resnet_node2(rn1_2, c_map1, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
+    input_norm = element_times(feat_scale, input)
+
+    conv = conv_bn_relu_layer(input, c_map1, [3, 3], [1, 1], bn_time_const)
+    r1_1 = resnet_basic_stack3(conv, c_map1, bn_time_const)
 
     c_map2 = 32
-    rn2_1_wProj = get_projection_map(c_map2, c_map1)
-    rn2_1 = resnet_node2_inc(rn1_3, c_map2, kernel_width, kernel_height,
-                             conv1_w_scale, conv_b_value, sc_value, bn_time_const, rn2_1_wProj)
-    rn2_2 = resnet_node2(rn2_1, c_map2, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
-    rn2_3 = resnet_node2(rn2_2, c_map2, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
+
+    r2_1 = resnet_basic_inc(r1_1, c_map2, [2, 2], bn_time_const)
+    r2_2 = resnet_basic_stack2(r2_1, c_map2, bn_time_const)
 
     c_map3 = 64
-    rn3_1_wProj = get_projection_map(c_map3, c_map2)
-    rn3_1 = resnet_node2_inc(rn2_3, c_map3, kernel_width, kernel_height,
-                             conv1_w_scale, conv_b_value, sc_value, bn_time_const, rn3_1_wProj)
-    rn3_2 = resnet_node2(rn3_1, c_map3, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
-    rn3_3 = resnet_node2(rn3_2, c_map3, kernel_width, kernel_height,
-                         conv1_w_scale, conv_b_value, sc_value, bn_time_const)
+    r3_1 = resnet_basic_inc(r2_2, c_map3, [2, 2], bn_time_const)
+    r3_2 = resnet_basic_stack2(r3_1, c_map3, bn_time_const)
 
     # Global average pooling
     poolw = 8
@@ -135,11 +123,8 @@ def resnet_classifer(input, num_classes):
     poolh_stride = 1
     poolv_stride = 1
 
-    pool = pooling(rn3_3, AVG_POOLING, (1, poolh, poolw), (1, poolv_stride, poolh_stride))
-    out_times_params = parameter(shape=(c_map3, 1, 1, num_classes), init=glorot_uniform())
-    out_bias_params = parameter(shape=(num_classes), init=0)
-    t = times(pool, out_times_params)
-    return t + out_bias_params
+    pool = pooling(r3_2, AVG_POOLING, (1, poolh, poolw), (1, poolv_stride, poolh_stride))
+    return linear_layer(pool, num_classes)
 
 # Trains a residual network model on the Cifar image dataset
 def cifar_resnet(base_path, debug_output=False):
@@ -166,14 +151,21 @@ def cifar_resnet(base_path, debug_output=False):
     ce = cross_entropy_with_softmax(classifier_output, label_var)
     pe = classification_error(classifier_output, label_var)
 
+    mb_size = 128
+    num_mb_per_epoch = 100
+    num_epochs = 10
+    num_mbs = num_mb_per_epoch * num_epochs
+
+    lr_per_sample = [1/mb_size]*80+[0.1/mb_size]*40+[0.01/mb_size]
+    lr_schedule = learning_rate_schedule(lr_per_sample, units=mb_size * num_mb_per_epoch)
+    momentum_per_sample=0.9**(1.0/128)
+    
     # Instantiate the trainer object to drive the model training
     trainer = Trainer(classifier_output, ce, pe,
-                      [sgd(classifier_output.parameters, lr=0.0078125)])
+                      [momentum_sgd(classifier_output.parameters, lr_schedule, momentum_per_sample, l2_regularization_weight=0.0001)])
 
     # Get minibatches of images to train with and perform model training
-    mb_size = 32
-    training_progress_output_freq = 60
-    num_mbs = 1000
+    training_progress_output_freq = 100
 
     if debug_output:
         training_progress_output_freq = training_progress_output_freq/3
@@ -196,8 +188,8 @@ def cifar_resnet(base_path, debug_output=False):
     features_si = test_minibatch_source[feats_stream_name]
     labels_si = test_minibatch_source[labels_stream_name]
 
-    mb_size = 64
-    num_mbs = 300
+    mb_size = 128
+    num_mbs = 100
 
     total_error = 0.0
     for i in range(0, num_mbs):
@@ -220,7 +212,7 @@ if __name__ == '__main__':
     # set_default_device(cpu())
 
     base_path = os.path.abspath(os.path.normpath(os.path.join(
-        *"../../../Examples/Image/Datasets/CIFAR-10/".split("/"))))
+        *"../../../../Examples/Image/Datasets/CIFAR-10/".split("/"))))
 
     os.chdir(base_path)
 
