88ad84d419971de7edf76d8be880669c10c1adb1
Alexey Orlov <alexeyo@microsoft.com>
2016-10-20 16:15:14 +0200

    Add 2.0 include files to Windows drop script

--- Tools/make_binary_drop_windows.ps1
+++ Tools/make_binary_drop_windows.ps1
@@ -55,9 +55,13 @@ If ($targetConfig -eq "CPU")
 {
 	$buildPath = "x64\Release_CpuOnly"
 }
+# Include Files
 $includePath = "Source\Common\Include"
-# TBD To be redone either via white-list or via array
-$includeFile = Join-Path $includePath -ChildPath Eval.h
+$includePath20 = "Source\CNTKv2LibraryDll\API"
+$includeFiles = New-Object string[] 3
+$includeFiles[0] = Join-Path $includePath -ChildPath Eval.h
+$includeFiles[1] = Join-Path $includePath20 -ChildPath CNTKLibrary.h
+$includeFiles[2] = Join-Path $includePath20 -ChildPath CNTKLibraryInternals.h
 $sharePath = Join-Path $sharePath -ChildPath $targetConfig
 
 
@@ -80,7 +84,7 @@ If (Test-Path $baseDropPath\cntk\Python)
 {
 	Remove-Item $baseDropPath\cntk\Python -Recurse
 }
-# Add alexeyo/Linux-Binary-Drop-Script-Beta-2.0 to 2.0 Beta Drop
+# Add CNTKLibrary-2.0.dll to 2.0 Beta Drop
 # If (Test-Path $baseDropPath\cntk\CNTKLibrary-2.0.dll)
 # {
 # 	Remove-Item $baseDropPath\cntk\CNTKLibrary-2.0.dll
@@ -107,7 +111,10 @@ New-Item -Path $baseIncludePath -ItemType directory
 
 # Copy Include
 Write-Verbose "Copying Include files ..."
-Copy-Item $includeFile -Destination $baseIncludePath
+Foreach ($includeFile in $includeFiles)
+{
+	Copy-Item $includeFile -Destination $baseIncludePath
+}
 
 # Copy Examples
 Write-Verbose "Copying Examples ..."
