3c66fbfae41fe4fc226c2434487fa9d861230bd9
Emad Barsoum <ebarsoum@microsoft.com>
2016-10-21 00:28:45 -0700

    Update based on CR feedbacks.

--- bindings/python/examples/ImageHandsOn/ImageHandsOn.py
+++ bindings/python/examples/ImageHandsOn/ImageHandsOn.py
@@ -11,16 +11,14 @@ import time
 import math
  
 from cntk.blocks import *  # non-layer like building blocks such as LSTM()
-from cntk.layers import *  # layer-like stuff such as Linear()
+from cntk.layers import *  # layer-like stuff
 from cntk.models import *  # higher abstraction level, e.g. entire standard models and also operators like Sequential()
 from cntk.utils import *
 from cntk.io import ReaderConfig, ImageDeserializer
 from cntk.initializer import glorot_uniform, gaussian, he_normal
 from cntk import Trainer
 from cntk.learner import momentum_sgd, learning_rate_schedule
-from cntk.ops import cross_entropy_with_softmax, classification_error, relu
-from cntk.device import gpu, set_default_device
-from cntk.ops import *
+from cntk.ops import cross_entropy_with_softmax, classification_error, relu, convolution, pooling, PoolingType_Max
 
 #
 # Paths relative to current python file.
@@ -69,109 +67,115 @@ def create_reader(path, map_file, mean_file, train):
 #
 # helper APIs that define layers and shows low level APIs usage. 
 #
-def conv_layer(input, num_filters, filter_size, init, strides = (1,1), nonlinearity = relu):
+def conv_layer(input, num_filters, filter_size, init, strides=(1,1), nonlinearity=relu):
+    if nonlinearity is None:
+        nonlinearity = lambda x: x
+
     channel_count = input.shape[0]
 
     b_param = parameter(shape=(num_filters, 1, 1))
     w_param = parameter(shape=(num_filters, channel_count, filter_size[0], filter_size[1]), init=init)
-    linear = convolution(w_param, input, (channel_count, strides[0], strides[1])) + b_param
+    r       = convolution(w_param, input, (channel_count, strides[0], strides[1])) + b_param
+    r       = nonlinearity(r)
 
-    if nonlinearity == None:
-        return linear
+    return r
 
-    return nonlinearity(linear)
+def bn_layer(input, spatial_rank, nonlinearity=relu, bn_time_const=5000, b_value=0, sc_value=1):
+    if nonlinearity is None:
+        nonlinearity = lambda x: x
 
-def bn_layer(input, spatial_rank, nonlinearity = relu, bn_time_const = 5000, b_value = 0, sc_value = 1):
     dims = input.shape[0]
-    bias_params    = parameter(shape=(dims), init = b_value)
-    scale_params   = parameter(shape=(dims), init = sc_value)
-    running_mean   = constant(0., (dims))
-    running_invstd = constant(0., (dims))
 
-    linear = batch_normalization(input, scale_params, bias_params, running_mean, running_invstd, spatial_rank > 0, bn_time_const, use_cudnn_engine = True)
-    if nonlinearity == None:
-        return linear
+    bias_params    = parameter((dims), init=b_value)
+    scale_params   = parameter((dims), init=sc_value)
+    running_mean   = constant(0, (dims))
+    running_invstd = constant(0, (dims))
 
-    return nonlinearity(linear)
+    r = batch_normalization(input, scale_params, bias_params, running_mean, running_invstd, spatial_rank > 0, bn_time_const, use_cudnn_engine=True)
+    r = nonlinearity(r)
+    return r
 
-def conv_bn_layer(input, num_filters, filter_size, init, strides = (1,1), nonlinearity = relu, bn_time_const = 5000, b_value = 0, sc_value = 1):
-    conv = conv_layer(input, num_filters, filter_size, init, strides = strides, nonlinearity = None)
-    return bn_layer(conv, 2, nonlinearity = nonlinearity)
+def conv_bn_layer(input, num_filters, filter_size, init, strides=(1,1), nonlinearity=relu, bn_time_const=5000, b_value=0, sc_value=1):
+    r = conv_layer(input, num_filters, filter_size, init, strides=strides, nonlinearity=None)
+    r = bn_layer(r, 2, nonlinearity=nonlinearity)
+    return r
 
-def dense_layer(input, num_units, init, nonlinearity = relu):
+def dense_layer(input, num_units, init, nonlinearity=relu):
+    if nonlinearity is None:
+        nonlinearity = lambda x: x
+
     b_param = parameter(shape=(num_units))
 
     if len(input.shape) >= 3:
-        w_param = parameter(shape=(input.shape[0], input.shape[1], input.shape[2], num_units), init = init)
+        w_param = parameter(shape=(input.shape[0], input.shape[1], input.shape[2], num_units), init=init)
     else:
-        w_param = parameter(shape=(input.shape[0], num_units), init = init)
+        w_param = parameter(shape=(input.shape[0], num_units), init=init)
 
-    linear = b_param + times(input, w_param)
-    if nonlinearity == None:
-        return linear
+    r = b_param + times(input, w_param)
+    r = nonlinearity(r)
+    return r
 
-    return nonlinearity(linear)
-
-def dense_bn_layer(input, num_units, init, nonlinearity = relu):
-    dense = dense_layer(input, num_units, init, nonlinearity = None)
-    return bn_layer(dense, 0, nonlinearity = nonlinearity)
+def dense_bn_layer(input, num_units, init, nonlinearity=relu):
+    r = dense_layer(input, num_units, init, nonlinearity=None)
+    r = bn_layer(r, 0, nonlinearity=nonlinearity)
+    return r
 
 def max_pool_layer(input, pool_size, stride):
     return pooling(input, PoolingType_Max, (1, pool_size[0], pool_size[1]), (1, stride[0], stride[1]))
 
 def dropout_layer(input, rate):
-    return dropout(input, dropout_rate = rate)
+    return dropout(input, dropout_rate=rate)
 
 # Define basic model
 def create_basic_model(input):
     net = {}
 
-    net['conv1'] = conv_layer(input, 32, (5,5), init = gaussian(scale = 0.0043))
+    net['conv1'] = conv_layer(input, 32, (5,5), init = gaussian(scale=0.0043))
     net['pool1'] = max_pool_layer(net['conv1'], (3,3), (2,2))
 
-    net['conv2'] = conv_layer(net['pool1'], 32, (5,5), init = gaussian(scale = 1.414))
+    net['conv2'] = conv_layer(net['pool1'], 32, (5,5), init = gaussian(scale=1.414))
     net['pool2'] = max_pool_layer(net['conv2'], (3,3), (2,2))
 
-    net['conv3'] = conv_layer(net['pool2'], 64, (5,5), init = gaussian(scale = 1.414))
+    net['conv3'] = conv_layer(net['pool2'], 64, (5,5), init = gaussian(scale=1.414))
     net['pool3'] = max_pool_layer(net['conv3'], (3,3), (2,2))
 
-    net['fc4']   = dense_layer(net['pool3'], 64, init = gaussian(scale = 12))
-    net['fc5']   = dense_layer(net['fc4'], 10, init = gaussian(scale = 1.5), nonlinearity = None)
+    net['fc4']   = dense_layer(net['pool3'], 64, init = gaussian(scale=12))
+    net['fc5']   = dense_layer(net['fc4'], 10, init = gaussian(scale=1.5), nonlinearity = None)
 
     return net
 
 # Task 1: Adding Dropout
 def create_basic_model_with_dropout(input):
     net = {}
-    net['conv1'] = conv_layer(input, 32, (5,5), init = gaussian(scale = 0.0043))
+    net['conv1'] = conv_layer(input, 32, (5,5), init = gaussian(scale=0.0043))
     net['pool1'] = max_pool_layer(net['conv1'], (3,3), (2,2))
 
-    net['conv2'] = conv_layer(net['pool1'], 32, (5,5), init = gaussian(scale = 1.414))
+    net['conv2'] = conv_layer(net['pool1'], 32, (5,5), init = gaussian(scale=1.414))
     net['pool2'] = max_pool_layer(net['conv2'], (3,3), (2,2))
 
-    net['conv3'] = conv_layer(net['pool2'], 64, (5,5), init = gaussian(scale = 1.414))
+    net['conv3'] = conv_layer(net['pool2'], 64, (5,5), init = gaussian(scale=1.414))
     net['pool3'] = max_pool_layer(net['conv3'], (3,3), (2,2))
 
-    net['fc4']   = dense_layer(net['pool3'], 64, init = gaussian(scale = 12))
+    net['fc4']   = dense_layer(net['pool3'], 64, init = gaussian(scale=12))
     net['drop4'] = dropout_layer(net['fc4'], 0.75)
-    net['fc5']   = dense_layer(net['drop4'], 10, init = gaussian(scale = 1.5), nonlinearity = None)
+    net['fc5']   = dense_layer(net['drop4'], 10, init = gaussian(scale=1.5), nonlinearity=None)
 
     return net
 
 # Task 2: Adding Batch Normalization
 def create_basic_model_with_batch_normalization(input):
     net = {}
-    net['conv1'] = conv_bn_layer(input, 32, (5,5), init = gaussian(scale = 0.0043))
+    net['conv1'] = conv_bn_layer(input, 32, (5,5), init = gaussian(scale=0.0043))
     net['pool1'] = max_pool_layer(net['conv1'], (3,3), (2,2))
 
-    net['conv2'] = conv_bn_layer(net['pool1'], 32, (5,5), init = gaussian(scale = 1.414))
+    net['conv2'] = conv_bn_layer(net['pool1'], 32, (5,5), init = gaussian(scale=1.414))
     net['pool2'] = max_pool_layer(net['conv2'], (3,3), (2,2))
 
-    net['conv3'] = conv_bn_layer(net['pool2'], 64, (5,5), init = gaussian(scale = 1.414))
+    net['conv3'] = conv_bn_layer(net['pool2'], 64, (5,5), init = gaussian(scale=1.414))
     net['pool3'] = max_pool_layer(net['conv3'], (3,3), (2,2))
 
-    net['fc4']   = dense_bn_layer(net['pool3'], 64, init = gaussian(scale = 12))
-    net['fc5']   = dense_layer(net['fc4'], 10, init = gaussian(scale = 1.5), nonlinearity = None)
+    net['fc4']   = dense_bn_layer(net['pool3'], 64, init = gaussian(scale=12))
+    net['fc5']   = dense_layer(net['fc4'], 10, init = gaussian(scale=1.5), nonlinearity=None)
 
     return net
 
@@ -246,14 +250,11 @@ def train_and_evaluate(reader_train, reader_test, max_epochs):
         metric_denom    = 0
         sample_count    = 0
 
-        while True:
+        while sample_count < epoch_size:
             current_minibatch = min(minibatch_size, epoch_size - sample_count)
-            if current_minibatch == 0:
-                break
 
+            # fetch next mini batch.
             data = reader_train.next_minibatch(current_minibatch)
-            if data is None:
-                break
 
             # minibatch data to be trained with
             trainer.train_minibatch({input_var: data[features_slot], label_var: data[labels_slot]})
@@ -283,14 +284,11 @@ def train_and_evaluate(reader_train, reader_test, max_epochs):
     sample_count    = 0
     minibatch_index = 0
 
-    while True:
+    while sample_count < epoch_size:
         current_minibatch = min(minibatch_size, epoch_size - sample_count)
-        if current_minibatch == 0:
-            break
 
+        # Fetch next test min batch.
         data = reader_test.next_minibatch(current_minibatch)
-        if data is None:
-            break
 
         # minibatch data to be trained with
         metric_numer += trainer.test_minibatch({input_var: data[features_slot], label_var: data[labels_slot]}) * current_minibatch
@@ -308,7 +306,8 @@ def train_and_evaluate(reader_train, reader_test, max_epochs):
 
 if __name__=='__main__':
     os.chdir(data_path)
+
     reader_train = create_reader(data_path, 'train_map.txt', 'CIFAR-10_mean.xml', True)
     reader_test  = create_reader(data_path, 'test_map.txt', 'CIFAR-10_mean.xml', False)
 
-    train_and_evaluate(reader_train, reader_test, 10)
+    train_and_evaluate(reader_train, reader_test, max_epochs=10)
