7fe279fcbd041d942cc63703580ee4728046da53
Frank Seide <fseide@microsoft.com>
2016-10-21 09:49:19 -0700

    Sequential now supports nested [ ], and returns them that way as .layers

--- bindings/python/cntk/models.py
+++ bindings/python/cntk/models.py
@@ -17,13 +17,13 @@ from cntk.utils.debughelpers import _name_node, _node_name, _node_description, _
 from cntk.utils import Record
 from cntk.blocks import identity, Block
 
-# Sequential -- composite that applies a sequence of functions onto an input
+# Sequential -- composite that applies a sequence of layers (or any functions) onto an input
 # Sequential ([F, G, H]) === F >> G >> H
 # TODO: address this feedback: "I find this arbitrary. You can have Sequential as part of a bigger layer.  Or you can view a linear layer already as a model (which is part of the bigger model)."
-# TODO: can we use *arrayOfFunctions to avoid the [ ]?
-# TODO: nested lists?
-def Sequential(functions):
+# TODO: Willi had an idea how to use *layers to avoid the [ ]?
+def Sequential(layers):
+    if not isinstance(layers, (list,tuple)): # to support nested lists, run every item recursively through Sequential()
+        return layers
     from functools import reduce
-    apply_x = reduce(lambda f, g: f >> g, functions, identity)
-    #apply_x = _wrap_rename_Function(apply_x, 'Sequential')
-    return Block(apply_x, 'Sequential', Record(functions=functions))
+    apply_x = reduce(lambda f, g: Sequential(f) >> Sequential(g), layers, identity)
+    return Block(apply_x, 'Sequential', Record(layers=layers))
