6ea9a159263d7c59eca4b871022527e1bc12cfa5
Zhou Wang <zhouwang@microsoft.com>
2016-10-21 19:17:02 +0200

    try to make swig happy

--- Source/CNTKv2LibraryDll/Common.cpp
+++ Source/CNTKv2LibraryDll/Common.cpp
@@ -370,7 +370,7 @@ namespace CNTK
     }
 
     // Todo: move into AllStaticAxes() after upgrade to VS2015
-    /*static*/ const Axis Axis::s_allStaticAxes(Axis::SentinelStaticAxisIndexValueForAllStaticAxes);
+    /*static*/ const Axis s_allStaticAxes(Axis::SentinelStaticAxisIndexValueForAllStaticAxes);
     /*static*/ const Axis& Axis::AllStaticAxes()
     {
         return s_allStaticAxes;
