47b43343d106062c678cc88c5e6de0eb69fd1ee0
William Darling <wdarling@microsoft.com>
2016-10-22 08:55:12 +0200

    initial file adding

---
+++ Tutorials/Sequence2Sequence/Sequence2Sequence_att.py
@@ -0,0 +1,289 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+import numpy as np
+import sys
+import os
+import logging
+import math
+import time
+import cntk as C
+from cntk import DeviceDescriptor, Trainer, Axis, text_format_minibatch_source, StreamConfiguration
+from cntk.learner import momentum_sgd, momentum_schedule
+from cntk.ops import input_variable, placeholder_variable, cross_entropy_with_softmax, classification_error, sequence, slice, past_value, future_value, element_select, times, hardmax, plus, transpose
+from cntk.ops.functions import CloneMethod
+from cntk.persist import save_model, load_model
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(abs_path, "..", ".."))
+sys.path.append("/home/wdarling/cntk/bindings/python")
+from examples.common.nn import LSTMP_component_with_self_stabilization, stabilize, linear_layer, print_training_progress, create_attention_augment_hook
+
+# Creates and trains a sequence to sequence model
+def sequence_to_sequence_translator(debug_output=False, save_model=False):
+
+    # some model params
+    input_vocab_dim = 69
+    label_vocab_dim = 69
+
+    hidden_dim = 128
+    num_layers = 1
+
+    use_attention = True
+    attention_dim = 128
+    attention_span = 20
+
+    # Source and target inputs to the model
+    batch_axis = Axis.default_batch_axis()
+    input_seq_axis = Axis('inputAxis')
+    label_seq_axis = Axis('labelAxis')
+
+    input_dynamic_axes = [batch_axis, input_seq_axis]
+    raw_input = input_variable(shape=(input_vocab_dim), dynamic_axes=input_dynamic_axes, name='raw_input')
+
+    label_dynamic_axes = [batch_axis, label_seq_axis]
+    raw_labels = input_variable(shape=(label_vocab_dim), dynamic_axes=label_dynamic_axes, name='raw_labels')
+
+    # Instantiate the sequence to sequence translation model
+    input_sequence = raw_input
+
+    # Drop the sentence start token from the label, for decoder training
+    label_sequence = slice(raw_labels, label_seq_axis, 1, 0)   # <s> A B C </s> --> A B C </s>
+    label_sentence_start = sequence.first(raw_labels)          # <s>
+
+    is_first_label = sequence.is_first(label_sequence)
+    label_sentence_start_scattered = sequence.scatter(label_sentence_start, is_first_label) # <s> 0 0 0 ...
+
+    # Encoder
+    encoder_outputH = stabilize(input_sequence)
+    for i in range(0, num_layers):
+        (encoder_outputH, encoder_outputC) = LSTMP_component_with_self_stabilization(
+            encoder_outputH.output, hidden_dim, hidden_dim, future_value, future_value)
+
+    thought_vectorH = sequence.first(encoder_outputH)
+    thought_vectorC = sequence.first(encoder_outputC)
+
+    thought_vector_broadcastH = sequence.broadcast_as(
+        thought_vectorH, label_sequence)
+    thought_vector_broadcastC = sequence.broadcast_as(
+        thought_vectorC, label_sequence)
+
+    # Decoder
+    decoder_history_hook = plus(label_sequence, 0, name='decoder_history_hook')   # work-around for 'alias'; we need a copy of label_sequence instead of a pointer
+
+    decoder_input = element_select(is_first_label, label_sentence_start_scattered, past_value(decoder_history_hook))
+
+    # Decoder params
+    augment_input_hook = None
+    if use_attention:
+        augment_input_hook = create_attention_augment_hook(attention_dim, attention_span, label_sequence, encoder_outputH)
+
+    decoder_outputH = stabilize(decoder_input)
+    for i in range(0, num_layers):
+        if (i > 0) or use_attention:
+            recurrence_hookH = past_value
+            recurrence_hookC = past_value
+        else:
+            isFirst = sequence.is_first(label_sequence)
+            recurrence_hookH = lambda operand: element_select(
+                isFirst, thought_vector_broadcastH, past_value(operand))
+            recurrence_hookC = lambda operand: element_select(
+                isFirst, thought_vector_broadcastC, past_value(operand))
+
+        (decoder_outputH, encoder_outputC) = LSTMP_component_with_self_stabilization(
+            decoder_outputH.output, hidden_dim, hidden_dim, recurrence_hookH, recurrence_hookC,
+            augment_input_hook, hidden_dim)
+
+    decoder_output = decoder_outputH
+
+    # Softmax output layer
+    final = stabilize(decoder_output)
+    z = linear_layer(final, label_vocab_dim)
+
+    ce = cross_entropy_with_softmax(z, label_sequence)
+    errs = classification_error(z, label_sequence)
+
+    # network output for decoder history
+    net_output = hardmax(z)
+
+    # make a clone of the graph where the ground truth is replaced by the network output
+    ng = z.clone(CloneMethod.share, {decoder_history_hook.output : net_output.output})
+
+    # load the vocab
+    vocab_path = "/home/wdarling/cntk/Examples/SequenceToSequence/CMUDict/Data/cmudict-0.7b.mapping"
+    vocab = [w.strip() for w in open(vocab_path).readlines()]
+    i2w = { i:ch for i,ch in enumerate(vocab) }
+
+    train_path = r"/home/wdarling/cntk/Examples/SequenceToSequence/CMUDict/Data/cmudict-0.7b.train-dev-20-21.ctf"
+    test_path = r"tiny.ctf"
+
+    # reader(s)
+    train_reader = text_format_minibatch_source(train_path, [
+                     StreamConfiguration('features', input_vocab_dim, is_sparse=True, stream_alias='S0'),
+                     StreamConfiguration('labels',   label_vocab_dim, is_sparse=True, stream_alias='S1')
+                   ], randomize=True)
+    features_si_tr = train_reader.stream_info('features')
+    labels_si_tr   = train_reader.stream_info('labels')
+
+    test_reader =  text_format_minibatch_source(test_path, [
+                     StreamConfiguration('features', input_vocab_dim, is_sparse=True, stream_alias='S0'),
+                     StreamConfiguration('labels',   label_vocab_dim, is_sparse=True, stream_alias='S1')
+                   ], randomize=False)
+    features_si_te = test_reader.stream_info('features')
+    labels_si_te   = test_reader.stream_info('labels')
+
+    # trainer params
+    epoch_size = 908241    # number of label words
+    max_epochs = 10
+    minibatch_size = 72
+    training_progress_output_freq = 500
+    lr = 0.007
+    momentum_time_constant = 1100
+    m_schedule = momentum_schedule(math.exp(-1.0 / momentum_time_constant))
+
+    # setup trainer
+    learner = momentum_sgd(z.parameters, lr, m_schedule, 
+                           gradient_clipping_threshold_per_sample=2.3, gradient_clipping_with_truncation=True)
+
+    trainer = Trainer(z, ce, errs, [learner])
+
+    # Get minibatches of sequences to train with and perform model training
+    i = 0
+    mbs = 0
+    for epoch in range(max_epochs):
+        loss_numer = 0
+        metric_numer = 0
+        denom = 0
+
+        while i < (epoch+1) * epoch_size:
+
+            # get next minibatch of training data
+            mb_train = train_reader.next_minibatch(minibatch_size)
+
+            train_args = {'raw_input': mb_train[features_si_tr], 'raw_labels': mb_train[labels_si_tr]}
+            trainer.train_minibatch(train_args)
+
+            samples = trainer.previous_minibatch_sample_count
+            loss_numer += trainer.previous_minibatch_loss_average * samples
+            metric_numer += trainer.previous_minibatch_evaluation_average * samples
+            denom += samples
+
+            # every 500 MBs evaluate on a test sequence to visually show how we're doing
+            if mbs % 500 == 0:
+
+                mb_test = test_reader.next_minibatch(minibatch_size)
+                test_args = {'raw_input': mb_test[features_si_te], 'raw_labels': mb_test[labels_si_te]}
+
+                e = ng.eval(test_args)
+                print_sequences(e, i2w)
+
+                print_training_progress(trainer, mbs, training_progress_output_freq)
+
+            i += mb_train[labels_si_tr].num_samples
+            mbs += 1
+
+        print("--- EPOCH %d DONE: loss = %f, errs = %f ---" % (epoch, loss_numer/denom, 100.0*(metric_numer/denom)))
+
+        if save_model:
+            # save the model every epoch
+            model_filename = "model_epoch%d.dnn" % epoch
+            save_model(z, model_filename)
+            print("Saved model to '%s'" % model_filename)
+
+    return 0
+
+
+def dfs_walk(node, visitor, accum, visited):
+    if node in visited:
+        return
+    visited.add(node)
+    if hasattr(node, 'root_function'):
+        node = node.root_function
+        for child in node.inputs:
+            dfs_walk(child, visitor, accum, visited)
+    elif hasattr(node, 'is_output') and node.is_output:
+        dfs_walk(node.owner, visitor, accum, visited)
+
+    if visitor(node):
+        accum.append(node)
+
+def visit(root_node, visitor):
+    nodes = []
+    dfs_walk(root_node, visitor, nodes, set())
+    return nodes
+
+def find_nodes_by_name(root_node, node_name):
+    return visit(root_node, lambda x: x.name == node_name)
+
+
+def write(model_filename):
+
+    # params
+    input_vocab_dim = 69
+    label_vocab_dim = 69
+
+    # load the model...
+    model = load_model(np.float32, model_filename)
+
+    # load the vocab
+    vocab_path = "/home/wdarling/cntk/Examples/SequenceToSequence/CMUDict/Data/cmudict-0.7b.mapping"
+    vocab = [w.strip() for w in open(vocab_path).readlines()]
+    i2w = { i:ch for i,ch in enumerate(vocab) }
+
+    # setup data...
+    rel_path = r"/home/wdarling/cntk/Examples/SequenceToSequence/CMUDict/Data/cmudict-0.7b.test.ctf"
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), rel_path)
+
+    path = "tiny.ctf"
+
+    test_reader = text_format_minibatch_source(path, [
+        StreamConfiguration("features", input_vocab_dim, True, 'S0'),
+        StreamConfiguration("labels"  , label_vocab_dim, True, 'S1')
+    ], 0)
+    features_si = test_reader.stream_info("features")
+    labels_si   = test_reader.stream_info("labels")
+
+    test_minibatch_size = 1024
+
+    # get references to decoder history hook
+    #decoder_history_hook = find_nodes_by_name(model, 'decoder_history_hook')[0]
+
+    # clone graph and modify node
+    #ng = model.clone(CloneMethod.share, {decoder_history_hook.output : hardmax(model).output})
+
+    # Get minibatches of sequences to write with
+    i = 0
+    while True:
+        mb = test_reader.next_minibatch(test_minibatch_size)
+        if len(mb) == 0:
+            break
+
+        args = {'raw_input': mb[features_si], 'raw_labels': mb[labels_si]}
+
+        #C.cntk_py.set_computation_network_trace_level(1000000)
+
+        e = model.eval(args)
+        print_sequences(e, i2w)
+
+
+def print_sequences(sequences, i2w):
+    for s in sequences:
+        print([i2w[np.argmax(w)] for w in s], sep=" ")
+
+
+if __name__ == '__main__':
+    # Specify the target device to be used for computing
+    target_device = DeviceDescriptor.gpu_device(0)
+    # If it is crashing, probably you don't have a GPU, so try with CPU:
+    #target_device = DeviceDescriptor.cpu_device()
+    DeviceDescriptor.set_default_device(target_device)
+
+    # train
+    sequence_to_sequence_translator(True)
+
+    # write / decode
+    model_filename = "model_epoch2.dnn"
+    #write(model_filename)
---
+++ Tutorials/Sequence2Sequence/nn.py
@@ -0,0 +1,338 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+import numpy as np
+from cntk.ops import *
+from cntk.utils import get_train_eval_criterion, get_train_loss
+from cntk.initializer import glorot_uniform
+
+
+def linear_layer(input_var, output_dim):
+    input_dim = input_var.shape[0]
+    times_param = parameter(shape=(input_dim, output_dim), init=glorot_uniform())
+    bias_param = parameter(shape=(output_dim), init=0)
+
+    t = times(input_var, times_param)
+    return bias_param + t
+
+
+def fully_connected_layer(input, output_dim, nonlinearity):
+    p = linear_layer(input, output_dim)
+    return nonlinearity(p)
+
+# Defines a multilayer feedforward classification model
+
+
+def fully_connected_classifier_net(input, num_output_classes, hidden_layer_dim, num_hidden_layers, nonlinearity):
+    r = fully_connected_layer(input, hidden_layer_dim, nonlinearity)
+    for i in range(1, num_hidden_layers):
+        r = fully_connected_layer(r, hidden_layer_dim, nonlinearity)
+
+    return linear_layer(r, num_output_classes)
+
+
+def conv_bn_layer(input, out_feature_map_count, kernel_width, kernel_height, h_stride, v_stride, w_scale, b_value, sc_value, bn_time_const):
+    num_in_channels = input.shape[0]
+    #TODO: use RandomNormal to initialize, needs to be exposed in the python api
+    conv_params = parameter(shape=(out_feature_map_count, num_in_channels, kernel_height, kernel_width), init=glorot_uniform(output_rank=-1, filter_rank=2))
+    conv_func = convolution(conv_params, input, (num_in_channels, v_stride, h_stride))
+
+    #TODO: initialize using b_value and sc_value, needs to be exposed in the python api
+    bias_params = parameter(shape=(out_feature_map_count), init=b_value)
+    scale_params = parameter(shape=(out_feature_map_count), init=sc_value)
+    running_mean = constant(0., (out_feature_map_count))
+    running_invstd = constant(0., (out_feature_map_count))
+    return batch_normalization(conv_func, scale_params, bias_params, running_mean, running_invstd, True, bn_time_const, 0.0, 0.000000001)
+
+
+def conv_bn_relu_layer(input, out_feature_map_count, kernel_width, kernel_height, h_stride, v_stride, w_scale, b_value, sc_value, bn_time_const):
+    conv_bn_function = conv_bn_layer(input, out_feature_map_count, kernel_width,
+                                     kernel_height, h_stride, v_stride, w_scale, b_value, sc_value, bn_time_const)
+    return relu(conv_bn_function)
+
+
+def resnet_node2(input, out_feature_map_count, kernel_width, kernel_height, w_scale, b_value, sc_value, bn_time_const):
+    c1 = conv_bn_relu_layer(input, out_feature_map_count, kernel_width,
+                            kernel_height, 1, 1, w_scale, b_value, sc_value, bn_time_const)
+    c2 = conv_bn_layer(c1, out_feature_map_count, kernel_width,
+                       kernel_height, 1, 1, w_scale, b_value, sc_value, bn_time_const)
+    p = c2 + input
+    return relu(p)
+
+
+def proj_layer(w_proj, input, h_stride, v_stride, b_value, sc_value, bn_time_const):
+    num_in_channels = input.shape[0]
+    conv_func = convolution(w_proj, input, (num_in_channels, v_stride, h_stride))
+    out_feature_map_count = w_proj.shape[-1];
+    #TODO: initialize using b_value and sc_value, needs to be exposed in the python api
+    bias_params = parameter(shape=(out_feature_map_count), init=b_value)
+    scale_params = parameter(shape=(out_feature_map_count), init=sc_value)
+    running_mean = constant(0.0, (out_feature_map_count))
+    running_invstd = constant(0.0, (out_feature_map_count))
+    return batch_normalization(conv_func, scale_params, bias_params, running_mean, running_invstd, True, bn_time_const)
+
+
+def resnet_node2_inc(input, out_feature_map_count, kernel_width, kernel_height, w_scale, b_value, sc_value, bn_time_const, w_proj):
+    c1 = conv_bn_relu_layer(input, out_feature_map_count, kernel_width,
+                            kernel_height, 2, 2, w_scale, b_value, sc_value, bn_time_const)
+    c2 = conv_bn_layer(c1, out_feature_map_count, kernel_width,
+                       kernel_height, 1, 1, w_scale, b_value, sc_value, bn_time_const)
+
+    c_proj = proj_layer(w_proj, input, 2, 2, b_value, sc_value, bn_time_const)
+    p = c2 + c_proj
+    return relu(p)
+
+
+def embedding(input, embedding_dim):
+    input_dim = input.shape[0]
+
+    embedding_parameters = parameter(shape=(input_dim, embedding_dim), init=glorot_uniform())
+    return times(input, embedding_parameters)
+
+
+def select_last(operand):
+    return slice(operand, Axis.default_dynamic_axis(), -1, 0)
+
+
+def stabilize(operand):
+    scalar_constant = 4.0
+    f = constant(scalar_constant)
+    fInv = constant(1.0 / scalar_constant)
+
+    beta = element_times(fInv, 
+            log(1.0 + exp(element_times(f, parameter(init=0.99537863)))))
+    return element_times(beta, operand)
+
+
+def LSTMP_cell_with_self_stabilization(input, prev_output, prev_cell_state, aux, aux_dim):
+    input_dim = input.shape[0]
+    output_dim = prev_output.shape[0]
+    cell_dim = prev_cell_state.shape[0]
+
+    # setup parameters
+    Wxo = parameter(shape=(input_dim, cell_dim), init=glorot_uniform())
+    Wxi = parameter(shape=(input_dim, cell_dim), init=glorot_uniform())
+    Wxf = parameter(shape=(input_dim, cell_dim), init=glorot_uniform())
+    Wxc = parameter(shape=(input_dim, cell_dim), init=glorot_uniform())
+
+    Aao = parameter(shape=(cell_dim, aux_dim), init=glorot_uniform())
+    Aai = parameter(shape=(cell_dim, aux_dim), init=glorot_uniform())
+    Aaf = parameter(shape=(cell_dim, aux_dim), init=glorot_uniform())
+    Aac = parameter(shape=(cell_dim, aux_dim), init=glorot_uniform())
+
+    Bo = parameter(shape=(cell_dim), init=0)
+    Bc = parameter(shape=(cell_dim), init=0)
+    Bi = parameter(shape=(cell_dim), init=0)
+    Bf = parameter(shape=(cell_dim), init=0)
+
+    Whi = parameter(shape=(output_dim, cell_dim), init=glorot_uniform())
+    Wci = parameter(shape=(cell_dim), init=glorot_uniform())
+
+    Whf = parameter(shape=(output_dim, cell_dim), init=glorot_uniform())
+    Wcf = parameter(shape=(cell_dim), init=glorot_uniform())
+
+    Who = parameter(shape=(output_dim, cell_dim), init=glorot_uniform())
+    Wco = parameter(shape=(cell_dim), init=glorot_uniform())
+
+    Whc = parameter(shape=(output_dim, cell_dim), init=glorot_uniform())
+
+    Wmr = parameter(shape=(cell_dim, output_dim), init=glorot_uniform())
+
+    # Stabilization by routing input through an extra scalar parameter
+    sWxo = parameter(init=0)
+    sWxi = parameter(init=0)
+    sWxf = parameter(init=0)
+    sWxc = parameter(init=0)
+
+    sAao = parameter(init=0)
+    sAai = parameter(init=0)
+    sAaf = parameter(init=0)
+    sAac = parameter(init=0)
+
+    sWhi = parameter(init=0)
+    sWci = parameter(init=0)
+
+    sWhf = parameter(init=0)
+    sWcf = parameter(init=0)
+    sWho = parameter(init=0)
+    sWco = parameter(init=0)
+    sWhc = parameter(init=0)
+
+    sWmr = parameter(init=0)
+
+    expsWxo = exp(sWxo)
+    expsWxi = exp(sWxi)
+    expsWxf = exp(sWxf)
+    expsWxc = exp(sWxc)
+
+    expsAao = exp(sAao)
+    expsAai = exp(sAai)
+    expsAaf = exp(sAaf)
+    expsAac = exp(sAac)
+
+    expsWhi = exp(sWhi)
+    expsWci = exp(sWci)
+
+    expsWhf = exp(sWhf)
+    expsWcf = exp(sWcf)
+    expsWho = exp(sWho)
+    expsWco = exp(sWco)
+    expsWhc = exp(sWhc)
+
+    expsWmr = exp(sWmr)
+
+    Aaia = 0
+    Aaca = 0
+    Aafa = 0
+    Aaoa = 0
+    if aux != None:
+        Aaia = times(element_times(expsAai, aux), Aai)
+        Aaca = times(element_times(expsAac, aux), Aac)
+        Aafa = times(element_times(expsAaf, aux), Aaf)
+        Aaoa = times(element_times(expsAao, aux), Aao)
+
+    Wxix = times(element_times(expsWxi, input), Wxi)
+    Whidh = times(element_times(expsWhi, prev_output), Whi)
+    Wcidc = element_times(Wci, element_times(expsWci, prev_cell_state))
+
+    it = sigmoid(Wxix + Bi + Aaia + Whidh + Wcidc)
+    Wxcx = times(element_times(expsWxc, input), Wxc)
+    Whcdh = times(element_times(expsWhc, prev_output), Whc)
+    bit = element_times(it, tanh(Wxcx + Bc + Aaca + Whcdh))
+    Wxfx = times(element_times(expsWxf, input), Wxf)
+    Whfdh = times(element_times(expsWhf, prev_output), Whf)
+    Wcfdc = element_times(Wcf, element_times(expsWcf, prev_cell_state))
+
+    ft = sigmoid(Wxfx + Bf + Aafa + Whfdh + Wcfdc)
+    bft = element_times(ft, prev_cell_state)
+
+    ct = bft + bit
+
+    Wxox = times(element_times(expsWxo, input), Wxo)
+    Whodh = times(element_times(expsWho, prev_output), Who)
+    Wcoct = element_times(Wco, element_times(expsWco, ct))
+
+    ot = sigmoid(Wxox + Bo + Aaoa + Whodh + Wcoct)
+
+    mt = element_times(ot, tanh(ct))
+    return (times(element_times(expsWmr, mt), Wmr), ct)
+
+def ones_like(x):
+    zero = constant(value=0, shape=(1))
+    one  = constant(value=1, shape=(1))
+    def zeroes_like(y):
+        return element_times(y, zero)
+    return plus(zeroes_like(x), one)
+
+def past_value_window(N, input, axis=1):
+
+    ones_like_input = ones_like(input)
+
+    last_value=[]
+    last_valid=[]
+    value = None
+    valid = None
+
+    for t in range(N):
+        if t == 0:
+            value = input
+            valid = ones_like_input
+        else:
+            value = past_value(input, time_step=t)
+            # TODO: valid needs to be a different shape from value... fix!!!
+            valid = past_value(ones_like_input, time_step=t)
+
+        last_value.append(sequence.last(value))
+        last_valid.append(sequence.last(valid))
+
+    # can't get splice to stack rows 'beside' each other, so stack on top and then reshape...
+    value_a = splice(last_value, axis=0)
+    valid_a = splice(last_valid, axis=0)
+
+    # workaround
+    value = reshape(value_a, shape=(input.shape[0], N))
+    valid = reshape(valid_a, shape=(input.shape[0], N))
+
+    # value[t] = value of t steps in the past; valid[t] = true if there was a value t steps in the past
+    return value, valid
+
+def my_softmax(z, axis):
+    Z = reduce_log_sum(z, axis=axis) # reduce along axis
+    P = exp(z - Z)
+    return P
+
+def create_attention_augment_hook(attention_dim, attention_span, decoder_dynamic_axis, encoder_outputH):
+
+    # useful var
+    encoder_output_dim = encoder_outputH.shape[0]
+
+    # create the attention window
+    (aw_value, aw_valid) = past_value_window(attention_span, encoder_outputH, axis=1)
+
+    # setup the projection of the attention window to go into the tanh()
+    def projected_attention_window_broadcast():
+        W = parameter(shape=(attention_dim, encoder_output_dim), init=glorot_uniform())
+
+        projected_value = sequence.broadcast_as(times(W, stabilize(element_times(aw_value, aw_valid))), 
+                                                          decoder_dynamic_axis)
+        value           = sequence.broadcast_as(aw_value, decoder_dynamic_axis)
+        valid           = sequence.broadcast_as(aw_valid, decoder_dynamic_axis)
+
+        return projected_value, value, valid
+
+    # the function that gets passed to the LSTM function as the augment_input_hook parameter
+    def augment_input_hook(input, prev_state):
+        output_dim = prev_state.shape[0]
+        W = parameter(shape=(attention_dim, output_dim), init=glorot_uniform())
+        projectedH = times(W, stabilize(reshape(prev_state, shape=(output_dim,1))), output_rank=1)
+
+        tanh_out = tanh(projectedH + projected_attention_window_broadcast()[0])  # (attention_dim, attention_span)
+
+        # u = v * tanh(W1h + W2d)
+        v = parameter(shape=(1, attention_dim))
+        u = times(v, stabilize(element_times(tanh_out, projected_attention_window_broadcast()[2])))
+        u_valid = plus(u, log(projected_attention_window_broadcast()[2])) # broadcast u's first axis up to the attention_dim
+
+        attention_weights = my_softmax(u_valid, 1)
+        weighted_attention_window = element_times(projected_attention_window_broadcast()[1], attention_weights)
+
+        ones = constant(value=1, shape=(attention_span, 1))
+        weighted_attention_avg = stabilize(times(weighted_attention_window, ones, output_rank=1))
+
+        return reshape(weighted_attention_avg, shape=(output_dim))
+
+    return augment_input_hook
+
+
+def LSTMP_component_with_self_stabilization(input, output_dim, cell_dim, recurrence_hookH=past_value, recurrence_hookC=past_value, augment_input_hook=None, augment_input_dim=0):
+    dh = placeholder_variable(
+        shape=(output_dim), dynamic_axes=input.dynamic_axes)
+    dc = placeholder_variable(
+        shape=(cell_dim), dynamic_axes=input.dynamic_axes)
+
+    aux_input = None
+    if augment_input_hook != None:
+        aux_input = augment_input_hook(input, dh)
+
+    LSTMCell = LSTMP_cell_with_self_stabilization(input, dh, dc, aux_input, augment_input_dim)
+    actualDh = recurrence_hookH(LSTMCell[0])
+    actualDc = recurrence_hookC(LSTMCell[1])
+
+    # Form the recurrence loop by replacing the dh and dc placeholders with
+    # the actualDh and actualDc
+    LSTMCell[0].replace_placeholders(
+        {dh: actualDh.output, dc: actualDc.output})
+    
+    return (LSTMCell[0], LSTMCell[1])
+
+
+def print_training_progress(trainer, mb, frequency):
+
+    if mb % frequency == 0:
+        training_loss = get_train_loss(trainer)
+        eval_crit = get_train_eval_criterion(trainer)
+        print("Minibatch: {}, Train Loss: {}, Train Evaluation Criterion: {}".format(
+            mb, training_loss, eval_crit))
