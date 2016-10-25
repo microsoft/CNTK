cc608502519ebf1797cc4cb9628ff74ce03c2936
KeDengMS <kedeng@microsoft.com>
2016-10-24 20:54:35 -0700

    Reduce example redundancy

--- bindings/python/examples/distributed/CifarResNet_parallel.py
+++ bindings/python/examples/distributed/CifarResNet_parallel.py
@@ -12,6 +12,7 @@ from cntk.learner import momentum_sgd, learning_rate_schedule
 from cntk.ops import input_variable, constant, parameter, cross_entropy_with_softmax, combine, classification_error, times, element_times, pooling, AVG_POOLING, relu
 from cntk.io import ReaderConfig, ImageDeserializer
 from cntk.initializer import he_normal, glorot_uniform
+from examples.CifarResNet.CifarResNet import create_test_mb_source, resnet_classifer
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(abs_path, "..", ".."))
@@ -48,83 +49,6 @@ def create_mb_source(features_stream_name, labels_stream_name, image_height,
 
     rc = ReaderConfig(image, epoch_size=sys.maxsize)
     return rc.minibatch_source(distributed_communicator)
-
-def create_test_mb_source(features_stream_name, labels_stream_name, image_height,
-                     image_width, num_channels, num_classes, cifar_data_path):
-
-    path = os.path.normpath(os.path.join(abs_path, cifar_data_path))
-
-    map_file = os.path.join(path, TEST_MAP_FILENAME)
-    mean_file = os.path.join(path, MEAN_FILENAME)
-
-    if not os.path.exists(map_file) or not os.path.exists(mean_file):
-        cifar_py3 = "" if sys.version_info.major < 3 else "_py3"
-        raise RuntimeError("File '%s' or '%s' does not exist. Please run CifarDownload%s.py and CifarConverter%s.py from CIFAR-10 to fetch them" %
-                           (map_file, mean_file, cifar_py3, cifar_py3))
-
-    image = ImageDeserializer(map_file)
-    image.map_features(features_stream_name,
-            [ImageDeserializer.scale(width=image_width, height=image_height,
-                 channels=num_channels, interpolations='linear'),
-             ImageDeserializer.mean(mean_file)])
-    image.map_labels(labels_stream_name, num_classes)
-
-    rc = ReaderConfig(image, epoch_size=sys.maxsize)
-    return rc.minibatch_source()
-
-def resnet_basic(input, out_feature_map_count, bn_time_const):
-    c1 = conv_bn_relu_layer(input, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
-    c2 = conv_bn_layer(c1, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
-    p = c2 + input
-    return relu(p)
-
-def resnet_basic_inc(input, out_feature_map_count, strides, bn_time_const):
-    c1 = conv_bn_relu_layer(input, out_feature_map_count, [3, 3], strides, bn_time_const)
-    c2 = conv_bn_layer(c1, out_feature_map_count, [3, 3], [1, 1], bn_time_const)
-    s  = conv_bn_layer(input, out_feature_map_count, [1, 1], strides, bn_time_const)
-    p = c2 + s
-    return relu(p)
-
-def resnet_basic_stack2(input, out_feature_map_count, bn_time_const):
-    r1 = resnet_basic(input, out_feature_map_count, bn_time_const)
-    r2 = resnet_basic(r1, out_feature_map_count, bn_time_const)
-    return r2
-
-def resnet_basic_stack3(input, out_feature_map_count, bn_time_const):
-    r12 = resnet_basic_stack2(input, out_feature_map_count, bn_time_const)
-    r3 = resnet_basic(r12, out_feature_map_count, bn_time_const)
-    return r3
-    
-# Defines the residual network model for classifying images
-def resnet_classifer(input, num_classes):
-    bn_time_const = 4096
-
-    c_map1 = 16
-    
-    feat_scale = 0.00390625
-
-    input_norm = element_times(feat_scale, input)
-
-    conv = conv_bn_relu_layer(input, c_map1, [3, 3], [1, 1], bn_time_const)
-    r1_1 = resnet_basic_stack3(conv, c_map1, bn_time_const)
-
-    c_map2 = 32
-
-    r2_1 = resnet_basic_inc(r1_1, c_map2, [2, 2], bn_time_const)
-    r2_2 = resnet_basic_stack2(r2_1, c_map2, bn_time_const)
-
-    c_map3 = 64
-    r3_1 = resnet_basic_inc(r2_2, c_map3, [2, 2], bn_time_const)
-    r3_2 = resnet_basic_stack2(r3_1, c_map3, bn_time_const)
-
-    # Global average pooling
-    poolw = 8
-    poolh = 8
-    poolh_stride = 1
-    poolv_stride = 1
-
-    pool = pooling(r3_2, AVG_POOLING, (1, poolh, poolw), (1, poolv_stride, poolh_stride))
-    return linear_layer(pool, num_classes)
 
 # Trains a residual network model on the Cifar image dataset
 def cifar_resnet(base_path, run_test, num_epochs, communicator=None, save_model_filename=None, load_model_filename=None, debug_output=False):
