12821a41cfa950f496dda68e9c01527eda265264
Wolfgang Manousek <wolfma@microsoft.com>
2016-10-27 15:20:12 +0200

    check that script is run in Powershell 4 or later. For powershell version 3 show a warning, quit script for earlier versions

--- Scripts/windows/_info.ps1
+++ Scripts/windows/_info.ps1
@@ -21,9 +21,9 @@ function GetKey(
     return $key
 }
 
-function DisplayStart()
+function DisplayStartMessage
 {
-    Write-Host "
+"
 
 This script will setup the CNTK prequisites and the CNTK Python environment onto the machine.
 More help is given by calling 'get-help .\install.ps1' in your powershell environment.
@@ -39,20 +39,62 @@ Repeated operation of this script will reuse already downloaded components.
  - Anaconda3 will be installed into [$AnacondaBasePath]
  - A CNTK-PY34 environment will be created in [$AnacondaBasePath\envs]
  - CNTK will be installed into the CNTK-PY34 environment
-
 "
-if (-not $Execute) {
-    Write-Host "
+}
+
+function DisplayVersionWarningMessage(
+    [string] $version)
+{
+"
+You are executing this script from Powershell Version $version.
+We recommend that you execute the script from Powershell Version 4 or later. You can install Powershell Version 4 from:
+    https://www.microsoft.com/en-us/download/details.aspx?id=40855
+"
+}
+
+function DisplayWarningNoExecuteMessage
+{
+"
 The parameter '-Execute' hasn't be supplied to the script.
 The script will execute withouth making any actual changes to the machine.
 "
 }
-Write-Host "
 
+function DisplayStartContinueMessage
+{
+"
 1 - I agree and want to continue
 Q - Quit the installation process
 "
+}
 
+function CheckPowershellVersion
+{
+    $psVersion = $PSVersionTable.PSVersion.Major
+    if ($psVersion -ge 4) {
+        return $true
+    }
+
+    Write-Host $(DisplayVersionWarningMessage $psVersion)
+    if ($psVersion -eq 3) {
+        return $true
+    }
+    return $false
+}
+
+function DisplayStart()
+{
+    Write-Host $(DisplayStartMessage)
+
+    if (-not (CheckPowershellVersion)) {
+        return $false
+    }
+
+    if (-not $Execute) {
+        Write-Host $(DisplayWarningNoExecuteMessage)
+    }
+    
+    Write-Host $(DisplayStartContinueMessage)
     $choice = GetKey '^[1qQ]+$'
 
     if ($choice -contains "1") {
