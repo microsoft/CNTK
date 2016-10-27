95e0dc8d70bef1a3d659bac63d2e9877bd087353
Alona Kharchenko <t-alkhar@microsoft.com>
2016-10-27 13:54:24 +0200

    updated graph build and display

--- bindings/python/cntk/graph.py
+++ bindings/python/cntk/graph.py
@@ -60,3 +60,52 @@ def find_nodes_by_name(node, node_name):
     '''
     return dfs_walk(node, lambda x: x.name == node_name)
 
+def build_graph(node,visitor,path):
+    import pydot_ng as pydot
+
+    # initialize a dot object to store vertices and edges
+    dot_object = pydot.Dot(graph_name="network_graph",rankdir='TB')
+    dot_object.set_node_defaults(shape='rectangle', fixedsize='false',
+                             height=.85, width=.85, fontsize=12)
+    dot_object.set_edge_defaults(fontsize=10)
+
+    # walk the graph iteratively
+    stack = [node]
+    accum = []
+    visited = set()
+
+    while stack:
+        node = stack.pop()
+        
+        if node in visited:
+            continue
+
+        try:
+            # Function node
+            node = node.root_function
+            stack.extend(node.inputs)
+            cur_node = pydot.Node(node.op_name+' '+node.uid, label=node.op_name,shape='circle',
+                                    fixedsize='true', height=1, width=1)
+            dot_object.add_node(cur_node)
+            out_node = pydot.Node(node.outputs[0].uid)#,shape="rectangle")#,label=node.outputs[0].name)
+            dot_object.add_node(out_node)
+            dot_object.add_edge(pydot.Edge(cur_node,out_node,label=str(node.outputs[0].shape)))
+            for child in node.inputs:
+                child_node = pydot.Node(child.uid)#,shape="rectangle")#,label=child.name)
+                dot_object.add_node(child_node)
+                dot_object.add_edge(pydot.Edge(child_node, cur_node,label=str(child.shape)))
+
+        except AttributeError:
+            # OutputVariable node
+            try:
+                if node.is_output:
+                    stack.append(node.owner)
+            except AttributeError:
+                pass
+
+    if visitor(node):
+        accum.append(node)
+
+    # save to png
+    dot_object.write_png(path + '\\network_graph.png', prog='dot')
+
