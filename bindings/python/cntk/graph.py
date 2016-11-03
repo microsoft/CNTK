73013e78ca0ca45a60d432ce9e6bf2bf0f7496a2
Alona Kharchenko <t-alkhar@microsoft.com>
2016-11-03 12:04:31 +0100

    moved network graph to utils, added save as a DOT file

--- bindings/python/cntk/graph.py
+++ bindings/python/cntk/graph.py
@@ -9,7 +9,7 @@ def dfs_walk(node, visitor):
     Generic function that walks through the graph starting at ``node`` and
     uses function ``visitor`` on each node to check whether it should be
     returned.
-    
+ 
     Args:
         node (graph node): the node to start the journey from
         visitor (Python function or lambda): function that takes a node as
@@ -38,10 +38,10 @@ def dfs_walk(node, visitor):
             except AttributeError:
                 pass
 
-		if visitor(node):
-			accum.append(node)
+        if visitor(node):
+            accum.append(node)
 
-		visited.add(node)
+        visited.add(node)
 
     return accum
 
@@ -61,16 +61,46 @@ def find_nodes_by_name(node, node_name):
     '''
     return dfs_walk(node, lambda x: x.name == node_name)
 
-def build_graph(node,visitor,path):
-    import pydot_ng as pydot
+def output_function_graph(node,dot_file_path=None,png_file_path=None):
+    '''
+    Walks through every node of the graph starting at ``node``,
+    creates a network graph, and saves it as a string. If dot_file_name or 
+    png_file_name specified corresponding files will be saved.
     
-    # initialize a dot object to store vertices and edges
-    dot_object = pydot.Dot(graph_name="network_graph",rankdir='TB')
-    dot_object.set_node_defaults(shape='rectangle', fixedsize='false',
-                             height=.85, width=.85, fontsize=12)
-    dot_object.set_edge_defaults(fontsize=10)
+    
+    Requirements for DOT output: pydot_ng
+    Requirements for PNG output: pydot_ng and graphviz
 
-    # walk the graph iteratively
+    Args:
+        node (graph node): the node to start the journey from
+        dot_file_path (`str`, optional): DOT file path
+        png_file_path (`str`, optional): PNG file path
+
+    Returns:
+        `str` containing all nodes and edges
+    '''
+
+    dot = (dot_file_path != None)
+    png = (png_file_path != None)
+
+    if (dot or png):
+
+        try:
+            import pydot_ng as pydot
+        except ImportError:
+            raise ImportError("PNG and DOT format requires pydot_ng package. Unable to import pydot_ng.")
+
+        # initialize a dot object to store vertices and edges
+        dot_object = pydot.Dot(graph_name="network_graph",rankdir='TB')
+        dot_object.set_node_defaults(shape='rectangle', fixedsize='false',
+                                 height=.85, width=.85, fontsize=12)
+        dot_object.set_edge_defaults(fontsize=10)
+    
+    # string to store model 
+    model = ''
+
+    # walk every node of the graph iteratively
+    visitor = lambda x: True
     stack = [node]
     accum = []
     visited = set()
@@ -85,16 +115,34 @@ def build_graph(node,visitor,path):
             # Function node
             node = node.root_function
             stack.extend(node.inputs)
-            cur_node = pydot.Node(node.op_name+' '+node.uid, label=node.op_name,shape='circle',
-                                    fixedsize='true', height=1, width=1)
-            dot_object.add_node(cur_node)
-            out_node = pydot.Node(node.outputs[0].uid)#,shape="rectangle")#,label=node.outputs[0].name)
-            dot_object.add_node(out_node)
-            dot_object.add_edge(pydot.Edge(cur_node,out_node,label=str(node.outputs[0].shape)))
-            for child in node.inputs:
-                child_node = pydot.Node(child.uid)#,shape="rectangle")#,label=child.name)
-                dot_object.add_node(child_node)
-                dot_object.add_edge(pydot.Edge(child_node, cur_node,label=str(child.shape)))
+
+            # add current node
+            model += node.op_name + '('
+            if (dot or png):
+                cur_node = pydot.Node(node.op_name+' '+node.uid,label=node.op_name,shape='circle',
+                                        fixedsize='true', height=1, width=1)
+                dot_object.add_node(cur_node)
+
+            # add node's inputs
+            for i in range(len(node.inputs)):
+                child = node.inputs[i]
+                
+                model += child.uid
+                if (i != len(node.inputs) - 1):
+                    model += ", "
+
+                if (dot or png):
+                    child_node = pydot.Node(child.uid)
+                    dot_object.add_node(child_node)
+                    dot_object.add_edge(pydot.Edge(child_node, cur_node,label=str(child.shape)))
+
+            # ad node's output
+            model += ") -> " + node.outputs[0].uid +'\n'
+
+            if (dot or png):
+                out_node = pydot.Node(node.outputs[0].uid)
+                dot_object.add_node(out_node)
+                dot_object.add_edge(pydot.Edge(cur_node,out_node,label=str(node.outputs[0].shape)))
 
         except AttributeError:
             # OutputVariable node
@@ -104,9 +152,13 @@ def build_graph(node,visitor,path):
             except AttributeError:
                 pass
 
-        if visitor(node):
-            accum.append(node)
+    if visitor(node):
+        accum.append(node)
 
-    # save to png
-    dot_object.write_png(path + '\\network_graph.png', prog='dot')
+    if (png):
+        dot_object.write_png(png_file_path, prog='dot')
+    if (dot):
+        dot_object.write_raw(dot_file_path)
 
+    # return lines in reversed order
+    return "\n".join(model.split("\n")[::-1])
