061be6eaefd65d166804b9fe35769b44f026f981
Mark Hillebrand <mahilleb@microsoft.com>
2016-11-07 11:14:53 +0100

    Added some headers

--- Source/Common/Include/MPIWrapper.h
+++ Source/Common/Include/MPIWrapper.h
@@ -1,3 +1,8 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
 #pragma once
 
 // Please see https://github.com/Microsoft/CNTK/wiki/Setup-CNTK-on-Windows#ms-mpi or
