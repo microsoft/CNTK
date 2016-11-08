eabc0afd03b9d2aab11e944758df3a9d8ffb308e
Ivan Stojiljkovic <ivanst@microsoft.com>
2016-11-08 23:15:55 +0100

    CNTK performance profiler
    Equivalent to zsoltm/profiler as of 2016-11-08, rebased to master@{2016-12-12}.

---
+++ Source/PerformanceProfilerDll/PerformanceProfiler.h
@@ -0,0 +1,213 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// Real-time thread-safe profiler that generats a summary report and a detail profile log.
+// The profiler is highly performant and light weight and meant to be left on all the time.
+//
+// Profiler Usage
+//
+// To initialize and tear down the profiler, call ProfilerInit() and ProfilerClose(). The scoped
+// object, ProfilerContext can also be used for managing the lifetime of the profiler. The profiler
+// works by accumulating events in a pre-allocated buffer, up until the buffer is full. At the
+// time when the profiler is torn down, a summary report and a detailed log file is written to disk.
+//
+// When profiling code, two types of events can be used - fixed or custom. A fixed event is
+// predifined in the ProfilerEvents enum and by the FixedEventDesc struct. A custom event is
+// simply a string.
+//
+// To profile a section of code, call ProfilerTimeBegin() and ProfilerTimeEnd(), or the scoped
+// object ScopeProfile. When the GPU sync flag is set in the FixedEventDesc and in ProfilerInit(),
+// the GPU will be synced at the time ProfilerTimeEnd() is called, for fixed events only. For
+// custom events, ProfilerSyncGpu() can be called to sync the GPU.
+//
+// When profiling CUDA kernels, use the CUDA APIs to measure elapsed kernel time and call
+// ProfilerCudaTimeEnd() to record the event.
+//
+// When there is a need to profile I/O bandwidth (or throughput), the ProfilerThroughputBegin()
+// and ProfilerThroughputBegin() calls should be used. The throughput APIs can only be used
+// with fixed events.
+//
+
+#pragma once
+
+#include "Basics.h"
+#include <stdio.h>
+
+namespace Microsoft { namespace MSR { namespace CNTK {
+
+#ifdef _WIN32
+#if defined(PERFORMANCE_PROFILER_DLL)
+#define PERF_PROFILER_API __declspec(dllexport)
+#else
+#define PERF_PROFILER_API __declspec(dllimport)
+#endif
+#else
+#define PERF_PROFILER_API
+#endif
+
+//
+// Fixed/predetermined profiler events. These appear in the summary report.
+//
+enum ProfilerEvents
+{
+    // Main thread header (dummy events)
+    profilerSepMainThread = 0,
+    profilerSepSpace0,
+
+    // Main thread events
+    profilerEvtMainEpoch,                   // Train epoch loop time
+    profilerEvtMainMinibatch,               // One minibatch loop time
+    profilerEvtMainGetMinibatch,            // GetMinibatch() function time
+    profilerEvtMainFB,                      // Forward + Backward pass time
+    profilerEvtMainGradient,                // Gradient aggregation time
+    profilerEvtMainWeights,                 // Weight update time
+    profilerEvtMainPost,                    // Remainder time in minibatch loop
+
+    // Data reader header (dummy events)
+    profilerSepSpace1,
+    profilerSepDataReader,
+    profilerSepSpace2,
+
+    // Data reader events
+    profilerEvtReadMinibatch,               // Time spend reading minibatch on async thread
+
+    profilerEvtMax
+};
+
+
+//
+// Initialize all resources to enable profiling.
+// profilerDir: Directory where the profiler logs will be saved.
+// customEventBufferBytes: Bytes to allocate for the custom event buffer.
+// logSuffix: Suffix string to append to log files.
+// syncGpu: Wait for GPU to complete processing for each profiling event.
+// syncCudaKernels: Synchronize every cuda kernel.
+//
+void PERF_PROFILER_API ProfilerInit(const std::wstring& profilerDir, const unsigned long long customEventBufferBytes,
+    const std::wstring& logSuffix, const bool syncGpu, const bool syncCudaKernels);
+
+
+//
+// Enable/disable profiling.
+// By default, profiling is disabled after a ProfilerInit call.
+// This can be used to temporarily turn profiling on/off during execution.
+//
+void PERF_PROFILER_API ProfilerEnable(bool enable);
+
+
+//
+// Measure either a fixed or custom event time.
+// ProfilerTimeBegin() returns a stateId that is passed to ProfilerTimeEnd().
+// If ProfilerTimeEnd() is not called, the event is not recorded.
+//
+long long PERF_PROFILER_API ProfilerTimeBegin();
+void PERF_PROFILER_API ProfilerTimeEnd(const long long stateId, const int eventId);
+void PERF_PROFILER_API ProfilerTimeEnd(const long long stateId, const char* eventDescription);
+
+//
+// Conditionally sync the GPU if the syncGPU flag is set. This only needs to be excplicitly
+// called for custom events.
+//
+void PERF_PROFILER_API ProfilerSyncGpu();
+
+//
+// CUDA kernel profiling.
+// CUDA kernels are profiled using a single call to this function.
+//
+void PERF_PROFILER_API ProfilerCudaTimeEnd(const float deltaSeconds, const char* eventDescription);
+
+
+//
+// Measure throughput given the number of bytes.
+// ProfilerThroughputBegin() returns a stateId that is passed to ProfilerThroughputEnd().
+// If ProfilerThroughputEnd() is not called, the event is not recorded.
+//
+long long PERF_PROFILER_API ProfilerThroughputBegin();
+void PERF_PROFILER_API ProfilerThroughputEnd(const long long stateId, const int eventId, const long long bytes);
+
+//
+// Generate reports and release all resources.
+//
+void PERF_PROFILER_API ProfilerClose();
+
+
+//
+// Scoped profiler instantiation.
+//
+struct PERF_PROFILER_API ProfilerContext
+{
+    void Init(const std::wstring& profilerDir = L"", const unsigned long long customEventBufferBytes = (32 * 1024 * 1024), const std::wstring& logSuffix = L"", const bool syncGpu = false, const bool syncCudaKernels = false);
+    ~ProfilerContext();
+};
+
+
+//
+// Scoped time profiling.
+//
+struct PERF_PROFILER_API ScopeProfile
+{
+    ScopeProfile(int eventId);
+    ScopeProfile(const char* description);
+    ~ScopeProfile();
+
+private:
+    unsigned long long  m_stateId;
+    int                 m_eventId;
+    const char*         m_description;
+};
+
+#define PROFILE_SCOPE(eventId)      ScopeProfile __sp##eventId(eventId);
+#define PROFILE_FUNCTION            ScopeProfile __fsp(__FUNCTION__);
+
+
+
+//
+// Scoped throughput profiling.
+//
+struct PERF_PROFILER_API ScopeThroughput
+{
+    ScopeThroughput(int eventId, long long bytes);
+    ~ScopeThroughput();
+
+private:
+    unsigned long long              m_stateId;
+    int                             m_eventId;
+    long long                       m_bytes;
+};
+
+#define THROUGHPUT_SCOPE(eventId, bytes)    ScopeThroughput __st##eventId(eventId, bytes);
+
+
+//
+// CUDA profiling helpers.
+//
+
+void PERF_PROFILER_API SyncCudaScopeSetFlags(bool syncEnabled, bool profilingEnabled);
+void PERF_PROFILER_API SyncCudaScopeGetFlags(bool& syncEnabled, bool& profilingEnabled);
+
+struct PERF_PROFILER_API CudaProfilerTimer
+{
+    //
+    // Setup the CUDA profiler.
+    // enable: flag indicating if profiler is enabled
+    // syncIterations: Number of iterations between profiling & sync (ex: 100 means that we profile every 100 iterations)
+    // maxIterations: Stop syncing & profiling after this many iterations. -1 indicates to never stop.
+    //                (ex: 1000 means that we stop after 1000 iterations)
+    //
+    CudaProfilerTimer(bool enable, int syncIterations, int maxIterations);
+
+    //
+    // Main function that enables profiling, to be called at the beggining of each iteration.
+    //
+    void Update();
+
+private:
+    bool    m_enable;
+    int     m_syncIterations;
+    int     m_maxIterations;
+    int     m_iterationCnt;
+    int     m_iterationCntTotal;
+};
+
+}}}
