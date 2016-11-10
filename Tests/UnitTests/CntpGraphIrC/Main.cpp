6132e082ed6554853ea95e05391a636592fe56ab
Friedel van Megen <fmegen@microsoft.com>
2016-11-10 13:34:19 +0100

    initial testing app

---
+++ Tests/UnitTests/CntpGraphIrC/Main.cpp
@@ -0,0 +1,30 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+#include "CNTKLibrary.h"
+#include <functional>
+
+using namespace CNTK;
+
+void MultiThreadsEvaluation(bool);
+
+int main()
+{
+
+#ifndef CPUONLY
+#error "must use CPU Only"
+#else
+    fprintf(stderr, "Run tests using CPU-only build.\n");
+#endif
+
+    // Lets disable automatic unpacking of PackedValue object to detect any accidental unpacking 
+    // which will have a silent performance degradation otherwise
+    Internal::SetAutomaticUnpackingOfPackedValues(/*disable =*/ true);
+
+    MultiThreadsEvaluation(false);
+
+    fprintf(stderr, "\nCNTKv2Library tests: Passed\n");
+    fflush(stderr);
+}
