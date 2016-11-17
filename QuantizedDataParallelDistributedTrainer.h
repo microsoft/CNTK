1ccd6dbfc6b016fe8bc6b092ff44b53bc4b7c5d0
KeDengMS <kedeng@microsoft.com>
2016-11-17 11:13:47 -0800

    Rename function

--- QuantizedDataParallelDistributedTrainer.h
+++ QuantizedDataParallelDistributedTrainer.h
@@ -30,7 +30,7 @@ namespace CNTK
         // Optional override that gets called per minibatch after finishing gradient computation but before updating model parameters
         bool PreParameterUpdateCallback(const Trainer& /*trainer*/, std::vector<std::pair<Parameter, NDArrayViewPtr>>& gradientValues, MinibatchInfo& info) override
         {
-            HandleEmptyMinibatches(gradientValues, info);
+            HandleEmptyMinibatch(gradientValues, info);
 
             std::vector<NDArrayViewPtr> headerToAggregate;
             headerToAggregate.push_back(info.evalCriterionValue);
