cc9a5e75e331c55325765710be30c09eda6a1446
Zhou Wang <zhouwang@microsoft.com>
2016-11-17 19:34:12 +0100

    added unit tests and update baseline

--- Source/Extensibility/EvalWrapper/EvalWrapper.cpp
+++ Source/Extensibility/EvalWrapper/EvalWrapper.cpp
@@ -146,8 +146,8 @@ public:
     }
 
     /// <summary>Evaluates the model using a single forward feed pass and retrieves the output layer data</summary>
-    /// <param name="outputKey"></param>
-    /// <param name="outputSize"></param>
+    /// <param name="outputKey">The output layer name</param>
+    /// <param name="outputSize">The dimension size of the output layer</param>
     /// <returns>Results for specified layer</returns>
     __declspec(deprecated) List<ElemType>^ Evaluate(String^ outputKey, int outputSize)
     {
@@ -177,8 +177,7 @@ public:
     }
 
     /// <summary>Evaluates the model using a single forward feed pass and retrieves the output layer data</summary>
-    /// <param name="outputKey"></param>
-    /// <param name="outputSize"></param>
+    /// <param name="outputKey">The output layer name</param>
     /// <returns>Results for specified layer</returns>
     List<ElemType>^ Evaluate(String^ outputKey)
     {
@@ -210,8 +209,8 @@ public:
     }
 
     /// <summary>Evaluates the model against input data and retrieves the output layer data</summary>
-    /// <param name="inputs"></param>
-    /// <param name="outputs"></param>
+    /// <param name="inputs">The input nodes and their values</param>
+    /// <param name="outputs">The output nodes and their values</param>
     void Evaluate(Dictionary<String^, List<ElemType>^>^ inputs, Dictionary<String^, List<ElemType>^>^ outputs)
     {
         if (m_eval == nullptr)
@@ -252,7 +251,7 @@ public:
                 throw GetCustomException(ex);
             }
 
-            WriteOutput(outputs, stdOutputs);
+            CopyOutput(outputs, stdOutputs);
         }
         catch (Exception^)
         {
@@ -261,9 +260,9 @@ public:
     }
 
     /// <summary>Evaluates the model against input data and retrieves the output layer data</summary>
-    /// <param name="inputs"></param>
-    /// <param name="outputKey"></param>
-    /// <param name="outputSize"></param>
+    /// <param name="inputs">The input nodes and their values</param>
+    /// <param name="outputKey">The output layer name</param>
+    /// <param name="outputSize">The dimension size of the output layer</param>
     /// <returns>Results for specified layer</returns>
     __declspec(deprecated) List<ElemType>^ Evaluate(Dictionary<String^, List<ElemType>^>^ inputs, String^ outputKey, int outputSize)
     {
@@ -410,8 +409,8 @@ public:
     }
 
     /// <summary>Evaluates the model against input data and retrieves the desired output layer data</summary>
-    /// <param name="inputs"></param>
-    /// <param name="outputKey"></param>
+    /// <param name="inputs">The input nodes and their values</param>
+    /// <param name="outputKey">The output layer name</param>
     /// <returns>Results for requested layer</returns>
     List<ElemType>^ Evaluate(Dictionary<String^, List<ElemType>^>^ inputs, String^ outputKey)
     {
@@ -505,6 +504,9 @@ private:
         return lower;
     }
 
+    /// <summary>Evaluates the model using a single forward feed pass without input and retrieves the output layer data</summary>
+    /// <param name="outputs">The output nodes and output buffers</param>
+    /// <returns>none</returns>
     void Evaluate(Dictionary<String^, List<ElemType>^>^ outputs)
     {
         std::vector<shared_ptr<std::vector<ElemType>>> sharedOutputVectors;
@@ -527,15 +529,19 @@ private:
             throw GetCustomException(ex);
         }
 
-        WriteOutput(outputs, stdOutputs);
+        CopyOutput(outputs, stdOutputs);
     }
 
-    void WriteOutput(Dictionary<String^, List<ElemType>^>^ outputs, std::map<std::wstring, std::vector<ElemType>*>& stdOutputs)
+    /// <summary>Copy output data to the output buffer</summary>
+    /// <param name="outputs">The output nodes and output buffers</param>
+    /// <param name="outputData">The output data</param>
+    /// <returns>none</returns>
+    void CopyOutput(Dictionary<String^, List<ElemType>^>^ outputs, std::map<std::wstring, std::vector<ElemType>*>& outputData)
     {
         for each (auto item in outputs)
         {
             pin_ptr<const WCHAR> key = PtrToStringChars(item.Key);
-            std::vector<ElemType> *pVec = stdOutputs[key];
+            std::vector<ElemType> *pVec = outputData[key];
             if (pVec == nullptr)
             {
                 throw gcnew NullReferenceException("No output value available.");
--- Tests/UnitTests/ManagedEvalTests/EvalManagedTests.cs
+++ Tests/UnitTests/ManagedEvalTests/EvalManagedTests.cs
@@ -21,6 +21,182 @@ namespace Microsoft.MSR.CNTK.Extensibility.Managed.Tests
     public class EvalManagedTests
     {
         [TestMethod]
+        public void EvalManagedEvaluateNoInputTest()
+        {
+            string modelDefinition = @"precision = ""float""
+                traceLevel = 1
+                run=NDLNetworkBuilder 
+                NDLNetworkBuilder=[ 
+                v1 = Constant(1)
+                v2 = Constant(2) 
+                o1 = Plus(v1, v2, tag=""output"")
+                FeatureNodes = (v1)
+                ]";
+
+            using (var model = new IEvaluateModelManagedF())
+            {
+                model.CreateNetwork(modelDefinition);
+
+                var inDims = model.GetNodeDimensions(NodeGroup.Input);
+                Assert.AreEqual(inDims.Count(), 0);
+
+                var outDims = model.GetNodeDimensions(NodeGroup.Output);
+                Assert.AreEqual(outDims.Count(), 1);
+                Assert.AreEqual(outDims.First().Key, "o1");
+                Assert.AreEqual(outDims.First().Value, 1);
+
+                var outputVal = model.Evaluate(outDims.First().Key);
+
+                var expected = new List<float>() {3};
+                CollectionAssert.AreEqual(expected, outputVal);
+            }
+        }
+
+        [TestMethod]
+        public void EvalManagedEvaluateSingleOutputTest()
+        {
+            string modelDefinition = @"precision = ""float"" 
+                traceLevel = 1
+                run=NDLNetworkBuilder
+                NDLNetworkBuilder=[
+                i1 = Input(1)
+                o1 = Times(Constant(3), i1, tag=""output"") 
+                FeatureNodes = (i1)
+                ]";
+
+            using (var model = new IEvaluateModelManagedF())
+            {
+                model.CreateNetwork(modelDefinition);
+
+                var inDims = model.GetNodeDimensions(NodeGroup.Input);
+                Assert.AreEqual(inDims.Count(), 1);
+                Assert.AreEqual(inDims.First().Key, "i1");
+                Assert.AreEqual(inDims.First().Value, 1);
+
+                var inputs = new Dictionary<string, List<float>>()
+                    {
+                        {inDims.First().Key, new List<float>() {2}}
+                    };
+
+                var outDims = model.GetNodeDimensions(NodeGroup.Output);
+                Assert.AreEqual(outDims.Count(), 1);
+                Assert.AreEqual(outDims.First().Key, "o1");
+                Assert.AreEqual(outDims.First().Value, 1);
+
+                var outputs = new Dictionary<string, List<float>>()
+                    {
+                        {outDims.First().Key, new List<float>() {0}}
+                    };
+
+                model.Evaluate(inputs, outputs);
+
+                var expected = new List<float>() {6};
+                CollectionAssert.AreEqual(expected, outputs.First().Value);
+            }
+        }
+
+        [TestMethod]
+        public void EvalManagedEvaluateDualOutputsTest()
+        {
+            string modelDefinition = @"deviceId = -1 
+                precision = ""float""
+                traceLevel = 1
+                run=NDLNetworkBuilder
+                NDLNetworkBuilder=[
+                i1 = Input(1)
+                o1 = Times(Constant(3), i1, tag=""output"")
+                o2 = Times(Constant(5), i1, tag=""output"")
+                FeatureNodes = (i1)
+                ]";
+
+            using (var model = new IEvaluateModelManagedF())
+            {
+                model.CreateNetwork(modelDefinition);
+
+                var inDims = model.GetNodeDimensions(NodeGroup.Input);
+                Assert.AreEqual(inDims.Count(), 1);
+                Assert.AreEqual(inDims.First().Key, "i1");
+                Assert.AreEqual(inDims.First().Value, 1);
+
+                var inputs = new Dictionary<string, List<float>>()
+                    {
+                        {"i1", new List<float>() {2}}
+                    };
+
+                var outDims = model.GetNodeDimensions(NodeGroup.Output);
+                Assert.AreEqual(outDims.Count(), 2);
+                Assert.AreEqual(outDims["o1"], 1);
+                Assert.AreEqual(outDims["o2"], 1);
+
+                var outputs = new Dictionary<string, List<float>>()
+                    {
+                        {"o1", new List<float>() {0}},
+                        {"o2", new List<float>() {0}}
+                    };
+
+                model.Evaluate(inputs, outputs);
+
+                var expected1 = new List<float>() {6};
+                var expected2 = new List<float>() {10};
+                CollectionAssert.AreEqual(expected1, outputs["o1"]);
+                CollectionAssert.AreEqual(expected2, outputs["o2"]);
+            }
+        }
+
+        [TestMethod]
+        public void EvalManagedEvaluateHiddenLayerTest()
+        {
+            string modelDefinition = @"deviceId = -1 
+                precision = ""float""
+                traceLevel = 1
+                run=NDLNetworkBuilder
+                NDLNetworkBuilder=[
+                i1 = Input(1)
+                pool5 = Times(Constant(2), i1)
+                OutputNodes.z = Plus(pool5, Constant(7), tag=""output"")
+                FeatureNodes = (i1)
+                ]";
+
+            using (var model = new IEvaluateModelManagedF())
+            {
+                var desiredOutputLayers = new List<string>() { "pool5", "OutputNodes.z" };
+
+                model.CreateNetwork(modelDefinition, deviceId: -1, outputNodeNames: desiredOutputLayers);
+
+                var inDims = model.GetNodeDimensions(NodeGroup.Input);
+                Assert.AreEqual(inDims.Count(), 1);
+                Assert.AreEqual(inDims["i1"], 1);
+
+                var inputs = new Dictionary<string, List<float>>()
+                    {
+                        { "i1", new List<float>() {5} }
+                    };
+
+                // We request the output layer names(s) and dimension, we'll use the first one.
+                var outDims = model.GetNodeDimensions(NodeGroup.Output);
+                Assert.AreEqual(outDims.Count(), 2);
+                Assert.AreEqual(outDims["pool5"], 1);
+                Assert.AreEqual(outDims["OutputNodes.z"], 1);
+
+                var outputs = new Dictionary<string, List<float>>()
+                    {
+                        // The order of node name below is different than that returned by outDims,
+                        // in order to test whether the output values are correctly mapped to the name.
+                        { "pool5", new List<float>() {0} },
+                        { "OutputNodes.z", new List<float>() {0} }
+                    };
+
+                // We can call the evaluate method and get back the results (single layer)...
+                model.Evaluate(inputs, outputs);
+
+                var expected1 = new List<float>() {10};
+                var expected2 = new List<float>() {17};
+                CollectionAssert.AreEqual(expected1, outputs["pool5"]);
+                CollectionAssert.AreEqual(expected2, outputs["OutputNodes.z"]);
+            }
+        }
+
+        [TestMethod]
         public void EvalManagedValuesBufferTest()
         {
             int bufferSize = 2;
