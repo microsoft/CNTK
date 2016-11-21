c5ecb7dc13ab98e7c683d6977513867ff723e3c1
Zhou Wang <zhouwang@microsoft.com>
2016-11-21 18:01:55 +0100

    change the project type to use custom build instead of nmake; add CNTKLibrary.h to cntk_cs.i; exclude unused part in CNTKLibrary.h for SWIG C#

--- Source/CNTKv2LibraryDll/API/CNTKLibrary.h
+++ Source/CNTKv2LibraryDll/API/CNTKLibrary.h
@@ -414,6 +414,7 @@ namespace CNTK
             : NDArrayView(dataType, viewShape, const_cast<void*>(dataBuffer), bufferSizeInBytes, device, /*readOnly =*/ true)
         {}
 
+#ifndef SWIGCSHARP
         ///
         /// Construct a NDArrayView with newly allocated sparse storage in SparseCSC format on the specified 'device' and initialize its contents
         // with the specified Sparse CSC format data.
@@ -511,6 +512,7 @@ namespace CNTK
             m_isReadOnly = readOnly;
         }
 
+#endif
         ///
         /// Destruct 'this' NDArrayView object
         ///
@@ -1588,7 +1590,9 @@ namespace CNTK
     {
         friend bool operator==(const Variable& first, const Variable& second);
         friend class Function;
+#ifndef SWIGCSHARP
         friend class CompositeFunction;
+
         friend class Trainer;
         friend class PrimitiveFunction;
 
@@ -1596,6 +1600,13 @@ namespace CNTK
         friend struct std::hash;
 
         friend class Internal::VariableResolver;
+ 
+        template <typename ElementType>
+        friend Variable Internal::GetVariable(const Microsoft::MSR::CNTK::ComputationNodeBasePtr& node,
+                                              std::unordered_map<Microsoft::MSR::CNTK::ComputationNodeBasePtr, Variable>& nodeToVariableMap,
+                                              std::unordered_map<Variable, Variable>& placeholderReplacements,
+                                              std::unordered_set<FunctionPtr>& allPrimitiveFunctions);
+#endif
 
 #ifndef SWIG
     private:
@@ -1820,6 +1831,8 @@ private:
         return !(first == second);
     }
 
+#ifndef SWIGCSHARP
+
     ///
     /// Create a Placeholder variable to be used as a temporary/placeholder input to a Function.
     /// All placeholder inputs of a Function must be replaced with non-placeholder Variables before Forward evaluation of the Function.
@@ -1847,7 +1860,7 @@ private:
     {
         return PlaceholderVariable(NDShape::Unknown, name, Axis::UnknownDynamicAxes());
     }
-
+#endif
     ///
     /// Create an 'Input' Variable denoting sparse data and specify if gradients are to be computed for this input
     ///
@@ -1935,6 +1948,8 @@ private:
     CNTK_API ParameterInitializer HeNormalInitializer(int outputRank = SentinelValueForInferParamInitRank, int filterRank = SentinelValueForInferParamInitRank, double scale = DefaultParamInitScale, unsigned long seed = SentinelValueForAutoSelectRandomSeed);
     CNTK_API ParameterInitializer BilinearInitializer(size_t kernelWidth, size_t kernelHeight);
     CNTK_API ParameterInitializer RandomInitializerWithRank(const ParameterInitializer& initializer, int outputRank, int filterRank);
+
+#ifndef SWIGCSHARP
 
     ///
     /// Denotes Parameter inputs of a Function.
@@ -2107,7 +2122,10 @@ private:
     // This check is weak in that the derives types may sneak in some additional fields if the base type had some padding at the end, without changing the object size
     // but it should be good enough for catching any accidental addiiton of fields.
     static_assert(sizeof(Constant) == sizeof(Variable), "The Constant type should not have any data fields beyond what its base type 'Variable' has.");
+#endif
 }
+
+#ifndef SWIGCSHARP
 
 namespace std {
     
@@ -2145,6 +2163,7 @@ namespace std {
     };
 }
 
+#endif
 namespace CNTK
 {
     ///
@@ -2206,8 +2225,11 @@ namespace CNTK
     ///
     class Function : public std::enable_shared_from_this<Function>, public IDictionarySerializable
     {
+#ifndef SWIGCSHARP
+
         friend class CompositeFunction;
         friend class Trainer;
+#endif
 
     public:
         ///
@@ -2229,6 +2251,8 @@ namespace CNTK
                                          const DeviceDescriptor& computeDevice = DeviceDescriptor::UseDefaultDevice(),
                                          const std::unordered_set<Variable>& outputsToRetainBackwardStateFor = {}) = 0;
 
+#ifndef SWIGCSHARP
+
         ///
         /// Backpropagates supplied 'rootGradientValues' for one or more of the output variables of the Function, to produce gradient Values
         /// corresponding to the specified set of input variables in 'backPropagatedGradientValuesForInputs'.
@@ -2238,9 +2262,10 @@ namespace CNTK
         /// The 'state' parameter is an instance of an BackPropState instance obtained from a previous call to the Forward method on 'this; Function for the 
         /// computation that this gradient backpropagation corresponds to.
         ///
-        virtual void Backward(const BackPropStatePtr& state,
+        virtual void Backward(const std::wstring& state,
                               const std::unordered_map<Variable, ValuePtr>& rootGradientValues,
                               std::unordered_map<Variable, ValuePtr>& backPropagatedGradientValuesForInputs) = 0;
+#endif 
 
         ///
         /// Returns the name of the operation that this Function denotes
@@ -2262,6 +2287,8 @@ namespace CNTK
         ///
         CNTK_API FunctionPtr Clone(ParameterCloningMethod parameterCloneMethod = ParameterCloningMethod::Clone, const std::unordered_map<Variable, Variable>& replacements = {}) const;
 
+#ifndef SWIGCSHARP
+
         ///
         /// Generates a dictionary that captures the state of the Function graph underlying this Function.
         ///
@@ -2274,6 +2301,7 @@ namespace CNTK
         ///
         CNTK_API static FunctionPtr Deserialize(const Dictionary& dictionary, const ::CNTK::DeviceDescriptor& device = DeviceDescriptor::UseDefaultDevice());
 
+#endif 
     public:
         ///
         /// Returns the name of 'this' function.
@@ -2328,6 +2356,8 @@ namespace CNTK
             });
         }
 
+#ifndef SWIGCSHARP
+
         ///
         /// Returns the set of all Parameter variables of 'this' Function.
         ///
@@ -2374,6 +2404,7 @@ namespace CNTK
         ///
         CNTK_API FunctionPtr ReplacePlaceholder(const Variable& placeholderReplacement);
 
+#endif
         ///
         /// Save this function graph into a model file
         ///
@@ -2458,6 +2489,7 @@ namespace CNTK
         Dictionary m_attributes;
     };
 
+#ifndef SWIGCSHARP // Not needed for SWIG C# Eval Binding
     ///
     /// Create an instance of the CNTK built-in elementwise negate operation with the specified input operand.
     ///
@@ -3426,7 +3458,11 @@ namespace CNTK
                 (left.m_elementType == right.m_elementType) &&
                 (left.m_sampleLayout == right.m_sampleLayout));
     }
+
+#endif // SWIGCSHARP
 }
+
+#ifndef SWIGCSHARP // Not need for SWIG C# Eval Binding
 
 namespace std {
     template <> struct hash<::CNTK::StreamInformation>
@@ -3793,3 +3829,5 @@ namespace std
         }
     };
 }
+
+#endif // SWIGCSHARP
--- bindings/csharp/cntk_cs.i
+++ bindings/csharp/cntk_cs.i
@@ -10,4 +10,67 @@
 %include <windows.i>
 %include <attribute.i>
 
+%template() std::vector<size_t>;
+%template() std::vector<bool>;
+%template(DoubleVector) std::vector<double>;
+%template(FloatVector) std::vector<float>;
+%template(SizeTVector) std::vector<std::vector<size_t>>;
+%template(FloatVectorVector) std::vector<std::vector<float>>;
+%template(DoubleVectorVector) std::vector<std::vector<double>>;
+%template() std::vector<CNTK::Variable>;
+%template() std::vector<CNTK::Parameter>;
+%template() std::vector<CNTK::Constant>;
+%template(AxisVector) std::vector<CNTK::Axis>;
+%template() std::vector<CNTK::DeviceDescriptor>;
+%template() std::vector<CNTK::StreamConfiguration>;
+
+//%template() std::vector<CNTK::DictionaryValue>;
+
+%template() std::vector<std::shared_ptr<CNTK::Function>>;
+%template() std::vector<std::shared_ptr<CNTK::Learner>>;
+%template() std::pair<size_t, double>;
+%template() std::vector<std::pair<size_t, double>>;
+
+%shared_ptr(CNTK::BackPropState);
+%shared_ptr(CNTK::Function);
+%shared_ptr(CNTK::CompositeFunction);
+%shared_ptr(CNTK::Value);
+%shared_ptr(CNTK::NDShape);
+%shared_ptr(CNTK::NDArrayView);
+%shared_ptr(std::vector<float>);
+
+%ignore CNTK::IDictionarySerializable;
+%ignore CNTK::DictionaryValue;
+%ignore CNTK::Dictionary;
+
+%ignore CNTK::Parameter;
+%ignore CNTK::Constant;
+%ignore CNTK::PoolingType;
+%ignore CNTK::TrainingParameterSchedule;
+%ignore CNTK::TrainingParameterPerUnitSchedule;
+%ignore CNTK::MomentumAsTimeConstantSchedule;
+%ignore CNTK::AdditionalLearningOptions;
+%ignore CNTK::Learner;
+%ignore CNTK::Trainer;
+%ignore CNTK::StreamInformation;
+%ignore CNTK::MinibatchData;
+%ignore CNTK::MinibatchSource;
+%ignore CNTK::StreamConfiguration;
+%ignore CNTK::DistributedWorkerDescriptor;
+%ignore CNTK::DistributedCommunicator;
+%ignore CNTK::QuantizedDistributedCommunicator;
+%ignore CNTK::MinibatchInfo;
+%ignore CNTK::DistributedTrainer;
+
+
+%{
+    #include "CNTKLibrary.h"
+%}
+
+%include "CNTKLibraryInternals.h"
+%include "CNTKLibrary.h"
+
+
+
+
 
