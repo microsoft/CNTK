44b8f79b79d2e9250bb1199b89d5a2bc6f01e042
fmegen@microsoft.com <fmegen@PIKESPEAK-16>
2016-11-22 13:00:18 -0800

    restructure code

--- Tests/UnitTests/CntpGraphIrC/Main.cpp
+++ Tests/UnitTests/CntpGraphIrC/Main.cpp
@@ -5,17 +5,25 @@
 
 #include "CNTKLibrary.h"
 #include <functional>
-
+#include "LSTM/LstmGraphNode.h"
 
 using namespace CNTK;
 
+#pragma warning(push, 0)
+#include <graphid.pb.h>
+#include <google/protobuf/util/json_util.h>
+#pragma warning(pop)
+
 using namespace std;
 
-void MultiThreadsEvaluation();
-
+extern FunctionPtr GraphIrToCntkGraph(graphIR::Graph &/*graphIrPtr*/, FunctionPtr /*modelFuncPtr*/);
+extern graphIR::Graph CntkGraphToGraphIr(FunctionPtr evalFunc);
+extern void EvaluateGraph(FunctionPtr evalFunc, const DeviceDescriptor& device);
 
 int main()
 {
+	auto device = DeviceDescriptor::CPUDevice();
+
 #ifndef CPUONLY
 #error "must use CPU Only"
 #else
@@ -26,8 +34,18 @@ int main()
     // which will have a silent performance degradation otherwise
     Internal::SetAutomaticUnpackingOfPackedValues(/*disable =*/ true);
 
-    MultiThreadsEvaluation();
+	// The model file will be trained and copied to the current runtime directory first.
+	auto modelFuncPtr = CNTK::Function::LoadModel(DataType::Float, L"\\CNTK\\Tests\\UnitTests\\CntpGraphIrC\\BingModelRoot\\Out\\proto2.dnn", device, LstmGraphNodeFactory);
 
+	EvaluateGraph(modelFuncPtr, device);
+
+	// convert cntk to graphir
+	auto graphIrPtr = CntkGraphToGraphIr(modelFuncPtr);
+
+	// convert graphir back to cntk (with the original cntk model as template)
+	auto modelImportFuncPtr = GraphIrToCntkGraph(graphIrPtr, modelFuncPtr);
+
+	// TODO: verify that roundtrip is completed.
     fprintf(stderr, "\nCNTKv2Library tests: Passed\n");
     fflush(stderr);
 }
