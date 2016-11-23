ec88098edcf16aa52f46bdf89919f6f87b7260e7
Aayush Garg <aayush.garg@microsoft.com>
2016-11-23 08:12:02 -0800

    Attention

--- Examples/SequenceToSequence/CMUDict/Python/Sequence2Sequence_att.py
+++ Examples/SequenceToSequence/CMUDict/Python/Sequence2Sequence_att.py
@@ -13,17 +13,15 @@ from cntk import Trainer, Axis, save_model, load_model, distributed, persist
 from cntk.io import MinibatchSource, CTFDeserializer, StreamDef, StreamDefs, INFINITELY_REPEAT, INFINITE_SAMPLES
 from cntk.learner import momentum_sgd, momentum_as_time_constant_schedule, learning_rate_schedule, UnitType
 from cntk.ops import input_variable, cross_entropy_with_softmax, classification_error, sequence, past_value, future_value, \
-                     element_select, alias, hardmax, placeholder_variable, combine, parameter, plus, times, reduce_log_sum, exp, \
-                     splice, constant, reshape, element_times, log, tanh
+                     element_select, alias, hardmax, placeholder_variable, combine
 from cntk.ops.functions import CloneMethod
 from cntk.graph import find_nodes_by_name
 from cntk.device import cpu, gpu, set_default_device, best
 
 from localblocks import LSTM, Stabilizer
-
-from cntk.initializer import glorot_uniform
+from cntk.layers import Dense
 from cntk.utils import get_train_eval_criterion, get_train_loss
-
+from attention import create_attention_augment_hook
 
 ########################
 # variables and stuff  #
@@ -42,7 +40,7 @@ hidden_dim = 128
 num_layers = 1
 attention_dim = 128
 attention_span = 20
-use_attention = False
+use_attention = True
 
 # stabilizer
 stabilize = Stabilizer()
@@ -57,97 +55,6 @@ def create_reader(path, randomize, size=INFINITELY_REPEAT, distributed_after=INF
         labels    = StreamDef(field='S1', shape=label_vocab_dim, is_sparse=True)
     )), randomize=randomize, epoch_size = size, distributed_after = distributed_after)
 
-
-#########################################
-# attention -- should go somewhere else #
-#########################################
-
-# create a past value window that returns two records: a value, shape=(N,dim), and a valid window, shape=(1,dim)
-def past_value_window(N, input, axis=0):
-
-    ones_like_input = plus(times(input, constant(0, shape=(input.shape[0],1))), constant(1, shape=(1)))
-        
-    last_value=[]
-    last_valid=[]
-    value = None
-    valid = None
-
-    for t in range(N):
-        if t == 0:
-            value = input
-            valid = ones_like_input
-        else:
-            value = past_value(input, time_step=t)
-            valid = past_value(ones_like_input, time_step=t)     
-        
-        last_value.append(sequence.last(value))
-        last_valid.append(sequence.last(valid))
-
-    # can't get splice to stack rows 'beside' each other, so stack on top and then reshape...
-    value_a = splice(last_value, axis=axis)
-    valid_a = splice(last_valid, axis=axis)
-
-    # now reshape
-    value = reshape(value_a, shape=(N, input.shape[0]))
-    valid = reshape(valid_a, shape=(N, 1))
-
-    # value[t] = value of t steps in the past; valid[t] = true if there was a value t steps in the past
-    return value, valid
-
-def my_softmax(z, axis):
-    Z = reduce_log_sum(z, axis=axis) # reduce along axis
-    P = exp(z - Z)
-    
-    return P
-
-def create_attention_augment_hook(attention_dim, attention_span, decoder_dynamic_axis, encoder_outputH):
-
-    # useful var
-    encoder_output_dim = encoder_outputH.shape[0]
-
-    # create the attention window
-    (aw_value, aw_valid) = past_value_window(attention_span, encoder_outputH, axis=0)
-
-    # setup the projection of the attention window to go into the tanh()
-    def projected_attention_window_broadcast():
-        W = parameter(shape=(attention_dim, encoder_output_dim), init=glorot_uniform())
-
-        projected_value = sequence.broadcast_as(times(stabilize(element_times(aw_value, aw_valid)), W), 
-                                                          decoder_dynamic_axis)
-        value           = sequence.broadcast_as(aw_value, decoder_dynamic_axis)
-        valid           = sequence.broadcast_as(aw_valid, decoder_dynamic_axis)
-
-        # should be shape=(attention_span, attention_dim)
-        return projected_value, value, valid
-
-    # the function that gets passed to the LSTM function as the augment_input_hook parameter
-    def augment_input_hook(input, prev_state):
-        output_dim = prev_state.shape[0]
-        W = parameter(shape=(output_dim, attention_dim), init=glorot_uniform())
-
-        projectedH = times(stabilize(prev_state), W, output_rank=1)
-
-        tanh_out = tanh(projectedH + projected_attention_window_broadcast()[0])  # (attention_span, attention_dim)
-        
-        # u = v * tanh(W1h + W2d)
-        v = parameter(shape=(attention_dim, 1))
-               
-        u = times(stabilize(element_times(tanh_out, projected_attention_window_broadcast()[2])), v) # (attention_span, 1)
-        u_valid = plus(u, log(projected_attention_window_broadcast()[2]), name='u_valid')
-
-        attention_weights = alias(my_softmax(u_valid, 0), name='attention_weights')
-        # the window should be shape=(attention_span, output_dim)
-        weighted_attention_window = element_times(projected_attention_window_broadcast()[1], attention_weights, 
-                                                  name='weighted_attention_window')
-
-        ones = constant(value=1, shape=(attention_span))
-        # weighted_attention_avg should be shape=(output_dim)
-        weighted_attention_avg = times(ones, stabilize(weighted_attention_window), output_rank=1, 
-                                       name='weighted_attention_avg')
-
-        return weighted_attention_avg
-
-    return augment_input_hook
 
 
 ########################
@@ -164,7 +71,7 @@ def LSTM_layer(input, output_dim, recurrence_hook_h=past_value, recurrence_hook_
         aux_input = augment_input_hook(input, dh)
         has_aux = True
        
-    LSTM_cell = LSTM(output_dim, has_aux=has_aux)
+    LSTM_cell = LSTM(output_dim, enable_self_stabilization=True, has_aux=has_aux)
     if has_aux:    
         f_x_h_c = LSTM_cell(input, (dh, dc), aux_input)
     else:
@@ -201,8 +108,9 @@ def create_model():
     input_sequence = raw_input
 
     # Drop the sentence start token from the label, for decoder training
-    label_sequence = sequence.slice(raw_labels, 1, 0, name='label_sequence') # <s> A B C </s> --> A B C </s>
-    label_sentence_start = sequence.first(raw_labels)   # <s>
+    label_sequence = sequence.slice(raw_labels, 1, 0, 
+                                    name='label_sequence') # <s> A B C </s> --> A B C </s>
+    label_sentence_start = sequence.first(raw_labels)      # <s>
 
     # Setup primer for decoder
     is_first_label = sequence.is_first(label_sequence)  # 1 0 0 0 ...
@@ -250,10 +158,8 @@ def create_model():
             decoder_output_h.output, hidden_dim, recurrence_hook_h, recurrence_hook_c, 
             augment_input_hook)
 
-    # Linear output layer
-    W = parameter(shape=(decoder_output_h.shape[0], label_vocab_dim), init=glorot_uniform())
-    B = parameter(shape=(label_vocab_dim), init=0)
-    z = plus(B, times(stabilize(decoder_output_h), W))    
+    # dense Linear output layer    
+    z = Dense(label_vocab_dim) (stabilize(decoder_output_h))    
     
     return z
 
@@ -263,7 +169,7 @@ def create_model():
 
 def train(distributed_trainer, train_reader, valid_reader, vocab, i2w, model, max_epochs):
     
-    # do some hooks that we won't need in the future
+    # do some hooks so that we can direct data to the right place
     label_sequence = find_nodes_by_name(model, 'label_sequence')[0]    
     decoder_history_hook = find_nodes_by_name(model, 'decoder_history_hook')[0]  
         
@@ -282,14 +188,13 @@ def train(distributed_trainer, train_reader, valid_reader, vocab, i2w, model, ma
     new_model = clone_and_hook()
 
     # Instantiate the trainer object to drive the model training
-    lr_per_sample = 0.007
-    lr_schedule = learning_rate_schedule(lr_per_sample, UnitType.sample)
+    lr_per_sample = learning_rate_schedule(0.007, UnitType.sample)
     minibatch_size = 72
     momentum_time_constant = momentum_as_time_constant_schedule(1100)
     clipping_threshold_per_sample = 2.3
     gradient_clipping_with_truncation = True
     learner = momentum_sgd(model.parameters,
-                           lr_schedule, momentum_time_constant,
+                           lr_per_sample, momentum_time_constant,
                            gradient_clipping_threshold_per_sample=clipping_threshold_per_sample,
                            gradient_clipping_with_truncation=gradient_clipping_with_truncation)
     trainer = Trainer(model, ce, errs, learner, distributed_trainer)
@@ -355,6 +260,63 @@ def train(distributed_trainer, train_reader, valid_reader, vocab, i2w, model, ma
                 print("Saved model to '%s'" % model_filename)
 
 ########################
+# write action         #
+########################
+
+def write(reader, model_filename, vocab, i2w):
+    
+    model = load_model(model_filename)
+    
+    binder = {
+        find_arg_by_name('raw_input' , model) : reader.streams.features,
+        find_arg_by_name('raw_labels', model) : reader.streams.labels
+    }
+
+    for i in range(1):
+        # get next minibatch of data
+        mb = reader.next_minibatch(1, input_map=binder)
+                
+        e = model.eval(mb)
+        print_sequences(e, i2w)
+
+#######################
+# test action         #
+#######################
+
+def test(reader, model_filename):
+
+    z = load_model(model_filename)
+    
+    # we use the test_minibatch() function so need to setup a trainer
+    label_sequence = sequence.slice(find_arg_by_name('raw_labels', z), 1, 0)
+    lr = learning_rate_schedule(0.007, UnitType.sample)
+    momentum = momentum_as_time_constant_schedule(1100)
+    ce = cross_entropy_with_softmax(z, label_sequence)
+    errs = classification_error(z, label_sequence)
+    trainer = Trainer(z, ce, errs, [momentum_sgd(z.parameters, lr, momentum)])
+
+    test_bind = {
+        find_arg_by_name('raw_input' ,z) : reader.streams.features,
+        find_arg_by_name('raw_labels',z) : reader.streams.labels
+    }
+
+    test_minibatch_size = 1024
+
+    # Get minibatches of sequences to test and perform testing
+    i = 0
+    total_error = 0.0
+    while True:
+        mb = reader.next_minibatch(test_minibatch_size, input_map=test_bind)
+        if mb is None: break
+        mb_error = trainer.test_minibatch(mb)
+        total_error += mb_error
+        i += 1
+
+    # and return the test error
+    return total_error/i
+
+
+########################
 # helper functions     #
 ########################
 
@@ -378,39 +340,40 @@ def find_arg_by_name(name, expression):
     return vars[0]
 
 
+###########################
+# automated test function #
+###########################
 
-def write(reader, model_filename, vocab, i2w):
+def seq2seq_automated_test():
     
-    model = load_model(model_filename)
+    # hook up data (train_reader gets False randomization to get consistent error)
+    train_reader = create_reader(os.path.join(data_dir, "cmudict-0.7b.train-dev-20-21.ctf"), False)
+    valid_reader = create_reader(os.path.join(data_dir, "tiny.ctf"), False)
+    test_reader  = create_reader(os.path.join(data_dir, "cmudict-0.7b.test.ctf"), False, FULL_DATA_SWEEP)
+    vocab, i2w = get_vocab(os.path.join(data_dir, "cmudict-0.7b.mapping"))
+
+    # create model
+    model = create_model()
     
-    binder = {
-        find_arg_by_name('raw_input' , model) : reader.streams.features,
-        find_arg_by_name('raw_labels', model) : reader.streams.labels
-    }
+    # train (with small numbers to finish in a reasonable amount of time)
+    train(train_reader, valid_reader, vocab, i2w, model, max_epochs=1, epoch_size=5000)
 
-    for i in range(1):
-        # get next minibatch of data
-        mb = reader.next_minibatch(1, input_map=binder)
-                
-        e = model.eval(mb)
-        print_sequences(e, i2w)
-
-           
-
-
-
+    # now test the model and print out test error (for automated test)
+    model_filename = os.path.join(model_dir, "model_epoch0.dnn")
+    error = test(test_reader, model_filename)
+    
+    return error    
+    
 #############################
 # main function boilerplate #
 #############################
     
 if __name__ == '__main__':
-
     set_default_device(best())
 
     # hook up data
     train_reader = create_reader(data_dir + "cmudict-0.7b.train-dev-1-21.ctf", True)
     valid_reader = create_reader(data_dir + "tiny.ctf", False)
-    
     vocab, i2w = get_vocab(data_dir + "cmudict-0.7b.mapping")
     
     parser = argparse.ArgumentParser()
