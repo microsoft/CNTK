d02f51fd330ea0546facf4eeca3e2859d5136bd6
Friedel van Megen <fmegen@microsoft.com>
2016-11-25 10:47:04 +0100

    cleanup model invocation sample code.

--- Tests/UnitTests/CntpGraphIrC/EvalMultithreads.cpp
+++ Tests/UnitTests/CntpGraphIrC/EvalMultithreads.cpp
@@ -4,134 +4,59 @@
 //
 // EvalMultithreads.cpp : Sample application shows how to evaluate a model in multiple threading environment. 
 //
-#include <functional>
-#include <thread>
-#include <iostream>
 #include "CNTKLibrary.h"
-#include "LSTM/LstmGraphNode.h"
 
 using namespace CNTK;
 
 
-bool GetVariableByName(std::vector<Variable> variableLists, std::wstring varName, Variable& var)
-{
-    for (auto it = variableLists.begin(); it != variableLists.end(); ++it)
-    {
-        if (it->Name().compare(varName) == 0)
-        {
-            var = *it;
-            return true;
-        }
-    }
-    return false;
-}
-
-inline bool GetInputVariableByName(FunctionPtr evalFunc, std::wstring varName, Variable& var)
-{
-    return GetVariableByName(evalFunc->Arguments(), varName, var);
-}
-
-inline bool GetOutputVaraiableByName(FunctionPtr evalFunc, std::wstring varName, Variable& var)
-{
-    return GetVariableByName(evalFunc->Outputs(), varName, var);
-}
-
 void EvaluateGraph(FunctionPtr evalFunc, const DeviceDescriptor& device)
 {
-    std::vector<std::wstring> inputNodeNames = { L"rawAnswer", L"rawContext", L"rawQuery"/*, L"contextSeqAxis", L"sourceSeqAxis"*/ };
+    fprintf(stderr, "input  count #%lu\n", (unsigned long)evalFunc->Arguments().size());
+    fprintf(stderr, "output count #%lu\n", (unsigned long)evalFunc->Outputs().size());
 
-    std::vector<Variable> inputVars;
-    for (auto inputNodeName : inputNodeNames)
+    // TODO remove once we get rid of random input data
+    srand(2);
+
+    // Prepare inputs
+    std::unordered_map<Variable, ValuePtr> inputs;
+    for (auto& input : evalFunc->Arguments())
     {
-        Variable inputVar;
+        // TODO: HERE is out INPUT VECTOR
+        std::vector<float> inputData(input.Shape().TotalSize());
 
-        if (!GetInputVariableByName(evalFunc, inputNodeName, inputVar))
+        // add some random data to the input vector
+        for (size_t i = 0; i < inputData.size(); ++i)
         {
-            fprintf(stderr, "Input variable %S is not available.\n", inputNodeName.c_str());
-            throw("Input variable not found error.");
+            inputData[i] = ((float)rand()) / RAND_MAX;
         }
 
-        inputVars.push_back(inputVar);
+        // Todo: add convenience APIs to simplify data preparation here.
+        ValuePtr value = MakeSharedObject<Value>(MakeSharedObject<NDArrayView>(input.Shape(), inputData, true /* isReadOnly */));
+
+        fprintf(stderr, "  input: %S %S (#%lu elements)\n", input.Name().c_str(), value->Shape().AsString().c_str(), inputData.size());
+        inputs[input] = value;
     }
 
-    // Evaluate the network in several runs 
-    size_t numSamples = 3;
-    size_t iterationCount = 4;
-    unsigned int randSeed = 2;
-    srand(randSeed);
-    for (size_t t = 0; t < iterationCount; ++t)
+    // Prepare outputs.
+    std::unordered_map<Variable, ValuePtr> outputs;
+    for (auto output : evalFunc->Outputs())
     {
-        printf("\n\n\n");
+        outputs[output] = nullptr; // actual value will be filled by evaluating the model.
+    }
 
-        std::unordered_map<Variable, ValuePtr> arguments;
+    // Compute outputs by evaluating the model
+    evalFunc->Forward(inputs, outputs, device);
+    fprintf(stderr, "\n");
 
-        for (auto inputVar : inputVars)
-        {
-            std::vector<float> inputData(inputVar.Shape().TotalSize() * numSamples);
+    // Show results by printing the outputs
+    for (auto& output : outputs)
+    {
+        ValuePtr value = output.second;
 
-            for (size_t i = 0; i < inputData.size(); ++i)
-            {
-                inputData[i] = ((float)rand()) / RAND_MAX;
-            }
+        // TODO: HERE is out OUTPUT VECTOR
+        // TODO: add convenience APIs to simplify data retrieval here.
+        std::vector<float> outputData(value->Data()->DataBuffer<float>(), value->Data()->DataBuffer<float>() + value->Data()->Shape().TotalSize());
 
-            // Create input data shape. Adding sequence length and numSamples as axes.
-            // Todo: remove sequence length when only numSamples is supported.
-            // Todo: add convenience APIs to simplify data preparation here.
-            NDShape inputShape = inputVar.Shape().AppendShape({ 1, numSamples });
-            ValuePtr inputValue = MakeSharedObject<Value>(MakeSharedObject<NDArrayView>(inputShape, inputData, true));
-
-            arguments[inputVar] = inputValue;
-        }
-
-        // Define output.
-        std::unordered_map<Variable, ValuePtr> outputs;
-
-        for (auto ov : evalFunc->Outputs())
-        {
-            ValuePtr outputValue;
-            Variable outputVar;
-
-            outputVar = ov;
-            outputs[ov] = outputValue;
-        }
-
-        // Evaluate the model
-        evalFunc->Forward(arguments, outputs, device);
-
-        ////for (auto outputTuple : outputs)
-        ////{
-        ////    // Get output value
-        ////    auto outputVar = outputTuple.first;
-        ////    auto outputValue = outputTuple.second;
-
-        ////    // Todo: remove sequence length when only numSamples is supported.
-        ////    // Todo: add convenience APIs to simplify retrieval of output results.
-        ////    NDShape outputShape = outputVar.Shape().AppendShape({ 1, numSamples });
-        ////    std::vector<float> outputData(outputShape.TotalSize());
-        ////    NDArrayViewPtr cpuArrayOutput = MakeSharedObject<NDArrayView>(outputShape, outputData, false);
-        ////    cpuArrayOutput->CopyFrom(*outputValue->Data());
-
-        ////    assert(outputData.size() == outputVar.Shape()[0] * numSamples);
-        ////    fprintf(stderr, "Evaluation result:\n");
-        ////    size_t dataIndex = 0;
-        ////    auto outputDim = outputVar.Shape()[0];
-        ////    for (size_t i = 0; i < numSamples; i++)
-        ////    {
-        ////        fprintf(stderr, "Iteration:%lu, Sample %lu:\n", t, i);
-        ////        fprintf(stderr, "    ");
-        ////        dataIndex = i * outputDim;
-        ////        for (size_t j = 0; j < std::min((size_t)10, outputDim); j++)
-        ////        {
-        ////            fprintf(stderr, "%f ", outputData[dataIndex++]);
-        ////        }
-        ////        if (outputDim > 10)
-        ////        {
-        ////            fprintf(stderr, "...");
-        ////        }
-        ////        fprintf(stderr, "\n");
-        ////    }
-        ////}
+        fprintf(stderr, "  output: %S %S (#%lu elements)\n", output.first.Name().c_str(), value->Shape().AsString().c_str(), outputData.size());
     }
 }
-
-
