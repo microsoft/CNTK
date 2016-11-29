5cb9dd510e20c11c1f36a8796848526500cb13a8
Zhou Wang <zhouwang@microsoft.com>
2016-11-29 11:41:36 +0100

    add CSEvalV2Library project, add file header

---
+++ bindings/csharp/CSEvalV2Library/CSEvalV2Library.cs
@@ -0,0 +1,20 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+// CSEvalV2Library -- C# Eval V2 Library
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNTK.CSharp
+{
+    public class Evaluation
+    {
+
+    }
+}
