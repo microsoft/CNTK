198e6ff4cd1d46a527b4d65898f1df0f6947fa86
Thilo Will <thilow@microsoft.com>
2016-12-09 14:38:44 +0100

    applied autopep8 to random_sample_test.py

--- bindings/python/cntk/ops/tests/random_sample_test.py
+++ bindings/python/cntk/ops/tests/random_sample_test.py
@@ -11,57 +11,69 @@ Unit tests random sampling related operations
 import numpy as np
 import pytest
 from .ops_test_utils import AA, precision
-from  cntk import random_sample_inclusion_frequency, random_sample, times
+from cntk import random_sample_inclusion_frequency, random_sample, times
 
 INCLUSION_FREQUENCY_TEST_CASES = [
     # drawing 1 sample
-    (np.full((4), 42.),                                                               1,     True,   np.full((4), 1/4),                                    0.0001, False),
+    (np.full((4), 42.), 1, True, np.full((4), 1 / 4), 0.0001, False),
 
     # drawing 13 samples
-    (np.full((4), 42.),                                                              13,     True,   np.full((4), 13/4),                                   0.0001, False),
+    (np.full((4), 42.), 13,
+     True,   np.full((4), 13 / 4), 0.0001, False),
 
     # drawing more samples than there are classes
-    ([1.,2.,3.],                                                                     42,     True,   [42/(1+2+3), 2*42/(1+2+3), 3*42/(1+2+3)],             0.0001, False),
+    ([1., 2., 3.], 42, True, [42 / (1 + 2 + 3), 2 * 42 / \
+                              (1 + 2 + 3), 3 * 42 / (1 + 2 + 3)], 0.0001, False),
 
-    # Use 300 weights where the first 200 hundred weights are high compared to the rest. Sample 200 without replacement. 
-    (np.concatenate((np.full((100),100),np.full((100),10),np.full((100),0.1))),    200,      False,  np.concatenate((np.full((200),1),np.full((100),0))),  0.1,   False),
-    
-    # Having more classes than samples is not allowed when sampling without replacment. Check if exception is thrown.
-    (np.full((4), 42.),                                                              50,     False,  np.full((4), 13/4),                                   0.0001, True), 
+    # Use 300 weights where the first 200 hundred weights are high compared to
+    # the rest. Sample 200 without replacement.
+    (np.concatenate((np.full((100), 100), np.full((100), 10), np.full((100), 0.1))), 200,
+     False, np.concatenate((np.full((200), 1), np.full((100), 0))),  0.1,   False),
+
+    # Having more classes than samples is not allowed when sampling without
+    # replacment. Check if exception is thrown.
+    (np.full((4), 42.), 50,
+     False,  np.full((4), 13 / 4), 0.0001, True),
 
     # Number of requested samples must be positive
-    ([1., 2., 3.],                                                                    0,     False,  np.full((4), 13/4),                                   0.0001, True), 
+    ([1., 2., 3.], 0, False,  np.full((4), 13 / 4), 0.0001, True),
 
     # Negative sampling weigts are not allowed.
-    ([1,-1.],                                                                         1,     True,   [0],                                                  0.0001, True), 
-    ([1,-1.],                                                                         1,     False,  [0],                                                  0.0001, True), 
+    ([1, -1.], 1,
+     True, [0], 0.0001, True),
+    ([1, -1.], 1, False, [0], 0.0001, True),
 ]
+
 
 @pytest.mark.parametrize("weights, num_samples, allow_duplicates, expected, tolerance, raises_exception", INCLUSION_FREQUENCY_TEST_CASES)
 def test_random_sample_inclusion_frequency(weights, num_samples, allow_duplicates, expected, tolerance, raises_exception, device_id, precision):
 
-    weights = AA(weights);
+    weights = AA(weights)
 
     if raises_exception:
         with pytest.raises(ValueError):
-            result = random_sample_inclusion_frequency(weights, num_samples, allow_duplicates)
+            result = random_sample_inclusion_frequency(
+                weights, num_samples, allow_duplicates)
             result.eval()
     else:
-        result = random_sample_inclusion_frequency(weights, num_samples, allow_duplicates)
+        result = random_sample_inclusion_frequency(
+            weights, num_samples, allow_duplicates)
         assert np.allclose(result.eval(), expected, atol=tolerance)
 
 RANDOM_SAMPLE_TEST_CASES_WITH_REPLACEMENT = [
     ([1., 3., 5., 1.],  1000, 0.03, False),
     ([1., -1.],  100, 0.0, True),
 ]
-@pytest.mark.parametrize("weights, num_samples,  tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES_WITH_REPLACEMENT)
-def test_random_sample_with_replacement(weights, num_samples,  tolerance, raises_exception, device_id, precision):
 
-    weights = AA(weights);
+
+@pytest.mark.parametrize("weights, num_samples,  tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES_WITH_REPLACEMENT)
+def test_random_sample_with_replacement(weights, num_samples, tolerance, raises_exception, device_id, precision):
+
+    weights = AA(weights)
     expected_relative_frequency = weights / np.sum(weights)
-    num_calls = 10;
+    num_calls = 10
     identity = np.identity(weights.size)
-    allow_duplicates = True # sample with replacement
+    allow_duplicates = True  # sample with replacement
 
     if raises_exception:
         with pytest.raises(ValueError):
@@ -73,22 +85,25 @@ def test_random_sample_with_replacement(weights, num_samples,  tolerance, raises
             result = random_sample(weights, num_samples, allow_duplicates)
             denseResult = times(result, identity)
             observed_frequency += np.sum(denseResult.eval(), 0)
-        observed_relative_frequency = observed_frequency / (num_calls * num_samples)
-        assert np.allclose(observed_relative_frequency, expected_relative_frequency, atol=tolerance)
+        observed_relative_frequency = observed_frequency / \
+            (num_calls * num_samples)
+        assert np.allclose(observed_relative_frequency,
+                           expected_relative_frequency, atol=tolerance)
 
-        
 
 RANDOM_SAMPLE_TEST_CASES_WITHOUT_REPLACEMENT = [
     ([1., 3, 50., 1., 0.], 4, (0.25, 0.25, 0.25, 0.25, 0), 0.0, False),
     ([1., -1.],  1, None,   0.0, True),
 ]
+
+
 @pytest.mark.parametrize("weights, num_samples, expected_relative_frequency, tolerance, raises_exception", RANDOM_SAMPLE_TEST_CASES_WITHOUT_REPLACEMENT)
 def test_random_sample_without_replacement(weights, num_samples, expected_relative_frequency, tolerance, raises_exception, device_id, precision):
 
-    weights = AA(weights);
-    num_calls = 1;
+    weights = AA(weights)
+    num_calls = 1
     identity = np.identity(weights.size)
-    allow_duplicates = False # sample without replacement
+    allow_duplicates = False  # sample without replacement
 
     if raises_exception:
         with pytest.raises(ValueError):
@@ -101,4 +116,5 @@ def test_random_sample_without_replacement(weights, num_samples, expected_relati
             denseResult = times(result, identity)
             observed_frequency += np.sum(denseResult.eval(), 0)
         observed_frequency /= (num_calls * num_samples)
-        assert np.allclose(observed_frequency, expected_relative_frequency, atol=tolerance)
+        assert np.allclose(observed_frequency,
+                           expected_relative_frequency, atol=tolerance)
