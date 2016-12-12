80f70b638dfc4a4c5d5567d889436556115c35dc
Mark Hillebrand <mahilleb@microsoft.com>
2016-12-12 11:50:07 +0100

    Bump version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -163,7 +163,7 @@ else:
     kwargs = dict(package_data = package_data)
 
 setup(name="cntk",
-      version="2.0.beta5.0",
+      version="2.0.beta6.0",
       url="http://cntk.ai",
       ext_modules=[cntk_module],
       packages=packages,
