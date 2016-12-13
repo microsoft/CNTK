8395549d18c0060b13257f258418f63a29395f96
Ivan Rodriguez <t-ivrodr@microsoft.com>
2016-12-13 10:33:47 +0100

    Fixing recurrent tests

--- bindings/python/cntk/ops/tests/recurrent_test.py
+++ bindings/python/cntk/ops/tests/recurrent_test.py
@@ -13,41 +13,48 @@ from __future__ import division
 import numpy as np
 import pytest
 from .ops_test_utils import _test_binary_op, AA, precision, PRECISION_TO_TYPE,\
-        unittest_helper, I, sanitize_dtype_cntk
+        unittest_helper, I, sanitize_dtype_cntk, cntk_device
 from .. import parameter
 
 SEQUENCES = [
     # (shape of batch (sample size, seq size, rows, cols), time step, initial state)
-    # ((1, 4, 3, 2), 1, 0.1),
-    ((2, 2, 4, 2), 1, 0.5),
-    # ((2, 2, 4, 2), 2, 0.3)
+    ((1, 4, 3, 2), 1, 0.1),
+    #((2, 2, 4, 2), 1, 0.5),
+    #((2, 2, 4, 2), 2, 0.3)
 ]
 
 @pytest.mark.parametrize("input_size, time_step, initial_state", SEQUENCES)
 def test_op_future_value(input_size, time_step, initial_state, device_id, precision):
     dt = PRECISION_TO_TYPE[precision]
-    total_elements = np.product(input_size)
 
-    x = np.arange(total_elements, dtype=dt).reshape(input_size)
-    elements_to_roll = total_elements - np.product(input_size[2:]) * time_step
-    x_rolled = np.roll(AA(x, dtype=dt), elements_to_roll)
-    np.put(x_rolled, range(elements_to_roll, total_elements), initial_state)
+    expected_forward_list = []
+    expected_backward_list = []
 
-    expected_forward = AA([x_rolled], dtype=dt)
+    num_of_seq = input_size[0]
+    total_elements_in_seq = np.product(input_size[1:])
+    x = np.arange(total_elements_in_seq * num_of_seq, dtype=dt).reshape(input_size)
+    elem_shape = input_size[2:]
+    for seq in x:
+        elements_to_roll = total_elements_in_seq - np.product(elem_shape) * time_step
+        x_rolled = np.roll(AA(seq, dtype=dt), elements_to_roll)
+        np.put(x_rolled, range(elements_to_roll, total_elements_in_seq), initial_state)
 
-    backward = np.ones_like(x_rolled, dtype=dt)
-    np.put(backward, range(total_elements - elements_to_roll), 0.0)
+        expected_forward_list.append(AA(x_rolled, dtype=dt))
+        backward = np.ones_like(x_rolled, dtype=dt)
+        np.put(backward, range(total_elements_in_seq - elements_to_roll), 0.0)
+        expected_backward_list.append(backward)
 
-    a = I(shape=x.shape[2:],
+    expected_forward = AA(expected_forward_list, dtype=dt)
+
+    a = I(shape=elem_shape,
       dtype=sanitize_dtype_cntk(precision),
       needs_gradient=True,
       name='a')
 
     expected_backward = {
-        a: [backward]
+        a: AA(expected_backward_list, dtype=dt)
     }
-
-    init = parameter(init=AA(initial_state, dtype=dt))
+    init = parameter(init=AA(initial_state, dtype=dt), device=cntk_device(device_id))
 
     from .. import future_value
     input_op_input = future_value(a, init, time_step)
@@ -62,16 +69,17 @@ def test_op_past_value(input_size, time_step, initial_state, device_id, precisio
     total_elements = np.product(input_size)
 
     elem_shape = input_size[2:]
+    
+    elements_to_roll = np.product(elem_shape) * time_step
     x = np.arange(total_elements, dtype=dt).reshape(input_size)
 
+    expected_forward = np.zeros_like(x, dtype=dt)
+    
     for seq_idx in range(input_size[0]):
-        import ipdb;ipdb.set_trace()
-        x[seq_idx] = np.roll(AA(x[seq_idx], dtype=dt), time_step, axis=0)
-        x[seq_idx,0:time_step] = initial_state
+        expected_forward[seq_idx] = np.roll(AA(x[seq_idx], dtype=dt), time_step, axis=0)
+        expected_forward[seq_idx,0:time_step] = initial_state
 
-    expected_forward = x
-
-    a = I(shape=x.shape[2:],
+    a = I(shape=elem_shape,
       dtype=sanitize_dtype_cntk(precision),
       needs_gradient=True,
       name='a')
@@ -83,7 +91,7 @@ def test_op_past_value(input_size, time_step, initial_state, device_id, precisio
         a: backward
     }
 
-    init = parameter(init=AA(initial_state, dtype=dt))
+    init = parameter(init=AA(initial_state, dtype=dt), device=cntk_device(device_id))
 
     from .. import past_value
     input_op_input = past_value(a, init, time_step)
