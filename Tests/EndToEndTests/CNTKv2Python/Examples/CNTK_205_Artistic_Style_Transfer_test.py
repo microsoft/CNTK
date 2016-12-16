ecaa6cf423eb6a218a63c43a6ce57512ca656ca7
Nikos Karampatziakis <nikosk@microsoft.com>
2016-12-15 18:47:02 -0800

    addressed most CR feedback

---
+++ Tests/EndToEndTests/CNTKv2Python/Examples/CNTK_205_Artistic_Style_Transfer_test.py
@@ -0,0 +1,24 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+import os
+import re
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "CNTK_205_Artistic_Style_Transfer.ipynb")
+
+def test_cntk_205_artistic_style_transfer_noErrors(nb):
+    errors = [output for cell in nb.cells if 'outputs' in cell
+              for output in cell['outputs'] if output.output_type == "error"]
+    assert errors == []
+
+expectedObjective = '7916146.0'
+
+def test_cntk_205_artistic_style_transfer_evalCorrect(nb):
+    testCell = [cell for cell in nb.cells
+                if cell.cell_type == 'code' and re.search('objfun(xstar', cell.source)]
+    assert len(testCell) == 1
+    assert testCell[0].outputs[0]['data']['text/plain'] == expectedEvalError
