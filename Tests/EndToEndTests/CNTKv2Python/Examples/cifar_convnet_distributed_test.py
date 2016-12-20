0d21aaad8a233bc6a212963eff6ef1c762cdfa71
missxa <missxa@github.com>
2016-12-20 08:43:00 -0800

    moved mpiexec call to a separate function in distributed ResNet and ConvNet tests

--- Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
@@ -23,65 +23,46 @@ from run_cifar_convnet_distributed import run_cifar_convnet_distributed
 TOLERANCE_ABSOLUTE = 2E-1
 TIMEOUT_SECONDS = 300
 
-def test_cifar_convnet_distributed_mpiexec(device_id):
+def mpiexec_test(device_id, train_and_test_script, params, expected_test_error):
     if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
+       pytest.skip('test only runs on GPU')
 
-    cmd = ["mpiexec", "-n", "2", "python", os.path.join(abs_path, "run_cifar_convnet_distributed.py")]
+    cmd = ["mpiexec", "-n", "2", "python", train_and_test_script] + params
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    if sys.version_info[0] < 3:
-        # TODO add timeout for Py2?
-        out = p.communicate()[0]
+
+    try:
+        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
+    except subprocess.TimeoutExpired:
+        os.kill(p.pid, signal.CTRL_C_EVENT)
+        raise RuntimeError('Timeout in mpiexec, possibly hang')
+
+    str_out = out.decode(sys.getdefaultencoding())
+    pdb.set_trace()
+    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
+
+    assert len(results) == 2
+ 
+    if "-b" not in params:
+        assert results[0] == results[1]
     else:
-        try:
-            out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-        except subprocess.TimeoutExpired:
-            os.kill(p.pid, signal.CTRL_C_EVENT)
-            raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    pdb.set_trace()
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert results[0] == results[1]
-    expected_test_error = 0.617
+        assert np.allclose(float(results[0])/100, float(results[1])/100,
+                       atol=TOLERANCE_ABSOLUTE)
+
     assert np.allclose(float(results[0])/100, expected_test_error,
                        atol=TOLERANCE_ABSOLUTE)
+
+def test_cifar_convnet_distributed_mpiexec(device_id):
+   
+    params = [ "-e", "2"] # run only 2 epochs
+    mpiexec_test(device_id, train_and_test_script, params, 0.617)
 
 def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
-    if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
+    
+    params = ["-q", "1", "-e", "2"] # 2 epochs with 1BitSGD
+    mpiexec_test(device_id, train_and_test_script, params, 0.617)
 
-    cmd = ["mpiexec", "-n", "2", "python", os.path.join(abs_path, "run_cifar_convnet_distributed.py")]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    try:
-        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-    except subprocess.TimeoutExpired:
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-        raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    pdb.set_trace()
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert results[0] == results[1]
-    expected_test_error = 0.617
-    assert np.allclose(float(results[0])/100, expected_test_error,
-                       atol=TOLERANCE_ABSOLUTE)
+def test_cifar_convnet_distributed_blockmomentum_mpiexec(device_id):
 
-def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
-    if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
-
-    cmd = ["mpiexec", "-n", "2", "python", os.path.join(abs_path, "run_cifar_convnet_distributed.py")]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    try:
-        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-    except subprocess.TimeoutExpired:
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-        raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert results[0] == results[1]
-    expected_test_error = 0.617
-    assert np.allclose(float(results[0])/100, expected_test_error,
-                       atol=TOLERANCE_ABSOLUTE)
+    params = ["-b", "32000", "-e", "2"] # 2 epochs with BlockMomentum SGD using blocksize 32000
+    mpiexec_test(device_id, train_and_test_script, params, 0.6457)
+    
--- Tests/EndToEndTests/CNTKv2Python/Examples/cifar_resnet_distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/cifar_resnet_distributed_test.py
@@ -16,46 +16,30 @@ import pytest
 import subprocess
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "Classification", "ResNet", "Python"))
-from TrainResNet_CIFAR10_Distributed import train_and_evaluate, create_reader
+sys.path.append(abs_path)
+from cifar_convnet_distributed_test import mpiexec_test
+
+train_and_test_script = os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "Classification", "ResNet", "Python", "TrainResNet_CIFAR10_Distributed.py")
 
 TOLERANCE_ABSOLUTE = 2E-1
+TIMEOUT_SECONDS = 300
 
-def test_cifar_resnet_distributed_error(device_id, is_1bit_sgd):
-    if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
-    set_default_device(cntk_device(device_id))
+def test_cifar_convnet_distributed_mpiexec(device_id):
+   
+    params = [ "-e", "2"] # run only 2 epochs
+    mpiexec_test(device_id, train_and_test_script, params, 0.5946)
 
-    if not is_1bit_sgd:
-        pytest.skip('test only runs in 1-bit SGD')
+def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
+    
+    params = ["-q", "1", "-e", "2"] # 2 epochs with 1BitSGD
+    mpiexec_test(device_id, train_and_test_script, params, 0.5946)
 
-    try:
-        base_path = os.path.join(os.environ['CNTK_EXTERNAL_TESTDATA_SOURCE_DIRECTORY'],
-                                *"Image/CIFAR/v0/cifar-10-batches-py".split("/"))
-    except KeyError:
-        base_path = os.path.join(
-            *"../../../../Examples/Image/DataSets/CIFAR-10".split("/"))
 
-    base_path = os.path.normpath(base_path)
-    os.chdir(os.path.join(base_path, '..'))
+def test_cifar_convnet_distributed_blockmomentum_mpiexec(device_id):
 
-    from _cntk_py import set_computation_network_trace_level, set_fixed_random_seed, force_deterministic_algorithms
-    set_computation_network_trace_level(1) 
-    set_fixed_random_seed(1)  # BUGBUG: has no effect at present  # TODO: remove debugging facilities once this all works
-    #force_deterministic_algorithms()
-    # TODO: do the above; they lead to slightly different results, so not doing it for now
-
-    distributed_learner_factory = lambda learner: distributed.data_parallel_distributed_learner(
-        learner=learner,
-        num_quantization_bits=32,
-        distributed_after=0)
-
-    reader_train_factory = lambda data_size: create_reader(os.path.join(base_path, 'train_map.txt'), os.path.join(base_path, 'CIFAR-10_mean.xml'), True, data_size)
-    test_reader = create_reader(os.path.join(base_path, 'test_map.txt'), os.path.join(base_path, 'CIFAR-10_mean.xml'), False, FULL_DATA_SWEEP)
-
-    test_error = train_and_evaluate(reader_train_factory, test_reader, 'resnet20', 5, distributed_learner_factory)
-
-    expected_test_error = 0.282
+    params = ["-b", "32000", "-e", "2"] # 2 epochs with BlockMomentum SGD using blocksize 32000
+    mpiexec_test(device_id, train_and_test_script, params, 0.55)
+>>>>>>> moved mpiexec call to a separate function in distributed ResNet and ConvNet tests
 
     assert np.allclose(test_error, expected_test_error,
                        atol=TOLERANCE_ABSOLUTE)
