26b67a8b601d0a095604a9d21dcfbba0f1d6642a
missxa <missxa@github.com>
2016-12-20 08:43:00 -0800

    moved mpiexec call to a separate function in distributed ResNet and ConvNet tests

--- Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
@@ -22,61 +22,46 @@ train_and_test_script = os.path.join(abs_path, "..", "..", "..", "..", "Examples
 TOLERANCE_ABSOLUTE = 2E-1
 TIMEOUT_SECONDS = 300
 
+def mpiexec_test(device_id, train_and_test_script, params, expected_test_error):
+    if cntk_device(device_id).type() != DeviceKind_GPU:
+       pytest.skip('test only runs on GPU')
+
+    cmd = ["mpiexec", "-n", "2", "python", train_and_test_script] + params
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    try:
+        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
+    except subprocess.TimeoutExpired:
+        os.kill(p.pid, signal.CTRL_C_EVENT)
+        raise RuntimeError('Timeout in mpiexec, possibly hang')
+
+    str_out = out.decode(sys.getdefaultencoding())
+    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
+
+    assert len(results) == 2
+ 
+    if "-b" not in params:
+        assert results[0] == results[1]
+    else:
+        assert np.allclose(float(results[0])/100, float(results[1])/100,
+                       atol=TOLERANCE_ABSOLUTE)
+
+    assert np.allclose(float(results[0])/100, expected_test_error,
+                       atol=TOLERANCE_ABSOLUTE)
+
 def test_cifar_convnet_distributed_mpiexec(device_id):
    
-    skip_if_cpu(device_id)
-    cmd = ["mpiexec", "-n", "2", "python", train_and_test_script, "-e", "2"]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    try:
-        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-    except subprocess.TimeoutExpired:
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-        raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert results[0] == results[1]
-    expected_test_error = 0.617
-    assert np.allclose(float(results[0])/100, expected_test_error,
-                       atol=TOLERANCE_ABSOLUTE)
+    params = [ "-e", "2"] # run only 2 epochs
+    mpiexec_test(device_id, train_and_test_script, params, 0.617)
 
 def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
-    skip_if_cpu(device_id)
+    
+    params = ["-q", "1", "-e", "2"] # 2 epochs with 1BitSGD
+    mpiexec_test(device_id, train_and_test_script, params, 0.617)
 
-    cmd = ["mpiexec", "-n", "2", "python", train_and_test_script, "-q", "1", "-e", "2"]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    try:
-        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-    except subprocess.TimeoutExpired:
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-        raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert results[0] == results[1]
-    expected_test_error = 0.617
-    assert np.allclose(float(results[0])/100, expected_test_error,
-                       atol=TOLERANCE_ABSOLUTE)
 
 def test_cifar_convnet_distributed_blockmomentum_mpiexec(device_id):
-    skip_if_cpu(device_id)
 
-    cmd = ["mpiexec", "-n", "2", "python", train_and_test_script, "-b", "32000", "-e", "2"]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    try:
-        out = p.communicate(timeout=TIMEOUT_SECONDS)[0]  # in case we have a hang
-    except subprocess.TimeoutExpired:
-        os.kill(p.pid, signal.CTRL_C_EVENT)
-        raise RuntimeError('Timeout in mpiexec, possibly hang')
-    str_out = out.decode(sys.getdefaultencoding())
-    results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
-    assert len(results) == 2
-    assert np.allclose(float(results[0])/100, float(results[1])/100,
-                       atol=TOLERANCE_ABSOLUTE)
-    expected_test_error = 0.6457
-    assert np.allclose(float(results[0])/100, expected_test_error,
-                       atol=TOLERANCE_ABSOLUTE)
-
-def skip_if_cpu(device_id):
-    if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
+    params = ["-b", "32000", "-e", "2"] # 2 epochs with BlockMomentum SGD using blocksize 32000
+    mpiexec_test(device_id, train_and_test_script, params, 0.6457)
+    
