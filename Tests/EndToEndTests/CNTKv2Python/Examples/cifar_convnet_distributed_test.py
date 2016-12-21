59a815cbc1c04586c1685addcfb5f869a59cfb10
Alona Kharchenko <t-alkhar@microsoft.com>
2016-12-21 16:39:42 +0100

    allow error difference tolerance for workers when using BlockMomentum SGD in tests

--- Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/cifar_convnet_distributed_test.py
@@ -14,7 +14,6 @@ import pytest
 from cntk.utils import cntk_device
 from cntk.cntk_py import DeviceKind_GPU
 from cntk.device import set_default_device
-import pdb
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(abs_path)
@@ -23,9 +22,9 @@ train_and_test_script = os.path.join(abs_path, "..", "..", "..", "..", "Examples
 TOLERANCE_ABSOLUTE = 2E-1
 TIMEOUT_SECONDS = 300
 
-def mpiexec_test(device_id, train_and_test_script, params, expected_test_error):
+def mpiexec_test(device_id, train_and_test_script, params, expected_test_error, allowTolerance):
     if cntk_device(device_id).type() != DeviceKind_GPU:
-        pytest.skip('test only runs on GPU')
+       pytest.skip('test only runs on GPU')
 
     cmd = ["mpiexec", "-n", "2", "python", train_and_test_script] + params
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -37,13 +36,12 @@ def mpiexec_test(device_id, train_and_test_script, params, expected_test_error):
         raise RuntimeError('Timeout in mpiexec, possibly hang')
 
     str_out = out.decode(sys.getdefaultencoding())
-    pdb.set_trace()
     results = re.findall("Final Results: Minibatch\[.+?\]: errs = (.+?)%", str_out)
 
     assert len(results) == 2
  
-    if "-b" not in params:
-    assert results[0] == results[1]
+    if not allowTolerance:
+        assert results[0] == results[1]
     else:
         assert np.allclose(float(results[0])/100, float(results[1])/100,
                        atol=TOLERANCE_ABSOLUTE)
@@ -54,16 +52,16 @@ def mpiexec_test(device_id, train_and_test_script, params, expected_test_error):
 def test_cifar_convnet_distributed_mpiexec(device_id):
    
     params = [ "-e", "2"] # run only 2 epochs
-    mpiexec_test(device_id, train_and_test_script, params, 0.617)
+    mpiexec_test(device_id, train_and_test_script, params, 0.617, False)
 
 def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
     
     params = ["-q", "1", "-e", "2"] # 2 epochs with 1BitSGD
-    mpiexec_test(device_id, train_and_test_script, params, 0.617)
+    mpiexec_test(device_id, train_and_test_script, params, 0.617, False)
 
 
 def test_cifar_convnet_distributed_blockmomentum_mpiexec(device_id):
 
     params = ["-b", "32000", "-e", "2"] # 2 epochs with BlockMomentum SGD using blocksize 32000
-    mpiexec_test(device_id, train_and_test_script, params, 0.6457)
+    mpiexec_test(device_id, train_and_test_script, params, 0.6457, True)
     
--- Tests/EndToEndTests/CNTKv2Python/Examples/cifar_resnet_distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/cifar_resnet_distributed_test.py
@@ -25,18 +25,18 @@ TOLERANCE_ABSOLUTE = 2E-1
 TIMEOUT_SECONDS = 300
 
 def test_cifar_convnet_distributed_mpiexec(device_id):
-
+   
     params = [ "-e", "2"] # run only 2 epochs
-    mpiexec_test(device_id, train_and_test_script, params, 0.5946)
+    mpiexec_test(device_id, train_and_test_script, params, 0.5946, False)
 
 def test_cifar_convnet_distributed_1bitsgd_mpiexec(device_id):
-
+    
     params = ["-q", "1", "-e", "2"] # 2 epochs with 1BitSGD
-    mpiexec_test(device_id, train_and_test_script, params, 0.5946)
+    mpiexec_test(device_id, train_and_test_script, params, 0.5946, False)
 
 
 def test_cifar_convnet_distributed_blockmomentum_mpiexec(device_id):
 
     params = ["-b", "32000", "-e", "2"] # 2 epochs with BlockMomentum SGD using blocksize 32000
-    mpiexec_test(device_id, train_and_test_script, params, 0.55)
+    mpiexec_test(device_id, train_and_test_script, params, 0.55, True)
 
