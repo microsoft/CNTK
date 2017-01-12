042f05f8ba8554b95df17a9bcfcfe1dbd41be226
Ratan Rai Sur <ratsur@microsoft.com>
2017-01-12 16:24:35 -0500

    Java Bindings

--- Source/CNTKv2LibraryDll/API/CNTKLibrary.h
+++ Source/CNTKv2LibraryDll/API/CNTKLibrary.h
@@ -286,6 +286,10 @@ namespace CNTK
         
         /// Resets static properties, needed for unit-tests.
         CNTK_API static void Reset();
+#ifdef SWIGJAVA
+    public:
+        DeviceDescriptor() {UseDefaultDevice();};
+#endif
 
     private:
         unsigned int m_deviceId;
@@ -1843,7 +1847,7 @@ private:
         Variable NonCompositePreservingCopy() const;
 
     private:
-#ifdef SWIGCSHARP
+#if defined(SWIGCSHARP) || defined(SWIGJAVA)
     public:
         // TODO: a better way to get hash value?
         size_t GetHashValue()
@@ -2749,7 +2753,8 @@ namespace CNTK
         template <typename ValueType, typename DestType>
         void CopyVariableValueToImpl(const Variable& outputVariable, std::vector<std::vector<DestType>>& sequences);
 
-        virtual std::pair<size_t, size_t> GetSequenceAndBatchLength(const Variable& outputVariable);
+    private:
+        CNTK_API std::pair<size_t, size_t> GetSequenceAndBatchLength(const Variable& outputVariable);
 
         CNTK_API static void GetSequenceStartsAndLengths(const NDMaskPtr& mask, std::vector<ptrdiff_t>& sequenceBeginIndices, std::vector<size_t>& sequenceLengths, size_t numDynamicAxes);
 
