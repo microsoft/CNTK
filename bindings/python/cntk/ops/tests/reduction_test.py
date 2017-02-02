correct masking of sequences; tests

bindings/python/cntk/ops/tests/reduction_test.py
@@ -146,7 +146,26 @@ def test_op_reduce_log_sum(input_data, axis, device_id, precision):
     from .. import reduce_log_sum
     _test_unary_op(precision, device_id, reduce_log_sum, input_data,
                    expected_forward, expected_backward, {'axis': axis})
+                   
+@pytest.mark.parametrize("input_data, axis", REDUCE_TEST_OPERANDS)
+def test_op_reduce_prod(input_data, axis, device_id, precision):
+    dt = PRECISION_TO_TYPE[precision]
 
+    data = AA(input_data, dtype=dt)
+
+    p = np.prod(data, axis=(axis), keepdims=True)
+    expected_forward = [[p]]
+
+    backward = p / data
+
+    expected_backward = {
+        'arg': [[backward]]
+    }
+
+    from .. import reduce_prod
+    _test_unary_op(precision, device_id, reduce_prod, input_data,
+                   expected_forward, expected_backward, {'axis': axis})
+                   
 @pytest.mark.parametrize("input_data, axis", REDUCE_TEST_OPERANDS)
 def test_op_reduce_all(input_data, axis, device_id, precision):
     # FIXME: we'd like to do dt = PRECISION_TO_TYPE[precision]
@@ -161,32 +180,35 @@ def test_op_reduce_all(input_data, axis, device_id, precision):
           needs_gradient=True,
           name='a')
     # create batch
-    value = AA([[data,data+0.1],[data+0.2,data+0.3]], dtype=dt)
-    from .. import reduce_sum, reduce_max, reduce_min, reduce_mean, reduce_log_sum
+    value = [AA([data,data-0.5], dtype=dt),AA([data+0.25], dtype=dt)]
+    from .. import reduce_sum, reduce_max, reduce_min, reduce_mean, reduce_log_sum, reduce_prod
     from cntk import Axis
-    def max_bwd(x):
-        out = np.zeros_like(x)
-        out[np.unravel_index(np.argmax(x),x.shape)] = 1
-        return out
-    def min_bwd(x):
-        out = np.zeros_like(x)
-        out[np.unravel_index(np.argmin(x),x.shape)] = 1
-        return out
-    ops = [ (reduce_sum,     lambda x:AA(np.sum(x)),                 lambda x:np.ones_like(x)),
-            (reduce_max,     lambda x:AA(np.max(x)),                 lambda x:max_bwd(x)),
-            (reduce_min,     lambda x:AA(np.min(x)),                 lambda x:min_bwd(x)),
-            (reduce_mean,    lambda x:AA(np.mean(x)),                lambda x:np.ones_like(x) / x.size),
-            (reduce_log_sum, lambda x:AA(np.log(np.sum(np.exp(x)))), lambda x:np.exp(value)/np.sum(np.exp(x)))]
+    def max_bwd(x,f):
+        y = np.zeros_like(x)
+        yr = y.ravel()
+        xr = x.ravel()
+        for i in range(x.size):
+            if xr[i] == f: yr[i] = 1
+        return y
+
+    ops = [ (reduce_sum,     lambda x:AA(sum(np.sum(xi) for xi in x)),                           lambda x,f:[np.ones_like(xi) for xi in x]),
+            (reduce_max,     lambda x:AA(max(np.max(xi) for xi in x)),                           lambda x,f:[max_bwd(xi,f) for xi in x]),
+            (reduce_min,     lambda x:AA(min(np.min(xi) for xi in x)),                           lambda x,f:[max_bwd(xi,f) for xi in x]),
+            (reduce_mean,    lambda x:AA(sum(np.sum(xi) for xi in x)/sum(xi.size  for xi in x)), lambda x,f:[np.ones_like(xi)/sum(xj.size for xj in x) for xi in x]),
+            (reduce_log_sum, lambda x:AA(np.log(sum(np.sum(np.exp(xi)) for xi in x))),           lambda x,f:[np.exp(xi-f)     for xi in x]),
+            (reduce_prod,    lambda x:AA(np.prod([np.prod(xi) for xi in x])),                    lambda x,f:[f/xi             for xi in x])
+            ]
     
     for op,fwd,bwd in ops:
         input_op = op(a, axis=Axis.all_axes())
         expected_forward = fwd(value)
-        expected_backward = bwd(value)
+        expected_backward = bwd(value,expected_forward)
         binding = {a: value}
         actual_backward = input_op.grad(binding)[0]
         actual_forward  = np.copy(input_op.eval(binding))
         assert np.allclose(actual_forward, expected_forward)
-        assert np.allclose(actual_backward, expected_backward)
+        for ab,eb in zip (actual_backward, expected_backward):
+            assert np.allclose(ab, eb)
 
 @pytest.mark.parametrize("input_data, axis", REDUCE_TEST_OPERANDS)
 def test_op_reduce_mean_all_constant(input_data, axis, device_id, precision):