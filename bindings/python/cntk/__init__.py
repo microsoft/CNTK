b7e340a7b31f383c603eda16ba5beb03dd7f956c
Mark Hillebrand <mahilleb@microsoft.com>
2017-02-22 13:46:39 +0100

    Source/CNTK/CNTK.cpp, bindings/python/cntk/: CNTK 2.0.beta12.0 exactly

--- bindings/python/cntk/__init__.py
+++ bindings/python/cntk/__init__.py
@@ -3,7 +3,7 @@
 # for full license information.
 # ==============================================================================
 
-__version__ = '2.0.beta12.0+'
+__version__ = '2.0.beta12.0'
 
 import os
 import numpy as np
