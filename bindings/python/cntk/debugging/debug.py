3348155ff9c3572ddec3f8e72dca6401e53e71c5
Willi Richert <wilrich@microsoft.com>
2017-02-24 12:33:04 +0100

    Pass shared state instead of class variable

--- bindings/python/cntk/debugging/debug.py
+++ bindings/python/cntk/debugging/debug.py
@@ -5,12 +5,35 @@
 
 import sys
 import numpy as np
+from collections import defaultdict
 
 from cntk import cntk_py
 
 from cntk import output_variable, Constant, Parameter, CloneMethod
 
 from cntk.ops.functions import Function, UserFunction
+
+DEBUG_USAGE = '''\
+    Commands:
+        n - execute the next node
+        n <number> - execute the next <number> nodes
+        n f - run until forward pass (like 'n' when already in forward pass)
+        n b - run until backward pass (like 'n' when already in backward pass)
+
+        u name - execute until a node with that name is hit
+        u <lambda> - execute until the lambda expression is True. Examples:
+                     Until a Times node is hit:
+                         lambda arg, node: node.op_name == 'Times'
+                     Until a node is hit that has 3 dimensions:
+                         lambda arg, node: len(node.shape) == 3
+                     Until the variance of the input exceeds 1 (np = numpy):
+                         lambda arg, node: np.var(arg) > 1
+
+        c - run until end
+        p - print input (forward) or root gradients (backward)
+        d - drop into a pdb shell
+        q - quit\
+'''
 
 __doc__ = '''
 In order to debug a graph one simply needs to wrap the root node as follows::
@@ -26,22 +49,7 @@ following command-line interface::
 
     Forward after Parameter node with uid='Parameter28' shape=[](2,)
     [CNTK forward] >>> help
-    Commands:
-        n - execute the next node
-        n <number> - execute the next <number> nodes
-        n <lambda> - execute until the lambda expression is True. Examples:
-                     Until a Times node is hit:
-                         lambda arg, node: node.op_name == 'Times'
-                     Until a node is hit that has 3 dimensions:
-                         lambda arg, node: len(node.shape) == 3
-                     Until the variance of the input exceeds 1 (np = numpy):
-                         lambda arg, node: np.var(arg) > 1
-        f - run until forward pass (like 'n' when already in forward pass)
-        b - run until backward pass (like 'n' when already in backward pass)
-        c - run until end
-        p - print input (forward) or root gradients (backward)
-        d - drop into a pdb shell
-        q - quit
+    %s
 
     [CNTK backward] >>> n
 
@@ -83,7 +91,7 @@ At every stop the following information is given:
  * shape having the format [dynamic axis](static axes). E.g. ``[*,*](2,)``
    means that the node's output has two dynamic axes (batch and sequence) and
    one static axis (2 dimensions)
-'''
+''' % DEBUG_USAGE
 
 
 def save_as_legacy_model(root_op, filename):
@@ -97,6 +105,14 @@ def save_as_legacy_model(root_op, filename):
     '''
     cntk_py.save_as_legacy_model(root_op, filename)
 
+class _DebugState(object):
+    def __init__(self, all_nodes):
+        self.commands = []
+        self.last_pass = 'f'
+        self.all_nodes = all_nodes
+        self.name_to_node = defaultdict(lambda:[])
+        for n in self.all_nodes:
+            self.name_to_node[n.name].append(n)
 
 def set_computation_network_track_gap_nans(enable):
     '''
@@ -131,9 +147,11 @@ class DebugNode(UserFunction):
     _commands = []
     _last_pass = 'f'
 
-    def __init__(self, arg, name='DebugNode'):
+    def __init__(self, arg, debug_state, name='DebugNode'):
+        name += '_after_%s' % arg.uid
         super(DebugNode, self).__init__([arg], as_numpy=True, name=name)
         self.after = arg
+        self.debug_state = debug_state
 
     def __wait_for_input(self, prompt):
         understood = False
@@ -145,40 +163,46 @@ class DebugNode(UserFunction):
             if len(new_input) == 1 and new_input in 'bcdfp':
                 understood = [new_input]
             elif new_input[0] == 'n':
-                try:
-                    if len(new_input) > 1:
-                        number = int(new_input[1:])
-                    else:
-                        number = 1
-                    understood = ['n'] * number
-                except ValueError:
+                if len(new_input) > 1:
+                    remainder = new_input[1:]
                     try:
-                        code = eval(new_input[1:])
+                        number = int(remainder)
+                        understood = ['n'] * number
+                    except ValueError:
+                        pass
+
+                    if not understood:
+                        if "backward".startswith(remainder):
+                            understood = ['nb']
+                        elif "forward".startswith(remainder):
+                            understood = ['nf']
+
+                else:
+                    understood = ['n']
+
+            elif new_input[0] == 'u':
+                try:
+                    what = new_input[1:].strip()
+                    if what.startswith('lambda'):
+                        code = eval(what)
                         understood = [code]
-                    except SyntaxError:
-                        understood = False
+                    else:
+                        if what in self.debug_state.name_to_node:
+                            def code(arg, n):
+                                return n.name == what
+                            understood = [code]
+                        else:
+                            print('Your model does not contain a node with '
+                                  'name "%s"' % what)
+
+                except SyntaxError:
+                    understood = False
+
             elif new_input == 'q':
                 sys.exit(0)
 
             if not understood:
-                print('''\
-Commands:
-    n - execute the next node
-    n <number> - execute the next <number> nodes
-    n <lambda> - execute until the lambda expression is True. Examples:
-                 Until a Times node is hit:
-                     lambda arg, node: node.op_name == 'Times'
-                 Until a node is hit that has 3 dimensions:
-                     lambda arg, node: len(node.shape) == 3
-                 Until the variance of the input exceeds 1 (np = numpy):
-                     lambda arg, node: np.var(arg) > 1
-    f - run until forward pass (like 'n' when already in forward pass)
-    b - run until backward pass (like 'n' when already in backward pass)
-    c - run until end
-    p - print input (forward) or root gradients (backward)
-    d - drop into a pdb shell
-    q - quit
-''')
+                print(DEBUG_USAGE)
 
         return understood
 
@@ -223,41 +247,43 @@ Commands:
     def forward(self, argument, device=None, outputs_to_retain=None):
         self._print_status('f')
 
-        commands = DebugNode._commands
+        commands = self.debug_state.commands
 
         done = False
         while not done:
             if not commands:
-                DebugNode._commands = commands = self.__wait_for_input(
+                self.debug_state.commands = self.__wait_for_input(
                     '[CNTK forward] >>> ')
 
-            if commands[-1] == 'c':
+            commands = self.debug_state.commands
+
+            next_command = commands[-1]
+            if next_command == 'c':
                 done = True
 
-            elif commands[-1] == 'n':
-                commands.pop()
-                done = True
+            elif isinstance(next_command, str) and next_command.startswith('n'):
+                if len(next_command) == 1:
+                    commands.pop()
+                    done = True
+                elif next_command == "nf":
+                    commands.pop()
+                    done = True
+                elif next_command == "nb":
+                    done = True
 
-            elif commands[-1] == 'b':
-                done = True
-
-            elif commands[-1] == 'f':
-                commands.pop()
-                done = True
-
-            elif commands[-1] == 'p':
+            elif next_command == 'p':
                 print('Input: ')
                 print(argument)
                 commands.pop()
 
-            elif commands[-1] == 'd':
+            elif next_command == 'd':
                 commands.pop()
                 import pdb
                 pdb.set_trace()
                 done = True
 
-            elif callable(commands[-1]):
-                if commands[-1](argument, self.after):
+            elif callable(next_command):
+                if next_command(argument, self.after):
                     commands.pop()
                 else:
                     done = True
@@ -270,44 +296,44 @@ Commands:
         self._print_status('b')
 
         done = False
-        commands = DebugNode._commands
         while not done:
-            if not commands:
-                DebugNode._commands = commands = self.__wait_for_input(
+            if not self.debug_state.commands:
+                self.debug_state.commands = self.__wait_for_input(
                     '[CNTK backward] >>> ')
 
-            if commands[-1] == 'c':
+            commands = self.debug_state.commands
+            next_command = commands[-1]
+            if next_command == 'c':
                 done = True
 
-            elif commands[-1] == 'n':
-                commands.pop()
-                done = True
+            elif isinstance(next_command, str) and next_command.startswith('n'):
+                if len(next_command) == 1:
+                    commands.pop()
+                    done = True
+                elif next_command == "nf":
+                    done = True
+                elif next_command == "nb":
+                    commands.pop()
+                    done = True
 
-            elif commands[-1] == 'b':
-                commands.pop()
-                done = True
-
-            elif commands[-1] == 'f':
-                done = True
-
-            elif commands[-1] == 'p':
+            elif next_command == 'p':
                 print('State: %s' % str(state))
                 print('Root gradients: ')
                 print(root_gradients)
                 commands.pop()
 
-            elif commands[-1] == 'd':
+            elif next_command == 'd':
                 import pdb
                 pdb.set_trace()
                 done = True
 
-            elif callable(commands[-1]):
-                if commands[-1](root_gradients, self.after):
+            elif callable(next_command):
+                if next_command(root_gradients, self.after):
                     commands.pop()
                 else:
                     done = True
 
-        DebugNode._last_pass = 'b'
+        self.debug_state.last_pass = 'b'
 
         return root_gradients
 
@@ -324,7 +350,8 @@ def debug_model(model):
     from cntk.graph import depth_first_search
     nodes = depth_first_search(model, lambda x: True)
 
-    mod = {n: DebugNode(n) for n in nodes}
+    dbg_state = _DebugState(nodes)
+    mod = {n: DebugNode(n, dbg_state) for n in nodes}
     model = model.clone(CloneMethod.share, mod)
 
     return model
