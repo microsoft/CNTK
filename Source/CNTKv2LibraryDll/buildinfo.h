b4ec1a2e7fb111a5ef542583cd146fdb8f532bc0
Otto Lu <llw33333@gmail.com>
2017-03-01 19:40:04 +0800

    Update zipfacefilereader

---
+++ Source/CNTKv2LibraryDll/buildinfo.h
@@ -0,0 +1,16 @@
+#ifndef _BUILDINFO_H 
+#define _BUILDINFO_H 
+#define _GIT_EXIST 
+#define _BUILDBRANCH_  "v-lelu/facereader-test"      
+#define _BUILDSHA1_    "1403e82232e8e1ae390f71eeedc2dc342fd064ac (modified)"
+#define _MATHLIB_ "mkl"
+#define _BUILDER_ "v-lelu"     
+#define _BUILDMACHINE_ "7QMPBD2"    
+#define _BUILDPATH_    "D:\\CNTK-contribution\\CNTK\\Source\\CNTKv2LibraryDll\\"     
+#define _BUILDTYPE_ "Release"
+#define _BUILDTARGET_ "CPU-only"
+#define _WITH_1BITSGD_ "no"
+#define _WITH_ASGD_ "yes"
+#define _MPI_NAME_ "Microsoft MPI" 
+#define _MPI_VERSION_ "7.0.12437.6" 
+#endif 
