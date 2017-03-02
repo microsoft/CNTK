e9ddb817f3f7f6afc4650ef7cf4862aaccbe0d6e
Chris Basoglu <cbasoglu@microsoft.com>
2017-03-02 10:10:41 -0800

    Add log message back

--- bindings/python/cntk/utils/progress_print.py
+++ bindings/python/cntk/utils/progress_print.py
@@ -116,6 +116,15 @@ class ProgressPrinter(cntk_py.ProgressWriter):
         if msg != "" and self.log_to_file is not None:
             self.___logprint(msg)
 
+    def log(self, message):
+        '''
+        Prints any message the user wishes to place in the log.
+
+        Args:
+            msg (`string`): message to print.
+        '''
+        self.___logprint(message)
+
     def avg_loss_since_start(self):
         '''
         DEPRECATED.
