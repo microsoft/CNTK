9aafd4be0a7b39325b6bd5d560a190c74882b521
Nikos Karampatziakis <nikosk@microsoft.com>
2017-03-02 05:02:01 -0800

    added half-embedding

--- Examples/Text/BidirectionalAttentionFlow/bidaf.py
+++ Examples/Text/BidirectionalAttentionFlow/bidaf.py
@@ -2,24 +2,43 @@ import cntk as C
 import numpy as np
 from cntk import layers, blocks, models
 from helpers import PastValueWindow, seqlogZ
+import pickle
+
+with open('vocabs.pkl', 'rb') as vf:
+    known, vocab, chars = pickle.load(vf)
+
+word_size = 20
+w_dim = len(vocab)
+c_dim = len(chars)*word_size
+a_dim = 1
+dim = 100
+convs = 100
+rf = 5
 
 C.set_default_device(C.cpu())
 
-c_dim = 20*102
-w_dim = 3371
-a_dim = 1
-glove_dim = 100
-convs = 100
-dim = 100
+# todo are the char cnns sharing parameters?
+def charcnn(x):
+    return C.models.Sequential([
+        C.layers.Convolution1D((5,), convs, activation=C.relu, init=C.glorot_uniform(), pad=[True], strides=1, bias=True, init_bias=True),
+        C.GlobalMaxPooling()])(x)
 
-mb_source = C.MinibatchSource(C.CTFDeserializer('squad_small.ctf', C.StreamDefs(
-    context_words = C.StreamDef('cw',  shape=w_dim, is_sparse=True),
-    query_words   = C.StreamDef('qw',  shape=w_dim, is_sparse=True),
-    answer_begin  = C.StreamDef('ab',  shape=1,     is_sparse=False),
-    answer_end    = C.StreamDef('ae',  shape=1,     is_sparse=False),
-    context_chars = C.StreamDef('cc',  shape=c_dim, is_sparse=True),
-    query_chars   = C.StreamDef('qc',  shape=c_dim, is_sparse=True)
-)), randomize=False)
+# todo are the lstms sharing parameters?
+def bilstm(x):
+    return C.splice(C.Recurrence(C.blocks.LSTM(dim))(x), C.Recurrence(C.blocks.LSTM(dim),go_backwards=True)(x))
+
+def combine_glove_and_learnable(known, vocab):
+    # load glove
+    npglove = np.zeros((known,dim), dtype=np.float32)
+    with open('glove.6B.100d.txt', encoding='utf-8') as f:
+        for line in f:
+            parts = line.split()
+            word = parts[0].lower()
+            if word in vocab:
+                npglove[vocab[word],:] = np.asarray([float(p) for p in parts[1:]])
+    glove = C.constant(npglove)
+    nonglove = C.parameter(shape=(len(vocab) - known, dim), init=C.glorot_uniform())
+    return C.splice(glove, nonglove, axis=0)
 
 c = C.Axis.new_unique_dynamic_axis('c')
 q = C.Axis.new_unique_dynamic_axis('q')
@@ -30,6 +49,15 @@ cc = C.input_variable(c_dim, dynamic_axes=[b,c], name='cc')
 qc = C.input_variable(c_dim, dynamic_axes=[b,q], name='qc')
 ab = C.input_variable(a_dim, dynamic_axes=[b,c], name='ab')
 ae = C.input_variable(a_dim, dynamic_axes=[b,c], name='ae')
+
+mb_source = C.MinibatchSource(C.CTFDeserializer('val.ctf', C.StreamDefs(
+    context_words = C.StreamDef('cw',  shape=w_dim, is_sparse=True),
+    query_words   = C.StreamDef('qw',  shape=w_dim, is_sparse=True),
+    answer_begin  = C.StreamDef('ab',  shape=1,     is_sparse=False),
+    answer_end    = C.StreamDef('ae',  shape=1,     is_sparse=False),
+    context_chars = C.StreamDef('cc',  shape=c_dim, is_sparse=True),
+    query_chars   = C.StreamDef('qc',  shape=c_dim, is_sparse=True)
+)), randomize=False)
 
 input_map = {
     cw: mb_source.streams.context_words,
@@ -43,23 +71,12 @@ input_map = {
 mb_data = mb_source.next_minibatch(256, input_map=input_map)
 print(mb_data)
 
-# todo load actual glove
-glove = C.parameter(shape=(C.InferredDimension, glove_dim), init=C.glorot_uniform())
-
-# todo are the char cnns sharing parameters?
-def charcnn(x):
-    return C.models.Sequential([
-        C.layers.Convolution1D((5,), convs, activation=C.relu, init=C.glorot_uniform(), pad=[True], strides=1, bias=True, init_bias=True),
-        C.GlobalMaxPooling()])(x)
-
-# todo are the lstms sharing parameters?
-def bilstm(x):
-    return C.splice(C.Recurrence(C.blocks.LSTM(dim))(x), C.Recurrence(C.blocks.LSTM(dim),go_backwards=True)(x))
+embedding = combine_glove_and_learnable(known, vocab)
 
 qchars = C.reshape(qc,(qc.shape[0]//20,20))
 
 # todo we need to reshape because GlobalMaxPooling is retaining a trailing singleton dimension
-q_spliced_input = C.splice(C.times(qw, glove), C.reshape(charcnn(qchars), convs))
+q_spliced_input = C.splice(C.times(qw, embedding), C.reshape(charcnn(qchars), convs))
 
 # todo highway network
 
@@ -72,12 +89,18 @@ print(pvw[0])
 cchars = C.reshape(cc,(cc.shape[0]//20,20))
 
 # todo we need to reshape because GlobalMaxPooling is retaining a trailing singleton dimension
-c_spliced_input = C.splice(C.times(cw, glove), C.reshape(charcnn(cchars), convs))
+c_spliced_input = C.splice(C.times(cw, embedding), C.reshape(charcnn(cchars), convs))
 
 # todo highway network
 c_lstm_output = bilstm(c_spliced_input)
 print('lstm output',c_lstm_output)
 
+# This part deserves some explanation
+# It is the attention layer
+# In the paper they use a 6 * dim dimensional vector
+# here we split it in three parts because the different parts
+# participate in very different operations
+# so W * [h; u; h.* u] becomes w1 * h + w2 * u + w3 * (h.*u)
 ws1 = C.parameter(shape=(2 * dim, 1), init=C.glorot_uniform())
 ws2 = C.parameter(shape=(2 * dim, 1), init=C.glorot_uniform())
 ws3 = C.parameter(shape=(1, 2 * dim), init=C.glorot_uniform())
@@ -120,7 +143,7 @@ end_logits = C.times(end_input, end_weights)
 end_loss = seqloss(end_logits, ae)
 
 loss = begin_loss + end_loss
-np.
+
 #print(loss.grad(mb_data))
 func = loss
 print([t.shape for t in func.grad(mb_data, wrt=loss.parameters)])
