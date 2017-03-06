e774b9972a0722f4fc1a9ca0bbd02feeb06b95a3
Zhou Wang <zhouwang@microsoft.com>
2017-03-06 16:12:06 +0100

    create repo for issue from RenBo

--- Tests/EndToEndTests/EvalClientTests/CNTKLibraryCSEvalExamplesTest/Program.cs
+++ Tests/EndToEndTests/EvalClientTests/CNTKLibraryCSEvalExamplesTest/Program.cs
@@ -1,113 +1,143 @@
-﻿//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-//
-// Program.cs -- Tests of CNTK Library C# Eval examples.
-//
-
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using CNTK;
-
-namespace CNTKLibraryCSEvalExamples
+using Amoc.Core;
+namespace Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Todo: move to a separate unit test.
-            Console.WriteLine("Test Utils");
 
-            int maxThreads = Utils.GetMaxNumCPUThreads();
-            Utils.SetMaxNumCPUThreads(2);
-            Console.WriteLine("MaxNumCPUThreads: before: " + maxThreads + ", after " + Utils.GetMaxNumCPUThreads());
-            Utils.SetMaxNumCPUThreads(maxThreads);
-            Console.WriteLine("reset MaxNumCPuThreads to " + Utils.GetMaxNumCPUThreads());
-
-            var level = Utils.GetTraceLevel();
-            Utils.SetTraceLevel(TraceLevel.Info);
-            Console.WriteLine("TraceLevel: before: " + level + ", after " + Utils.GetTraceLevel());
-            Utils.SetTraceLevel(level);
-            Console.WriteLine("reset TraceLevel to " + Utils.GetTraceLevel());
-
-            Console.WriteLine(Utils.DataTypeName(DataType.Float));
-            Console.WriteLine(Utils.DataTypeSize(DataType.Double));
-            Console.WriteLine(Utils.DeviceKindName(DeviceDescriptor.CPUDevice.Type));
-            Console.WriteLine(Utils.DeviceKindName(DeviceKind.GPU));
-            Console.WriteLine(Utils.IsSparseStorageFormat(StorageFormat.Dense));
-            Console.WriteLine(Utils.IsSparseStorageFormat(StorageFormat.SparseCSC));
-            Console.WriteLine(Utils.IsSparseStorageFormat(StorageFormat.SparseBlockCol));
-            Console.WriteLine(Utils.VariableKindName(VariableKind.Constant));
-            Console.WriteLine(Utils.VariableKindName(VariableKind.Placeholder));
-            Console.WriteLine(Utils.VariableKindName(VariableKind.Input));
-            Console.WriteLine(Utils.VariableKindName(VariableKind.Output));
-            Console.WriteLine(Utils.VariableKindName(VariableKind.Parameter));
-
-#if CPUONLY
-            Console.WriteLine("======== Evaluate model on CPU using CPUOnly build ========");
-#else
-            Console.WriteLine("======== Evaluate model on CPU using GPU build ========");
-#endif
-
-            if (ShouldRunOnCpu())
+            try
             {
-                var device = DeviceDescriptor.CPUDevice;
+                string modelFilePath = "FCN2.094";
+                ThrowIfFileNotExist(modelFilePath, string.Format("Error: The model '{0}' does not exist. Please follow instructions in README.md in <CNTK>/Examples/Image/Classification/ResNet to create the model.", modelFilePath));
 
-                CNTKLibraryManagedExamples.EvaluationSingleImage(device);
-                // Run memory tests.
-                MemoryTests.ValidateObjectReferences(device);
-                CNTKLibraryManagedExamples.EvaluationBatchOfImages(device);
-                CNTKLibraryManagedExamples.EvaluateMultipleImagesInParallel(device);
-                // Run memory tests again.
-                MemoryTests.ValidateObjectReferences(device);
+                //this is the crashed location
+                Function modelFunc = Function.LoadModel(modelFilePath, DeviceDescriptor.CPUDevice);
 
-                CNTKLibraryManagedExamples.EvaluationSingleSequenceUsingOneHot(device);
-                CNTKLibraryManagedExamples.EvaluationBatchOfSequencesUsingOneHot(device);
-                CNTKLibraryManagedExamples.EvaluationSingleSequenceUsingSparse(device);
-                // It is sufficient to test loading model from memory buffer only on CPU.
-                CNTKLibraryManagedExamples.LoadModelFromMemory(device);
+                Console.WriteLine("\n===== Evaluate single image =====");
+                foreach (var file in new List<string> { "F04_423C020A.mfc", "F04_423C020N.mfc" })
+                {
+                    //this is the correct location
+                    //Function modelFunc = Function.LoadModel(modelFilePath, DeviceDescriptor.CPUDevice);
+                    Variable outputVar = modelFunc.Output;
+                    Variable inputVar = modelFunc.Arguments.Single();
 
-                MemoryTests.WriteOutputs();
+                    //HtkFeatureFile htkFile = HtkFeatureFile.Read(new MemoryStream(row[inputRow].Binary));
+                    HtkFeatureFile htkFile = HtkFeatureFile.Read(file);
+                    int featDim = htkFile.BytesPerSample / sizeof(float);
+                    int context = (Convert.ToInt32(inputVar.Shape[0]) / featDim - 1) / 2;
+                    List<List<float>> frms = htkFile.Frames.Select(x => x.ToList()).ToList();
+                    var data = new List<float>();
+                    foreach (var frm in ContextFrames(frms, context))
+                    {
+                        data.AddRange(frm);
+                    }
+
+
+                    //Value outputVal;
+                    var outputBuffer = new List<List<float>>();
+
+                    var inputDataMap = new Dictionary<Variable, Value> { { inputVar, Value.CreateBatch(inputVar.Shape, data, DeviceDescriptor.CPUDevice) } };
+                    var outputDataMap = new Dictionary<Variable, Value> { { outputVar, null } };
+
+                    modelFunc.Evaluate(inputDataMap, outputDataMap, DeviceDescriptor.CPUDevice);
+
+                    outputDataMap[outputVar].CopyVariableValueTo(outputVar, outputBuffer);
+
+                    PrintOutput(outputVar.Shape.TotalSize, outputBuffer);
+                }
+
             }
-
-            if (ShouldRunOnGpu())
+            catch (Exception ex)
             {
-                Console.WriteLine(" ====== Evaluate model on GPU =====");
-                var device = DeviceDescriptor.GPUDevice(0);
-                // Run memory tests.
-                MemoryTests.ValidateObjectReferences(device);
-                CNTKLibraryManagedExamples.EvaluationSingleImage(device);
-                CNTKLibraryManagedExamples.EvaluationBatchOfImages(device);
-                CNTKLibraryManagedExamples.EvaluateMultipleImagesInParallel(device);
-                // Run memory tests.
-                MemoryTests.ValidateObjectReferences(device);
-
-                CNTKLibraryManagedExamples.EvaluationSingleSequenceUsingOneHot(device);
-                CNTKLibraryManagedExamples.EvaluationBatchOfSequencesUsingOneHot(device);
-                CNTKLibraryManagedExamples.EvaluationSingleSequenceUsingSparse(device);
-
-                // Run memory tests again.
-                MemoryTests.WriteOutputs();
+                Console.WriteLine("Error: {0}\nCallStack: {1}\n Inner Exception: {2}", ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.Message : "No Inner Exception");
+                throw ex;
             }
-
-            Console.WriteLine("======== Evaluation completes. ========");
         }
-
-        static bool ShouldRunOnGpu()
+        public static List<List<float>> ContextFrames(List<List<float>> frames, int context)
         {
-#if CPUONLY
-            return false;
-#else
-            string testDeviceSetting = Environment.GetEnvironmentVariable("TEST_DEVICE");
+            var extented = new List<List<float>>();
+            for (int i = 0; i < frames.Count; i++)
+            {
+                var item = new List<float>();
 
-            return (string.IsNullOrEmpty(testDeviceSetting) || string.Equals(testDeviceSetting.ToLower(), "gpu"));
-#endif
+                for (int j = -context; j <= context; j++)
+                {
+                    if (i + j < 0)
+                        item.AddRange(frames.First());
+                    else if (i + j >= frames.Count)
+                        item.AddRange(frames.Last());
+                    else
+                        item.AddRange(frames[i + j]);
+                }
+                extented.Add(item);
+            }
+            return extented;
         }
 
-        static bool ShouldRunOnCpu()
+        /// <summary>
+        /// Print out the evalaution results.
+        /// </summary>
+        /// <typeparam name="T">The data value type</typeparam>
+        /// <param name="sampleSize">The size of each sample.</param>
+        /// <param name="outputBuffer">The evaluation result data.</param>
+        private static void PrintOutput<T>(uint sampleSize, List<List<T>> outputBuffer)
         {
-            string testDeviceSetting = Environment.GetEnvironmentVariable("TEST_DEVICE");
+            Console.WriteLine("The number of sequences in the batch: " + outputBuffer.Count);
+            int seqNo = 0;
+            uint outputSampleSize = sampleSize;
+            foreach (var seq in outputBuffer)
+            {
+                if (seq.Count % outputSampleSize != 0)
+                {
+                    throw new ApplicationException("The number of elements in the sequence is not a multiple of sample size");
+                }
 
-            return (string.IsNullOrEmpty(testDeviceSetting) || string.Equals(testDeviceSetting.ToLower(), "cpu"));
+                Console.WriteLine(String.Format("Sequence {0} contains {1} samples.", seqNo++, seq.Count / outputSampleSize));
+                uint i = 0;
+                uint sampleNo = 0;
+                foreach (var element in seq)
+                {
+                    if (i++ % outputSampleSize == 0)
+                    {
+                        Console.Write(String.Format("    sample {0}: ", sampleNo));
+                    }
+                    Console.Write(element);
+                    if (i % outputSampleSize == 0)
+                    {
+                        Console.WriteLine(".");
+                        sampleNo++;
+                    }
+                    else
+                    {
+                        Console.Write(",");
+                    }
+                }
+            }
         }
+        /// <summary>
+        /// Checks whether the file exists. If not, write the error message on the console and throw FileNotFoundException.
+        /// </summary>
+        /// <param name="filePath">The file to check.</param>
+        /// <param name="errorMsg">The message to write on console if the file does not exist.</param>
+        private static void ThrowIfFileNotExist(string filePath, string errorMsg)
+        {
+            if (!File.Exists(filePath))
+            {
+                if (!string.IsNullOrEmpty(errorMsg))
+                {
+                    Console.WriteLine(errorMsg);
+                }
+                throw new FileNotFoundException(string.Format("File '{0}' not found.", filePath));
+            }
+        }
+
+
     }
 }
