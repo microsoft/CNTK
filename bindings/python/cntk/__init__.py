d9429624ae808852fa7e1b80ca3de78b8850f0d6
Emad Barsoum <ebarsoum@microsoft.com>
2017-03-07 19:11:01 -0800

    Add more proxies and wrap one_hot.

--- bindings/python/cntk/__init__.py
+++ bindings/python/cntk/__init__.py
@@ -24,6 +24,9 @@ from .sample_installer import install_samples
 
 # To __remove__
 from .io import *
+
+def one_hot(batch, num_classes, dtype=None, device=None):
+    return Value.one_hot(batch, num_classes, dtype, device)
 # End of to remove
 
 DATATYPE = np.float32
