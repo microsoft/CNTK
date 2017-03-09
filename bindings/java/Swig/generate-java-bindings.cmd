85dbb3154aaacd9c75f7603fed5ac7e8a22bc8ba
marhamil723 <mhamilton723@gmail.com>
2017-03-09 11:37:17 -0500

    fixup of binding script

--- bindings/java/Swig/generate-java-bindings.cmd
+++ bindings/java/Swig/generate-java-bindings.cmd
@@ -1,9 +1,6 @@
 SET SOL_DIR=%~f1
-echo %SOL_DIR%
-SET START_DIR=%CD%
-echo %START_DIR%
 %SWIG_PATH%\swig.exe -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%SOL_DIR%bindings\common -package com.microsoft.CNTK -outdir  %SOL_DIR%bindings\java\SwigProxyClasses\com\microsoft\CNTK  %SOL_DIR%bindings\java\Swig\cntk_java.i
-cd %SOL_DIR%\bindings\java\SwigProxyClasses
+pushd %SOL_DIR%\bindings\java\SwigProxyClasses
 javac .\com\microsoft\CNTK\*.java
 jar -cvf cntk.jar .\com\microsoft\CNTK\*.class
-cd %START_DIR%
+popd
