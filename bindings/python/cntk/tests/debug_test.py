23326a11407030433cfc514e1b33c86f5b34492a
Willi Richert <wilrich@microsoft.com>
2017-03-09 21:22:11 +0100

    Add test for combine

--- bindings/python/cntk/tests/debug_test.py
+++ bindings/python/cntk/tests/debug_test.py
@@ -9,7 +9,7 @@ import pytest
 
 from cntk import sgd, Trainer, learning_rate_schedule, parameter, \
                  input_variable, times, cross_entropy_with_softmax, \
-                 classification_error, UnitType
+                 classification_error, UnitType, combine
 from cntk.ops.tests.ops_test_utils import cntk_device
 from cntk.debug import debug_model, _DebugNode
 
@@ -29,7 +29,7 @@ def _generate_random_data_sample(sample_size, feature_dim, num_classes):
 def _train(z, loss, eval_error,
            f_input, l_input,
            num_output_classes,
-           steps=1, device_id=-1):
+           steps, device_id):
     np.random.seed(0)
 
     input_dim = 2
@@ -73,7 +73,7 @@ class OutStream(object):
     def flush(self):
         pass
 
-def test_debug_1():
+def test_debug_1(device_id):
     input_dim = 2
     num_output_classes = 2
 
@@ -95,7 +95,59 @@ def test_debug_1():
     _train(z, loss, eval_error,
            loss.find_by_name('features'),
            loss.find_by_name('labels'),
-           num_output_classes, 1)
+           num_output_classes, 1, device_id)
+
+    # outs.written contains something like
+    # =================================== forward  ===================================
+    # Parameter('p', [], [2]) with uid 'Parameter4'
+    # Input('features', [#, *], [2]) with uid 'Input3'
+    # Times: Output('UserDefinedFunction12_Output_0', [#, *], [2]), Output('UserDefinedFunction15_Output_0', [], [2]) -> Output('z', [#, *], [2 x 2]) with uid 'Times21'
+    # =================================== backward ===================================
+    # Times: Output('UserDefinedFunction12_Output_0', [#, *], [2]), Output('UserDefinedFunction15_Output_0', [], [2]) -> Output('z', [#, *], [2 x 2]) with uid 'Times21'
+    # Input('features', [#, *], [2]) with uid 'Input3'
+    # Parameter('p', [], [2]) with uid 'Parameter4'   assert outs.written == out_stuff
+
+    assert len(outs.written) == 8
+
+    v_p = "Parameter('p', "
+    v_i = "Input('features'"
+    v_t = 'Times: '
+
+    assert outs.written[0].startswith('=') and 'forward' in outs.written[0]
+    line_1, line_2, line_3 = outs.written[1:4]
+
+    assert outs.written[4].startswith('=') and 'backward' in outs.written[4]
+    line_5, line_6, line_7 = outs.written[5:8]
+    assert line_5.startswith(v_t)
+    assert line_6.startswith(v_p) and line_7.startswith(v_i) or \
+           line_6.startswith(v_i) and line_7.startswith(v_p)
+
+
+def test_debug_multi_output(device_id):
+    input_dim = 2
+    num_output_classes = 2
+
+    f_input = input_variable(input_dim, np.float32,
+                             needs_gradient=True, name='features')
+
+    p = parameter(shape=(input_dim,), init=10, name='p')
+
+    comb = combine([f_input, p])
+
+    ins = InStream(['n', 'n', 'n', 'n', 'n'])
+    outs = OutStream()
+
+    z = times(comb.outputs[0], comb.outputs[1], name='z')
+    z = debug_model(z, ins, outs)
+
+    l_input = input_variable(num_output_classes, np.float32, name='labels')
+    loss = cross_entropy_with_softmax(z, l_input)
+    eval_error = classification_error(z, l_input)
+
+    _train(z, loss, eval_error,
+           loss.find_by_name('features'),
+           loss.find_by_name('labels'),
+           num_output_classes, 1, device_id)
 
     # outs.written contains something like
     # =================================== forward  ===================================
