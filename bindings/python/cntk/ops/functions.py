0d3b098d28c5187f402464a7df3cfef02a45959e
Alexey Reznichenko <alrezni@microsoft.com>
2017-03-13 16:52:26 +0100

    Properly set device for cached BackPropState
      Make sure that the device referenced in a cached empty BackPropState
      object corresponds to the correct device used in the forward prop.

--- bindings/python/cntk/ops/functions.py
+++ bindings/python/cntk/ops/functions.py
@@ -1,5 +1,5 @@
 from cntk import cntk_py
-from cntk.device import DeviceDescriptor
+from cntk.device import cpu
 from cntk.utils import variable_value_to_seq, Record, \
         get_python_function_arguments, map_function_arguments
 from cntk.internal import map_if_possible, typemap, sanitize_var_map, sanitize_batch, sanitize_dtype_cntk, _as_tuple
@@ -1083,14 +1083,18 @@ class UserFunction(Function):
 
         # Since the state will frequently not be used, we cache the None-state
         # to speed up.
-        self._none_state =  cntk_py.UserBackPropState(self,
-                DeviceDescriptor.cpu_device(), None)
+        self._none_state =  cntk_py.UserBackPropState(self, cpu(), None)
 
         # Memory management for user defined functions has to be controlled by
         # the C++ side. For more information:
         # http://www.swig.org/Doc3.0/Python.html#Python_nn35
         self.__disown__()
 
+    def _get_none_state(self, device=cpu()):
+        if self._none_state.device() != device:
+            self._none_state =  cntk_py.UserBackPropState(self, device, None)
+
+        return self._none_state
 
     def _forward(self, arguments, outputs, device=None, outputs_to_retain=None):
         '''
@@ -1127,7 +1131,7 @@ class UserFunction(Function):
             state = self.forward(args, outputs, device, outputs_to_retain)
 
         if state is None:
-            state = self._none_state
+            state = self._get_none_state(device)
         elif not isinstance(state, cntk_py.BackPropState):
             state = cntk_py.UserBackPropState(self, device, state)
 
@@ -1173,11 +1177,8 @@ class UserFunction(Function):
 
         else:
             if not isinstance(state, cntk_py.BackPropState):
-                if state is None:
-                    state = self._none_state
-                else:
-                    raise ValueError('if as_numpy=False, state must be of '
-                            'type BackPropState')
+                raise ValueError('if as_numpy=False, state must be of '
+                        'type BackPropState')
 
         map_if_possible(variables)
 
