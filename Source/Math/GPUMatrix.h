fe81c76cc14d274f95cd3d0b6b839b71becff2d7
Cheng Tang <chenta@microsoft.com>
2017-03-15 13:30:54 -0700

    gather gpu impl and ut

--- Source/Math/GPUMatrix.h
+++ Source/Math/GPUMatrix.h
@@ -274,6 +274,9 @@ public:
     void SetGaussianRandomValue(const ElemType mean, const ElemType sigma, unsigned long seed = USE_TIME_BASED_SEED);
     void SetUniformRandomMask(const ElemType maskRate, const ElemType scaleValue, RNGHandle& rngHandle);
 
+    GPUMatrix<ElemType>& GatherFromTarget(const GPUMatrix<ElemType>& indices, const GPUMatrix<ElemType>& target, size_t row_elements);
+    GPUMatrix<ElemType>& ScatterAccordingIndices(const GPUMatrix<ElemType>& values, const GPUMatrix<ElemType>& indices, size_t row_elements);
+
     GPUMatrix<ElemType> Transpose() const;
     GPUMatrix<ElemType>& AssignTransposeOf(const GPUMatrix<ElemType>& a);
 
--- Source/Math/Matrix.h
+++ Source/Math/Matrix.h
@@ -299,8 +299,8 @@ public:
     Matrix<ElemType>& AssignSoftmaxSum(const Matrix<ElemType>& a, const Matrix<ElemType>& softmax);
     Matrix<ElemType>& AssignNceUnnormalizedEval(const Matrix<ElemType>& a, const Matrix<ElemType>& b, const Matrix<ElemType>& c, const Matrix<ElemType>& bias);
 
-    Matrix<ElemType>& GatherFromTarget(const Matrix<ElemType>& indices, const Matrix<ElemType>& target, const SmallVector<size_t>& targetShape);
-    Matrix<ElemType>& ScatterAccordingIndices(const Matrix<ElemType>& values, const Matrix<ElemType>& indices, const SmallVector<size_t>& shape);
+    Matrix<ElemType>& GatherFromTarget(const Matrix<ElemType>& indices, const Matrix<ElemType>& target, size_t row_elements);
+    Matrix<ElemType>& ScatterAccordingIndices(const Matrix<ElemType>& values, const Matrix<ElemType>& indices, size_t row_elements);
 
     Matrix<ElemType> Transpose(); // This method doesn't change state of Matrix. It should be a const function
     Matrix<ElemType>& AssignTransposeOf(const Matrix<ElemType>& a);
