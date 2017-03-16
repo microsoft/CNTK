033556c6c0aa7bb18d4001e3c7a141931b08ae57
Mark Hamilton <marhamil@microsoft.com>
2017-03-16 18:30:19 +0100

    fixing tests

--- bindings/java/Swig/generate-java-bindings.cmd
+++ bindings/java/Swig/generate-java-bindings.cmd
@@ -1,8 +1,8 @@
 SET SOL_DIR=%~f1
-if not exist %SOL_DIR%\bindings\java\SwigProxyClasses\com\microsoft\CNTK\ mkdir %SOL_DIR%\bindings\java\SwigProxyClasses\com\microsoft\CNTK\
-%SWIG_PATH%\swig.exe -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%SOL_DIR%bindings\common -package com.microsoft.CNTK -outdir  %SOL_DIR%bindings\java\SwigProxyClasses\com\microsoft\CNTK  %SOL_DIR%bindings\java\Swig\cntk_java.i
+if not exist %SOL_DIR%\bindings\java\Swig\com\microsoft\CNTK\ mkdir %SOL_DIR%\bindings\java\Swig\com\microsoft\CNTK\
+%SWIG_PATH%\swig.exe -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%SOL_DIR%bindings\common -package com.microsoft.CNTK -outdir  %SOL_DIR%bindings\java\Swig\com\microsoft\CNTK  %SOL_DIR%bindings\java\Swig\cntk_java.i
 
-pushd %SOL_DIR%\bindings\java\SwigProxyClasses
+pushd %SOL_DIR%\bindings\java\Swig
 javac .\com\microsoft\CNTK\*.java
 jar -cvf cntk.jar .\com\microsoft\CNTK\*.class
 rd com /q /s
