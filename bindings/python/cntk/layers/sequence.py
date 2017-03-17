2a4241c0becc8cbbfadef6328ef8d2d6c4ddc0b6
Frank Seide <fseide@microsoft.com>
2017-03-16 17:31:44 -0700

    docstrings for Fold() and UnfoldFrom()

--- bindings/python/cntk/layers/sequence.py
+++ bindings/python/cntk/layers/sequence.py
@@ -199,13 +199,14 @@ def RecurrenceFrom(step_function, go_backwards=default_override_or(False), retur
       This function must have N+1 inputs and N outputs, where N is the number of state variables
       (typically 1 for GRU and plain RNNs, and 2 for LSTMs).
      go_backwards (bool, defaults to ``False``): if ``True`` then run the recurrence from the end of the sequence to the start.
-, c     initial_state (scalar or tensor without batch dimension; or a tuple thereof):
+     initial_state (scalar or tensor without batch dimension; or a tuple thereof):
       the initial value for the state. This can be a constant or a learnable parameter.
       In the latter case, if the step function has more than 1 state variable,
       this parameter must be a tuple providing one initial state for every state variable.
      return_full_state (bool, defaults to ``False``): if ``True`` and the step function has more than one
       state variable, then the layer returns a all state variables (a tuple of sequences);
       whereas if not given or ``False``, only the first state variable is returned to the caller.
+     name (str, optional): the name of the Function instance in the network
 
     Returns:
         cntk.ops.functions.Function: 
@@ -346,6 +347,7 @@ def Recurrence(step_function, go_backwards=default_override_or(False), initial_s
      return_full_state (bool, defaults to ``False``): if ``True`` and the step function has more than one
       state variable, then the layer returns a all state variables (a tuple of sequences);
       whereas if not given or ``False``, only the first state variable is returned to the caller.
+     name (str, optional): the name of the Function instance in the network
 
     Returns:
         cntk.ops.functions.Function: 
@@ -384,16 +386,69 @@ def Recurrence(step_function, go_backwards=default_override_or(False), initial_s
 
 def Fold(folder_function, go_backwards=default_override_or(False), initial_state=default_override_or(0), return_full_state=False, name=''):
     '''
-    Layer factory function to create a function that implements the fold() catamorphism.
-    It is like ``Recurrence()`` except that it returns only the final state.
+    Layer factory function to create a function that runs a step function recurrently over an input sequence,
+    and returns the final state.
     This is often used for embeddings of sequences, e.g. in a sequence-to-sequence model.
 
+    ``Fold()`` is the same as :function:`~cntk.layers.Recurrence` except that only the final state is returned
+    (whereas ``Recurrence()`` returns the entire state sequence).
+    Hence, this documentation will only focus on the differences to ``Recurrence()``,
+    please see :function:`~cntk.layers.Recurrence` for a detailed information on parameters.
+
     Commonly, the ``folder_function`` is a recurrent block such as an LSTM.
-    One can, however, pass any binary function. E.g. passing ``plus`` will sum
+    However, one can pass any binary function. E.g. passing ``plus`` will sum
     up all items of a sequence; while ``element_max`` would perform a max-pooling over all items of the sequence.
 
-    ``go_backwards=False`` selects a fold-left, while ``True`` a fold-right,
-    but note that the ``folder_function`` signature is always the one of fold-left.
+    Note: CNTK's Fold() is similar to the fold() catamorphism known from functional programming.
+    ``go_backwards=False`` corresponds to a fold-left, and ``True`` to a fold-right,
+    except that the ``folder_function`` signature is always the one of fold-left.
+
+    Example:
+     >>> from cntk.layers import *
+     >>> from cntk.layers.typing import *
+
+     >>> # sequence classifier. Maps a one-hot word sequence to a scalar probability value.
+     >>> # The recurrence is a Fold(), meaning only the final hidden state is produced.
+     >>> # The Label() layer allows to access the final hidden layer by name.
+     >>> sequence_classifier = Sequential([ Embedding(300),
+     ...                                    Fold(LSTM(500)),
+     ...                                    Dense(1, activation=sigmoid) ])
+
+     >>> # element-wise max-pooling over an input sequence
+     >>> x = Input(**Sequence[Tensor[2]])
+     >>> x0 = np.array([[ 1, 2 ],
+     ...                [ 6, 3 ],
+     ...                [ 4, 2 ],
+     ...                [ 8, 1 ],
+     ...                [ 6, 0 ]])
+     >>> seq_max_pool = Fold(C.element_max)
+     >>> y = seq_max_pool(x)
+     >>> y(x0)
+         array([[ 8.,   3.]], dtype=float32)
+
+     >>> # element-wise sum over an input sequence
+     >>> seq_sum = Fold(C.plus)
+     >>> y = seq_sum(x)
+     >>> y(x0)
+         array([[ 25.,   8.]], dtype=float32)
+
+    Args:
+     folder_function (:class:`~cntk.ops.functions.Function` or equivalent Python function):
+      This function must have N+1 inputs and N outputs, where N is the number of state variables
+      (typically 1 for GRU and plain RNNs, and 2 for LSTMs).
+     go_backwards (bool, defaults to ``False``): if ``True`` then run the recurrence from the end of the sequence to the start.
+     initial_state (scalar or tensor without batch dimension; or a tuple thereof):
+      the initial value for the state. This can be a constant or a learnable parameter.
+      In the latter case, if the step function has more than 1 state variable,
+      this parameter must be a tuple providing one initial state for every state variable.
+     return_full_state (bool, defaults to ``False``): if ``True`` and the step function has more than one
+      state variable, then the layer returns the final value of a all state variables (a tuple of sequences);
+      whereas if not given or ``False``, only the final value of the first of the state variables is returned to the caller.
+     name (str, optional): the name of the Function instance in the network
+
+    Returns:
+        cntk.ops.functions.Function: 
+        A function that accepts one argument (which must be a sequence) and performs the fold operation on it
     '''
 
     go_backwards  = get_default_override(Fold, go_backwards=go_backwards)
@@ -409,26 +464,69 @@ def Fold(folder_function, go_backwards=default_override_or(False), initial_state
     return _inject_name(fold, name)
 
 
-# TODO: This is still a bit messy. The returned unfold_from() function should take the encoding instead of 'input'.
+# TODO: This API is still suboptimal, and should be fixed as follows:
+#  - the returned layer function should take the initial_state
+#  - the input length factor should be a layer function; in addition, a fixed max length should be possible
+#  - map_state_function is unused and should be removed
+#  - BUGBUG: tuple-valued state should be supported
 def UnfoldFrom(generator_function, map_state_function=identity, until_predicate=None, length_increase=1, initial_state=None, name=''):
     '''
-    Layer factory function to create a function that implements the unfold() anamorphism. It creates a function that, starting with a seed input,
-    applies 'generator_function' repeatedly and emits the sequence of results. Depending on the recurrent block,
-    it may have this form::
+    UnfoldFrom(generator_function, until_predicate=None, length_increase=1, initial_state=None, name='')
 
-      result = f(... f(f([g(input), initial_state])) ... )
+    Layer factory function to create a function that implements a recurrent generator.
+    Starting with a seed state, the ``UnfoldFrom()`` layer
+    repeatedly applies ``generator_function`` and emits the sequence of results.
+    ``UnfoldFrom(f, initial_state=s)``
+    emits the sequence ``f(s), f(f(s)), f(f(f(s))), ...``.
+    ``s`` can be tuple-valued.
 
-    or this form::
+    A typical application is the decoder of a sequence-to-sequence model,
+    the generator function ``f`` accepts a two-valued state, with the first
+    being an emitted word, and the second being an internal recurrent state.
+    The initial state would be a tuple ``(w0, h0)``
+    where ``w0`` represents the sentence-start symbol,
+    and ``h0`` is a thought vector that encodes the input sequence (as obtained
+    from a :function:`~cntk.layers.sequence.Fold()` operation).
 
-      result = f(g(input), ... f(g(input), f(g(input), initial_state)) ... )
+    BUGBUG: ^^ the above is not right; it presently does not allow tuple-valued state.
 
-    where `f` is `generator_function`.
-    An example use of this is sequence-to-sequence decoding, where `g(input)` is the sequence encoder,
-    `initial_state` is the sentence-start symbol, and `f` is the decoder. The first
-    of the two forms above is a plain sequence-to-sequence model where encoder output
-    is the start state for the output recursion.
-    The second form is an attention-based decoder, where the encoded input affects every application
-    of `f` differently.
+    A variant allows the state and the emitted sequence to be different. In that case,
+    ``f`` returns a tuple (output value, new state), and
+    ``UnfoldFrom(f, initial_state=s)``
+    would emit the sequence ``f(s)[0], f(f(s)[1])[0], f(f(f(s)[1])[1])[0], ...``.
+
+    The maximum length of the output sequence is not unlimited, but determined by the argument to
+    the layer function, multiplied by an optional increase factor.
+
+    Optionally, a function can be provided to denote that the end of the sequence has been reached.
+
+    Note: In the context of functional programming, the first form of this operation is known as the unfold() anamorphism.
+
+    Example:
+     TO BE PROVIDED after signature changes.
+
+    Args:
+     generator_function (:class:`~cntk.ops.functions.Function` or equivalent Python function):
+      This function must have N inputs and a N-tuple-valued output, where N is the number of state variables.
+      If the emitted value should be different from the state, then the function should have
+      a tuple of N+1 outputs, where the first output is the value to emit, while the others are the state.
+     until_predicate (:class:`~cntk.ops.functions.Function` or equivalent Python function):
+      A function that denotes when the last element of the unfold has been emitted.
+      It takes the same number of argments as the generator, and returns a scalar that must be 1
+      for the last element of the sequence, and 0 otherwise.
+      This is subject to the maximum length as determined by the input sequence and ``length_increase``.
+      If this parameter is not provided, the output length will be equal to the specified maximum length.
+     length_increase (float, defaults to 1): the maximum number of output items is equal to the
+      number of items of the argument to the unfold function, multiplied by this factor.
+      For example, pass 1.5 here if the output sequence can be at most 50% longer than the input.
+     initial_state (scalar or tensor without batch dimension; or a tuple thereof):
+      the seed value for the state
+     name (str, optional): the name of the Function instance in the network
+
+    Returns:
+        cntk.ops.functions.Function: 
+        A function that accepts one argument (which must be a sequence and provides
+        a reference for the maximum length of the output sequence), and performs the unfold operation on it
     '''
 
     generator_function = _sanitize_function(generator_function)
