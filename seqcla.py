c3adfc1b5161b01e20d01eddb5f6b5ab20324c33
Frank Seide <fseide@microsoft.com>
2017-03-31 16:45:16 -0700

    towards using dynamite

--- seqcla.py
+++ seqcla.py
@@ -14,58 +14,56 @@ from cntk.ops import input, sequence, relu
 from cntk.losses import cross_entropy_with_softmax
 from cntk.metrics import classification_error
 from cntk.logging import *
+from cntk import debugging
 from cntk.layers import *
+import cntk
+import dynamite
 
 
-
-# Creates the reader
+# Create the reader
 def create_reader(path, is_training, input_dim, label_dim):
     return MinibatchSource(CTFDeserializer(path, StreamDefs(
         features = StreamDef(field='x', shape=input_dim,   is_sparse=True),
         labels   = StreamDef(field='y', shape=label_dim,   is_sparse=False)
     )), randomize=is_training, max_sweeps = INFINITELY_REPEAT if is_training else 1)
 
-# Defines the LSTM model for classifying sequences
-def create_model(feature, num_output_classes, embedding_dim, LSTM_dim, cell_dim):
-    return Sequential([
+# Define the LSTM model for classifying sequences
+def create_model(namespace, num_output_classes, embedding_dim, hidden_dim):
+    return namespace.Sequential([
         Embedding(embedding_dim),
-        Fold(RNNUnit(cell_dim, activation=relu)),
+        Fold(RNNUnit(hidden_dim, activation=relu)),
         Dense(num_output_classes)
-    ])(feature)
+    ])
 
-# Creates and trains a LSTM sequence classification model
-def train_sequence_classifier(debug_output=False):
+# Create and train a LSTM sequence classification model
+def train(debug_output=False):
     input_dim = 2000
-    cell_dim = 25
     hidden_dim = 25
     embedding_dim = 50
     num_output_classes = 5
 
     # Input variables denoting the features and label data
-    features = sequence.input(shape=input_dim, is_sparse=True)
-    label = input(num_output_classes)
+    #features = sequence.input(shape=input_dim, is_sparse=True)
+    #label = input(num_output_classes)
 
     # Instantiate the sequence classification model
-    model = create_model(
-        features, num_output_classes, embedding_dim, hidden_dim, cell_dim)
+    model = create_model(cntk.layers, num_output_classes, embedding_dim, hidden_dim)
+    dmodel = create_model(dynamite, num_output_classes, embedding_dim, hidden_dim)
 
-    ce = cross_entropy_with_softmax(model, label)
-    pe = classification_error(model, label)
-
-    def criterion(input, label):
+    @Function
+    def criterion(input: Sequence[SparseTensor[input_dim]], label: Tensor[num_output_classes]):
         z = model(input)
         ce = cross_entropy_with_softmax(z, label)
         pe = classification_error(z, label)
         return (ce, pe)
+    #debugging.dump_signature(criterion)
 
-    rel_path = r"../CNTK/Tests/EndToEndTests/Text/SequenceClassification/Data/Train.ctf"
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), rel_path)
-
-    reader = create_reader(path, True, input_dim, num_output_classes)
+    rel_path = "../CNTK/Tests/EndToEndTests/Text/SequenceClassification/Data/Train.ctf"
+    reader = create_reader(os.path.dirname(os.path.abspath(__file__)) + '/' + rel_path, True, input_dim, num_output_classes)
 
     lr_per_sample = learning_rate_schedule(0.05, UnitType.sample)
     # Instantiate the trainer object to drive the model training
-    trainer = Trainer(model, (ce, pe),
+    trainer = Trainer(None, criterion,
                       sgd(model.parameters, lr=lr_per_sample))
 
     # process minibatches and perform model training
@@ -82,7 +80,7 @@ def train_sequence_classifier(debug_output=False):
 
     for i in range(251):
         mb = reader.next_minibatch(minibatch_size)
-        trainer.train_minibatch(ce.argument_map(mb[reader.streams.features], mb[reader.streams.labels]))
+        trainer.train_minibatch(criterion.argument_map(mb[reader.streams.features], mb[reader.streams.labels]))
         progress_printer.update_with_trainer(trainer, with_metric=True)    # log progress
     loss, metric, actual_samples = progress_printer.epoch_summary(with_metric=True)
 
@@ -99,5 +97,4 @@ if __name__ == '__main__':
     # use the best available one, e.g.
     # try_set_default_device(cpu())
 
-    error, _ = train_sequence_classifier()
-    print("Error: %f" % error)
+    train()
