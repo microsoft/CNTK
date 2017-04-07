81a572f1fd88758a117fb0580b535bc23e3c640f
Mark Hillebrand <mahilleb@microsoft.com>
2017-04-07 16:09:41 +0200

    WIP nitpick

--- bindings/python/doc/build.bat
+++ bindings/python/doc/build.bat
@@ -4,6 +4,9 @@ cd /d "%~dp0"
 set PYTHONPATH=%CD%\..
 set PATH=%CD%\..;%CD%\..\..\..\x64\Release;%PATH%
 
+@REM %SPHINX_APIDOC_OPTIONS%, comma-separated, default members,undoc-members,show-inheritance
+@REM do we want undoc-members?
+@REM set SPHINX_APIDOC_OPTIONS=members
 sphinx-apidoc.exe ..\cntk --module-first --separate --no-toc --output-dir=. --force ^
   ..\cntk\cntk_py.py ^
   ..\cntk\conftest.py ^
@@ -22,7 +25,10 @@ sphinx-apidoc.exe ..\cntk --module-first --separate --no-toc --output-dir=. --fo
 
 if errorlevel 1 exit /b 1
 
-.\make.bat html
+set SPHINXOPTS=-n %SPHINXOPTS%
+call .\make.bat html
 if errorlevel 1 exit /b 1
+@REM call .\make.bat linkcheck
+@REM if errorlevel 1 exit /b 1
 
 echo start _build\html\index.html
