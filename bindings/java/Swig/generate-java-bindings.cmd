2e8c99e71e131165f511e80da92e2965d7b15147
Zhou Wang <zhouwang@microsoft.com>
2017-04-10 12:09:58 +0200

    add path to javac/jar

--- bindings/java/Swig/generate-java-bindings.cmd
+++ bindings/java/Swig/generate-java-bindings.cmd
@@ -3,8 +3,9 @@ if not exist %SOL_DIR%\bindings\java\Swig\com\microsoft\CNTK\ mkdir %SOL_DIR%\bi
 %SWIG_PATH%\swig.exe -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%SOL_DIR%bindings\common -package com.microsoft.CNTK -outdir  %SOL_DIR%bindings\java\Swig\com\microsoft\CNTK  %SOL_DIR%bindings\java\Swig\cntk_java.i
 
 pushd %SOL_DIR%\bindings\java\Swig
-javac .\com\microsoft\CNTK\*.java
-jar -cvf cntk.jar .\com\microsoft\CNTK\*.class
+REM: TODO: add check whether javac/jar exist.
+%JAVA_HOME%\bin\javac .\com\microsoft\CNTK\*.java
+%JAVA_HOME%\bin\jar -cvf cntk.jar .\com\microsoft\CNTK\*.class
 rd com /q /s
 popd
 
