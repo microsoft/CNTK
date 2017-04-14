e32d87981791693c5594efba095332d5ec9915d7
abc <a@b.com>
2017-04-15 00:52:19 +0800

    New Feature: use a BAT file to set all the environment variables that are prerequisites for building

---
+++ PreBuild/SetEnvVar.bat
@@ -0,0 +1,25 @@
+﻿:: MKL
+setx CNTK_MKL_PATH "D:\Dev\Lib\CNTK_Dependencies\CNTKCustomMKL"
+
+:: MS-MPI
+:: Automatically be set by the installer system environment variables
+
+:: boost
+setx BOOST_INCLUDE_PATH "D:\Dev\Lib\boost_1_63_0"
+setx BOOST_LIB_PATH "D:\Dev\Lib\boost_1_63_0\stage\lib"
+
+:: Protobuf
+setx PROTOBUF_PATH "D:\Dev\Lib\CNTK_Dependencies\protobuf-3.1.0-vs15"
+
+:: SWIG
+setx SWIG_PATH "D:\Dev\Lib\CNTK_Dependencies\swigwin-3.0.10"
+
+:: NVIDIA CUDA 8
+:: Automatically be set by the installer system environment variables
+
+:: NVIDIA cuDNN
+setx CUDNN_PATH "D:\Dev\Lib\cudnn-8.0-windows10-x64-v5.1\cuda"
+
+:: NVIDIA CUB
+setx CUB_PATH "D:\Dev\Lib\CNTK_Dependencies\cub-1.4.1"
+
