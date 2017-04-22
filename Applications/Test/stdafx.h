cb582f8671b4c07cf3bc1667db5a2d1dd417da1b
abc <a@b.com>
2017-04-22 17:55:56 +0800

    add my cpp tests

---
+++ Applications/Test/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+
+#include "../../Source/CNTKv2LibraryDll/API/CNTKLibrary.h"
+//#pragma comment(lib, "../../x64/Release/CNTK.lib")
+//#pragma comment(lib, "../../x64/Release/Cntk.Common-2.0rc1.lib")
+#pragma comment(lib, "../../x64/Release/Cntk.Core-2.0rc1.lib")
