cbc29f016f4bd212e3b5dbd5d0bec0869f4ad752
Haoda Li <lihaoda9@163.com>
2017-04-22 10:54:27 +0000

    Add an example of user defined learning algorithm.

--- bindings/python/cntk/learners/__init__.py
+++ bindings/python/cntk/learners/__init__.py
@@ -233,6 +233,36 @@ class UserLearner(cntk_py.Learner):
         raise NotImplementedError('UserLearner.update must be overriden')
 
 
+class ExampleProximalGradientDescentLearner(UserLearner):
+
+    '''
+    Example class of an user-defined learners using proximal gradient descent methods.
+    '''
+
+    def __init__(self, parameters, lr_schedule, l1_regularization_weight=0.0, l2_regularization_weight=0.0):
+        super(ExampleProximalGradientDescentLearner, self).__init__(parameters, lr_schedule)
+        self.l1 = l1_regularization_weight
+        self.l2 = l2_regularization_weight
+
+    def update(self, gradient_values, training_sample_count, sweep_end):
+        l1 = self.l1
+        l2 = self.l2
+        lr = self.learning_rate()
+        
+        if l1 > 0.0:
+            for i in gradient_values:
+                i.value -= gradient_values[i].to_ndarray()*lr
+                i.value = np.sign(i.value) * np.maximum(np.abs(i.value) - lr*l1, 0.0) / (1.0 + l2*lr)
+        else:
+            for i in gradient_values:
+                i.value -= gradient_values[i].to_ndarray()*lr
+                i.value /= (1.0 + l2*lr)
+
+        return True
+        
+
+
+
 @typemap
 def training_parameter_schedule(schedule, unit, epoch_size=None):
     '''
