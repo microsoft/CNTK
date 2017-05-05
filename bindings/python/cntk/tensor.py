39245eefcd9ad72d1ca15b4bd11e3e424971bdaa
Cheng Tang <chenta@microsoft.com>
2017-05-05 16:09:43 -0700

    add keras learner

--- bindings/python/cntk/tensor.py
+++ bindings/python/cntk/tensor.py
@@ -98,6 +98,9 @@ class TensorOpsMixin(object):
         r = self
         axis0 = 0
 
+        if (hasattr(self, 'dynamic_axes') and len(self.dynamic_axes) > 0):
+            axis0 = -len(self.dynamic_axes)
+
         for axis, s in enumerate(arg):
             if s is Ellipsis: # ellipsis means index relative to end after this point
                 axis0 = -len(arg)
@@ -113,7 +116,7 @@ class TensorOpsMixin(object):
                 # implement as a CNTK slice() operation
                 begin = s.start or 0
                 end   = s.stop  or 0
-                if begin != 0 or end != 0:
+                if (begin != 0 or end != 0) and (axis + axis0) >= 0:
                     r = ops.slice(r, axis=axis + axis0, begin_index=begin, end_index=end)
             elif isinstance(s, (tuple, list)):
                 # Select multiple elements from the same dimension. This is
