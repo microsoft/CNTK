7b28d5bd57c15f405ca1d729f5566fec7c114987
Ivan Rodriguez <t-ivrodr@microsoft.com>
2017-05-08 17:16:22 +0200

    Adding convnet and mnist tests (py)

---
+++ Tests/EndToEndTests/CNTKv2Python/Examples/ConvNet_MNIST_test.py
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
+
+import os, sys
+import numpy as np
+from cntk.device import try_set_default_device, gpu
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "Classification", "ConvNet", "Python" ))
+
+from ConvNet_MNIST import convnet_mnist
+
+TOLERANCE_ABSOLUTE = 1E-2
+
+def test_convnet_mnist_error(device_id):
+    from cntk.ops.tests.ops_test_utils import cntk_device
+    try_set_default_device(cntk_device(device_id))
+
+    error = convnet_mnist(epoch_size=5000, minibatch_size=32, max_epochs=10)
+
+    expected_error = 0.0226
+    assert np.allclose(error, expected_error, atol=TOLERANCE_ABSOLUTE)
