70e05aa75e0e35e98564405007fb27156adedcc5
yuqtang <yuqtang@microsoft.com>
2017-05-15 11:01:52 -0700

    Updated the docstring in h5reader.py

--- bindings/python/cntk/io/h5reader.py
+++ bindings/python/cntk/io/h5reader.py
@@ -12,72 +12,57 @@ class H5DataSource(UserMinibatchSource):
     Data Source on HDF5 data. For more information on HDF5 format and the Python API: h5py, please see: http://docs.h5py.org/en/latest/index.html.
 
     Args:
-        datagroup: data group which is the minibatch source. It can be either
-            * an h5py data group which is composed of a dictionary of h5py dataset, 
-            * a Python dictionary mapping from name to h5py dataset
-                If a dataset is a sequence,
-                * it must  be a variable length 1-d array (see  https://support.hdfgroup.org/HDF5/doc1.6/UG/11_Datatypes.html); 
-                * it is required to set three attributes: 
-                    1) dataset.attrs['is_seq']: bool,  
-                    2) dataset.attrs['shape']: tuple or list for element data shape,
-                    3) dataset.attrs['dtype']: numpy element datatype string (see https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html), 
-                       e.g.:  'i4':   # 32-bit signed integer; 
-                              'f4':   # 32-bit floating-point number. 
-        device: The device to hold the data; cpu  or  gpu; default  is  use_default_device().
+        datagroup (a h5py datagroup or a `dictionary`): data group which is the source of the minibatch. It can be either 
+          an h5py data group which is composed of a dictionary of h5py dataset, 2) a Python dictionary mapping from name to 
+          h5py dataset. If a dataset is a sequence, it must be a variable length 1-d array (see https://support.hdfgroup.org/HDF5/doc1.6/UG/11_Datatypes.html);
+          it is required to set three attributes: 1) dataset.attrs['is_seq']: bool; 2) dataset.attrs['shape']: a tuple or list of integers which defines the element data 
+          shape; 3) dataset.attrs['dtype']: numpy element datatype string (see https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html), e.g.:  'i4': 32-bit 
+          signed integer; 'f4':   32-bit floating-point number.
+        device (`DeviceDescriptor`, defaults to `None`): CNTK DeviceDescriptor; default  is  use_default_device().
 
     Example:
-         with h5py.File("my-test-data.h5", "w") as f:
-             #sequence lenth is fixed to 3; element shape is (4, )
-             fixed_len_seq = f.create_dataset("fixed_len_seq", (100, 3, 4), dtype=np.float32)
-             fixed_len_seq.attrs['is_seq'] = True
-             fixed_len_seq.attrs['shape'] = (4,)
-             fixed_len_seq.attrs['dtype'] = 'f4' #float32
-
-             vlen_seq = f.create_dataset("vlen_seq", 
-                         (100, ), 
-                         h5py.special_dtype(vlen=np.dtype('f4'))
-                         #or using:                
-                         #h5py.special_dtype(vlen=np.float32)
-                                         )
-             vlen_seq.attrs['is_seq'] = True
-             vlen_seq.attrs['shape'] = (3,)
-             vlen_seq.attrs['dtype'] = 'f4' #float32
-
-             label = f.create_dataset("label", (100,1), dtype=np.float32) 
-
-             #create fake data
-             for i in range(100):
-                 fixed_len_seq[i] = np.ones((3, 4)) * i
-                 #vlen is between 3 and 10
-                 vlen_seq_inshape = np.ones((np.random.randint(3,10), 3)) * i
-
-                 vlen_seq[i] = np.reshape(vlen_seq_inshape, np.prod(vlen_seq_inshape.shape))
-                 label[i] = np.array([i])
-
-
-             #use the dataset with H5DataSource    
-             #ds = H5DataSource(f)
-             ds = H5DataSource({'fixed_len_seq': f['fixed_len_seq'], 'label': f['label'], 'vlen_seq': f['vlen_seq']})
-
-             #try minibatch
-             batch_size = 30
-             for i in range( (100 // batch_size) + 2):
-
-                 batch = ds.next_minibatch(batch_size)
-                 v_fixed_seq = C.sequence.input(4)
-                 fixed_seq_op_result = (C.sequence.last(v_fixed_seq) * 1).eval({v_fixed_seq: batch[ds.stream_info_mapping['fixed_len_seq']]})
-                 print(fixed_seq_op_result)
-                 v_vlen_seq = C.sequence.input(3)
-                 v_vlen_seq_op_result = (C.sequence.last(v_vlen_seq) * 1).eval({v_vlen_seq: batch[ds.stream_info_mapping['vlen_seq']]})
-                 print(v_vlen_seq_op_result)
-                 v_label = C.input(1)
-                 v_label_op_result = (v_label * 1).eval({v_label: batch[ds.stream_info_mapping['label']]})
-                 print(v_label_op_result)
-
-
-             f.close()
+     >>> with h5py.File("my-test-data.h5", "w") as f:
+     ...    #sequence lenth is fixed to 3; element shape is (4, )
+     ...    fixed_len_seq = f.create_dataset("fixed_len_seq", (100, 3, 4), dtype=np.float32)
+     ...    fixed_len_seq.attrs['is_seq'] = True
+     ...    fixed_len_seq.attrs['shape'] = (4,)
+     ...    fixed_len_seq.attrs['dtype'] = 'f4' #float32
+     ... 
+     ...    vlen_seq = f.create_dataset("vlen_seq", 
+     ...                 (100, ), 
+     ...                 h5py.special_dtype(vlen=np.dtype('f4'))
+     ...                 #or using:                
+     ...                 #h5py.special_dtype(vlen=np.float32)
+     ...                               )
+     ...    vlen_seq.attrs['is_seq'] = True
+     ...    vlen_seq.attrs['shape'] = (3,)
+     ...    vlen_seq.attrs['dtype'] = 'f4' #float32
+     ...    label = f.create_dataset("label", (100,1), dtype=np.float32) 
+     ...    #create fake data
+     ...    for i in range(100):
+     ...         fixed_len_seq[i] = np.ones((3, 4)) * i
+     ...         #vlen is between 3 and 10
+     ...         vlen_seq_inshape = np.ones((np.random.randint(3,10), 3)) * i
+     ...         vlen_seq[i] = np.reshape(vlen_seq_inshape, np.prod(vlen_seq_inshape.shape))
+     ...         label[i] = np.array([i])
+     ...    #use the dataset with H5DataSource    
+     ...    #ds = H5DataSource(f) # or dictionary to specify which fields to use as datasource: 
+     ...    ds = H5DataSource({'fixed_len_seq': f['fixed_len_seq'], 'label': f['label'], 'vlen_seq': f['vlen_seq']})
+     ...    #try minibatch
+     ...    batch_size = 30
+     ...    for i in range( (100 // batch_size) + 2):
+     ...         batch = ds.next_minibatch(batch_size)
+     ...         v_fixed_seq = C.sequence.input(4)
+     ...         fixed_seq_op_result = (C.sequence.last(v_fixed_seq) * 1).eval({v_fixed_seq: batch[ds.stream_info_mapping['fixed_len_seq']]})
+     ...         print(fixed_seq_op_result)
+     ...         v_vlen_seq = C.sequence.input(3)
+     ...         v_vlen_seq_op_result = (C.sequence.last(v_vlen_seq) * 1).eval({v_vlen_seq: batch[ds.stream_info_mapping['vlen_seq']]})
+     ...         print(v_vlen_seq_op_result)
+     ...         v_label = C.input(1)
+     ...         v_label_op_result = (v_label * 1).eval({v_label: batch[ds.stream_info_mapping['label']]})
+     ...         print(v_label_op_result)
+     ...    f.close()
      """
-
     def __init__(self, datagroup, device = use_default_device()):
 
         self.device = device
@@ -144,7 +129,7 @@ class H5DataSource(UserMinibatchSource):
         # worker_rank, which will limit the minibatch source to single GPU / single node
         # scenarios.
         # TODO: check self.num_samples > num_samples
-
+        mydevice = device if device else self.device
         start = self.next_idx
         end = start
         sample_count = 0
@@ -190,12 +175,12 @@ class H5DataSource(UserMinibatchSource):
                     res_data.append(np.reshape(seq, dshape))
                     sample_count = sample_count + seq_len
 
-                data = Value.create(var=sequence.input_variable(shape=elm_shape), data=res_data, device=self.device)
+                data = Value.create(var=sequence.input_variable(shape=elm_shape), data=res_data, device=mydevice)
                 sample_count = int(sample_count)
             else:
                 sample_count = num_seq
                 dshape = dataset.shape[1:]
-                data = Value.create(var=sequence.input_variable(shape=dshape), data=data, device=self.device)
+                data = Value.create(var=sequence.input_variable(shape=dshape), data=data, device=mydevice)
 
             batch_data = MinibatchData(data, num_seq, sample_count, sweep_end)
             result[strm_info] = batch_data
