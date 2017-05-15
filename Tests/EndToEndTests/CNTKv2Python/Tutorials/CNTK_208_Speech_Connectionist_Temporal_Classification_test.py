e5d30d3a7edf385c825ee85745c823ffa3756d94
Vadim Mazalov <vadimma@microsoft.com>
2017-05-15 12:17:38 -0700

    Update the CTC tutorial test and tutorial

--- Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_208_Speech_Connectionist_Temporal_Classification_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_208_Speech_Connectionist_Temporal_Classification_test.py
@@ -15,10 +15,11 @@ def test_cntk_208_speech_connectionist_temporal_classification_noErrors(nb):
               for output in cell['outputs'] if output.output_type == "error"]
     assert errors == []
 
-expectedEvalError = '0.12'
+expectedEvalError1 = '0.98'
+expectedEvalError2 = '0.99'
 
 def test_cntk_208_speech_connectionist_temporal_classification_evalCorrect(nb):
     testCell = [cell for cell in nb.cells
                 if cell.cell_type == 'code' and re.search('trainer\.test_minibatch', cell.source)]
     assert len(testCell) == 1
-    assert testCell[0].outputs[0]['data']['text/plain'] == expectedEvalError
+    assert testCell[0].outputs[0]['data']['text/plain'] == expectedEvalError1 or testCell[0].outputs[0]['data']['text/plain'] == expectedEvalError2
