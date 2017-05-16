e5a4ac156f234a3f17a1be50b0d87111fe2f86fb
Ivan Rodriguez <t-ivrodr@microsoft.com>
2017-05-16 11:17:02 +0200

    Refactoring of FastRCNN example, plus tests of files A* and B* py files

--- Examples/Image/Detection/FastRCNN/A3_ParseAndEvaluateOutput.py
+++ Examples/Image/Detection/FastRCNN/A3_ParseAndEvaluateOutput.py
@@ -7,11 +7,12 @@
 
 """Test a Fast R-CNN network on an image database."""
 from __future__ import print_function
-import importlib
-from fastRCNN.test import test_net
+import os
+from fastRCNN.test import test_net as evaluate_net
 from fastRCNN.timer import Timer
+from imdb_data import imdb_data
+from cntk_helpers import makeDirectory, parseCntkOutput, DummyNet, deleteAllFilesInDirectory
 import PARAMETERS
-locals().update(importlib.import_module("PARAMETERS").__dict__)
 
 
 ####################################
@@ -19,37 +20,39 @@ locals().update(importlib.import_module("PARAMETERS").__dict__)
 ####################################
 image_set = 'test' # 'train', 'test'
 
+def evaluate_output():
+    p = PARAMETERS.get_parameters_for_dataset()
+    # parse cntk output
+    print ("Parsing CNTK output for image set: " + image_set)
+    cntkImgsListPath = os.path.join(p.cntkFilesDir, image_set + ".txt")
+    outParsedDir = os.path.join(p.cntkFilesDir, image_set + "_parsed")
+    cntkOutputPath = os.path.join(p.cntkFilesDir, image_set + ".z")
 
-####################################
-# Main
-####################################
-# parse cntk output
-print ("Parsing CNTK output for image set: " + image_set)
-cntkImgsListPath = cntkFilesDir + image_set + ".txt"
-outParsedDir = cntkFilesDir + image_set + "_parsed/"
-cntkOutputPath = cntkFilesDir + image_set + ".z"
+    # write cntk output for each image to separate file
+    makeDirectory(outParsedDir)
+    parseCntkOutput(cntkImgsListPath, cntkOutputPath, outParsedDir, p.cntk_nrRois, p.cntk_featureDimensions[p.classifier],
+                    saveCompressed=True, skipCheck=True)
 
-# write cntk output for each image to separate file
-makeDirectory(outParsedDir)
-parseCntkOutput(cntkImgsListPath, cntkOutputPath, outParsedDir, cntk_nrRois, cntk_featureDimensions[classifier],
-                saveCompressed = True, skipCheck = True)
+    # delete cntk output file which can be very large
+    # deleteFile(cntkOutputPath)
 
-# delete cntk output file which can be very large
-# deleteFile(cntkOutputPath)
+    imdb = p.imdbs[image_set]
+    net = DummyNet(4096, imdb.num_classes, outParsedDir)
 
-imdb = imdbs[image_set]
-net = DummyNet(4096, imdb.num_classes, outParsedDir)
+    # create empty directory for evaluation files
+    if type(imdb) == imdb_data:
+        evalTempDir = None
+    else:
+        # pascal_voc implementation requires temporary directory for evaluation
+        evalTempDir = os.path.join(p.procDir, "eval_mAP_" + image_set)
+        makeDirectory(evalTempDir)
+        deleteAllFilesInDirectory(evalTempDir, None)
 
-# create empty directory for evaluation files
-if type(imdb) == imdb_data:
-    evalTempDir = None
-else:
-    # pascal_voc implementation requires temporary directory for evaluation
-    evalTempDir = os.path.join(procDir, "eval_mAP_" + image_set)
-    makeDirectory(evalTempDir)
-    deleteAllFilesInDirectory(evalTempDir, None)
+    # compute mAPs
+    evaluate_net(net, imdb, evalTempDir, None, p.classifier, p.nmsThreshold, boUsePythonImpl=True)
 
-# compute mAPs
-test_net(net, imdb, evalTempDir, None, classifier, nmsThreshold, boUsePythonImpl = True)
+    print ("DONE.")
+    return True
 
-print ("DONE.")
+if __name__=='__main__':
+    evaluate_output()
