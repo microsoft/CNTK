83ecbe8253b945c4f6a8b322e626851079dab608
Mark Hillebrand <mahilleb@microsoft.com>
2017-05-17 11:13:09 +0200

    Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_*DataLoader_test.py: enable for weekly
    Also, make sure cached data isn't used.

--- Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_103A_MNIST_DataLoader_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_103A_MNIST_DataLoader_test.py
@@ -7,11 +7,29 @@
 import os
 import re
 import numpy as np
+import pytest
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "CNTK_103A_MNIST_DataLoader.ipynb")
+datadir = os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "DataSets", "MNIST")
+reWeekly = re.compile(r'^weekly\b', re.IGNORECASE)
 
-def test_cntk_103a_mnist_dataloader_noErrors(nb):
+# Run this on CPU only
+notebook_deviceIdsToRun = [-1]
+
+@pytest.fixture(scope='module')
+def clean_data(device_id):
+  # Delete the data folder if we're supposed to run
+  if device_id in notebook_deviceIdsToRun:
+    import subprocess
+    args = ["git", "clean", "-fdx", datadir]
+    subprocess.check_call(args)
+
+@pytest.mark.skipif(not reWeekly.search(os.environ.get('TEST_TAG')),
+                    reason="only runs as part of the weekly tests")
+def test_cntk_103a_mnist_dataloader_noErrors(clean_data, nb):
     errors = [output for cell in nb.cells if 'outputs' in cell
               for output in cell['outputs'] if output.output_type == "error"]
     assert errors == []
+    assert os.path.exists(os.path.join(datadir, "Test-28x28_cntk_text.txt"))
+    assert os.path.exists(os.path.join(datadir, "Train-28x28_cntk_text.txt"))
--- Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_201A_CIFAR-10_DataLoader_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Tutorials/CNTK_201A_CIFAR-10_DataLoader_test.py
@@ -7,12 +7,29 @@
 import os
 import re
 import numpy as np
+import pytest
+import shutil
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "CNTK_201A_CIFAR-10_DataLoader.ipynb")
+datadir = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "data", "CIFAR-10")
+reWeekly = re.compile(r'^weekly\b', re.IGNORECASE)
+
+# Run this on CPU only
+notebook_deviceIdsToRun = [-1]
+
 notebook_timeoutSeconds = 900
 
-def test_cntk_201a_cifat_10_dataloader_noErrors(nb):
+@pytest.fixture(scope='module')
+def clean_data(device_id):
+  # Delete the data folder if we're supposed to run
+  if device_id in notebook_deviceIdsToRun and os.path.exists(datadir):
+    shutil.rmtree(datadir)
+
+@pytest.mark.skipif(not reWeekly.search(os.environ.get('TEST_TAG')),
+                    reason="only runs as part of the weekly tests")
+def test_cntk_201a_cifar_10_dataloader_noErrors(clean_data, nb):
     errors = [output for cell in nb.cells if 'outputs' in cell
               for output in cell['outputs'] if output.output_type == "error"]
     assert errors == []
+    assert os.path.exists(datadir)
