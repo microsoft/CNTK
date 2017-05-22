1011d2d16a1d746cb07ce82f0d7f6deac6a8522e
marhamil723 <mhamilton723@gmail.com>
2017-05-22 15:10:07 -0400

    adding fixes to post bild

--- bindings/java/Swig/post-build.cmd
+++ bindings/java/Swig/post-build.cmd
@@ -19,10 +19,8 @@ if not exist "%project_dir%com\microsoft\CNTK\lib\windows" mkdir "%project_dir%c
 for %%x in (libiomp5md.dll mkl_cntk_p.dll Cntk.Math-%version%.dll Cntk.PerformanceProfiler-%version%.dll Cntk.Core-%version%.dll Cntk.Core.JavaBinding-%version%.dll) do (
   copy "%output_dir%/%%x" ".\com\microsoft\CNTK\lib\windows\%%x" 
   echo %%x>> .\com\microsoft\CNTK\lib\windows\NATIVE_MANIFEST
-  echo %%x>> .\com\microsoft\CNTK\lib\windows\NATIVE_LOAD_MANIFEST
 )
 
-copy .\CNTK.java .\com\microsoft\CNTK\CNTK.java
 copy .\CNTKNativeUtils.java .\com\microsoft\CNTK\CNTKNativeUtils.java
 
 "%JAVA_HOME%\bin\javac" .\com\microsoft\CNTK\*.java || (
