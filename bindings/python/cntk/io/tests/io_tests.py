562667de1462c76a1c6c8da5f59b88c6b14ec4ef
Frank Seide <fseide@microsoft.com>
2017-05-25 15:06:29 -0700

    fixed a test after merge

--- bindings/python/cntk/io/tests/io_tests.py
+++ bindings/python/cntk/io/tests/io_tests.py
@@ -890,11 +890,11 @@ def test_usermbsource_training(tmpdir):
     }
 
     session = training_session(
-        trainer=trainer, mb_source=mbs,
+        trainer=trainer, minibatch_source=mbs,
         model_inputs_to_streams=input_map,
-        mb_size=4, max_samples=20,
-        cv_config = C.CrossValidationConfig(source=mbs_cv, max_samples=10,
-            mb_size=2)
+        minibatch_size=4, max_samples=20,
+        cv_config = C.CrossValidationConfig(minibatch_source=mbs_cv, max_samples=10,
+            minibatch_size=2)
     )
     session.train()
 
