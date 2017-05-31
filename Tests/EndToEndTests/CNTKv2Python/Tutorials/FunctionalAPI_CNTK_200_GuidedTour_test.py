48d60f72b3b2caf01c13580817b47219c84056fd
Nikos Karampatziakis <nikosk@microsoft.com>
2017-05-30 19:50:11 -0700

    Squashed commit of the following:
    commit fbe3fb0f8430e2361f93fc5c92dd87fb9f204a78
    Author: Nikos Karampatziakis <nikosk@microsoft.com>
    Date:   Tue May 30 19:47:35 2017 -0700
    
        clean up notebook
    
    commit b4386b92e5785db7d0efb19a68fe496d61141abd
    Author: Nikos Karampatziakis <nikosk@microsoft.com>
    Date:   Tue May 30 19:06:12 2017 -0700
    
        Added notebook test and address feedback
    
    commit f96708f91785ae12923fc7fbc38ff7d2d3f9fe7a
    Author: Nikos Karampatziakis <nikosk@microsoft.com>
    Date:   Tue May 30 17:13:56 2017 -0700
    
        Updated rst files
    
    commit 7c1e8b13bbea11198876766715d059a25ac5f8d7
    Author: Nikos Karampatziakis <nikosk@microsoft.com>
    Date:   Tue May 30 17:02:17 2017 -0700
    
        Removed all confusing stuff from 050

--- Tests/EndToEndTests/CNTKv2Python/Tutorials/FunctionalAPI_CNTK_200_GuidedTour_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Tutorials/FunctionalAPI_CNTK_200_GuidedTour_test.py
@@ -8,18 +8,18 @@ import os
 import re
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
-notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "CNTK_050_GuidedTour.ipynb")
+notebook = os.path.join(abs_path, "..", "..", "..", "..", "Tutorials", "FunctionalAPI", "CNTK_200_GuidedTour.ipynb")
 # Test only on GPU, since it is too slow on CPU.
 notebook_deviceIdsToRun = [0]
 
-def test_cntk_050_guidedtour_noErrors(nb):
+def test_cntk_200_guidedtour_noErrors(nb):
     errors = [output for cell in nb.cells if 'outputs' in cell
               for output in cell['outputs'] if output.output_type == "error"]
     assert errors == []
 
 expectedEvalError = '8.11%'
 
-def test_cntk_050_guidedtour_evalCorrect(nb):
+def test_cntk_200_guidedtour_evalCorrect(nb):
     testCell = [cell for cell in nb.cells
                 if cell.cell_type == 'code' and re.search('test_metric_lr =', cell.source)]
     assert len(testCell) == 1
