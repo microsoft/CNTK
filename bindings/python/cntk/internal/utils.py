6e3348c54fb595e5c56524edbe30d21033f2e602
Christoph Boeddeker <boeddeker@users.noreply.github.com>
2017-06-03 20:50:47 -0700

    Fix: isinstance np.inexact does not catch np.ndarray

--- bindings/python/cntk/internal/utils.py
+++ bindings/python/cntk/internal/utils.py
@@ -39,7 +39,7 @@ def get_data_type(*args):
                 cntk_dtypes.add(np.float64)
             elif cntk_py.DataType_Float == arg.get_data_type():
                 cntk_dtypes.add(np.float32)
-        elif isinstance(arg, np.inexact):
+        elif isinstance(arg, (np.ndarray, np.inexact)):
             # https://docs.scipy.org/doc/numpy/reference/arrays.scalars.html
             # integer are not np.inexact -> np.float32
             # only accepts numpy types
