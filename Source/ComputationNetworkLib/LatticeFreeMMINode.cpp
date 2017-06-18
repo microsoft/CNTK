a5d3dcc2039851336f3d080466a29226115b22af
t-zhehch <t-zhehch@microsoft.com>
2017-06-18 14:48:46 -0700

    to make jenkin really happy

--- Source/ComputationNetworkLib/LatticeFreeMMINode.cpp
+++ Source/ComputationNetworkLib/LatticeFreeMMINode.cpp
@@ -38,7 +38,8 @@ bool getIdxsFromStr(const wstring& full, const wstring& delim, vector<int> *out)
       const wchar_t *this_str = (split[i].c_str());
       wchar_t *end = NULL;
       int j = 0;
-      j = wcstoll(this_str, &end, (int)10);
+      int b=10;
+      j = wcstoll(this_str, &end, b);
       if (end == this_str || *end != L'\0') {
         out->clear();
         return false;
