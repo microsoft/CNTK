4f3075308377bcb3e3f040e266228a957b35d528
Philipp Kranen <pkranen@microsoft.com>
2017-06-19 16:25:12 +0200

    fixed BN behaviour for frozen cloned networks in training

--- Source/ComputationNetworkLib/TrainingNodes.h
+++ Source/ComputationNetworkLib/TrainingNodes.h
@@ -2665,6 +2665,17 @@ public:
         double expAvgFactor = ComputeExpAvgFactor(); // weight for the new MB statistics in the running estimate. The previous value of the running statistics is kept with weight (1-this)
         double blendFactor  = ComputeBlendFactor();  // interpolation weight for the running statistics (the current MB statistics are weighted with 1-this)
 
+        // if a BN node is part of a cloned network whos weights were frozen
+        // and this cloned frozen network is used in training (e.g. as a static featurizer)
+        // then we want the output of the BN node to be the same as in inference mode,
+        // i.e., for normalization only take the historic (trained) statistics into account and not the mini batch statistics
+        bool isFrozen = Input(SCALE)->GetLearningRateMultiplier() == 0.0 && Input(BIAS)->GetLearningRateMultiplier() == 0.0;
+        if (isFrozen)
+        {
+            expAvgFactor = 0;
+            blendFactor = 1;
+        }
+        
         // In inference-only mode, m_savedMean and m_saveInvStdDev will not be
         // produced and BackpropToNonLooping() may not be called. In
         // non-inference (training) mode, saved statistics must be produced.
