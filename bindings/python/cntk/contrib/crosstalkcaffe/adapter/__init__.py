b1f6a467f1863cff80e77d50b999da8f38347f0e
yuxiaoguo <v-yuxgu@microsoft.com>
2017-07-26 12:48:15 +0200

    crosstalkcaffe: convert caffe model to CNTK model

--- bindings/python/cntk/contrib/crosstalkcaffe/adapter/__init__.py
+++ bindings/python/cntk/contrib/crosstalkcaffe/adapter/__init__.py
@@ -1,16 +1,11 @@
+# ==============================================================================
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE.md file in the project root
 # for full license information.
 # ==============================================================================
 
-"""
-Extra utilities for CNTK, e.g. utilities that bridge to other deep learning toolkits.
-"""
+from .bvlccaffe import caffeadapter
 
-
-import numpy as np
-
-from . import crosstalk
-from . import crosstalkcaffe
-
-#note that crosstalk_* is not imported here to reduce load time
+ADAPTER_DICT = {
+    'Caffe': caffeadapter.CaffeAdapter()
+}
