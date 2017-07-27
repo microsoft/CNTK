e83d8f6d41978fa6c851b879a7e4423751bc132d
Lewei Lu <llw33333@gmail.com>
2017-07-27 18:50:23 +0800

    fix pylint hints

--- Tests/EndToEndTests/CNTKv2Python/Examples/InceptionV3_ImageNet_Distributed_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/InceptionV3_ImageNet_Distributed_test.py
@@ -11,9 +11,9 @@ import signal
 import subprocess
 import re
 import pytest
+
 from cntk.cntk_py import DeviceKind_GPU
 from cntk.device import try_set_default_device
-
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 example_dir = os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "Classification", "GoogLeNet", "InceptionV3", "Python")
@@ -24,14 +24,14 @@ from distributed_common import mpiexec_test
 from prepare_test_data import prepare_ImageNet_data
 script_under_test = os.path.join(example_dir, "InceptionV3_ImageNet_Distributed.py")
 
-mpiexec_params = [ "-n", "2"]
+mpiexec_params = ["-n", "2"]
 
 def test_inception_v3_imagenet_distributed(device_id):
-    params = [ "-n", "4",
-               "-datadir", prepare_ImageNet_data(),
-               "-q", "32",
-               "-e", "300",
-               "-m", "2",
-               "-r",
-               "-device", str(device_id) ]
+    params = ["-n", "4",
+              "-datadir", prepare_ImageNet_data(),
+              "-q", "32",
+              "-e", "300",
+              "-m", "2",
+              "-r",
+              "-device", str(device_id)]
     mpiexec_test(device_id, script_under_test, mpiexec_params, params, 0.99, True, timeout_seconds=400)
