2abe75e2851a173aa712aa4b30aeeac9c747b80d
Deyu Fu <deyuf@nvidia.com>
2017-08-03 16:52:04 -0700

    Tweaks for cuda9/cudnn7/nccl2/cub1.7 build support

--- Source/Math/CntkBatchNormalization.cuh
+++ Source/Math/CntkBatchNormalization.cuh
@@ -112,7 +112,7 @@ __device__ __forceinline__ T Shuffle(T input, int srcLane)
 #ifdef __CUDA_ARCH__
     // shfl is supported only on Kepler+
     static_assert(__CUDA_ARCH__ >= 300, "CNTK only supports only Kepler GPU architecture or newer.");
-    return cub::ShuffleIndex(input, srcLane);
+    return cub::ShuffleIndex(input, srcLane, CUB_PTX_WARP_THREADS, 0xffffffff);
 #else
     assert(false);
     return input; // keep compiler happy
