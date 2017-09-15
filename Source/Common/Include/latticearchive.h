af11c745b32067e64a2824f58f76c92bfa5410c3
Changliang Liu <chanliu@microsoft.com>
2017-09-15 16:41:06 -0700

    Fix syntax error

--- Source/Common/Include/latticearchive.h
+++ Source/Common/Include/latticearchive.h
@@ -1362,7 +1362,7 @@ public:
         }
         // check if number of frames is as expected
         if (expectedframes != SIZE_MAX && L.getnumframes() != expectedframes)
-            LogicError("getlattice: number of frames mismatch between numerator lattice and features, %d vs %d, in %s", L.getnumframes(), expectedframes, key);
+            LogicError("getlattice: number of frames mismatch between numerator lattice and features, %lu vs %lu, in %S", L.getnumframes(), expectedframes, key);
         // remember the latice key for diagnostics messages
         L.key = key;
     };
