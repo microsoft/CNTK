c6a5a4b7472cfce7637f481a549655345ca1efd0
Frank Seide <fseide@microsoft.com>
2017-10-13 23:32:29 -0700

    new class TransformingSpan

--- Source/CNTKv2LibraryDll/API/CNTKHelperTypes.h
+++ Source/CNTKv2LibraryDll/API/CNTKHelperTypes.h
@@ -7,6 +7,13 @@
 
 #pragma once
 
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <iterator>
+#include <utility> // std::forward
+
 namespace CNTK
 {
 
@@ -56,4 +63,60 @@ public:
     T&       operator[](size_t index)       { return at(index); }
 };
 
-}
+///
+/// A collection wrapper class that performs a map ("transform") operation given a lambda.
+///
+template<typename CollectionType, typename Lambda>
+class TransformingSpan
+{
+    typedef typename CollectionType::value_type T;
+    typedef typename CollectionType::const_iterator CollectionConstIterator;
+    typedef typename std::iterator_traits<CollectionConstIterator>::iterator_category CollectionConstIteratorCategory;
+    typedef decltype(std::declval<Lambda>()(std::declval<const T&>())) TLambda; // type of result of lambda call
+    typedef typename std::remove_reference<TLambda>::type TLambdaValue;
+    typedef typename std::remove_cv<TLambdaValue>::type TLambdaValueNonConst;
+    CollectionConstIterator beginIter, endIter;
+    const Lambda& lambda;
+public:
+    typedef TLambda value_type;
+    TransformingSpan(const CollectionType& collection, const Lambda& lambda) : beginIter(collection.cbegin()), endIter(collection.cend()), lambda(lambda) { }
+    TransformingSpan(const CollectionConstIterator& beginIter, const CollectionConstIterator& endIter, const Lambda& lambda) : beginIter(beginIter), endIter(endIter), lambda(lambda) { }
+    // transforming iterator
+    class const_iterator : public std::iterator<CollectionConstIteratorCategory, TLambdaValue>
+    {
+        const Lambda& lambda;
+        CollectionConstIterator argIter;
+    public:
+        const_iterator(const CollectionConstIterator& argIter, const Lambda& lambda) : argIter(argIter), lambda(lambda) { }
+        const_iterator operator++() { auto cur = *this; argIter++; return cur; }
+        const_iterator operator++(int) { argIter++; return *this; }
+        TLambda operator*() { return lambda(*argIter); }
+        auto operator->() { return &operator*(); }
+        bool operator==(const const_iterator& other) { return argIter == other.argIter; }
+        bool operator!=(const const_iterator& other) { return argIter != other.argIter; }
+        const_iterator operator+(difference_type offset) { return const_iterator(argIter + offset, lambda); }
+        const_iterator operator-(difference_type offset) { return const_iterator(argIter - offset, lambda); }
+        difference_type operator-(const const_iterator& other) { return argIter - other.argIter; }
+    };
+    const_iterator cbegin() const { return const_iterator(beginIter, lambda); }
+    const_iterator cend()   const { return const_iterator(endIter  , lambda); }
+    const_iterator begin()  const { return cbegin(); }
+    const_iterator end()    const { return cend();   }
+    // construct certain collection types directly
+    auto as_vector()       const { return std::vector<TLambdaValueNonConst>(cbegin(), cend()); }
+    auto as_list()         const { return std::list  <TLambdaValueNonConst>(cbegin(), cend()); }
+    auto as_forward_list() const { return std::forward_list  <TLambdaValueNonConst>(cbegin(), cend()); }
+    auto as_deque()        const { return std::deque <TLambdaValueNonConst>(cbegin(), cend()); }
+    operator std::vector      <TLambdaValueNonConst>() const { return as_vector(); }
+    operator std::list        <TLambdaValueNonConst>() const { return as_list(); }
+    operator std::forward_list<TLambdaValueNonConst>() const { return as_forward_list(); }
+    operator std::deque       <TLambdaValueNonConst>() const { return as_deque(); }
+};
+// main entry point
+// E.g. call as Transform(collection, lambda1, lambda2, ...).as_vector();
+template<typename CollectionType, typename Lambda>
+static inline auto Transform(const CollectionType& collection, const Lambda& lambda) { return TransformingSpan<CollectionType, Lambda>(collection, lambda); }
+template<typename CollectionType, typename Lambda, typename ...MoreLambdas>
+static inline auto Transform(const CollectionType& collection, const Lambda& lambda, MoreLambdas&& ...moreLambdas) { return Transform(TransformingSpan<CollectionType, Lambda>(collection, lambda), std::forward<MoreLambdas>(moreLambdas)...); }
+
+} // namespace
