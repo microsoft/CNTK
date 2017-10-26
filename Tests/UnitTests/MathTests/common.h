4e31d1a4e8219b2d52d82c4f53e5c5ace9a4126e
Deyu Fu <deyuf@nvidia.com>
2017-10-26 13:31:53 -0700

    add copyright

--- Tests/UnitTests/MathTests/common.h
+++ Tests/UnitTests/MathTests/common.h
@@ -1,5 +1,6 @@
 //
 // Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
 #pragma once
