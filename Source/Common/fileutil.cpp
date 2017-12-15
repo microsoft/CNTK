3a808a845ee4b1f2cfacefdf0ad12c7e2a7d30a0
Guoli Ye <guoye@microsoft.com>
2017-12-14 16:34:47 -0800

    embr_no_tab_debug

--- Source/Common/fileutil.cpp
+++ Source/Common/fileutil.cpp
@@ -2,9 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-/* guoye: start */
-#pragma once
-/* guoye: end */
+
 
 #ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS // "secure" CRT not available on all platforms  --add this at the top of all CPP files that give "function or variable may be unsafe" warnings
@@ -20,7 +18,7 @@
 #define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
 #include "Basics.h"
 #include "basetypes.h" //for attemp()
-#include "fileutil.h"
+// #include "fileutil.h"
 #include "ProgressTracing.h"
 
 #ifdef __unix__
