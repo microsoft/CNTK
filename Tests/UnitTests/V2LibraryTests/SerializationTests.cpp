12ea68cceafad4012abfdc4541a1a2bc6e74f852
Jie Zhu <zhujie@microsoft.com>
2018-05-16 10:36:56 -0700

    fix PrimitiveOpType discrepency in SerializationTests.cpp

--- Tests/UnitTests/V2LibraryTests/SerializationTests.cpp
+++ Tests/UnitTests/V2LibraryTests/SerializationTests.cpp
@@ -353,7 +353,7 @@ void CheckEnumValuesNotModified() {
                   static_cast<size_t>(PrimitiveOpType::ConstantOp) == 89 &&
                   static_cast<size_t>(PrimitiveOpType::LatticeSequenceWithSoftmax) == 90 &&
                   static_cast<size_t>(PrimitiveOpType::Cast) == 91 &&
-                  static_cast<size_t>(PrimitiveOpType::StraightThrough) == 92,
+                  static_cast<size_t>(PrimitiveOpType::StraightThrough) == 93,
                   "PrimitiveOpType enum value was modified.");
 }
 
