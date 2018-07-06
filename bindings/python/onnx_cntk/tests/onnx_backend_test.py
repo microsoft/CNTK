7b3a840ca4a181f58b47d94d2b9643f24873f563
Sergii Dymchenko <sedymche@microsoft.com>
2018-07-06 13:18:41 -0700

    Update skip list for ONNX backend tests.

--- bindings/python/onnx_cntk/tests/onnx_backend_test.py
+++ bindings/python/onnx_cntk/tests/onnx_backend_test.py
@@ -1,8 +1,8 @@
-#-------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for
-# license information.
-#--------------------------------------------------------------------------
+# ==============================================================================
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root
+# for full license information.
+# ==============================================================================
 
 from __future__ import absolute_import
 from __future__ import division
@@ -20,6 +20,7 @@ pytest_plugins = 'onnx.backend.test.report'
 backend_test = onnx.backend.test.BackendTest(cntk_backend, __name__)
 
 skip_models = [
+    # These tests crash and prevent tests after them from running.
     'bvlc_alexnet',
     'densenet121',
     'inception_v1',
@@ -28,11 +29,32 @@ skip_models = [
     'shufflenet',
     'vgg16',
     'vgg19',
+    'zfnet512',  # Attempt to use DefaultLogger but none has been registered.
+    'PixelShuffle'
 ]
 
 skip_ops = [
-    'test_max',
-    'test_min',
+    # These tests crash and prevent tests after them from running.
+    'test_max_one_input_cpu',
+    'test_max_one_input_cuda',
+    'test_min_one_input_cpu',
+    'test_min_one_input_cuda',
+    'test_reshape_extended_dims_cpu',
+    'test_reshape_extended_dims_cuda',
+    'test_reshape_negative_dim_cpu',
+    'test_reshape_negative_dim_cuda',
+    'test_reshape_one_dim_cpu',
+    'test_reshape_one_dim_cuda',
+    'test_reshape_reduced_dims_cpu',
+    'test_reshape_reduced_dims_cuda',
+    'test_reshape_reordered_dims_cpu',
+    'test_reshape_reordered_dims_cuda',
+    'test_pow_bcast_scalar_cpu',
+    'test_pow_bcast_scalar_cuda',
+    'test_operator_repeat_dim_overflow_cpu',
+    'test_operator_repeat_dim_overflow_cuda',
+
+    # These use pre-ONNX 1.2 incompatible versions of the operators.
 ]
 
 skip_tests = skip_models + skip_ops
