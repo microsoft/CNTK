eb639b6d12cbeeba645bab2ad9a9008ce4657c20
liqfu <liqfu@microsoft.com>
2018-08-27 15:57:24 -0700

    export onnx test cases

--- bindings/python/cntk/tests/onnx_op_test.py
+++ bindings/python/cntk/tests/onnx_op_test.py
@@ -6,6 +6,7 @@
 import os
 import numpy as np
 import cntk as C
+import onnx
 import pytest
 from copy import deepcopy
 from cntk.ops.tests.ops_test_utils import cntk_device
@@ -30,11 +31,74 @@ set_of_batch_irrelevant_ops = {'Flatten'}
 #############
 #helpers
 #############
+
+# this is to check whether input data is a bacth of sparse matrices 
+def is_list_of_sparse(data):
+    return type(data)==list and type(data[0])==scipy.sparse.csr.csr_matrix
+
+# convert list of sparse data to dense matrix for ONNX models 
+def sparse_to_dense(sparse_data):
+    dense_data = [sparse_data[0].todense()]
+    for i in range(1, len(dense_data)):
+        dense_data = np.concatenate(dense_data, sparse_data[i].todense()) 
+    return dense_data
+
+def TranposeDynamicAxis(data):
+    rank = data.ndim
+    assert rank >= 2
+    perm = np.arange(rank)
+    perm[0], perm[1] = perm[1], perm[0]
+    return np.transpose(data, perm)
+
+# save numpy ndarray in ONNX tensor format.
+def SaveTensorProto(file_path, variable, data, onnx_value_info_proto):
+    # sequence mode data shape: (batch, sequecen, ...) 
+    # to make onnx model work, batch must be 1
+    # swith to onnx shape: (sequence, batch, ...)
+    if is_list_of_sparse(data):
+        data = sparse_to_dense(data)
+    if variable.has_sequence_axis(): # and variable.is_input:
+        data = TranposeDynamicAxis(data)
+
+    tp = onnx.TensorProto()
+    tp.name = variable.uid
+
+    shape = np.shape(data)
+    for i in range(0, len(shape)):
+        tp.dims.append(shape[i])
+
+    if type(data) == list:
+        # this is probably because of batch (list of samples)
+        data = data[0]
+
+    tp.data_type = onnx_value_info_proto.type.tensor_type.elem_type
+    if onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.DOUBLE:
+        data=data.astype(np.double)
+        tp.raw_data = data.tobytes()
+    elif onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.FLOAT:
+        tp.raw_data = data.tobytes()
+    elif onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.FLOAT16:
+        tp.raw_data = data.tobytes()
+    elif onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.INT64:
+        data=data.astype(np.int64)
+        tp.raw_data = data.tobytes()
+    elif onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.INT32:
+        data=data.astype(np.int)
+        tp.raw_data = data.tobytes()
+    elif onnx_value_info_proto.type.tensor_type.elem_type == onnx.TensorProto.BOOL:
+        data=data.astype(np.bool)
+        tp.raw_data = data.tobytes()
+    else:
+        assert False, "Tensor element type not supported: " + onnx.TensorProto.DataType.Name(onnx_value_info_proto.type.tensor_type.elem_type)
+        
+    with open(file_path, 'wb') as f:
+        f.write(tp.SerializeToString())
+
 def verify_no_input(model, tmpdir, name):
     opname = model.owner.op_name
 
     loaded_model = None
-    loaded_model = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
+    loaded_model, _, _, _ = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
 
     model_shape = model.shape
     dim_denotation = None
@@ -59,13 +123,47 @@ def verify_no_input(model, tmpdir, name):
     return loaded_model
 
 def try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model):
+    onnx_model = None
+    test_model_path = os.path.join(tmpdir, r"test_" + name)
+    os.mkdir(test_model_path)
+    test_data_path = os.path.join(test_model_path, "test_data_set_0")
+    os.mkdir(test_data_path)
+    resave_model_path = os.path.join(tmpdir, R'resave' + name)
+    os.mkdir(resave_model_path)
     if not loaded_model:
-        filename = os.path.join(str(tmpdir), name + R'.onnx')
+        filename = os.path.join(test_model_path, name + R'.onnx')
         model.save(filename, format=C.ModelFormat.ONNX)
         loaded_model = C.Function.load(filename, format=C.ModelFormat.ONNX)
-        filename_resave = os.path.join(str(tmpdir), name + R'_resave.onnx')
+        onnx_model = onnx.load(filename);
+
+        filename_resave = os.path.join(resave_model_path, name + R'_resave.onnx')
         loaded_model.save(filename_resave, format=C.ModelFormat.ONNX)
-    return loaded_model
+        
+    return loaded_model, onnx_model, test_model_path, test_data_path
+
+def save_test_data(model, onnx_model, test_data_path, input_data, output_data, name, tmpdir):
+    if not onnx_model:
+        return;
+
+    if (len(model.arguments) == 1):
+        SaveTensorProto(os.path.join(test_data_path, 'input_{0}.pb'.format(0)), 
+                        model.arguments[0], input_data, onnx_model.graph.input[0]) #, data_type = np.int)
+    else:
+        for i in range(len(model.arguments)):
+            SaveTensorProto(os.path.join(test_data_path, 'input_{0}.pb'.format(i)), 
+                            model.arguments[i], input_data[i], onnx_model.graph.input[i])
+
+    if (len(model.outputs) > 1):
+        for i in range(0, len(model.outputs)): 
+            output_data_i = output_data[model.outputs[i]]
+            SaveTensorProto(os.path.join(test_data_path, 'output_{0}.pb'.format(i)), 
+                            model.outputs[i], output_data_i, onnx_model.graph.output[i])
+    else:
+        SaveTensorProto(os.path.join(test_data_path, 'output_{0}.pb'.format(0)), 
+                        model.outputs[0], output_data, onnx_model.graph.output[0])
+
+    # print out command line for onnx test runner
+    print(R"onnx_test_runner.exe -n " + name + " " + str(tmpdir))
 
 def verify_one_input(model, data, tmpdir, name, device=None, loaded_model=None, rtol = 1e-05, atol = 1e-08):
     # TODO: eventually we want this test method to be more general to suport 
@@ -79,7 +177,7 @@ def verify_one_input(model, data, tmpdir, name, device=None, loaded_model=None, 
     # outputs share the same owner
     opname = model.outputs[0].owner.op_name
 
-    loaded_model = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
+    loaded_model, onnx_model, test_model_path, test_data_path = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
 
     # TODO: it is better to compare data.shape with model.arguments[0] and
     # to pad batch dimension as needed.
@@ -113,21 +211,16 @@ def verify_one_input(model, data, tmpdir, name, device=None, loaded_model=None, 
             o1i = o1[loaded_model.outputs[i]]
             assert np.allclose(o0i, o1i, rtol, atol)
 
+    save_test_data(model, onnx_model, test_data_path, data, o0, name, tmpdir)
+
     return loaded_model
 
 def verify_sequence_model(model, data, tmpdir, name, device=None, loaded_model=None):
-    def TranposeDynamicAxis(data):
-        rank = np.rank(data)
-        assert rank >= 2
-        perm = np.arange(rank)
-        perm[0], perm[1] = perm[1], perm[0]
-        return np.transpose(data, perm)
-
     # data here is reference to the outside data object. create deepcopy to avoid changing the outside data since it might get reused.
     data = deepcopy(data)
     opname = model.owner.op_name
 
-    loaded_model = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
+    loaded_model, onnx_model, test_model_path, test_data_path = try_save_load_resave_onnx_model(model, tmpdir, name, loaded_model)
 
     dataOnnx = TranposeDynamicAxis(data)
     if device:
@@ -146,7 +239,8 @@ def verify_sequence_model(model, data, tmpdir, name, device=None, loaded_model=N
         o1 = TranposeDynamicAxis(o1)
 
     assert np.allclose(o0, o1)
-    return loaded_model
+    
+    save_test_data(model, onnx_model, test_data_path, data, o0, name, tmpdir)
 
 def verify_two_input(model, data1, data2, tmpdir, name):
     # data here is reference to the outside data object. create deepcopy to avoid changing the outside data since it might get reused.
@@ -339,9 +433,9 @@ def verify_BN(x, init_scale, init_bias, mean, var, epsilon, spatial, tmpdir, dty
         loaded_model = None
         for i in range(len(x)):
             if dtype==np.float16:
-                loaded_model = verify_one_input(op_node, x[i], tmpdir, 'BatchNormalization', loaded_model=loaded_model, rtol = 1e-03, atol = 1e-03)
+                loaded_model = verify_one_input(op_node, x[i], tmpdir, 'BatchNormalization_float16' + str(i), loaded_model=loaded_model, rtol = 1e-03, atol = 1e-03)
             else:
-                loaded_model = verify_one_input(op_node, x[i], tmpdir, 'BatchNormalization', loaded_model=loaded_model)
+                loaded_model = verify_one_input(op_node, x[i], tmpdir, 'BatchNormalization_float32' + str(i), loaded_model=loaded_model)
 
 
 # Case 1 - Non-Spatial BN with More > 1 batches    
@@ -710,7 +804,6 @@ def test_GRU(tmpdir, dtype):
 
         for config in list(product(direction_options, initial_state_options, activation_options)):
             model_filename = MakeGRUNameFromConfig(*config)
-            print(model_filename)
             backward, initial_state, activation =  config
     
             x = C.input_variable(input_dim, dynamic_axes=[C.Axis.default_batch_axis(), C.Axis('sequenceAxis')]) 
@@ -785,7 +878,7 @@ def test_LayerNormalization(tmpdir, dtype, device_id):
             data = np.reshape(np.arange(np.prod(shape), dtype = dtype), shape)
             input_operand = C.input_variable(shape=shape)
             model0 = C.layers.LayerNormalization(initial_scale=1, initial_bias=2, epsilon=0.00001)(input_operand)
-            verify_one_input(model0, data, tmpdir, 'LayerNorm_0')
+            verify_one_input(model0, data, tmpdir, 'LayerNorm_0' + str(shape).replace(',', '_'))
 
         # This test point tests especially with epsilon = 0, because that creates a graph with 
         # different number of ops. However, we don't expect the numbers to match in round trip
@@ -1424,7 +1517,6 @@ def test_RNN(tmpdir, dtype):
 
         for config in list(product(direction_options, num_layers_options, initial_state_options, activation_options)):
             model_filename = MakeRNNNameFromConfig(*config)
-            print(model_filename)
             direction, num_layers, initial_state, activation = config
     
             x = C.input_variable(input_dim, dynamic_axes=[C.Axis.default_batch_axis(), C.Axis('sequenceAxis')]) 
@@ -1479,7 +1571,6 @@ def test_SequenceSlice(tmpdir, dtype, beginIndex, endIndex):
     shape = (batch_size, sequence_length, input_size)
     data = np.reshape(range(0, np.prod(shape)), shape).astype(dtype)
     testName = "test_sequence_slice_{0}.{1}".format(beginIndex, endIndex)
-    print(testName)
     model = C.sequence.slice(C.sequence.input_variable((feature_shape)), beginIndex, endIndex)
     verify_sequence_model(model, data, tmpdir, testName)
 
