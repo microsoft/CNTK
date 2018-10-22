4b89975f2adfcf235fe69f085cfbc0ed4252b1d2
Guoli Ye <guoye@microsoft.com>
2018-10-21 22:19:52 -0700

    debug

--- Source/ActionsLib/NetworkFactory.cpp
+++ Source/ActionsLib/NetworkFactory.cpp
@@ -76,15 +76,15 @@ bool TryGetNetworkFactory(const ConfigRecordType& config, function<ComputationNe
         // We interface with outer old CNTK config by taking the inner part, which we get as a string, as BrainScript.
         // We prepend a few standard definitions, and also definition of deviceId and precision, which all objects will pull out again when they are being constructed.
         // BUGBUG: We are not getting TextLocations right in this way! Do we need to inject location markers into the source? Moot once we fully switch to BS
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 1.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 1.\n");
         wstring sourceOfNetwork = config.Exists(L"BrainScriptNetworkBuilder") ? config(L"BrainScriptNetworkBuilder") : config(L"ExperimentalNetworkBuilder");
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 2.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 2.\n");
         if (sourceOfNetwork.find_first_of(L"([{") != 0)
             InvalidArgument("BrainScript network description must be either a BS expression in ( ) or a config record in { }");
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 3.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 3.\n");
         // set the include paths to all paths that configs were read from; no additional configurable include paths are supported by BrainScriptNetworkBuilder
         auto includePaths = ConfigParameters::GetBrainScriptNetworkBuilderIncludePaths();
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 4.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 4.\n");
 
         // inject additional items into the source code
         // We support two ways of specifying the network in BrainScript:
@@ -93,7 +93,7 @@ bool TryGetNetworkFactory(const ConfigRecordType& config, function<ComputationNe
         // For back-compat, [ ] is allowed and means the same as { }
         if (sourceOfNetwork[0] == '{' || sourceOfNetwork[0] == '[') // if { } form then we turn it into ComputationNetwork by constructing a ComputationNetwork from it
             sourceOfNetwork = L"new ComputationNetwork " + sourceOfNetwork;
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 5.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 5.\n");
 
         let sourceOfBS = msra::strfun::wstrprintf(L"include \'cntk.core.bs\'\n" // include our core lib. Note: Using lowercase here to match the Linux name of the CNTK exe.
             L"deviceId = %d\n"            // deviceId as passed in
@@ -101,10 +101,10 @@ bool TryGetNetworkFactory(const ConfigRecordType& config, function<ComputationNe
             L"precision = '%ls'\n"        // 'float' or 'double'
             L"network = %ls",             // source code of expression that evaluates to a ComputationNetwork
             (int)deviceId, traceLevel, ElemTypeName<ElemType>(), sourceOfNetwork.c_str());
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 6.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 6.\n");
 
         let expr = BS::ParseConfigDictFromString(sourceOfBS, L"BrainScriptNetworkBuilder", move(includePaths));
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 7.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 7.\n");
 
         // the rest is done in a lambda that is only evaluated when a virgin network is needed
         // Note that evaluating the BrainScript *is* instantiating the network, so the evaluate call must be inside the lambda.
@@ -112,21 +112,21 @@ bool TryGetNetworkFactory(const ConfigRecordType& config, function<ComputationNe
         {
             // evaluate the parse tree, particularly the top-level field 'network'
             // Evaluating it will create the network.
-            LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 8.\n");
+            fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 8.\n");
 
             let object = EvaluateField(expr, L"network");                   // this comes back as a BS::Object
-            LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 9.\n");
+            fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 9.\n");
 
             let network = dynamic_pointer_cast<ComputationNetwork>(object); // cast it
-            LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 10.\n");
+            fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 10.\n");
 
             if (!network)
                 LogicError("BuildNetworkFromDescription: ComputationNetwork not what it was meant to be");
-            LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 11.\n");
+            fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 11.\n");
 
             return network;
         };
-        LOGPRINTF(stderr, "\nguoye: TryGetNetworkFactory: debug 12.\n");
+        fprintf(stderr, "\nguoye: TryGetNetworkFactory: debug 12.\n");
 
         return true;
     }
@@ -137,11 +137,11 @@ bool TryGetNetworkFactory(const ConfigRecordType& config, function<ComputationNe
 template <class ConfigRecordType, typename ElemType>
 function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory(const ConfigRecordType& config)
 {
-    LOGPRINTF(stderr, "\nguoye: GetNetworkFactory: debug 1.\n");
+    fprintf(stderr, "\nguoye: GetNetworkFactory: debug 1.\n");
     function<ComputationNetworkPtr(DEVICEID_TYPE)> createNetworkFn;
-    LOGPRINTF(stderr, "\nguoye: GetNetworkFactory: debug 2.\n");
+    fprintf(stderr, "\nguoye: GetNetworkFactory: debug 2.\n");
     bool gotIt = TryGetNetworkFactory<ConfigRecordType, ElemType>(config, createNetworkFn);
-    LOGPRINTF(stderr, "\nguoye: GetNetworkFactory: debug 3.\n");
+    fprintf(stderr, "\nguoye: GetNetworkFactory: debug 3.\n");
     if (!gotIt)
         RuntimeError("No network builder found in the config file. NDLNetworkBuilder, SimpleNetworkBuilder, or BrainScriptNetworkBuilder must be specified");
     else
