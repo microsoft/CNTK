4f791902098dac9d514559b5eda07c27095343fa
Aghagolzadeh <aghagolzadeh@gmail.com>
2018-11-08 16:42:11 -0800

    Update BlockMomentumDistributedLearner.h
    updated BMUF --> ResetBuffer to accept float16 type

--- Source/1BitSGD/BlockMomentumDistributedLearner.h
+++ Source/1BitSGD/BlockMomentumDistributedLearner.h
@@ -524,6 +524,8 @@ namespace CNTK
                     ResetBuffer<double>(i, p);
                 else if (p->GetDataType() == DataType::Float)
                     ResetBuffer<float>(i, p);
+                else if (p->GetDataType() == DataType::Float16)
+                    ResetBuffer<float16>(i, p);
                 else
                     RuntimeError("Unsupported type.");
             }
