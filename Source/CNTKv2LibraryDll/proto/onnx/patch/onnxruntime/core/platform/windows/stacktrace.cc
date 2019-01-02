04088ae598ef2d51ca4fc06210ac667900f4b1e1
Bowen Bao <bowbao@microsoft.com>
2019-01-02 14:53:09 -0800

    nit

--- Source/CNTKv2LibraryDll/proto/onnx/patch/onnxruntime/core/platform/windows/stacktrace.cc
+++ Source/CNTKv2LibraryDll/proto/onnx/patch/onnxruntime/core/platform/windows/stacktrace.cc
@@ -82,7 +82,7 @@ class SymbolHelper {
   }
 
  private:
-  ONNXRUNTIME_DISALLOW_COPY_ASSIGNMENT_AND_MOVE(SymbolHelper);
+  ORT_DISALLOW_COPY_ASSIGNMENT_AND_MOVE(SymbolHelper);
 
   HANDLE process_ = GetCurrentProcess();
   bool cleanup_ = false;
