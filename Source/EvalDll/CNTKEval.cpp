59caedede2dbbb2568605aa9722a49eae9b76d46
Peggy Peng <rpeng@microsoft.com>
2019-03-14 23:53:18 -0700

    fp16 brain script - add _CRT_SECURE_NO_WARNINGS

--- Source/EvalDll/CNTKEval.cpp
+++ Source/EvalDll/CNTKEval.cpp
@@ -11,10 +11,6 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // "secure" CRT not available on all platforms  --add this at the top of all CPP files that give "function or variable may be unsafe" warnings
-#endif
-
 #include <stdio.h>
 #include <math.h>
 #define EVAL_EXPORTS // creating the exports here
