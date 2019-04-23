767c88325b412a79d351fc0daa18d81393db2e4b
v-zhimao <v-zhimao@MSRAVC-FACEDNN2>
2019-04-22 17:08:30 -0700

    bug fix

--- Source/Math/NcclComm.cpp
+++ Source/Math/NcclComm.cpp
@@ -185,7 +185,7 @@ void NcclComm::AllGatherImpl(void* inputBuffer, void* outputBuffer, size_t count
 
     static NcclTypeLookup s_ncclTypeLookup;
 
-    res = ncclAllGather(inputbuffer, outputbuffer, count, s_ncclTypeLookup.Lookup(dtype), m_ncclComm, m_stream)
+    res = ncclAllGather(inputBuffer, outputBuffer, count, s_ncclTypeLookup.Lookup(dtype), m_ncclComm, m_stream)
 
     if (res != ncclSuccess)
         RuntimeError("NcclComm ncclAllReduce failed: %s", ncclGetErrorString(res));
