0165a6d668f5a2d16f56b96b027a3dcea8e83990
Zhiming Mao <v-zhimao@microsoft.com>
2019-06-17 14:24:44 +0800

    bug fix

--- Source/ComputationNetworkLib/TrainingNodes.h
+++ Source/ComputationNetworkLib/TrainingNodes.h
@@ -4293,7 +4293,7 @@ public:
 
     virtual void BackpropToNonLooping(size_t inputIndex) override
     {
-        if (m_segmentNum - 1 == m_segmentIndex)
+        if (m_segmentNum == m_segmentIndex)
             m_gradientGlobalMemoryBlock->resetMinibatchSize(InputRef(0).Gradient().GetNumCols(), true);
 
         FrameRange fr(InputRef(0).GetMBLayout());
@@ -4384,7 +4384,7 @@ public:
     void RequestMatricesBeforeBackprop(MatrixPool& matrixPool) override
     {
         Base::RequestMatricesBeforeBackprop(matrixPool);
-        if (m_segmentNum - 1 == m_segmentIndex)
+        if (m_segmentNum == m_segmentIndex)
             RequestMatrixFromPool(m_gradientGlobalMemoryBlock->m_globalMemoryMatrix, matrixPool, m_gradientGlobalMemoryBlock->m_memoryLength, true);
     }
 
